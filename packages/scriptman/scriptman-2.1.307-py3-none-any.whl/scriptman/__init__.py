653e7b564b850bcfe155434c7f1867cc713f02f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:01 +0000

    Add package scriptman/2.1.307/scriptman-2.1.307-py3-none-any.whl

---
+++ packages/scriptman/scriptman-2.1.307-py3-none-any.whl/scriptman/__init__.py
@@ -0,0 +1,21 @@
+from scriptman import powers
+from scriptman.core.config import config
+
+"""
+######################################################################################
+##                                                                                  ##
+## #######  #######  #######  ######   ####### ########  ##   ##  #######  ###  ##  ##
+##                        ##    ##          ##    ##     ### ###       ##  #### ##  ##
+## #######  ##       #######    ##     #######    ##     #######  #######  ## ####  ##
+##      ##  ##       ##  ##     ##     ##         ##     ## # ##  ##   ##  ##  ###  ##
+## #######  #######  ##   ##  ######   ##         ##     ##   ##  ##   ##  ##   ##  ##
+##                                                                                  ##
+######################################################################################
+"""
+__all__: list[str] = ["powers", "config"]
+
+# TODO: Scriptman not running files with imports
+# TODO: Scriptman API not running
+# TODO: Script aliases
+# TODO: Exit from scriptman while running things on multiple threads
+# TODO: Setting Literals on config leading to errors
