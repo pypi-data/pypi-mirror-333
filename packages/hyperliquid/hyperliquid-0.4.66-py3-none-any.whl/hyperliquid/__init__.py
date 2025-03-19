82627abaee205a13f1d2247407a6a11355dab16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:37 +0000

    Add package hyperliquid/0.4.66/hyperliquid-0.4.66-py3-none-any.whl

---
+++ packages/hyperliquid/hyperliquid-0.4.66-py3-none-any.whl/hyperliquid/__init__.py
@@ -0,0 +1,7 @@
+import sys
+import hyperliquid.ccxt as ccxt_module
+sys.modules['ccxt'] = ccxt_module
+
+from hyperliquid.ccxt import hyperliquid as HyperliquidSync
+from hyperliquid.ccxt.async_support.hyperliquid import hyperliquid as HyperliquidAsync
+from hyperliquid.ccxt.pro.hyperliquid import hyperliquid as HyperliquidWs
