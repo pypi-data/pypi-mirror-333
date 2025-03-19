2ee7232a9f42d4e584c72d0dc680632610945860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:16:04 +0000

    Add package CTAB-XTRA/0.2.1/ctab_xtra-0.2.1.tar.gz

---
+++ packages/CTAB-XTRA/ctab_xtra-0.2.1.tar.gz/ctab_xtra-0.2.1/CTAB_XTRA/__init__.py
@@ -0,0 +1,6 @@
+# package_example/__init__.py
+from .module import fav_car, halvor
+
+
+
+__version__ = "0.1.0"
---
+++ packages/CTAB-XTRA/ctab_xtra-0.2.1.tar.gz/ctab_xtra-0.2.1/CTAB_XTRA/module.py
@@ -0,0 +1,6 @@
+# package_example/module.py
+def fav_car():
+    return "Sara sin postmann pat bil"
+
+def halvor():
+    return "Halvor er CEO av halvorhub.com"
