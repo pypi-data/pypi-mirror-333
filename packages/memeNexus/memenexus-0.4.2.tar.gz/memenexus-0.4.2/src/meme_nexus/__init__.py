c5333bb35321508e29569fbfe76b3e0744f2708c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:05:13 +0000

    Add package memeNexus/0.4.2/memenexus-0.4.2.tar.gz

---
+++ packages/memeNexus/memenexus-0.4.2.tar.gz/memenexus-0.4.2/src/meme_nexus/__init__.py
@@ -0,0 +1,8 @@
+"""
+MemeNexus: A Python library for interacting with various blockchain and DeFi services.
+
+This package provides tools and utilities for:
+- Fetching token information from DexScreener
+- Analyzing token performance
+- Monitoring trading activities
+"""
