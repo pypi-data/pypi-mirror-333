4d0f309210c36e65794bc10109e2afe7cfd481b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:50 +0000

    Add package PySimpleGUI/5.0.8.1/pysimplegui-5.0.8.1-py3-none-any.whl

---
+++ packages/PySimpleGUI/pysimplegui-5.0.8.1-py3-none-any.whl/PySimpleGUI/PySimpleGUI.py
@@ -0,0 +1,12 @@
+
+def main():
+    print("PySimpleGUI is now located on a private PyPI server.  Please add to your pip command: -i https://PySimpleGUI.net/install")
+    print("\nThe version you just installed will need to be uninstalled:")
+    print("   python -m pip uninstall PySimpleGUI")
+    print("   python -m pip cache purge")
+    print("Then install the latest from the private server:")
+    print("   python -m pip install -i https://PySimpleGUI.net/install PySimpleGUI")
+    print("\nUse python3 command if you're running on the Mac or Linux")
+
+main()
+
