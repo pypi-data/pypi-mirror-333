a47ac1db510b213628bb921eaa0f6b602a1ab094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:13 +0000

    Add package user-manual/0.0.11/user_manual-0.0.11.tar.gz

---
+++ packages/user-manual/user_manual-0.0.11.tar.gz/user_manual-0.0.11/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="user_manual",
+    version="0.0.11",
+    packages=find_packages(),
+    install_requires= ["user-manual>=0.0.9"],    
+    entry_points={
+        'console_scripts': [
+            'user_manual=user_manual.user_manual:main',
+        ],
+    },
+    author="Aviral Srivastava",
+    author_email="aviralsrivastava284@gmail.com",
+    description="Generate the User Manual by ChatGPT",
+    long_description_content_type='text/markdown',
+    url="https://github.com/A284viral/protections_v1",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
