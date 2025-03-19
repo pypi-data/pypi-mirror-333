435fd6b6d84b4b8b14164eb01543adb3cd737253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:53 +0000

    Add package shared-utils-ems/3.0.7/shared_utils_ems-3.0.7-py3-none-any.whl

---
+++ packages/shared-utils-ems/shared_utils_ems-3.0.7-py3-none-any.whl/shared_utils/update_model.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.utils import timezone
+from rest_framework.response import Response
+from rest_framework import status
+
+
+def model_update(instance, fields, data, auto_updated_at=True):
+    """
+    Generic update service meant to be reused in local update services.
+
+    For example:
+
+    def user_update(*, user: User, data) -> User:
+        fields = ['first_name', 'last_name']
+        user, has_updated = model_update(
+            instance=user, fields=fields, data=data)
+
+        // Do other actions with the user here
+
+        return user
+
+    Return value: Tuple with the following elements:
+        1. The instance we updated.
+        2. A boolean value representing whether we performed an update or not.
+
+    Some important notes:
+
+        - Only keys present in `fields` will be taken from `data`.
+        - If something is present in `fields` but not present in `data`, we simply skip.
+        - There's a strict assertion that all values in `fields` are actual fields in `instance`.
+        - `fields` can support m2m fields, which are handled after the update on `instance`.
+        - If `auto_updated_at` is True, we'll try bumping `updated_at` with the current timestmap.
+    """
+
+    has_updated = False
+    m2m_data = {}
+    update_fields = []
+
+    model_fields = {field.name: field for field in instance._meta.get_fields()}
+
+    for field in fields:
+        if field not in data:
+            raise Response(
+                {field: f"'{field}' cant update this field."}, status=status.HTTP_400_BAD_REQUEST)
+
+        model_field = model_fields.get(field)
+
+        assert model_field is not None, f"{field} is not part of {instance.__class__.__name__} fields."
+
+        if isinstance(model_field, models.ManyToManyField):
+            m2m_data[field] = data[field]
+            continue
+
+        if getattr(instance, field) != data[field]:
+            has_updated = True
+            update_fields.append(field)
+            setattr(instance, field, data[field])
+
+    if has_updated:
+        if auto_updated_at:
+            if "updated_at" in model_fields and "updated_at" not in update_fields:
+                update_fields.append("updated_at")
+                instance.updated_at = timezone.now()
+
+        instance.full_clean()
+        instance.save(update_fields=update_fields)
+
+    for field_name, value in m2m_data.items():
+        related_manager = getattr(instance, field_name)
+        related_manager.set(value)
+
+        has_updated = True
+
+    return instance, has_updated
