0bd78daf757c94eea57af122bb251bad9b11ed28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:57 +0000

    Add package django-mermaid/0.0.8/django_mermaid-0.0.8-py3-none-any.whl

---
+++ packages/django-mermaid/django_mermaid-0.0.8-py3-none-any.whl/django_mermaid/templatetags/__init__.py
@@ -0,0 +1,7 @@
+DEFAULT_VERSION = "9.4.3"  # default to latest stable version
+DEFAULT_THEME = "default"  # use the mermaid 'default' theme
+
+__all__ = [
+    "DEFAULT_VERSION",
+    "DEFAULT_THEME",
+]
