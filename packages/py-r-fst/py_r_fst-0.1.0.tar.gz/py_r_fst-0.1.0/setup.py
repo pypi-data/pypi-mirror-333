ebeee8bbab73ac3986245756458f277e0ac06173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:08:49 +0000

    Add package py-r-fst/0.1.0/py_r_fst-0.1.0.tar.gz

---
+++ packages/py-r-fst/py_r_fst-0.1.0.tar.gz/py_r_fst-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-r-fst",
+    version="0.1.0",
+    packages=find_packages(),
+    package_data={"": ["*.py"]},
+    py_modules=["pyfst"],
+    install_requires=[
+        "pandas>=1.0.0",
+        "rpy2>=3.4.0",
+    ],
+    author="msdavid",
+    author_email="mauro@sauco.net",
+    description="Python wrapper for the R fst package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/msdavid/py-fst",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
