45a736b3c7402eae70040ad909e059bb1f3e7d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:46 +0000

    Add package aim/3.28.0.dev20250311/aim-3.28.0.dev20250311-cp37-cp37m-manylinux_2_24_x86_64.whl

---
+++ packages/aim/aim-3.28.0.dev20250311-cp37-cp37m-manylinux_2_24_x86_64.whl/aim/web/api/db.py
@@ -0,0 +1,37 @@
+import logging
+import os
+
+from contextlib import contextmanager
+
+from aim.web.configs import AIM_LOG_LEVEL_KEY
+from aim.web.utils import get_db_url
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine(
+    get_db_url(),
+    echo=(logging.INFO >= int(os.environ.get(AIM_LOG_LEVEL_KEY, logging.WARNING))),
+    connect_args={'check_same_thread': False},
+)
+Base = declarative_base()
+
+
+def get_session():
+    session_cls = sessionmaker(autoflush=False, bind=engine)
+    session = session_cls()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+@contextmanager
+def get_contexted_session():
+    session_cls = sessionmaker(autoflush=False, bind=engine)
+    session = session_cls()
+    try:
+        yield session
+    finally:
+        session.close()
