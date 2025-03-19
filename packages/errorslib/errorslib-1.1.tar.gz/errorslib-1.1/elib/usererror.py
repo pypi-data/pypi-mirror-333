974a5946662e48ebf17d2306b5a3cd5e058e5d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:07 +0000

    Add package errorslib/1.1/errorslib-1.1.tar.gz

---
+++ packages/errorslib/errorslib-1.1.tar.gz/errorslib-1.1/elib/usererror.py
@@ -0,0 +1,39 @@
+"""
+User Error
+"""
+
+class UserError(Exception):
+    """User needs to RTFM."""
+    pass
+
+class UserIsAnIdiotError(UserError):
+    """problem exists between keyboard and chair."""
+    pass
+
+class UserIsNotAnIdiotError(UserError):
+    """problem does not exist between keyboard and chair."""
+    pass
+
+class PICNICError(UserError):
+    """Problem In Chair, Not In Computer."""
+    pass
+
+class FeatureNotABugError(UserError):
+    """it's a feature. Not a bug."""
+    pass
+
+class UserIsUsingWindowsError(UserError):
+    """the user is using an inferior operating system."""
+    pass
+
+class UserIsUsingMacOSError(UserError):
+    """the user is using MacOS."""
+    pass
+
+class UserIsAppleSheepError(UserError):
+    """the user is an Apple sheep."""
+    pass
+
+class UserIsUsingLinuxError(UserError):
+    """the user is a Linux elitist."""
+    pass
