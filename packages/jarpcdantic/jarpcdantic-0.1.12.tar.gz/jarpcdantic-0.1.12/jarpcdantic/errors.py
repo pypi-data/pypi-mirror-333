dec0a037b32392e49aacab899632223dbd0b6218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:57 +0000

    Add package jarpcdantic/0.1.12/jarpcdantic-0.1.12.tar.gz

---
+++ packages/jarpcdantic/jarpcdantic-0.1.12.tar.gz/jarpcdantic-0.1.12/jarpcdantic/errors.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+JARPC exception classes.
+
+Modelled after JSON-RPC 2.0 errors.
+
+"""
+import inspect
+import sys
+from typing import Type, TypeVar
+
+_T = TypeVar("_T", bound=Type["JarpcError"])
+
+
+class JarpcError(Exception):
+    """Base JARPC exception"""
+
+    code = None
+    message = None
+
+    def __init__(self, data=None):
+        if isinstance(data, Exception):
+            self.data = f"{data.__class__.__name__}: {data}"
+        else:
+            self.data = data
+
+    def __str__(self):
+        return f"{self.code} {self.message}: {self.data}"
+
+    def as_dict(self):
+        return {"code": self.code, "message": self.message, "data": self.data}
+
+
+class JarpcUnknownError(JarpcError):
+    """Unknown error: unknown exception code"""
+
+    def __init__(self, code, message, data):
+        self.code = code
+        self.message = message
+        super().__init__(data=data)
+
+
+class ExceptionManager:
+    """Global manager for handling JARPC exceptions."""
+
+    def __init__(self) -> None:
+        self._exceptions: dict[int, Type[JarpcError]] = {}
+
+    def add(self, exception_class: _T) -> _T:
+        """
+        Registers a custom exception.
+
+        Can be used as a decorator:
+        @exception_manager.add
+        class MyError(JarpcError):
+            code = 1000
+            message = "Custom error"
+        """
+        if not issubclass(exception_class, JarpcError):
+            raise TypeError(
+                f"{exception_class.__name__} must be a subclass of JarpcError"
+            )
+        if not hasattr(exception_class, "code") or not isinstance(
+            exception_class.code, int
+        ):
+            raise ValueError(
+                f"{exception_class.__name__} must define an integer 'code' attribute"
+            )
+
+        self._exceptions[exception_class.code] = exception_class
+        return exception_class  # Allows usage as a decorator
+
+    def get(self, code: int) -> Type[JarpcError] | None:
+        """Retrieves an exception class by its error code."""
+        return self._exceptions.get(code)
+
+    def raise_exception(
+        self, code: int, data: str | None = None, message: str | None = None
+    ) -> None:
+        """
+        Creates and raises an exception based on the provided code.
+        If the code is unrecognized, raises `JarpcUnknownError`.
+        """
+        exception_class = self.get(code) or JarpcUnknownError
+        raise exception_class(data=data)
+
+    def list_registered_exceptions(self) -> list[int]:
+        """Returns a list of registered exception codes."""
+        return list(self._exceptions.keys())
+
+
+# Global instance
+jarpcdantic_exceptions = ExceptionManager()
+
+
+@jarpcdantic_exceptions.add
+class JarpcParseError(JarpcError):
+    """Parse Error: invalid JSON format."""
+
+    code = -32700
+    message = "Parse error"
+
+
+@jarpcdantic_exceptions.add
+class JarpcInvalidRequest(JarpcError):
+    """Invalid Request: the JSON object is not a JARPC 1.0 request."""
+
+    code = -32600
+    message = "Invalid Request"
+
+
+@jarpcdantic_exceptions.add
+class JarpcMethodNotFound(JarpcError):
+    """Method not found: method with requested name does not exist."""
+
+    code = -32601
+    message = "Method not found"
+
+
+@jarpcdantic_exceptions.add
+class JarpcInvalidParams(JarpcError):
+    """Invalid params: invalid method call."""
+
+    code = -32602
+    message = "Invalid params"
+
+
+@jarpcdantic_exceptions.add
+class JarpcInternalError(JarpcError):
+    """Internal error: internal JARPC error."""
+
+    code = -32603
+    message = "Internal error"
+
+
+@jarpcdantic_exceptions.add
+class JarpcTimeout(JarpcError):
+    """Timeout: request could not be completed in time."""
+
+    code = -32604
+    message = "Timeout"
+
+
+@jarpcdantic_exceptions.add
+class JarpcServerError(JarpcError):
+    """Server error: server could not complete the request."""
+
+    code = -32000
+    message = "Server error"
+
+
+# Should be thrown in dispatch methods.
+
+# 1xxx - Access errors
+
+
+@jarpcdantic_exceptions.add
+class JarpcUnauthorized(JarpcError):
+    """Unauthorized: Similar to JarpcForbidden, but specifically for use when authentication is required and
+    has failed or has not yet been provided."""
+
+    code = 1000
+    message = "Unauthorized"
+
+
+@jarpcdantic_exceptions.add
+class JarpcForbidden(JarpcError):
+    """Forbidden: The request was valid, but the server is refusing action.
+    The user might not have the necessary permissions for a resource, or may need an account of some sort.
+    """
+
+    code = 1001
+    message = "Forbidden"
+
+
+# 2xxx - invalid arguments
+
+
+@jarpcdantic_exceptions.add
+class JarpcValidationError(JarpcError):
+    """ValidationError: error validating input parameters"""
+
+    code = 2000
+    message = "Validation error"
+
+
+# 3xxx - integration errors
+
+
+@jarpcdantic_exceptions.add
+class JarpcExternalServiceUnavailable(JarpcError):
+    """ExternalServiceUnavailable: ошибка запроса удаленной системы"""
+
+    code = 3000
+    message = "External service unavailable"
