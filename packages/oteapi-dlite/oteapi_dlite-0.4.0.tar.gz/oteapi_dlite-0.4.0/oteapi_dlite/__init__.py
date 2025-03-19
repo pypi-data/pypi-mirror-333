c7d07331325f377537192fd8b1af23da71af962c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:26 +0000

    Add package oteapi-dlite/0.4.0/oteapi_dlite-0.4.0.tar.gz

---
+++ packages/oteapi-dlite/oteapi_dlite-0.4.0.tar.gz/oteapi_dlite-0.4.0/oteapi_dlite/__init__.py
@@ -0,0 +1,13 @@
+"""OTEAPI DLite Plugin
+
+A plugin for OTE-API.
+
+Authored by team4.0@sintef.no, SINTEF, 2022
+Created from cookiecutter-oteapi-plugin, SINTEF, 2022
+"""
+
+from __future__ import annotations
+
+__version__ = "0.4.0"
+__author__ = "team4.0@sintef.no"
+__author_email__ = "team4.0@sintef.no"
