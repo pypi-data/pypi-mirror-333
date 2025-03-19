67d23ed58e3d8f8258487b376115907baeb8b833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:28 +0000

    Add package nidec-inventory/0.0a3/nidec_inventory-0.0a3.tar.gz

---
+++ packages/nidec-inventory/nidec_inventory-0.0a3.tar.gz/nidec_inventory-0.0a3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nidec-inventory",
+    version="0.0.a3",
+    description="Nidec Unidrive M motor control drives inventory generator.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Carlos Alonso Martín",
+    author_email="cam.codeweaver@gmail.com",
+    url="https://github.com/carlosalma/nidec-inventory",
+    license="MIT License",
+    install_requires=["numpy", "pandas", "scipy"],
+    packages=find_packages(),
+    #
+    package_data={'': ["*.yaml"]}
+)
