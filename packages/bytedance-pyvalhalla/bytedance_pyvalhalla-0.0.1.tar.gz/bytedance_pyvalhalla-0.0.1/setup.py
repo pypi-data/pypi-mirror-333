f413ff64d592c47d1109afc0e3d078354ba1ce73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:57 +0000

    Add package bytedance-pyvalhalla/0.0.1/bytedance_pyvalhalla-0.0.1.tar.gz

---
+++ packages/bytedance-pyvalhalla/bytedance_pyvalhalla-0.0.1.tar.gz/bytedance_pyvalhalla-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_pyvalhalla",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_pyvalhalla",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_pyvalhalla.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
