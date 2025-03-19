955eeff2149f7592b291f2f744a394d8fb4b3d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:55 +0000

    Add package mydockeruse/0.0.2/mydockeruse-0.0.2.tar.gz

---
+++ packages/mydockeruse/mydockeruse-0.0.2.tar.gz/mydockeruse-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mydockeruse',
+    version='0.0.2',
+    description='mydockeruse tool',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/mydockeruse',
+    packages=find_packages(exclude=[]),
+    install_requires=['mytool4docker'],
+    keywords=['mydockeruse', 'mydockeruse tool', 'mytool4docker'],
+    python_requires='>=3.4',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
