6bb9663b40868f63127d0be96f5a1830c753b2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:06:22 +0000

    Add package pixeltable/0.3.5/pixeltable-0.3.5.tar.gz

---
+++ packages/pixeltable/pixeltable-0.3.5.tar.gz/pixeltable-0.3.5/pixeltable/catalog/dir.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import dataclasses
+import logging
+from uuid import UUID
+
+import sqlalchemy as sql
+
+from pixeltable.env import Env
+from pixeltable.metadata import schema
+
+from .schema_object import SchemaObject
+
+_logger = logging.getLogger('pixeltable')
+
+
+class Dir(SchemaObject):
+    def __init__(self, id: UUID, parent_id: UUID, name: str):
+        super().__init__(id, name, parent_id)
+
+    @classmethod
+    def _display_name(cls) -> str:
+        return 'directory'
+
+    @property
+    def _has_dependents(self) -> bool:
+        """Returns True if this directory has any children."""
+        from pixeltable.catalog import Catalog, Path
+
+        return len(Catalog.get().paths.get_children(Path(self._path), child_type=None, recursive=False)) > 0
+
+    def _move(self, new_name: str, new_dir_id: UUID) -> None:
+        super()._move(new_name, new_dir_id)
+        with Env.get().engine.begin() as conn:
+            dir_md = schema.DirMd(name=new_name, user=None, additional_md={})
+            conn.execute(
+                sql.update(schema.Dir.__table__)
+                .values({schema.Dir.parent_id: self._dir_id, schema.Dir.md: dataclasses.asdict(dir_md)})
+                .where(schema.Dir.id == self._id)
+            )
