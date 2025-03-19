edc97f1eba71b863d82ef52bc54a1d9368ce36b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:27 +0000

    Add package vhd/1.0.7/vhd-1.0.7-py3-none-any.whl

---
+++ packages/vhd/vhd-1.0.7-py3-none-any.whl/vhd/__init__.py
@@ -0,0 +1,15 @@
+from ._vhd import _create_fixed_vhd,_read_vhd_footer
+__version__ = "1.0.7"
+__author__ = "钟阳"
+
+def test():
+    print("__int__")
+    return 1
+
+
+def create(filename, size_mb):
+    return _create_fixed_vhd(filename, size_mb)
+
+def read(filename):
+    return _read_vhd_footer(filename)
+
