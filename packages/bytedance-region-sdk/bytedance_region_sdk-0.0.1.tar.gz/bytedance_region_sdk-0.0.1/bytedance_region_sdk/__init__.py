063b526cc50184b27dc0c6343c8e82a6e1599d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:34 +0000

    Add package bytedance-region-sdk/0.0.1/bytedance_region_sdk-0.0.1.tar.gz

---
+++ packages/bytedance-region-sdk/bytedance_region_sdk-0.0.1.tar.gz/bytedance_region_sdk-0.0.1/bytedance_region_sdk/__init__.py
@@ -0,0 +1,2 @@
+# bytedance_region_sdk - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedance-region-sdk/bytedance_region_sdk-0.0.1.tar.gz/bytedance_region_sdk-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_region_sdk",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_region_sdk",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_region_sdk.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
