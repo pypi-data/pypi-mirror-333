9ebf2845e99680991c834eeff4b20f1977b1ff24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:06 +0000

    Add package lino-xl/25.3.0/lino_xl-25.3.0-py3-none-any.whl

---
+++ packages/lino-xl/lino_xl-25.3.0-py3-none-any.whl/lino_xl/lib/ibanity/choicelists.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2025 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+# Developer docs: https://dev.lino-framework.org/plugins/ibanity.html
+
+
+# from lino.api import dd, _
+# from lino_xl.lib.accounting.roles import LedgerStaff
+#
+# with_suppliers = dd.get_plugin_setting("ibanity", "with_suppliers", False)
+# with_documents = dd.get_plugin_setting("ibanity", "with_documents", False)
+# outbound_model = dd.get_plugin_setting("ibanity", "outbound_model", None)
