691eb92725e233ba7d4bc3236b96f73413ac731e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:08:53 +0000

    Add package telepack/0.0.3/telepack-0.0.3-py3-none-any.whl

---
+++ packages/telepack/telepack-0.0.3-py3-none-any.whl/telepack/endpoints.py
@@ -0,0 +1,22 @@
+# Copyright 2025 Koales Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__all__ = (
+    "METRIC_API_HOST_EU",
+    "TRACE_API_HOST_EU",
+    )
+
+# Endpoints for New Relic API deployments within the EU (Europe) region
+METRIC_API_HOST_EU = "metric-api.eu.newrelic.com"
+TRACE_API_HOST_EU = "trace-api.eu.newrelic.com"
