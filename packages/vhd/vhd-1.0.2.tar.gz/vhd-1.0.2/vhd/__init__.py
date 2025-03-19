40d0f485e6db0de270211efc73b679b04f7dea15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:39 +0000

    Add package vhd/1.0.2/vhd-1.0.2.tar.gz

---
+++ packages/vhd/vhd-1.0.2.tar.gz/vhd-1.0.2/vhd/__init__.py
@@ -0,0 +1,15 @@
+from .vhd import create_fixed_vhd,read_vhd_footer
+__version__ = "1.0.2"
+__author__ = "Zhong Yang"
+
+def test():
+    print("__int__")
+    return 1
+
+
+def create(filename, size_mb):
+    return create_fixed_vhd(filename, size_mb)
+
+def read(filename):
+    return read_vhd_footer(filename)
+
