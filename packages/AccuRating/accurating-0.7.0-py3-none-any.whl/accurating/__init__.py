f53269bde74ae11ce59ed9d1448cddd5db8e1872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:50 +0000

    Add package AccuRating/0.7.0/accurating-0.7.0-py3-none-any.whl

---
+++ packages/AccuRating/accurating-0.7.0-py3-none-any.whl/accurating/__init__.py
@@ -0,0 +1,18 @@
+from .model import (
+    MatchResultArrays,
+    Config,
+    Model,
+    fit,
+    win_prob,
+    data_from_dicts,
+)
+
+
+__all__ = [
+    "fit",
+    "data_from_dicts",
+    "MatchResultArrays",
+    "win_prob",
+    "Config",
+    "Model",
+]
