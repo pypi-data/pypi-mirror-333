bc3e4beec559244f6e7c4c13c655847f96c64587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:56 +0000

    Add package weac/2.6.0/weac-2.6.0.tar.gz

---
+++ packages/weac/weac-2.6.0.tar.gz/weac-2.6.0/weac/__init__.py
@@ -0,0 +1,21 @@
+"""
+WEak Layer AntiCrack nucleation model.
+
+Implementation of closed-form analytical models for the analysis of
+dry-snow slab avalanche release.
+"""
+
+# Module imports
+from weac.layered import Layered
+from weac.inverse import Inverse
+from weac import plot
+
+# Version
+__version__ = '2.6.0'
+
+# Public names
+__all__ = [
+    'Layered',
+    'Inverse',
+    'plot'
+]
