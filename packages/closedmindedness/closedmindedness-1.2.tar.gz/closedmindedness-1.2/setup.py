3957eab3ccde873cc4ea07bf29a91b0f212f9541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:36 +0000

    Add package closedmindedness/1.2/closedmindedness-1.2.tar.gz

---
+++ packages/closedmindedness/closedmindedness-1.2.tar.gz/closedmindedness-1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='closedmindedness',
+      version='1.2',
+      #packages=find_packages(),
+      py_modules=['closedmindedness'],
+      install_requires=['bitsandbytes',
+                        'transformers',
+                        'peft',
+                        'torch',
+                        'torchvision',
+                        'numpy'],
+      author='Michalis Mamakos',
+      author_email='mamakos@u.northwestern.edu',
+      description='Text classifier of closed-mindedness',
+      url='https://huggingface.co/mamakos/CMClassifier/',
+      classifiers=['Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   ],
+      python_requires='>=3.6'
+)
