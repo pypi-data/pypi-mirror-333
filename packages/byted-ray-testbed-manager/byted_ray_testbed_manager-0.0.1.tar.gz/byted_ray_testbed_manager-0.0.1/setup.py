94742f897fa2ab1765b3fc0550b5486b36e1b47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:25 +0000

    Add package byted-ray-testbed-manager/0.0.1/byted_ray_testbed_manager-0.0.1.tar.gz

---
+++ packages/byted-ray-testbed-manager/byted_ray_testbed_manager-0.0.1.tar.gz/byted_ray_testbed_manager-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_ray_testbed_manager",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_ray_testbed_manager",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_ray_testbed_manager.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
