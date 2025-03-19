c50b6d83d599cc16089fe35c2787bd21a2e9bdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:58 +0000

    Add package cogzen/0.0.1/cogzen-0.0.1.tar.gz

---
+++ packages/cogzen/cogzen-0.0.1.tar.gz/cogzen-0.0.1/cogzen/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Top-level package for CogZen."""
+
+# fmt: off
+from . import _version
+__version__ = _version.get_versions()['version']
+__version_dict__ = _version.get_versions()
+# fmt: on
+
+__author__ = """cogsys.io"""
+__email__ = "cogsys@cogsys.io"
+
+
+from . import aux_sys
+from .aux_sys import chdir
+from .aux_sys import pushdir
+from .aux_sys import pushdir as pdir
+
+from . import aux_log  # noqa: F401
+from .aux_log import Log0
+
+from . import aux_str
+from .aux_str import clean_str
+from .aux_str.now import now
+
+from . import aux_pandas
+from .aux_pandas import disp_df
+from .aux_pandas import repr_df
+from .aux_pandas import disp_df as ddf
+from .aux_pandas import repr_df as rdf
+
+from . import aux_srsly
+from .aux_srsly import jsonable
+from .aux_srsly import yamlstr
+
+
+def get_module_version():
+    return __version__
+
+
+# end
---
+++ packages/cogzen/cogzen-0.0.1.tar.gz/cogzen-0.0.1/docs/source/usage.rst
@@ -0,0 +1,11 @@
+=====
+Usage
+=====
+
+After following :ref:`installation:Installation` instructions,
+to use CogZen in a project:
+
+    >>> import cogzen
+
+.. note::
+   See :ref:`package docs <cogzen:cogzen package>` section for documentation.
