47e8adc615c8ff0ebf49a050d1bd1005465fd336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:29 +0000

    Add package pyhyypapihawkmod/1.5.2b1/pyhyypapihawkmod-1.5.2b1.tar.gz

---
+++ packages/pyhyypapihawkmod/pyhyypapihawkmod-1.5.2b1.tar.gz/pyhyypapihawkmod-1.5.2b1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyhyypapihawkmod',
+    version="1.5.2b1",
+    license='Apache Software License 2.0',
+    author='hawky358',
+    author_email='hawky358@users.github.com',
+    description='IDS Hyyp/ADT Secure Home API',
+    long_description="API for accessing IDS Hyyp. This is used by ADT Home Connect and possibly others.",
+    url='https://github.com/hawky358/pyHyypApi',
+    packages=setuptools.find_packages(),
+    setup_requires=[
+        'requests',
+        'setuptools'
+    ],
+    install_requires=[
+        'requests',
+        'pandas',
+        'oscrypto',
+        'protobuf',
+        'http-ece',
+        'firebase_messaging==0.4.4',
+        'appdirs'
+    ],
+    python_requires = '>=3.6'
+)
