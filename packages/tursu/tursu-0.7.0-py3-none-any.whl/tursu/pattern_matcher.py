fa6c7fb0764e41ab591bb7e5e4399a6106c9946c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:20 +0000

    Add package tursu/0.7.0/tursu-0.7.0-py3-none-any.whl

---
+++ packages/tursu/tursu-0.7.0-py3-none-any.whl/tursu/pattern_matcher.py
@@ -0,0 +1,152 @@
+import abc
+import re
+from collections.abc import Mapping
+from enum import Enum
+from inspect import Signature
+from typing import Any, Literal, get_args, get_origin
+
+
+def cast_to_annotation(
+    value: str, annotation: type[int | float | bool | str | Enum]
+) -> int | float | bool | str | Enum:
+    """
+    Safely casts a string to the given annotation.
+
+    :param value: The parameter to be cast.
+    :param annotation: the constructor.
+
+    :return: The casted value if successful, otherwise raises a ValueError.
+    """
+
+    # Define safe standard library types
+    safe_types = (int, float, bool, str)
+
+    if annotation in safe_types:
+        # Handle special case for bool
+        if annotation is bool:
+            true_vals = {"true", "1", "yes", "on"}
+            false_vals = {"false", "0", "no", "off"}
+            lower_val = value.lower()
+            if lower_val in true_vals:
+                return True
+            elif lower_val in false_vals:
+                return False
+            else:
+                raise ValueError(
+                    f"Cannot cast '{value}' to bool: "
+                    f"use one of {(', ').join(sorted([*true_vals, *false_vals]))}"
+                )
+
+        try:
+            return annotation(value)
+        except (ValueError, TypeError) as exc:
+            raise ValueError(f"Cannot cast '{value}' to {annotation}: {exc}") from exc
+
+    if get_origin(annotation) is Literal:
+        if value in get_args(annotation):
+            return value
+        raise ValueError(
+            f"Value '{value}' is not a valid Literal: {get_args(annotation)}"
+        )
+
+    if isinstance(annotation, type) and issubclass(annotation, Enum):  # type: ignore
+        try:
+            return annotation[value]
+        except KeyError:
+            raise ValueError(
+                f"Cannot cast '{value}' to Enum {annotation.__name__}"
+            ) from None
+
+    raise TypeError(f"Unsafe or unsupported type: {annotation}")
+
+
+class AbstractPatternMatcher(abc.ABC):
+    def __init__(self, pattern: str, signature: Signature) -> None:
+        self.pattern = pattern
+        self.signature = signature
+
+    def __eq__(self, other: Any) -> bool:
+        if self.__class__ != other.__class__:
+            return False
+        return self.pattern == self.pattern
+
+    def __str__(self) -> str:
+        return self.pattern
+
+    def __repr__(self) -> str:
+        return f'"{self.pattern}"'
+
+    @abc.abstractmethod
+    def get_matches(
+        self, text: str, kwargs: Mapping[str, Any]
+    ) -> Mapping[str, Any] | None: ...
+
+    @abc.abstractmethod
+    def extract_fixtures(self, text: str) -> Mapping[str, Any] | None: ...
+
+
+class AbstractPattern(abc.ABC):
+    def __init__(self, pattern: str) -> None:
+        self.pattern = pattern
+
+    @classmethod
+    @abc.abstractmethod
+    def get_matcher(cls) -> type[AbstractPatternMatcher]: ...
+
+
+class DefaultPatternMatcher(AbstractPatternMatcher):
+    def __init__(self, pattern: str, signature: Signature) -> None:
+        super().__init__(pattern, signature)
+        re_pattern = pattern
+        for key, val in signature.parameters.items():
+            match val.annotation:
+                case type() if val.annotation is int:
+                    re_pattern = re_pattern.replace(f"{{{key}}}", rf"(?P<{key}>\d+)")
+                case _:
+                    # if enclosed by double quote, use double quote as escaper
+                    # not a gherkin spec.
+                    re_pattern = re_pattern.replace(
+                        f'"{{{key}}}"', rf'"(?P<{key}>[^"]+)"'
+                    )
+                    # otherwise, match one word
+                    re_pattern = re_pattern.replace(f"{{{key}}}", rf"(?P<{key}>[^\s]+)")
+        self.re_pattern = re.compile(f"^{re_pattern}$")
+
+    def get_matches(
+        self, text: str, kwargs: Mapping[str, Any]
+    ) -> Mapping[str, Any] | None:
+        matches = self.re_pattern.match(text)
+        if matches:
+            res = {}
+            matchdict = matches.groupdict()
+            for key, val in self.signature.parameters.items():
+                if key in matchdict:
+                    # transform the annotation to call the constructror with the value
+                    typ = self.signature.parameters[key].annotation
+                    res[key] = cast_to_annotation(matchdict[key], typ)
+                elif key in kwargs:
+                    res[key] = kwargs[key]
+                elif val.default and val.default != val.empty:
+                    res[key] = val.default
+            return res
+
+        return None
+
+    def extract_fixtures(self, text: str) -> Mapping[str, Any] | None:
+        matches = self.re_pattern.match(text)
+        if matches:
+            res = {}
+            matchdict = matches.groupdict()
+            for key, val in self.signature.parameters.items():
+                if key in matchdict:
+                    continue
+                elif key == "doc_string":
+                    continue
+                elif key == "data_table":
+                    continue
+                if val.default != val.empty:
+                    continue
+                res[key] = val.annotation
+            return res
+
+        return None
