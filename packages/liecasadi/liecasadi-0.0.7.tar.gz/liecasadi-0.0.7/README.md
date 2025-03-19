eaef0d1fd1f67f1491acd66c2d8baf2351fcd46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:05 +0000

    Add package liecasadi/0.0.7/liecasadi-0.0.7.tar.gz

---
+++ packages/liecasadi/liecasadi-0.0.7.tar.gz/liecasadi-0.0.7/README.md
@@ -0,0 +1,152 @@
+# liecasadi
+
+[![liecasadi](https://github.com/ami-iit/liecasadi/actions/workflows/tests.yml/badge.svg)](https://github.com/ami-iit/liecasadi/actions/workflows/tests.yml)
+
+`liecasadi` implements Lie groups operation written in CasADi, mainly directed to optimization problem formulation.
+
+Inspired by [A micro Lie theory for state estimation in robotics](https://arxiv.org/pdf/1812.01537.pdf) and the library [Manif](https://github.com/artivis/manif).
+
+## 🐍 Install
+
+### Conda
+
+Create a [conda environment](https://github.com/robotology/robotology-superbuild/blob/master/doc/conda-forge.md)
+```
+conda create -n liecasadienv
+```
+and install the library 
+```
+conda install liecasadi
+```
+
+### Pip
+Create a [virtual environment](https://docs.python.org/3/library/venv.html#venv-def), if you prefer. For example:
+
+```bash
+pip install virtualenv
+python3 -m venv your_virtual_env
+source your_virtual_env/bin/activate
+```
+
+Inside the virtual environment, install the library from pip:
+
+```bash
+pip install liecasadi
+```
+
+If you want the last version:
+
+```bash
+pip install "liecasadi @ git+https://github.com/ami-iit/lie-casadi.git"
+```
+
+## Implemented Groups
+
+| **Group** | Description        |
+| --------- | ------------------ |
+| SO3       | 3D Rotations       |
+| SE3       | 3D Rigid Transform |
+
+### 🚀 Operations
+
+Being:
+
+- $X, Y \in SO3, \ SE3$
+
+- $w \in \text{SO3Tangent}, \ \text{SE3Tangent}$
+
+- $v \in \mathbb{R}^3$
+
+| Operation           |                                       |     Code      |
+| :------------------ | :-----------------------------------: | :-----------: |
+| Inverse             |               $X^{-1}$                | `X.inverse()` |
+| Composition         |              $X \circ Y$              |     `X*Y`     |
+| Exponential         |            $\text{exp}(w)$            | `phi.exp() `  |
+| Act on vector       |              $X \circ v$              |  `X.act(v)`   |
+| Logarithm           |            $\text{log}(X)$            |   `X.log()`   |
+| Manifold right plus | $X \oplus  w = X \circ \text{exp}(w)$ |   `X + phi`   |
+| Manifold left plus  | $w \oplus X = \text{exp}(w) \circ X$  |   `phi + X`   |
+| Manifold minus      |  $X-Y = \text{log}(Y^{-1} \circ X)$   |     `X-Y`     |
+
+## 🦸‍♂️ Example
+
+```python
+from liecasadi import SE3, SO3, SE3Tangent, SO3Tangent
+
+# Random quaternion + normalization
+quat = (np.random.rand(4) - 0.5) * 5
+quat = quat / np.linalg.norm(quat)
+# Random vector
+vector3d = (np.random.rand(3) - 0.5) * 2 * np.pi
+
+# Create SO3 object
+rotation = SO3(quat)
+
+# Create Identity
+identity = SO3.Identity()
+
+# Create SO3Tangent object
+tangent = SO3Tangent(vector3d)
+
+# Random translation vector
+pos = (np.random.rand(3) - 0.5) * 5
+
+# Create SE3 object
+transform = SE3(pos=pos, xyzw=quat)
+
+# Random vector
+vector6d = (np.random.rand(3) - 0.5) * 5
+
+# Create SE3Tangent object
+tangent = SO3Tangent(vector6d)
+```
+
+### Dual Quaternion example
+
+```python
+from liecasadi import SE3, DualQuaternion
+from numpy import np
+
+# orientation quaternion generation
+quat1 = (np.random.rand(4) - 0.5) * 5
+quat1 = quat1 / np.linalg.norm(quat1)
+quat2 = (np.random.rand(4) - 0.5) * 5
+quat2 = quat2 / np.linalg.norm(quat2)
+
+# translation vector generation
+pos1 = (np.random.rand(3) - 0.5) * 5
+pos2 = (np.random.rand(3) - 0.5) * 5
+
+dual_quaternion1 = DualQuaternion(quat1, pos1)
+dual_quaternion2 = DualQuaternion(quat2, pos2)
+
+# from a homogenous matrix
+# (using liecasadi.SE3 to generate the corresponding homogenous matrix)
+H = SE3.from_position_quaternion(pos, quat).as_matrix()
+dual_quaternion1 = DualQuaternion.from_matrix(H)
+
+# Concatenation of rigid transforms
+q1xq2 = dual_quaternion1 * dual_quaternion2
+
+# to homogeneous matrix
+print(q1xq2.as_matrix())
+
+# obtain translation
+print(q1xq2.translation())
+
+# obtain rotation
+print(q1xq2.rotation().as_matrix())
+
+# transform a point
+point = np.random.randn(3,1)
+transformed_point = dual_quaternion1.transform_point(point)
+
+# create an identity dual quaternion
+I = DualQuaternion.Identity()
+```
+
+## 🦸‍♂️ Contributing
+
+**liecasadi** is an open-source project. Contributions are very welcome!
+
+Open an issue with your feature request or if you spot a bug. Then, you can also proceed with a Pull-requests! :rocket:
---
+++ packages/liecasadi/liecasadi-0.0.7.tar.gz/liecasadi-0.0.7/src/liecasadi/dualquaternion.py
@@ -0,0 +1,214 @@
+# Copyright (C) Istituto Italiano di Tecnologia (IIT). All rights reserved.
+import dataclasses
+import warnings
+from dataclasses import field
+
+import casadi as cs
+
+from liecasadi import SE3, SO3, Quaternion
+from liecasadi.hints import Matrix, Scalar, Vector
+
+
+@dataclasses.dataclass
+class DualQuaternion:
+    """
+    Dual Quaternion class
+    Have a look at the documents https://cs.gmu.edu/~jmlien/teaching/cs451/uploads/Main/dual-quaternion.pdf
+    https://faculty.sites.iastate.edu/jia/files/inline-files/dual-quaternion.pdf for some intuitions.
+    """
+
+    qr: Vector
+    qd: Vector
+    Qr: Quaternion = field(init=False)
+    Qd: Quaternion = field(init=False)
+
+    def __post_init__(self):
+        """Build two Quaternion objects from xyzw quaternion and xyz translation"""
+        self.Qr = Quaternion(self.qr)
+        self.Qd = Quaternion(self.qd)
+
+    def __repr__(self) -> str:
+        """
+        Returns:
+            str: when you print the dual quaternion
+        """
+        return f"Rotation quaternion: {self.Qr.xyzw} \nTranslation quaternion: {self.Qd.coeffs()}"
+
+    def __mul__(self, other: "DualQuaternion") -> "DualQuaternion":
+        """Dual quaternion multiplication
+
+        Args:
+            other (DualQuaternion): a Dual Quaternion
+
+        Returns:
+            DualQuaternion: the multiplication product
+        """
+        qr = self.Qr * other.Qr
+        qd = self.Qr * other.Qd + self.Qd * other.Qr
+        return DualQuaternion(qr=qr.coeffs(), qd=qd.coeffs())
+
+    def __rmul__(self, other: Scalar) -> "DualQuaternion":
+        """Multiplication with a scalar
+
+        Returns:
+            Dual Quaternion
+        """
+        return DualQuaternion(qr=other * self.qr, qd=other * self.qd)
+
+    def __add__(self, other: "DualQuaternion") -> "DualQuaternion":
+        """Sum of 2 Dual quaternions
+
+        Args:
+            other (DualQuaternion): a Dual Quaternion
+
+        Returns:
+            DualQuaternion: the sum of Dual Quaternions
+        """
+        qr = self.Qr + other.Qr
+        qd = self.Qd + other.Qd
+        return DualQuaternion(qr=qr.coeffs(), qd=qd.coeffs())
+
+    def __sub__(self, other: "DualQuaternion") -> "DualQuaternion":
+        """Difference of 2 Dual quaternions
+
+        Args:
+            other (DualQuaternion): a Dual Quaternion
+
+        Returns:
+            DualQuaternion: the difference of Dual Quaternions
+        """
+        qr = self.Qr - other.Qr
+        qd = self.Qd - other.Qd
+        return DualQuaternion(qr=qr.coeffs(), qd=qd.coeffs())
+
+    @staticmethod
+    def from_quaternion_and_translation(
+        quat: Vector, transl: Vector
+    ) -> "DualQuaternion":
+        """Build dual quaternion from a quaternion (xyzw) and a translation vector (xyz)
+
+        Args:
+            quat (Vector): xyzw quaternion vector
+            transl (Vector): xyz translation vector
+
+        Returns:
+            DualQuaternion: a dual quaternion
+        """
+        t = Quaternion(cs.vertcat(transl, 0))
+        r = Quaternion(quat)
+        qd = 0.5 * (t * r).coeffs()
+        return DualQuaternion(qr=r.coeffs(), qd=qd)
+
+    @staticmethod
+    def from_matrix(m: Matrix) -> "DualQuaternion":
+        """Build dual quaternion from an homogenous matrix
+
+        Args:
+            m (Matrix): homogenous matrix
+
+        Returns:
+            DualQuaternion: a dual quaternion
+        """
+        se3 = SE3.from_matrix(m)
+        r = se3.rotation().as_quat()
+        t = Quaternion(cs.vertcat(se3.translation(), 0))
+        qd = 0.5 * (t * r).coeffs()
+        return DualQuaternion(qr=r.coeffs(), qd=qd)
+
+    @staticmethod
+    def from_SE3(se3: SE3) -> "DualQuaternion":
+        """Build dual quaternion from an SE3 object
+
+        Args:
+            se3 (SE3): an SE3 object
+
+        Returns:
+            DualQuaternion: a dual quaternion
+        """
+        r = se3.rotation().as_quat()
+        t = Quaternion(cs.vertcat(se3.translation(), 0))
+        qd = 0.5 * (t * r).coeffs()
+        return DualQuaternion(qr=r.coeffs(), qd=qd)
+
+    def coeffs(self) -> Vector:
+        """
+        Returns:
+            Vector: the dual quaternion vector xyzwxyz
+        """
+        return cs.vertcat(self.qd, self.qr)
+
+    def translation(self) -> Vector:
+        """
+        Returns:
+            Vector: Translation vector xyz
+        """
+        return 2.0 * (self.Qd * self.Qr.conjugate()).coeffs()[:3]
+
+    def rotation(self) -> SO3:
+        """
+        Returns:
+            SO3: an SO3 object
+        """
+        return SO3(xyzw=self.Qr.coeffs())
+
+    def inverse(self) -> "DualQuaternion":
+        """
+        Returns:
+            DualQuaternion: a dual quaternion, inverse of the original
+        """
+        qr_inv = self.Qr.conjugate()
+        qd = -qr_inv * self.Qd * qr_inv
+        return DualQuaternion(qr=qr_inv.coeffs(), qd=qd.coeffs())
+
+    def conjugate(self) -> "DualQuaternion":
+        """
+        Returns:
+            DualQuaternion: conjugate dual quaternion
+        """
+        qr = self.Qr.conjugate()
+        qd = self.Qd.conjugate()
+        return DualQuaternion(qr=qr.coeffs(), qd=qd.coeffs())
+
+    def dual_conjugate(self) -> "DualQuaternion":
+        """
+        Returns:
+            DualQuaternion: dual number conjugate, used in point transformation
+        """
+        qr = self.Qr.conjugate()
+        qd = self.Qd.conjugate()
+        return DualQuaternion(qr=qr.coeffs(), qd=-qd.coeffs())
+
+    def as_matrix(self) -> Matrix:
+        """
+        Returns:
+            Matrix: the corresponding homogenous matrix
+        """
+        r = self.rotation().as_matrix()
+        t = self.translation()
+        return cs.vertcat(
+            cs.horzcat(r, t),
+            cs.horzcat([0, 0, 0, 1]).T,
+        )
+
+    @staticmethod
+    def Identity() -> "DualQuaternion":
+        """
+        Returns:
+            DualQuaternion: the identity dual quaternion
+        """
+        return DualQuaternion(
+            qr=SO3.Identity().as_quat().coeffs(), qd=Quaternion(cs.DM.zeros(4)).coeffs()
+        )
+
+    def transform_point(self, xyz: Vector) -> Vector:
+        """Rototranlates a point
+        Args:
+            xyz (Vector): the point
+
+        Returns:
+            Vector: the transformed point
+        """
+        p = DualQuaternion.Identity()
+        xyzw = cs.vertcat(xyz[0], xyz[1], xyz[2], 0)
+        p.Qd = Quaternion(xyzw)
+        return (self * p * self.dual_conjugate()).coeffs()[:3]
