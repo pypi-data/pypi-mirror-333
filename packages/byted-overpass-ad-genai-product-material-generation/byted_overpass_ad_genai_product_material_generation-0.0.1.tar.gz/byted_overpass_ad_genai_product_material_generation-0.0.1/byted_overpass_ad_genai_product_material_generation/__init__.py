690c0a91e60de419b8dd25c6001746cb9503a534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:13 +0000

    Add package byted-overpass-ad-genai-product-material-generation/0.0.1/byted_overpass_ad_genai_product_material_generation-0.0.1.tar.gz

---
+++ packages/byted-overpass-ad-genai-product-material-generation/byted_overpass_ad_genai_product_material_generation-0.0.1.tar.gz/byted_overpass_ad_genai_product_material_generation-0.0.1/byted_overpass_ad_genai_product_material_generation/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_ad_genai_product_material_generation - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-ad-genai-product-material-generation/byted_overpass_ad_genai_product_material_generation-0.0.1.tar.gz/byted_overpass_ad_genai_product_material_generation-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_ad_genai_product_material_generation",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_ad_genai_product_material_generation",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_ad_genai_product_material_generation.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
