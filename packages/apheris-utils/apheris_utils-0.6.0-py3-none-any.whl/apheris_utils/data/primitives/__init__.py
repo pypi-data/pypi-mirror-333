4017812208b66be5b22f0b0be5738d56d621a110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:33 +0000

    Add package apheris-utils/0.6.0/apheris_utils-0.6.0-py3-none-any.whl

---
+++ packages/apheris-utils/apheris_utils-0.6.0-py3-none-any.whl/apheris_utils/data/primitives/__init__.py
@@ -0,0 +1,17 @@
+from ._primitives import (
+    RemoteFile,
+    get,
+    list_remote_files,
+    settings,
+    to_dict,
+    to_folder,
+)
+
+__all__ = [
+    "get",
+    "list_remote_files",
+    "to_folder",
+    "to_dict",
+    "settings",
+    "RemoteFile",
+]
