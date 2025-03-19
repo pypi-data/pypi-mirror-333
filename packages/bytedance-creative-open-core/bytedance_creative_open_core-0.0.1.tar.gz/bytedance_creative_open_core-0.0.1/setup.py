ba00562b5abe732d64c8c27083776911ab8916a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:15 +0000

    Add package bytedance-creative-open-core/0.0.1/bytedance_creative_open_core-0.0.1.tar.gz

---
+++ packages/bytedance-creative-open-core/bytedance_creative_open_core-0.0.1.tar.gz/bytedance_creative_open_core-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_creative_open_core",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_creative_open_core",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_creative_open_core.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
