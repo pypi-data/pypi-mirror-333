f36b5a2326d219e7a5e758b63ea30a8e6b2e54ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:32 +0000

    Add package bcpkgfox/0.12.9/bcpkgfox-0.12.9.tar.gz

---
+++ packages/bcpkgfox/bcpkgfox-0.12.9.tar.gz/bcpkgfox-0.12.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bcpkgfox",
+    version="0.12.9",
+    author="Guilherme Neri",
+    author_email="guilherme.neri@bcfox.com.br",
+    description="Biblioteca BCFOX",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/robotsbcfox/PacotePythonBCFOX",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'undetected-chromedriver',
+        'webdriver-manager',
+        'selenium-stealth',
+        'opencv-python',
+        'pygetwindow',
+        'setuptools',
+        'pyscreeze',
+        'pyautogui',
+        'requests',
+        'selenium',
+        'pymupdf',
+        'Pillow',
+        'psutil'
+    ],
+)
