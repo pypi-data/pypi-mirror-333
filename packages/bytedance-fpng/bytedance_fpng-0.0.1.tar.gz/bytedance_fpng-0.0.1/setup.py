befecf780f8a1b11325907313d791d2aa7e65d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:35 +0000

    Add package bytedance-fpng/0.0.1/bytedance_fpng-0.0.1.tar.gz

---
+++ packages/bytedance-fpng/bytedance_fpng-0.0.1.tar.gz/bytedance_fpng-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_fpng",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_fpng",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_fpng.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
