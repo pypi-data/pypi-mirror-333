bc24da4834076eccce2fe1ff25e731e78726f7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:13 +0000

    Add package tap-ga360/0.2.2/tap-ga360-0.2.2.tar.gz

---
+++ packages/tap-ga360/tap-ga360-0.2.2.tar.gz/tap-ga360-0.2.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="tap-ga360",
+    version="0.2.2",
+    description="Singer.io tap for extracting data from Google Analytics 360 via BigQuery",
+    author="Stitch",
+    url="https://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_ga360"],
+    install_requires=["google-cloud-bigquery==3.30.0", "singer-python==5.12.2"],
+    extras_require={
+        'dev': [
+            'pylint',
+            'ipdb'
+        ]
+      },
+    entry_points="""
+          [console_scripts]
+          tap-ga360=tap_ga360:main
+      """,
+    packages=["tap_ga360"],
+    package_data = {
+        "schemas": ["tap_ga360/schemas/*.json"]
+    },
+    include_package_data=True,
+)
