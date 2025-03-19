4daa58ffe59b863a9fc84c096978196414b472c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:06:35 +0000

    Add package drf-apischema/0.2.2/drf_apischema-0.2.2.tar.gz

---
+++ packages/drf-apischema/drf_apischema-0.2.2.tar.gz/drf_apischema-0.2.2/src/drf_apischema/urls.py
@@ -0,0 +1,11 @@
+from django.urls import URLPattern, URLResolver, include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+
+def api_path(prefix: str, urlpatterns: list[URLResolver | URLPattern]):
+    docs_urlpatterns = [
+        path("schema/", SpectacularAPIView.as_view(), name="schema"),
+        path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+        path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    ]
+    return path(prefix, include(docs_urlpatterns + urlpatterns))
