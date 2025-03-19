c67c0caf534ab8a3e2f74a54c8ca217cd7313392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:36 +0000

    Add package bytedance-tls-client/0.0.1/bytedance_tls_client-0.0.1.tar.gz

---
+++ packages/bytedance-tls-client/bytedance_tls_client-0.0.1.tar.gz/bytedance_tls_client-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_tls_client",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_tls_client",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_tls_client.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
