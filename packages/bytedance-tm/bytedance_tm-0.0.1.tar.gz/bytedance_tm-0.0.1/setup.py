69055959b3fd143238a8644c716a2562ab26d267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:33 +0000

    Add package bytedance-tm/0.0.1/bytedance_tm-0.0.1.tar.gz

---
+++ packages/bytedance-tm/bytedance_tm-0.0.1.tar.gz/bytedance_tm-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_tm",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_tm",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_tm.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
