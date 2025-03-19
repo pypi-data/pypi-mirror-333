d5658f911478ed4a118175dfc5108aca96fd5f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:05:08 +0000

    Add package snl-exp-regression-quality/0.1.1/snl_exp_regression_quality-0.1.1.tar.gz

---
+++ packages/snl-exp-regression-quality/snl_exp_regression_quality-0.1.1.tar.gz/snl_exp_regression_quality-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup (
+    name='snl_exp_regression_quality',
+    version='0.1.1',
+    license='MIT',
+    description='simple no linear regression quality (Exponential)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author= 'EdelH, Aplatag',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'de_embedding': ['data/*'],
+        
+    },
+
+    install_requires = ['numpy','pandas','scikit-rf','matplotlib','scipy'],
+
+    url='https://github.com/aplatag/project_SNL_regression_quality'
+)
