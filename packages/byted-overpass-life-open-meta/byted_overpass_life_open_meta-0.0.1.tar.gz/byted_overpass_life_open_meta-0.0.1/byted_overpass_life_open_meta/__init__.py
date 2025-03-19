e71db96f3368e6f63ba241fb5e7a37e936a1cce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:12 +0000

    Add package byted-overpass-life-open-meta/0.0.1/byted_overpass_life_open_meta-0.0.1.tar.gz

---
+++ packages/byted-overpass-life-open-meta/byted_overpass_life_open_meta-0.0.1.tar.gz/byted_overpass_life_open_meta-0.0.1/byted_overpass_life_open_meta/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_life_open_meta - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-life-open-meta/byted_overpass_life_open_meta-0.0.1.tar.gz/byted_overpass_life_open_meta-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_life_open_meta",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_life_open_meta",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_life_open_meta.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
