0dad45765107c2367d78f35cd90c3006e24b5fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:51 +0000

    Add package vhd/1.0.5/vhd-1.0.5.tar.gz

---
+++ packages/vhd/vhd-1.0.5.tar.gz/vhd-1.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vhd',
+    version='1.0.5',
+    packages=find_packages(),
+    install_requires=[
+        # 依赖列表
+    ],
+    author='ZhongYang',
+    author_email='19865697458@163.com',
+    description='VHD操作库',
+    long_description=open('README.md',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitee.com/yangyingyun/vhd',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
