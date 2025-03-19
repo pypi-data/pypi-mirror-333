a71f2bca761507bbb275ddd3fb18c58110d78d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:56 +0000

    Add package ks_futu_market_api/1.0.7/ks_futu_market_api-1.0.7.tar.gz

---
+++ packages/ks_futu_market_api/ks_futu_market_api-1.0.7.tar.gz/ks_futu_market_api-1.0.7/__about__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024-present liuyanzhi08 <702368372@qq.com>
+#
+# SPDX-License-Identifier: MIT
+# __version__ = "0.1.66" # 周期增加月份支持
+# __version__ = "1.0.0" # 移除对老虎api依赖
+# __version__ = "1.0.3" # query_hisotry_n由decimal改为返回float
+# __version__ = "1.0.4" # get_trading_time默认返回原来交易所时区
+# __version__ = "1.0.5" # query_history_n 支持是否盘前
+__version__ = "1.0.7" # query_history_n修正week和month的查询
