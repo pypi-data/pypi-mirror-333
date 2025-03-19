02155a8418a1904ac1abee80b43a518be54a6e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:30 +0000

    Add package logisticpca/0.1.1/logisticpca-0.1.1.tar.gz

---
+++ packages/logisticpca/logisticpca-0.1.1.tar.gz/logisticpca-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="logisticpca",  # Package name users will install via pip
+    version="0.1.1",  # Update version numbers for new releases
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "numpy"
+    ],
+    author="Reda Abouzaid",
+    author_email="azaidr00@gmail.com",
+    description="A Python implementation of Logistic PCA for binary data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/azaidr/logisticpca",  # Your GitHub repo
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
