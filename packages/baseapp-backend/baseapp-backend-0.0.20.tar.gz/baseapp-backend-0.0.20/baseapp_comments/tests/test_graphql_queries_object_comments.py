ee095a6c331499c4abcf852372ad3b9e484f5555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:59 +0000

    Add package baseapp-backend/0.0.20/baseapp-backend-0.0.20.tar.gz

---
+++ packages/baseapp-backend/baseapp-backend-0.0.20.tar.gz/baseapp-backend-0.0.20/baseapp_comments/tests/test_graphql_queries_object_comments.py
@@ -0,0 +1,178 @@
+import pytest
+from django.test import override_settings
+
+from baseapp_core.tests.factories import UserFactory
+from baseapp_reactions.tests.factories import ReactionFactory
+
+from .factories import CommentFactory
+
+pytestmark = pytest.mark.django_db
+
+VIEW_ALL_QUERY = """
+    query GetObject($id: ID!, $orderBy: String, $q: String) {
+        node(id: $id) {
+            ... on CommentsInterface {
+                commentsCount {
+                    main
+                    replies
+                }
+                comments(orderBy: $orderBy, q: $q) {
+                    edges {
+                        node {
+                            id
+                            pk
+                            commentsCount {
+                                main
+                                replies
+                            }
+                            comments {
+                                edges {
+                                    node {
+                                        id
+                                        pk
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+"""
+
+
+def test_anon_see_comments_and_replies(django_user_client, graphql_client):
+    target = CommentFactory()
+    user = django_user_client.user
+    replying_user = UserFactory()
+    comment = CommentFactory(target=target, user=user)
+    CommentFactory.create_batch(target=target, in_reply_to=comment, size=2, user=replying_user)
+
+    response = graphql_client(VIEW_ALL_QUERY, variables={"id": target.relay_id})
+    content = response.json()
+
+    assert content["data"]["node"]["commentsCount"]["main"] == 1
+    assert content["data"]["node"]["commentsCount"]["replies"] == 2
+    assert content["data"]["node"]["comments"]["edges"][0]["node"]["id"] == comment.relay_id
+    assert content["data"]["node"]["comments"]["edges"][0]["node"]["commentsCount"]["main"] == 2
+    assert len(content["data"]["node"]["comments"]["edges"][0]["node"]["comments"]["edges"]) == 2
+
+
+def test_anon_cant_see_comments_when_disabled(graphql_client):
+    target = CommentFactory(is_comments_enabled=False)
+    CommentFactory(target=target)
+
+    response = graphql_client(VIEW_ALL_QUERY, variables={"id": target.relay_id})
+    content = response.json()
+
+    assert len(content["data"]["node"]["comments"]["edges"]) == 0
+
+
+def test_search(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+    comment = CommentFactory(target=target, body="the s1lv3r logic")
+
+    response = graphql_client(VIEW_ALL_QUERY, variables={"id": target.relay_id, "q": "s1lv3r"})
+    content = response.json()
+
+    assert len(content["data"]["node"]["comments"]["edges"]) == 1
+    assert content["data"]["node"]["comments"]["edges"][0]["node"]["id"] == comment.relay_id
+
+
+def test_order_by_pinned_first(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+    comment = CommentFactory(target=target, is_pinned=True)
+
+    response = graphql_client(
+        VIEW_ALL_QUERY, variables={"id": target.relay_id, "orderBy": "-is_pinned"}
+    )
+    content = response.json()
+
+    assert len(content["data"]["node"]["comments"]["edges"]) == 2
+    assert content["data"]["node"]["comments"]["edges"][0]["node"]["id"] == comment.relay_id
+
+
+def test_order_by_pinned_last(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+    comment = CommentFactory(target=target, is_pinned=True)
+
+    response = graphql_client(
+        VIEW_ALL_QUERY, variables={"id": target.relay_id, "orderBy": "is_pinned"}
+    )
+    content = response.json()
+
+    assert len(content["data"]["node"]["comments"]["edges"]) == 2
+    assert content["data"]["node"]["comments"]["edges"][-1]["node"]["id"] == comment.relay_id
+
+
+def test_order_by_reactions_total_desc(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+    comment = CommentFactory(target=target)
+    ReactionFactory(target=comment)
+
+    response = graphql_client(
+        VIEW_ALL_QUERY, variables={"id": target.relay_id, "orderBy": "-reactions_count_total"}
+    )
+    content = response.json()
+    assert len(content["data"]["node"]["comments"]["edges"]) == 2
+    assert content["data"]["node"]["comments"]["edges"][0]["node"]["id"] == comment.relay_id
+
+
+def test_order_by_reactions_total_asc(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+    comment = CommentFactory(target=target)
+    ReactionFactory(target=comment)
+
+    response = graphql_client(
+        VIEW_ALL_QUERY, variables={"id": target.relay_id, "orderBy": "reactions_count_total"}
+    )
+    content = response.json()
+
+    assert len(content["data"]["node"]["comments"]["edges"]) == 2
+    assert content["data"]["node"]["comments"]["edges"][-1]["node"]["id"] == comment.relay_id
+
+
+def test_order_by_replies_total_desc(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+    comment = CommentFactory(target=target)
+    CommentFactory(target=target, in_reply_to=comment)
+
+    response = graphql_client(
+        VIEW_ALL_QUERY, variables={"id": target.relay_id, "orderBy": "-replies_count_total"}
+    )
+    content = response.json()
+    assert len(content["data"]["node"]["comments"]["edges"]) == 2
+    assert content["data"]["node"]["comments"]["edges"][0]["node"]["id"] == comment.relay_id
+
+
+def test_order_by_replies_total_asc(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+    comment = CommentFactory(target=target, is_pinned=True)
+    CommentFactory(target=target, in_reply_to=comment)
+
+    response = graphql_client(
+        VIEW_ALL_QUERY, variables={"id": target.relay_id, "orderBy": "replies_count_total"}
+    )
+    content = response.json()
+
+    assert len(content["data"]["node"]["comments"]["edges"]) == 2
+    assert content["data"]["node"]["comments"]["edges"][-1]["node"]["id"] == comment.relay_id
+
+
+@override_settings(BASEAPP_COMMENTS_CAN_ANONYMOUS_VIEW_COMMENTS=False)
+def test_anon_cant_see_comments(graphql_client):
+    target = CommentFactory()
+    CommentFactory(target=target)
+
+    response = graphql_client(VIEW_ALL_QUERY, variables={"id": target.relay_id})
+    content = response.json()
+
+    assert len(content["data"]["node"]["comments"]["edges"]) == 0
