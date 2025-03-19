60372f6fecf980ff2703d215db799c94b0f8c95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:25 +0000

    Add package Geoarchpy/1.2.0/geoarchpy-1.2.0.tar.gz

---
+++ packages/Geoarchpy/geoarchpy-1.2.0.tar.gz/geoarchpy-1.2.0/ArchPy/__init__.py
@@ -0,0 +1,11 @@
+# __version__ = '1.0.5'
+
+from . import base
+from . import data_transfo
+from . import ineq
+from . import infer
+from . import inputs
+from . import x_valid
+from . import _version
+
+__version__ = _version.__version__
