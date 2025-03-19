d096d91aa66cd32a1587582346e5b84e46437da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:31 +0000

    Add package aqua-blue/0.1.5/aqua_blue-0.1.5-py3-none-any.whl

---
+++ packages/aqua-blue/aqua_blue-0.1.5-py3-none-any.whl/aqua_blue/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "0.1.5"
+__authors__ = [
+    "Jacob Jeffries",
+    "Ameen Mahmood",
+    "Avik Thumati",
+    "Samuel Josephs",
+    "Hrishikesh Belagali"
+]
+
+# need to change hrishi's email once there's a ccmc one for him
+__author_emails__ = [
+    "jacob.jeffries@chicagoclubteam.org",
+    "ameen.mahmood@chicagoclubteam.org",
+    "avik.thumati@chicagoclubteam.org",
+    "samuel.joseph@chicagoclubteam.org",
+    "hkbelagali@gmail.com"
+]
+__url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue"
+
+from . import utilities as utilities
+from . import reservoirs as reservoirs
+from . import readouts as readouts
+from . import models as models
+from . import time_series as time_series
