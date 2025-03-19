57e356c8733b0f5752aaca767a07ab3284bc42b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:59 +0000

    Add package bytedance-circle-engineer/0.0.1/bytedance_circle_engineer-0.0.1.tar.gz

---
+++ packages/bytedance-circle-engineer/bytedance_circle_engineer-0.0.1.tar.gz/bytedance_circle_engineer-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_circle_engineer",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_circle_engineer",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_circle_engineer.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
