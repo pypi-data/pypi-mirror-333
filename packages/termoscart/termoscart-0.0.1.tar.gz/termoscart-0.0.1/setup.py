84c57016a43507f007b059f3b1b9a58c178ad226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:46 +0000

    Add package termoscart/0.0.1/termoscart-0.0.1.tar.gz

---
+++ packages/termoscart/termoscart-0.0.1.tar.gz/termoscart-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="termoscart",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://github.com/kvnglb/termoscart",
+    author="Kevin Golob",
+    author_email="151143873+kvnglb@users.noreply.github.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
+
