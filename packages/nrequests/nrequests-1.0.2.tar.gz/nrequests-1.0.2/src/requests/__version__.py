2c71e8de141af3fd88d34c05eab1ff5c8062e74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:20 +0000

    Add package nrequests/1.0.2/nrequests-1.0.2.tar.gz

---
+++ packages/nrequests/nrequests-1.0.2.tar.gz/nrequests-1.0.2/src/requests/__version__.py
@@ -0,0 +1,14 @@
+# .-. .-. .-. . . .-. .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-. `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-' `-'
+
+__title__ = "nrequests"
+__description__ = "Python HTTP for Humans with added telemetry support."
+__url__ = ""
+__version__ = "1.0.2"
+__build__ = 0x010002
+__author__ = "Aad3b435"
+__author_email__ = ""
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright Aad3b435"
+__cake__ = "\u2728 \U0001f370 \u2728"
