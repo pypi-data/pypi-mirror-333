9faddc546bb0f8af12d08d689decfbeb079a6c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:06:50 +0000

    Add package gochocolatego/1.3/gochocolatego-1.3.tar.gz

---
+++ packages/gochocolatego/gochocolatego-1.3.tar.gz/gochocolatego-1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gochocolatego",
+    version="1.2",
+    packages=find_packages(),
+    install_requires=[
+        "rich"
+    ],
+    entry_points={
+        "console_scripts": [
+            "chocolate=chocolate.__main__:main",
+        ],
+    },
+    author="Chocolateisfr",
+    description="A project manager CLI tool.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/frchocolate/chocolate",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
