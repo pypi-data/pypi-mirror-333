b8e87920ebcd6002c2dd55fbfda556e4776f5628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:08:54 +0000

    Add package tuf/6.0.0/tuf-6.0.0-py3-none-any.whl

---
+++ packages/tuf/tuf-6.0.0-py3-none-any.whl/tuf/__init__.py
@@ -0,0 +1,7 @@
+# Copyright New York University and the TUF contributors
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+"""TUF."""
+
+# This value is used in the ngclient user agent.
+__version__ = "6.0.0"
