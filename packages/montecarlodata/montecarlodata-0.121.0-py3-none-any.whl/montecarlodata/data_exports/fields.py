7c5a36919d61e451f21ce6a9e29c433b755434d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:54 +0000

    Add package montecarlodata/0.121.0/montecarlodata-0.121.0-py3-none-any.whl

---
+++ packages/montecarlodata/montecarlodata-0.121.0-py3-none-any.whl/montecarlodata/data_exports/fields.py
@@ -0,0 +1,13 @@
+# Query responses
+EXPECTED_GET_DATA_EXPORT_URL = "getDataExportUrl"
+
+# Data Export Names
+ALL_MONITORS_DATA_EXPORT_NAME = "ALL_MONITORS"
+
+# Supported Schemes
+FILE_SCHEME = "file"
+S3_SCHEME = "s3"
+SCHEME_DELIM = "://"
+
+# Data Export headers
+LIST_DATA_EXPORTS_HEADERS = ["Data Export (Name)", "Description"]
