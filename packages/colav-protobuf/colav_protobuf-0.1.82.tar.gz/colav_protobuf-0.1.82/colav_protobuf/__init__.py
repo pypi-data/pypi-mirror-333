e8843518b2aa3bda81209c73f442a0662f0d0f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:16:03 +0000

    Add package colav-protobuf/0.1.82/colav_protobuf-0.1.82.tar.gz

---
+++ packages/colav-protobuf/colav_protobuf-0.1.82.tar.gz/colav_protobuf-0.1.82/colav_protobuf/__init__.py
@@ -0,0 +1,13 @@
+from colav_protobuf.missionRequest_pb2 import MissionRequest
+from colav_protobuf.missionResponse_pb2 import MissionResponse
+from colav_protobuf.agentUpdate_pb2 import AgentUpdate
+from colav_protobuf.obstaclesUpdate_pb2 import ObstaclesUpdate
+from colav_protobuf.controllerFeedback_pb2 import ControllerFeedback
+
+__all__ = [
+    "MissionRequest",
+    "MissionResponse",
+    "AgentUpdate",
+    "ObstaclesUpdate",
+    "ControllerFeedback",
+]
