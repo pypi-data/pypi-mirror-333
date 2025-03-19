eed64604374a1944e0e344cc5f2aee9e02b0f535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:54 +0000

    Add package terminal-shop/1.1.0/terminal_shop-1.1.0.tar.gz

---
+++ packages/terminal-shop/terminal_shop-1.1.0.tar.gz/terminal_shop-1.1.0/src/terminal_shop/types/view_init_response.py
@@ -0,0 +1,43 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .app import App
+from .card import Card
+from .cart import Cart
+from .order import Order
+from .token import Token
+from .address import Address
+from .product import Product
+from .profile import Profile
+from .._models import BaseModel
+from .subscription import Subscription
+
+__all__ = ["ViewInitResponse", "Data"]
+
+
+class Data(BaseModel):
+    addresses: List[Address]
+
+    apps: List[App]
+
+    cards: List[Card]
+
+    cart: Cart
+    """The current Terminal shop user's cart."""
+
+    orders: List[Order]
+
+    products: List[Product]
+
+    profile: Profile
+    """A Terminal shop user's profile. (We have users, btw.)"""
+
+    subscriptions: List[Subscription]
+
+    tokens: List[Token]
+
+
+class ViewInitResponse(BaseModel):
+    data: Data
+    """Initial app data."""
