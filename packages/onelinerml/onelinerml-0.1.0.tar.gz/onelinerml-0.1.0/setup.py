b6fb37d1567aea5b8f3a546fb3dcf1aadb88800e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:30 +0000

    Add package onelinerml/0.1.0/onelinerml-0.1.0.tar.gz

---
+++ packages/onelinerml/onelinerml-0.1.0.tar.gz/onelinerml-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="onelinerml",
+    version="0.1.0",
+    description="A one-line machine learning library with remote API and dashboard support.",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "fastapi",
+        "uvicorn",
+        "streamlit",
+        "pyngrok",
+        "python-multipart",  # Required by FastAPI to handle form data
+    ],
+    entry_points={
+        "console_scripts": [
+            "onelinerml-serve=onelinerml.serve:main"
+        ]
+    },
+)
