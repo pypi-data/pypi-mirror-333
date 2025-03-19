33ac58ca2e7530b7b2d92b1a03f57725f4638c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:29 +0000

    Add package semantiva/0.2.1.dev0/semantiva-0.2.1.dev0-py3-none-any.whl

---
+++ packages/semantiva/semantiva-0.2.1.dev0-py3-none-any.whl/semantiva/context_processors/context_observer.py
@@ -0,0 +1,39 @@
+from typing import Any
+from .context_types import ContextType
+
+
+class ContextObserver:
+    """
+    Class for managing and observing context updates within the semantic framework.
+
+    This class facilitates the tracking and updating of contextual information
+    that may influence the behavior of data processors.
+    """
+
+    def __init__(self):
+        """
+        Initialize the ContextObserver with an empty context.
+
+        Attributes:
+            context (dict): A dictionary to store contextual key-value pairs.
+        """
+        self.observer_context = ContextType
+
+    def update_context(self, key: str, value: Any):
+        """
+        Update the context with a new key-value pair or modify an existing one.
+
+        Args:
+            key (str): The key associated with the context value.
+            value (any): The value to be stored in the context.
+        """
+        self.observer_context.set_value(key, value)
+
+    def __str__(self):
+        """
+        Provide a string representation of the current context.
+
+        Returns:
+            str: A string representation of the context dictionary.
+        """
+        return str(self.observer_context)
