bfaf72e6681b179074352fb3ce812d90db4a235b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:14 +0000

    Add package errorprinter/1.0/errorprinter-1.0.tar.gz

---
+++ packages/errorprinter/errorprinter-1.0.tar.gz/errorprinter-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="errorprinter",
+    version="1.0",
+    packages=find_packages(),
+    description="A Python package to print errors beautifully with detailed explanations.",
+    author="Hobe",  # Your name
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
