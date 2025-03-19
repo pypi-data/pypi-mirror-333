a65dcbfe61ffe91d3c759f8414486cb324ca7f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:06:33 +0000

    Add package byted-overpass-toutiao-live-talk-gotalk/0.0.1/byted_overpass_toutiao_live_talk_gotalk-0.0.1.tar.gz

---
+++ packages/byted-overpass-toutiao-live-talk-gotalk/byted_overpass_toutiao_live_talk_gotalk-0.0.1.tar.gz/byted_overpass_toutiao_live_talk_gotalk-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_toutiao_live_talk_gotalk",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_toutiao_live_talk_gotalk",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_toutiao_live_talk_gotalk.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
