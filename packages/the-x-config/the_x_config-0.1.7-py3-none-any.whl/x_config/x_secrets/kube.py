82fafc7ccdcf8c50d9aaa28cd7cbd7abea5034ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:06:30 +0000

    Add package the-x-config/0.1.7/the_x_config-0.1.7-py3-none-any.whl

---
+++ packages/the-x-config/the_x_config-0.1.7-py3-none-any.whl/x_config/x_secrets/kube.py
@@ -0,0 +1,25 @@
+from x_config import XConfigError
+from kubernetes import client, config as kube_config
+
+SECRETS_KUBE_SECRET_NAME = 'secrets_kube_secret_name'
+SECRETS_KUBE_NAMESPACE = 'secrets_kube_namespace'
+
+
+def load_kube_secrets(config: dict) -> dict:
+    """
+    Loads secrets from Kubernetes and returns them in a dict format
+    """
+    try:
+        secret_name = config.pop(SECRETS_KUBE_SECRET_NAME)
+    except KeyError:
+        raise XConfigError(f'unable to extract `{SECRETS_KUBE_SECRET_NAME} property')
+
+    try:
+        namespace = config.pop(SECRETS_KUBE_NAMESPACE)
+    except KeyError:
+        raise XConfigError(f'unable to extract `{SECRETS_KUBE_NAMESPACE} property')
+
+    kube_config.load_kube_config()
+    v1 = client.CoreV1Api()
+    contents = v1.read_namespaced_secret(name=secret_name, namespace=namespace)
+    return {k.upper(): v for k, v in contents.items()}
