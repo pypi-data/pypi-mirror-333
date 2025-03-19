af384412df218dc7a4ab30a713bedfafb3dd4118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:39 +0000

    Add package talisman-dm/1.3.3/talisman-dm-1.3.3.tar.gz

---
+++ packages/talisman-dm/talisman-dm-1.3.3.tar.gz/talisman-dm-1.3.3/tdm/utils/fact_factory.py
@@ -0,0 +1,111 @@
+from functools import singledispatch
+from typing import Dict, Iterable, Set, Tuple, Type, TypeVar, Union
+
+from typing_extensions import Protocol
+
+from tdm import TalismanDocument
+from tdm.abstract.datamodel import AbstractDomainType, AbstractFact, AbstractLinkFact, AbstractNodeMention, AbstractValue, FactStatus
+from tdm.datamodel.domain import AtomValueType, ComponentValueType, DocumentType, PropertyType, RelationPropertyType, RelationType
+from tdm.datamodel.facts import AtomValueFact, ComponentFact, ConceptFact, KBConceptValue, MentionFact, PropertyFact, \
+    RelationFact, RelationPropertyFact
+from tdm.utils.copy_value import copy_value
+
+
+def get_metadata(document: TalismanDocument) -> Dict[str, Union[Tuple[AbstractValue, ...], AbstractValue]]:
+    document_facts = document.get_facts(ConceptFact, filter_=lambda f: isinstance(f.type_id, DocumentType))
+    metadata_property_facts: Set[PropertyFact] = set()
+    for fact in document_facts:
+        metadata_property_facts.update(document.related_facts(fact, PropertyFact))
+
+    return {property_fact.name: property_fact.target.value for property_fact in metadata_property_facts}
+
+
+class MentionedFactsFactory(Protocol):
+    def __call__(
+            self, mention: AbstractNodeMention,
+            status: FactStatus = FactStatus.NEW,
+            value: Union[AbstractValue, Iterable[AbstractValue]] = ()
+    ) -> Iterable[AbstractFact]:
+        ...
+
+
+@singledispatch
+def mentioned_fact_factory(type_: AbstractDomainType) -> MentionedFactsFactory:
+    raise NotImplementedError
+
+
+@mentioned_fact_factory.register(AtomValueType)
+def _value_fact_factory(type_: AtomValueType) -> MentionedFactsFactory:
+    def create_facts(
+            mention: AbstractNodeMention, status: FactStatus, value: Union[Tuple[AbstractValue, ...], AbstractValue] = ()
+    ) -> Tuple[AtomValueFact, MentionFact]:
+        value_fact = AtomValueFact(status, type_, value)
+        return value_fact, MentionFact(status, mention, value_fact)
+
+    return create_facts
+
+
+@mentioned_fact_factory.register(PropertyType)
+def _concept_fact_factory(type_: PropertyType) -> MentionedFactsFactory:
+    def create_facts(
+            mention: AbstractNodeMention, status: FactStatus, value: Union[Tuple[AbstractValue, ...], AbstractValue] = ()
+    ) -> Tuple[ConceptFact, AtomValueFact, PropertyFact, MentionFact]:
+        if status is FactStatus.APPROVED:
+            concept_fact = ConceptFact(status, type_.source, KBConceptValue(value.value))
+        else:
+            concept_fact = ConceptFact(status, type_.source)
+        value_fact = AtomValueFact(status, type_.target, value)
+        return concept_fact, value_fact, PropertyFact(status, type_, concept_fact, value_fact), MentionFact(status, mention, value_fact)
+
+    return create_facts
+
+
+class LinkFactsFactory(Protocol):
+    def __call__(
+            self,
+            source: AbstractFact,
+            target: AbstractFact,
+            doc: TalismanDocument,
+            status: FactStatus = FactStatus.NEW
+    ) -> Iterable[AbstractFact]:
+        ...
+
+
+@singledispatch
+def link_fact_factory(type_: AbstractDomainType) -> LinkFactsFactory:
+    raise NotImplementedError
+
+
+@link_fact_factory.register(RelationType)
+def _relation_fact_factory(type_: RelationType) -> LinkFactsFactory:
+    def create_facts(
+            source: AbstractFact, target: AbstractFact, doc: TalismanDocument, status: FactStatus = FactStatus.NEW
+    ) -> Tuple[AbstractFact, ...]:
+        return RelationFact(status, type_, source, target),
+
+    return create_facts
+
+
+_PropertyType = TypeVar('_PropertyType', bound=Type[AbstractLinkFact])
+
+
+def build_prop_fact_factory(type_: AbstractDomainType, fact_type: _PropertyType) -> LinkFactsFactory:
+    def create_facts(source: AbstractFact, target: AbstractFact, doc: TalismanDocument, status=FactStatus.NEW) -> Tuple[AbstractFact, ...]:
+        copied = copy_value(target, doc, status)
+        return (fact_type(status, type_, source, copied[0]), *copied)
+    return create_facts
+
+
+@link_fact_factory.register(PropertyType)
+def _(type_: PropertyType):
+    return build_prop_fact_factory(type_, PropertyFact)
+
+
+@link_fact_factory.register(RelationPropertyType)
+def _(type_: RelationPropertyType):
+    return build_prop_fact_factory(type_, RelationPropertyFact)
+
+
+@link_fact_factory.register(ComponentValueType)
+def _(type_: ComponentValueType):
+    return build_prop_fact_factory(type_, ComponentFact)
