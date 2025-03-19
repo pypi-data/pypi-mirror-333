e0f17ceb233d8a7bc3505903ba6d2feefd224e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:13 +0000

    Add package semantic-api/0.0.4/semantic_api-0.0.4-py3-none-any.whl

---
+++ packages/semantic-api/semantic_api-0.0.4-py3-none-any.whl/semantic/api/config.py
@@ -0,0 +1,33 @@
+from typing_extensions import Final
+import json
+from semantic.common.utils import msg
+
+
+'''
+    set batch_size to ~samples/sec 
+
+'''
+
+batch_size = 100
+
+schemaName: str = 'fera' 
+schemaVersion: str = 'v001'
+instanceName: str = 'proto'
+dbUri: str = 'http://127.0.0.1:6363/'
+
+brokerIp: Final = 'localhost'
+#brokerIp: Final = "10.94.85.101"
+port: Final = 1883
+username: Final = 'semantic'
+password: Final = 's3mant1c'
+publishTopic: Final = 'fcs/serviceRequestTopic'
+insertAcknowledgeTopic: Final = 'insertAcknowledgeTopic'
+
+semanticConfigTopic: Final ='fcs/semanticConfig'
+semanticConfigResponseTopic: Final ='fcs/semanticConfigResponse'
+serviceRequestTopic: Final ='fcs/serviceRequestTopic'
+serviceResponseTopic: Final ='fcs/serviceResponseTopic'
+topics: Final = [semanticConfigResponseTopic, serviceResponseTopic]
+
+
+
