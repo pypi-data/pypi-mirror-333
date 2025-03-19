da5a0492e0075a0f26e9ab4aaee95e1d35ac4757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:55 +0000

    Add package lyric-py/0.1.7/lyric_py-0.1.7-cp313-cp313-win_amd64.whl

---
+++ packages/lyric-py/lyric_py-0.1.7-cp313-cp313-win_amd64.whl/lyric/config.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_LYRIC_DIR = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_WORKER_FILE = "default_python_worker.py"
+DEFAULT_WORKER_PATH = os.path.join(BASE_LYRIC_DIR, DEFAULT_WORKER_FILE)
