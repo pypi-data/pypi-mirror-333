f9fb0560b20deb34a85e242d6f3328426a966c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:05:21 +0000

    Add package uxarray/2025.3.0/uxarray-2025.3.0.tar.gz

---
+++ packages/uxarray/uxarray-2025.3.0.tar.gz/uxarray-2025.3.0/uxarray/__init__.py
@@ -0,0 +1,36 @@
+from .core.api import open_grid, open_dataset, open_mfdataset, concat
+
+from .core.dataset import UxDataset
+from .core.dataarray import UxDataArray
+from .grid import Grid
+
+from .constants import INT_DTYPE, INT_FILL_VALUE
+from .cmaps import sequential, diverging, sequential_blue, sequential_green
+
+try:
+    from importlib.metadata import version as _version
+except Exception:
+    from importlib_metadata import version as _version
+
+try:
+    __version__ = _version("uxarray")
+except Exception:
+    # Placeholder version incase an error occurs, such as the library isn't installed
+    __version__ = "999"
+
+
+__all__ = (
+    "open_grid",
+    "open_dataset",
+    "open_mfdataset",
+    "concat",
+    "UxDataset",
+    "UxDataArray",
+    "INT_DTYPE",
+    "INT_FILL_VALUE",
+    "Grid",
+    "sequential",
+    "diverging",
+    "sequential_blue",
+    "sequential_green",
+)
---
+++ packages/uxarray/uxarray-2025.3.0.tar.gz/uxarray-2025.3.0/uxarray/core/__init__.py
@@ -0,0 +1,13 @@
+from .api import open_grid, open_dataset, open_mfdataset
+
+from .dataset import UxDataset
+from .dataarray import UxDataArray
+
+
+__all__ = (
+    "open_grid",
+    "open_dataset",
+    "open_mfdataset",
+    "UxDataset",
+    "UxDataArray",
+)
