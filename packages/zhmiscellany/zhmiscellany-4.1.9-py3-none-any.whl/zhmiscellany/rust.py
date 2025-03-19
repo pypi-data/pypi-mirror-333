5707ae86c920deabaa4ac0e7b41bbd1e044e5b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:39 +0000

    Add package zhmiscellany/4.1.9/zhmiscellany-4.1.9-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-4.1.9-py3-none-any.whl/zhmiscellany/rust.py
@@ -0,0 +1,19 @@
+from zhmiscellanyrusteffect import *
+import numpy as np
+
+np_mean = np_mean
+np_sum = np_sum
+np_median = np_median
+list_files_recursive = list_files_recursive
+
+def mean(lst):
+    array = np.array(lst)
+    return np_mean(array)
+
+def sum(lst):
+    array = np.array(lst)
+    return np_sum(array)
+
+def median(lst):
+    array = np.array(lst)
+    return np_median(array)
