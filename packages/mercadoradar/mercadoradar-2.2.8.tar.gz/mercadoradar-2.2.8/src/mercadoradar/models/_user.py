c9312a45ff4fe93e75589e6da96bb875a7cfe482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:36 +0000

    Add package mercadoradar/2.2.8/mercadoradar-2.2.8.tar.gz

---
+++ packages/mercadoradar/mercadoradar-2.2.8.tar.gz/mercadoradar-2.2.8/src/mercadoradar/models/_user.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import Optional, Any
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    is_deleted: Optional[bool] = None
+    deleted_at: Optional[datetime] = None
+    id: Optional[int] = None
+    name: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    account: Optional[Any] = None
+
+    def to_str(self) -> str:
+        data = self.model_dump()
+        sorted_items = [('id', data.pop('id'))] if 'id' in data else []
+        sorted_items += sorted(data.items())
+        fields = "\n    ".join(f"{key}={value}" for key, value in sorted_items)
+
+        return f"<{self.__class__.__name__}\n    {fields}\n>"
+
+    def __str__(self) -> str:
+        return self.to_str()
+
+    def __repr__(self) -> str:
+        id = getattr(self, 'id', None)
+        name = getattr(self, 'name', None)
+
+        if id and name:
+            return f"<{self.__class__.__name__} id={self.id} name={self.name}>"
+        if id:
+            return f"<{self.__class__.__name__} id={self.id}>"
+        return f"<{self.__class__.__name__}>"
