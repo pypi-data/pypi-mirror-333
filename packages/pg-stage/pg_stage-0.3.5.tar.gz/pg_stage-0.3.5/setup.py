ca08fba49dd41a126400a72621218bcc3e900333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:06:51 +0000

    Add package pg-stage/0.3.5/pg_stage-0.3.5.tar.gz

---
+++ packages/pg-stage/pg_stage-0.3.5.tar.gz/pg_stage-0.3.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from os.path import join, dirname
+
+setup(
+    name='pg_stage',
+    version='0.3.5',
+    packages=['pg_stage'],
+    package_dir={'': 'src'},
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    install_requires=['typing-extensions>=4.5.0', 'mimesis==4.1.3'],
+    extras_require={'dev': ['pytest']},
+    include_package_data=True,
+    license_files=('LICENSE.txt',),
+)
