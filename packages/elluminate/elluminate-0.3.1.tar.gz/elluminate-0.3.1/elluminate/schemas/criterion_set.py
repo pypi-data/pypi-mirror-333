e66bd4eb723944fd39baf006d8a732658edbc691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:17 +0000

    Add package elluminate/0.3.1/elluminate-0.3.1.tar.gz

---
+++ packages/elluminate/elluminate-0.3.1.tar.gz/elluminate-0.3.1/elluminate/schemas/criterion_set.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+from elluminate.schemas.prompt_template import PromptTemplate
+
+
+class CriterionSet(BaseModel):
+    """Criterion set model."""
+
+    id: int
+    name: str
+    prompt_templates: list[PromptTemplate] | None = None
+
+
+class CreateCriterionSetRequest(BaseModel):
+    """Request to create a new criterion set.
+
+    Args:
+        name: The name of the criterion set
+        prompt_template_id: The ID of the prompt template to associate with the criterion set
+        criteria: Optional list of criterion strings to create alongside the criterion set
+
+    """
+
+    name: str
+    prompt_template_id: int
+    criteria: list[str] | None = None
