95c82d8894d6011f04c3bc274d166b2dd2484cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:27 +0000

    Add package byted-rtc-assessment-meego-tools/0.0.1/byted_rtc_assessment_meego_tools-0.0.1.tar.gz

---
+++ packages/byted-rtc-assessment-meego-tools/byted_rtc_assessment_meego_tools-0.0.1.tar.gz/byted_rtc_assessment_meego_tools-0.0.1/byted_rtc_assessment_meego_tools/__init__.py
@@ -0,0 +1,2 @@
+# byted_rtc_assessment_meego_tools - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-rtc-assessment-meego-tools/byted_rtc_assessment_meego_tools-0.0.1.tar.gz/byted_rtc_assessment_meego_tools-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_rtc_assessment_meego_tools",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_rtc_assessment_meego_tools",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_rtc_assessment_meego_tools.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
