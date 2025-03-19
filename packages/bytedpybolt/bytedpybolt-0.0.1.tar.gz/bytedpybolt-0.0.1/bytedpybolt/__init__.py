dfebef24a7d1baa72209bc7662be5f52e84568bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:58 +0000

    Add package bytedpybolt/0.0.1/bytedpybolt-0.0.1.tar.gz

---
+++ packages/bytedpybolt/bytedpybolt-0.0.1.tar.gz/bytedpybolt-0.0.1/bytedpybolt/__init__.py
@@ -0,0 +1,2 @@
+# bytedpybolt - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedpybolt/bytedpybolt-0.0.1.tar.gz/bytedpybolt-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedpybolt",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedpybolt",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedpybolt.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
