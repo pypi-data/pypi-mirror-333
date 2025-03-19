73f1d719245bf1bb78a5d9264e67cb08edac6873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:16:03 +0000

    Add package bytedance-simple-rc-sampls-manage/0.0.1/bytedance_simple_rc_sampls_manage-0.0.1.tar.gz

---
+++ packages/bytedance-simple-rc-sampls-manage/bytedance_simple_rc_sampls_manage-0.0.1.tar.gz/bytedance_simple_rc_sampls_manage-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_simple_rc_sampls_manage",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_simple_rc_sampls_manage",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_simple_rc_sampls_manage.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
