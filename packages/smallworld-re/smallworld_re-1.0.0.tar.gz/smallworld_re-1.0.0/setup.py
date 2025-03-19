b70a7a167a74aed08e81b2288a373322a2d72173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:35 +0000

    Add package smallworld-re/1.0.0/smallworld_re-1.0.0.tar.gz

---
+++ packages/smallworld-re/smallworld_re-1.0.0.tar.gz/smallworld_re-1.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="smallworld-re",
+    version="1.0.0",
+    author="MIT Lincoln Laboratory",
+    author_email="smallworld@ll.mit.edu",
+    url="https://github.com/smallworld-re/smallworld",
+    description="An emulation stack tracking library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    license_files=["LICENSE.txt"],
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=["unicorn", "angr", "capstone", "lief", "pyhidra", "pypcode"],
+    extras_require={
+        "development": [
+            "black",
+            "isort",
+            "flake8",
+            "mypy",
+            "pip-tools",
+            "pre-commit",
+            "sphinx",
+            "sphinxcontrib-programoutput",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={"console_scripts": ["smallworld = smallworld.__main__:main"]},
+)
