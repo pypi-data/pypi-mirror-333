3c5c924c9a7280bb0804c1849bffca7844d77b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:08:25 +0000

    Add package goldie/0.1.1/goldie-0.1.1-py3-none-any.whl

---
+++ packages/goldie/goldie-0.1.1-py3-none-any.whl/goldie/tests/test_diff.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+
+import goldie.diff
+
+
+def _file_path(relative_path: str) -> str:
+    """
+    Returns the full path to a file relative to the testdata directory.
+
+    Parameters
+    ----------
+    relative_location : str
+        The relative location of the file (e.g.: just the file name).
+
+    Returns
+    -------
+    str
+        The full path to the file.
+    """
+
+    return os.path.join(os.path.dirname(__file__), "testdata", relative_path)
+
+
+class TestDiff(unittest.TestCase):
+    def test_diff(self):
+        with open(_file_path("comparison-1.txt")) as f:
+            comparison_1 = f.read()
+        with open(_file_path("comparison-2.txt")) as f:
+            comparison_2 = f.read()
+
+        diff = goldie.diff.diff_color_code_unified(comparison_1, comparison_2)
+        self.assertEquals(diff, "", diff)
