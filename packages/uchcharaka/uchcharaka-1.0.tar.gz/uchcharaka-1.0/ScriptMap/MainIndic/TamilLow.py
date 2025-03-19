e0835fee555be831ac41d92bf3c517d8304ffbb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:50 +0000

    Add package uchcharaka/1.0/uchcharaka-1.0.tar.gz

---
+++ packages/uchcharaka/uchcharaka-1.0.tar.gz/uchcharaka-1.0/ScriptMap/MainIndic/TamilLow.py
@@ -0,0 +1,106 @@
+from .Tamil import *
+
+ConsonantMap = ConsonantMap[:]
+ConsonantMap[29] = ConsonantMap[30]
+
+SouthConsonantMap = SouthConsonantMap[:]
+SouthConsonantMap[2] = ConsonantMap[26]
+
+ModernVowelMap = [
+                 'ஏ',
+                 'ஆ॒॑',
+                 ]
+
+ModernVowelSignMap =[
+                    'ே',
+                    'ா',
+                    ]
+
+IPAVowelMap = [
+                  'அ',
+                  'அ',
+
+                  'அ',
+                  'ஆ',
+                  'ஆ',
+                  'ஆ',
+
+                  'இ',
+                  'இ',
+
+                  'உ',
+                  'உ',
+                  'ஊ',
+
+                  'எ',
+                  'எ',
+                  'எர்',
+                  'ஏர்',
+
+                  'ஆ',
+                  'ஆ',
+
+                  'ஒ',
+                  'ஓ',
+                  'ஒ',
+                  'ஓ'
+                  ]
+
+NuktaConsonantMap =  [
+                     'க',
+                     'ஹ',
+                     'கᵍ',
+                     'ஜᶻ',
+                     'ட⁴',
+                     'ட⁴',
+                     'ஃப',
+                     'ய'
+                     ]
+
+IPAVowelSignMap = [
+                  '\u02BD',
+                  '\u02BD',
+
+                  'ா',
+                  'ா',
+                  'ா',
+                  'ா',
+
+                  'ி',
+                  'ி',
+
+                  'ு',
+                  'ு',
+                  'ூ',
+
+                  'ெ',
+                  'ெ',
+                  'ெர்',
+                  'ேர்',
+
+                  'ா',
+                  'ா',
+
+                  'ொ',
+                  'ோ',
+                  'ொ',
+                  'ோ'
+                ]
+
+IPAConsonantMap =[
+                     'டᵗ',
+                     'டᵗ',
+                     'டᵈ',
+                     'வ',
+                     'வ',
+                     'த',
+                     'தᵈ',
+                     'ர',
+                     'ஷ',
+                     'வ',
+                     'ய்ஜ',
+                     'ய',
+                     'ர',
+                     'ஹ',
+                     'ஷ'
+                      ]
