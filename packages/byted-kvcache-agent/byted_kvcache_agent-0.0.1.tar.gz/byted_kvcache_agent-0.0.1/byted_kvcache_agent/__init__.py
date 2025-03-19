a6c355812ff9ca68e5532d901d1b7cbbc01af685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:16:03 +0000

    Add package byted-kvcache-agent/0.0.1/byted_kvcache_agent-0.0.1.tar.gz

---
+++ packages/byted-kvcache-agent/byted_kvcache_agent-0.0.1.tar.gz/byted_kvcache_agent-0.0.1/byted_kvcache_agent/__init__.py
@@ -0,0 +1,2 @@
+# byted_kvcache_agent - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-kvcache-agent/byted_kvcache_agent-0.0.1.tar.gz/byted_kvcache_agent-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_kvcache_agent",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_kvcache_agent",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_kvcache_agent.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
