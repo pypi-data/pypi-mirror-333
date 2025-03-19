ee4962fe61f0c194da85f859e1e4c3ab3178b36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:44 +0000

    Add package ctab-xtra-dp/0.1.0/ctab_xtra_dp-0.1.0.tar.gz

---
+++ packages/ctab-xtra-dp/ctab_xtra_dp-0.1.0.tar.gz/ctab_xtra_dp-0.1.0/ctab_xtra_dp/adult.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import glob
+from model.ctabgan import CTABGAN
+df = pd.read_csv("Real_Datasets/adult.csv")
+#df = df.drop(columns=['Year','Model'])
+df = df[['age', 'workclass', 'capital-gain']]
+df = df.head(1000)
+df.loc[:9, 'capital-gain'] = np.nan
+
+synthesizer =  CTABGAN(df,
+                 test_ratio = 0.20,
+                 categorical_columns = ["workclass"], 
+                 log_columns = {}, # Fuck this log, task for tommorrow
+                 mixed_columns= {"capital-gain": [0,np.nan]},
+                 gaussian_columns = ["age"],
+                 non_categorical_columns = [],
+                 integer_columns = ["age","capital-gain"]) 
+
+
+synthesizer.fit(1)
