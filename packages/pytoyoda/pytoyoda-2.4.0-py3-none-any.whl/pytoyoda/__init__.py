42cf30673b9c54bef7447311ae0ec0bc1af9d4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:16:01 +0000

    Add package pytoyoda/2.4.0/pytoyoda-2.4.0-py3-none-any.whl

---
+++ packages/pytoyoda/pytoyoda-2.4.0-py3-none-any.whl/pytoyoda/__init__.py
@@ -0,0 +1,7 @@
+"""Toyota Connected Services Client."""
+
+from importlib_metadata import version
+
+from pytoyoda.client import MyT  # noqa
+
+__version__ = version(__name__)
---
+++ packages/pytoyoda/pytoyoda-2.4.0-py3-none-any.whl/pytoyoda/models/endpoints/location.py
@@ -0,0 +1,54 @@
+"""Toyota Connected Services API - Location Models."""
+
+from datetime import datetime
+from typing import Optional
+
+from pydantic.v1 import Field
+
+from pytoyoda.models.endpoints.common import StatusModel
+from pytoyoda.utils.models import CustomBaseModel
+
+
+class _VehicleLocationModel(CustomBaseModel):
+    display_name: Optional[str] = Field(alias="displayName")
+    latitude: Optional[float]
+    location_acquisition_datetime: Optional[datetime] = Field(
+        alias="locationAcquisitionDatetime"
+    )
+    longitude: Optional[float]
+
+
+class LocationModel(CustomBaseModel):
+    r"""Model representing the location of a vehicle.
+
+    Attributes
+    ----------
+        last_timestamp (Optional[datetime], optional): The last timestamp of the
+            location. Defaults to None.
+        vehicle_location (Optional[_VehicleLocationModel], optional): The location of
+            the vehicle. Defaults to None.
+        vin (Optional[str], optional): The VIN (Vehicle Identification Number) of the
+            vehicle. Defaults to None.
+
+    """
+
+    last_timestamp: Optional[datetime] = Field(alias="lastTimestamp", default=None)
+    vehicle_location: Optional[_VehicleLocationModel] = Field(
+        alias="vehicleLocation", default=None
+    )
+    vin: Optional[str] = None
+
+
+class LocationResponseModel(StatusModel):
+    """Model representing a location response.
+
+    Inherits from StatusModel.
+
+    Attributes
+    ----------
+        payload (Optional[LocationModel], optional): The location payload.
+            Defaults to None.
+
+    """
+
+    payload: Optional[LocationModel] = None
