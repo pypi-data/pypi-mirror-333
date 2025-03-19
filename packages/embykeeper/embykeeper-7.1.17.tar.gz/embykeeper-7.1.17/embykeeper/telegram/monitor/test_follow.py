39f83ee0c05e690afc4409fadde00ff0e94d6b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:12 +0000

    Add package embykeeper/7.1.17/embykeeper-7.1.17.tar.gz

---
+++ packages/embykeeper/embykeeper-7.1.17.tar.gz/embykeeper-7.1.17/embykeeper/telegram/monitor/test_follow.py
@@ -0,0 +1,13 @@
+from . import Monitor
+
+__ignore__ = True
+
+
+class TestFollowMonitor(Monitor):
+    name = "从众 测试"
+    chat_name = "api_group"
+    chat_allow_outgoing = True
+    chat_keyword = r"从众"
+    chat_follow_user = 3
+    chat_delay = 1
+    chat_reply = "我来"
