efad4af6e1e916e0d2a82b03da9fcf0f7566c0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:06:50 +0000

    Add package mdget/1.0.9/mdget-1.0.9.tar.gz

---
+++ packages/mdget/mdget-1.0.9.tar.gz/mdget-1.0.9/setup.py
@@ -0,0 +1,30 @@
+#from distutils.core import setup
+# import setuptools
+from setuptools import setup, find_packages
+from mdget import __version__ as v
+
+
+setup(name='mdget',
+      version=v.VERSION,
+      author='xigua, ',
+      author_email="2587125111@qq.com",
+      url='https://pypi.org/project/mdget',
+      long_description='''
+      世界上最庄严的问题：我能做什么好事？
+      ''',
+      packages=find_packages(include=['mdget', 'mdget.*']),
+      package_data={
+          'mdget': [],
+      },
+      exclude_package_data={
+          'mdget': ['conf', 'conf.*', 'spd.txt'],
+      },
+      entry_points={
+              'console_scripts': [
+                  'mdget=mdget.mdget:main',
+              ],
+          },
+      package_dir={'requests': 'requests'},
+      license="MIT",
+      python_requires='>=3.6',
+      )
