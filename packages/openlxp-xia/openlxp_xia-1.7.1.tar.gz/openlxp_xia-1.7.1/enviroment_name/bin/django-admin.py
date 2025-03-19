e96e29eaa19d9512d417a8642d5d56a1f2c6d9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:16 +0000

    Add package openlxp-xia/1.7.1/openlxp_xia-1.7.1.tar.gz

---
+++ packages/openlxp-xia/openlxp_xia-1.7.1.tar.gz/openlxp_xia-1.7.1/enviroment_name/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kjijo/Library/CloudStorage/OneDrive-Deloitte(O365D)/Github_Projects/ECC/openlxp-xia-1/app/enviroment_name/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
