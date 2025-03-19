227d7b83cac1e21af45fa761635b51016626b5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:08:48 +0000

    Add package microdata-tools/1.4.0/microdata_tools-1.4.0.tar.gz

---
+++ packages/microdata-tools/microdata_tools-1.4.0.tar.gz/microdata_tools-1.4.0/microdata_tools/packaging/exceptions/__init__.py
@@ -0,0 +1,18 @@
+class ValidationException(Exception):
+    ...
+
+
+class InvalidTarFileContents(Exception):
+    ...
+
+
+class InvalidKeyError(Exception):
+    ...
+
+
+class CsvConsistencyException(Exception):
+    ...
+
+
+class UnpackagingError(Exception):
+    ...
