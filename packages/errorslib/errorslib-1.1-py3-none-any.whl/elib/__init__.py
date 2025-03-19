32640bbb0a8acf37aa7c4b96a3ae2c0fbdb9ad0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:07 +0000

    Add package errorslib/1.1/errorslib-1.1-py3-none-any.whl

---
+++ packages/errorslib/errorslib-1.1-py3-none-any.whl/elib/__init__.py
@@ -0,0 +1,70 @@
+# elib/__init__.py
+
+# Import base errors
+from .codeerror import CodeError
+from .deverror import DevError
+from .dunnoerror import DunnoError
+from .hardwareerror import HardwareError
+from .logicerror import LogicError
+from .usererror import UserError
+
+# Import all subclasses from their respective modules
+from .usererror import (
+    UserIsAnIdiotError, UserIsNotAnIdiotError, PICNICError,
+    FeatureNotABugError, UserIsUsingWindowsError, UserIsUsingMacOSError,
+    UserIsAppleSheepError, UserIsUsingLinuxError
+)
+
+from .deverror import (
+    AuthorError, AuthorIsAnIdiotError, DebuggingTimeError,
+    DidntImplementedError, ForgotToImplementError
+)
+
+from .codeerror import (
+    CodeIsFuckedUpError, OvercomplicatedSolutionError,
+    UndercomplicatedSolutionError, OopsIDidItAgainError, NotAGoodError
+)
+
+from .hardwareerror import (
+    WTFNoBiosError, WTFNoOS, NoInternetError,
+    NoKeyboardError, iGPUExistsError
+)
+
+from .logicerror import (
+    NotAnError, ParadoxError, ThisErrorShouldNeverHappenError,
+    ThisErrorIsInevitableError
+)
+
+from .dunnoerror import (
+    WTFError, IdiotError
+)
+
+# Define what gets imported when doing "from elib import *"
+__all__ = [
+    # Base Errors
+    "UserError", "DevError", "CodeError", "HardwareError", "LogicError", "DunnoError",
+
+    # User Errors
+    "UserIsAnIdiotError", "UserIsNotAnIdiotError", "PICNICError",
+    "FeatureNotABugError", "UserIsUsingWindowsError", "UserIsUsingMacOSError",
+    "UserIsAppleSheepError", "UserIsUsingLinuxError",
+
+    # Developer Errors
+    "AuthorError", "AuthorIsAnIdiotError", "DebuggingTimeError",
+    "DidntImplementedError", "ForgotToImplementError",
+
+    # Code Errors
+    "CodeIsFuckedUpError", "OvercomplicatedSolutionError",
+    "UndercomplicatedSolutionError", "OopsIDidItAgainError", "NotAGoodError",
+
+    # Hardware Errors
+    "WTFNoBiosError", "WTFNoOS", "NoInternetError",
+    "NoKeyboardError", "iGPUExistsError",
+
+    # Logic Errors
+    "NotAnError", "ParadoxError", "ThisErrorShouldNeverHappenError",
+    "ThisErrorIsInevitableError",
+
+    # WTF Errors
+    "WTFError", "IdiotError"
+]
---
+++ packages/errorslib/errorslib-1.1-py3-none-any.whl/elib/dunnoerror.py
@@ -0,0 +1,15 @@
+"""
+WTF?
+"""
+
+class DunnoError(Exception):
+    """You have to ask 'wtf?' sometimes."""
+    pass
+
+class WTFError(DunnoError):
+    """there's no other error that can describe what's going on."""
+    pass
+
+class IdiotError(DunnoError):
+    """your problem exists between your window and the nearest exit."""
+    pass
