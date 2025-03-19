32f1018800872cb74c154f507135cbc573b3f937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:05:12 +0000

    Add package lyyak/1.10/lyyak-1.10.tar.gz

---
+++ packages/lyyak/lyyak-1.10.tar.gz/lyyak-1.10/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lyyak',
+    version='1.10',
+    author='yy',
+    author_email='',
+    description='lyydata for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+    ],
+)
