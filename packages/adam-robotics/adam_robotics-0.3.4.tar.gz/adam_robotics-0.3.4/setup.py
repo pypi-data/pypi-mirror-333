67f9cf4fb664fae72f3d50c7cd91efade53e5631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:36 +0000

    Add package adam-robotics/0.3.4/adam_robotics-0.3.4.tar.gz

---
+++ packages/adam-robotics/adam_robotics-0.3.4.tar.gz/adam_robotics-0.3.4/setup.py
@@ -0,0 +1,6 @@
+# Copyright (C) Istituto Italiano di Tecnologia (IIT). All rights reserved.
+# This software may be modified and distributed under the terms of the
+
+from setuptools import setup
+
+setup()
