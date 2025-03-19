1a614348460eaeede8f8cf9fb31f32fd5fea4203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:03 +0000

    Add package obspec/0.1.0b1/obspec-0.1.0b1-py3-none-any.whl

---
+++ packages/obspec/obspec-0.1.0b1-py3-none-any.whl/obspec/__init__.py
@@ -0,0 +1,29 @@
+# TODO: think through what should be exported at the top level
+from ._attributes import Attribute, Attributes
+from ._copy import Copy, CopyAsync
+from ._delete import Delete, DeleteAsync
+from ._get import (
+    BufferStream,
+    Get,
+    GetAsync,
+    GetOptions,
+    GetRange,
+    GetRangeAsync,
+    GetRanges,
+    GetRangesAsync,
+    GetResult,
+    OffsetRange,
+    SuffixRange,
+)
+from ._head import Head, HeadAsync
+from ._list import (
+    List,
+    ListChunkType_co,
+    ListResult,
+    ListStream,
+    ListWithDelimiter,
+    ListWithDelimiterAsync,
+)
+from ._meta import ObjectMeta
+from ._put import Put, PutAsync, PutMode, PutResult, UpdateVersion
+from ._rename import Rename, RenameAsync
