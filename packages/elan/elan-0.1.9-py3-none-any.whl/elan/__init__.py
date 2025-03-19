0a4650f34dfbb94ca9564e42f0d1ec48c30812cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:23 +0000

    Add package elan/0.1.9/elan-0.1.9-py3-none-any.whl

---
+++ packages/elan/elan-0.1.9-py3-none-any.whl/elan/__init__.py
@@ -0,0 +1,11 @@
+from math_utils import *
+from string_utils import *
+from list_utils import *
+from image_utils import *
+
+__all__ = ["math_utils", "string_utils", "list_utils", "image_utils"]
+
+
+__version__ = "0.1.0"
+__author__ = "Efekan Nefesoğlu"
+__license__ = "MIT"
