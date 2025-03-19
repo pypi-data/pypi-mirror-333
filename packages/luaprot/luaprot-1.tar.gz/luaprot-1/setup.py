a34b4cbaa21c95884ea2e09fc906485b0a027113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:47 +0000

    Add package luaprot/1/luaprot-1.tar.gz

---
+++ packages/luaprot/luaprot-1.tar.gz/luaprot-1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='luaprot',
+    version='1',
+    packages=['luaprot'],
+    author='luaprot',
+    author_email='protlua@gmail.com',
+    description='The offical python package for the LuaProt whitelisting service',
+)
