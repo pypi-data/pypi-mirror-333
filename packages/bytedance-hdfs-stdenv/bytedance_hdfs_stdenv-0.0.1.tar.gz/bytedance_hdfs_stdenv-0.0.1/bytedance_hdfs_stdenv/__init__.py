b9353b07db9919af55d62e66fdef62ff461e14a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:06:31 +0000

    Add package bytedance-hdfs-stdenv/0.0.1/bytedance_hdfs_stdenv-0.0.1.tar.gz

---
+++ packages/bytedance-hdfs-stdenv/bytedance_hdfs_stdenv-0.0.1.tar.gz/bytedance_hdfs_stdenv-0.0.1/bytedance_hdfs_stdenv/__init__.py
@@ -0,0 +1,2 @@
+# bytedance_hdfs_stdenv - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedance-hdfs-stdenv/bytedance_hdfs_stdenv-0.0.1.tar.gz/bytedance_hdfs_stdenv-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_hdfs_stdenv",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_hdfs_stdenv",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_hdfs_stdenv.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
