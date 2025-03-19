6d035fa45d9c404db458770c7f6191c30dcab413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:05:21 +0000

    Add package qbraid-authentication-server/0.1.3/qbraid_authentication_server-0.1.3-py3-none-any.whl

---
+++ packages/qbraid-authentication-server/qbraid_authentication_server-0.1.3-py3-none-any.whl/qbraid_authentication_server/tests/test_handlers.py
@@ -0,0 +1,10 @@
+import json
+
+
+async def test_get_config(jp_fetch):
+    """Test the get config endpoint"""
+    response = await jp_fetch("qbraid-authentication-server", "qbraid-config")
+
+    assert response.code == 200
+    payload = json.loads(response.body)
+    assert payload == {"email": None, "refreshToken": None, "apiKey": None, "url": None}
