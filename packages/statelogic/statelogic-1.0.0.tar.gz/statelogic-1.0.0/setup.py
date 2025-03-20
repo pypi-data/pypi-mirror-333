2712d7fb91feecf9770da9475590390d56ad8479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:26 +0000

    Add package statelogic/1.0.0/statelogic-1.0.0.tar.gz

---
+++ packages/statelogic/statelogic-1.0.0.tar.gz/statelogic-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='statelogic',
+    version='1.0.0',  
+    description='A library for state management with colored messages',
+    author='Wong Chun Fai',
+    author_email='wilgat.wong@gmail.com',
+    url='https://github.com/Wilgat/Statelogic',  
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
