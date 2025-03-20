f2ee2dcf5194888a2da2f7608af829cd55327226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:06:19 +0000

    Add package MeUtils/2025.3.14.8.41.43/meutils-2025.3.14.8.41.43.tar.gz

---
+++ packages/MeUtils/meutils-2025.3.14.8.41.43.tar.gz/meutils-2025.3.14.8.41.43/meutils/llm/completions/oi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : oi
+# @Time         : 2024/12/12 13:19
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+
+
+from openai import OpenAI
+
+
