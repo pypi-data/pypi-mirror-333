14236dbb89184d61aac7f0689975fcb3bfb76423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:33 +0000

    Add package bytedance-custom-rate-limiter/0.0.1/bytedance_custom_rate_limiter-0.0.1.tar.gz

---
+++ packages/bytedance-custom-rate-limiter/bytedance_custom_rate_limiter-0.0.1.tar.gz/bytedance_custom_rate_limiter-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_custom_rate_limiter",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_custom_rate_limiter",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_custom_rate_limiter.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
