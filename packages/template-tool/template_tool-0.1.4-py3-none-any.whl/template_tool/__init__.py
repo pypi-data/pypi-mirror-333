6726b0cd56a0380412e186992740f49a8078d112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:44 +0000

    Add package template-tool/0.1.4/template_tool-0.1.4-py3-none-any.whl

---
+++ packages/template-tool/template_tool-0.1.4-py3-none-any.whl/template_tool/__init__.py
@@ -0,0 +1,13 @@
+from os.path import join, exists
+from os import mkdir
+
+if not exists(join("C:\\", "Templates")):
+    mkdir(join("C:\\", "Templates"))
+path = "C:\\Templates"
+args_desc = {
+    "--load": "load a template",
+    "--save": "save a template",
+    "--list": "list your templates",
+}
+
+__all__ = ["path", "args_desc"]
