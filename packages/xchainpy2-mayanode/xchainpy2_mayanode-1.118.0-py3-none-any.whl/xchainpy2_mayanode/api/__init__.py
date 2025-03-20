29009025b63deb9cd8af102726efe2424fbb1676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:06:44 +0000

    Add package xchainpy2-mayanode/1.118.0/xchainpy2_mayanode-1.118.0-py3-none-any.whl

---
+++ packages/xchainpy2-mayanode/xchainpy2_mayanode-1.118.0-py3-none-any.whl/xchainpy2_mayanode/api/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from xchainpy2_mayanode.api.block_api import BlockApi
+from xchainpy2_mayanode.api.cacao_pool_api import CACAOPoolApi
+from xchainpy2_mayanode.api.health_api import HealthApi
+from xchainpy2_mayanode.api.invariants_api import InvariantsApi
+from xchainpy2_mayanode.api.liquidity_providers_api import LiquidityProvidersApi
+from xchainpy2_mayanode.api.mayanames_api import MayanamesApi
+from xchainpy2_mayanode.api.mimir_api import MimirApi
+from xchainpy2_mayanode.api.network_api import NetworkApi
+from xchainpy2_mayanode.api.nodes_api import NodesApi
+from xchainpy2_mayanode.api.pol_api import POLApi
+from xchainpy2_mayanode.api.pools_api import PoolsApi
+from xchainpy2_mayanode.api.queue_api import QueueApi
+from xchainpy2_mayanode.api.quote_api import QuoteApi
+from xchainpy2_mayanode.api.savers_api import SaversApi
+from xchainpy2_mayanode.api.streaming_swap_api import StreamingSwapApi
+from xchainpy2_mayanode.api.tss_api import TSSApi
+from xchainpy2_mayanode.api.transactions_api import TransactionsApi
+from xchainpy2_mayanode.api.vaults_api import VaultsApi
