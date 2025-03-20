2ca5e98827a3a889f4353ab3b9fb1f3fd570913a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:53 +0000

    Add package vidcrawler/1.0.49/vidcrawler-1.0.49.tar.gz

---
+++ packages/vidcrawler/vidcrawler-1.0.49.tar.gz/vidcrawler-1.0.49/vidcrawler/testing/test_cmd.py
@@ -0,0 +1,39 @@
+"""
+Testing cmd
+"""
+
+# pylint: disable=line-too-long,missing-function-docstring,missing-class-docstring,super-with-arguments,invalid-name,R0801
+
+import os
+import subprocess
+import unittest
+from pprint import pprint
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+FETCH_LIST = os.path.join(HERE, "fetch_list.json")
+OUT_LIST = os.path.join(HERE, "out_list.json")
+
+
+class IoTester(unittest.TestCase):
+    def test_env(self):  # pylint: disable
+        pprint(dict(os.environ))
+
+    def test_cmd(self):  # pylint: disable
+        subprocess.check_call(
+            "vidcrawler --help",
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+    def test_process_test(self):  # pylint: disable
+        subprocess.check_call(
+            f'vidcrawler --input_crawl_json "{FETCH_LIST}" --output_json "{OUT_LIST}"',
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
