d5f9be30340a865c66942de12a17da3598d5bca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:06:21 +0000

    Add package RNApolis/0.5.0/rnapolis-0.5.0.tar.gz

---
+++ packages/RNApolis/rnapolis-0.5.0.tar.gz/rnapolis-0.5.0/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="RNApolis",
+    version="0.5.0",
+    packages=["rnapolis"],
+    package_dir={"": "src"},
+    author="Tomasz Zok",
+    author_email="tomasz.zok@cs.put.poznan.pl",
+    description="A Python library containing RNA-related bioinformatics functions and classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tzok/rnapolis-py",
+    project_urls={"Bug Tracker": "https://github.com/tzok/rnapolis-py/issues"},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    entry_points={
+        "console_scripts": [
+            "annotator=rnapolis.annotator:main",
+            "clashfinder=rnapolis.clashfinder:main",
+            "metareader=rnapolis.metareader:main",
+            "molecule-filter=rnapolis.molecule_filter:main",
+            "motif-extractor=rnapolis.motif_extractor:main",
+            "transformer=rnapolis.transformer:main",
+            "rfam-folder=rnapolis.rfam_folder:main",
+        ]
+    },
+    include_package_data=True,
+    package_data={"rnapolis": ["mmcif_pdbx_v50.dic"]},
+    install_requires=[
+        "appdirs",
+        "graphviz",
+        "mmcif",
+        "numpy",
+        "ordered-set",
+        "orjson",
+        "pandas",
+        "pulp",
+        "requests",
+        "scipy",
+        "viennarna",
+    ],
+)
