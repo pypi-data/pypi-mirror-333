7bd3afce9579c32dbf54e029b6ebf35d84200949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:06:00 +0000

    Add package ostorlab/1.5.5/ostorlab-1.5.5.tar.gz

---
+++ packages/ostorlab/ostorlab-1.5.5.tar.gz/ostorlab-1.5.5/src/ostorlab/ui/static/_nuxt/D1jDBoMW.js
@@ -0,0 +1,1 @@
+import{cl as t,cn as a}from"./Bl2PtCmi.js";const i=t("scanners",{state:()=>({scanners:[]}),actions:{addOrUpdateScanner(e){const s=this.scanners.findIndex(n=>n.endpoint===e.endpoint);s===-1?this.scanners.push(e):this.scanners[s]=e},deleteScanner(e){this.scanners=this.scanners.filter(s=>s.endpoint!==e)}},persist:{storage:a.localStorage}});export{i as u};
