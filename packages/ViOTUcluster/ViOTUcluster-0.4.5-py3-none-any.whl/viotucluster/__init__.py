4df9d9072a8d12976b4c376fc22a82346b9d9599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:08:00 +0000

    Add package ViOTUcluster/0.4.5/ViOTUcluster-0.4.5-py3-none-any.whl

---
+++ packages/ViOTUcluster/ViOTUcluster-0.4.5-py3-none-any.whl/viotucluster/__init__.py
@@ -0,0 +1,7 @@
+"""
+ViOTUcluster: A high-speed, all-in-one solution that streamlines the entire virome analysis workflow
+"""
+
+__version__ = "0.4.1"
+__author__ = "Sihang Liu"
+__license__ = "GPL-2.0"
