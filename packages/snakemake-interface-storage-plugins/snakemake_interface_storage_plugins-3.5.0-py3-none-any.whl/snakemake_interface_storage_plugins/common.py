e71bcd3ad88f1be667b577f812d4345a07b88bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:06:22 +0000

    Add package snakemake-interface-storage-plugins/3.5.0/snakemake_interface_storage_plugins-3.5.0-py3-none-any.whl

---
+++ packages/snakemake-interface-storage-plugins/snakemake_interface_storage_plugins-3.5.0-py3-none-any.whl/snakemake_interface_storage_plugins/common.py
@@ -0,0 +1,21 @@
+__author__ = "Christopher Tomkins-Tinch, Johannes Köster"
+__copyright__ = "Copyright 2023, Christopher Tomkins-Tinch, Johannes Köster"
+__email__ = "johannes.koester@uni-due.de"
+__license__ = "MIT"
+
+
+from enum import Enum
+
+
+storage_plugin_prefix = "snakemake-storage-plugin-"
+storage_plugin_module_prefix = storage_plugin_prefix.replace("-", "_")
+
+
+class Operation(Enum):
+    RETRIEVE = "retrieve"
+    STORE = "store"
+    EXISTS = "exists"
+    MTIME = "mtime"
+    REMOVE = "remove"
+    SIZE = "size"
+    TOUCH = "touch"
