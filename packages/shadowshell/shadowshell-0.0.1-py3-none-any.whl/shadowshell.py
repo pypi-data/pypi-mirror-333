4bc195a3db9a144d64c8c26cf174c6a8a11ef57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:07:24 +0000

    Add package shadowshell/0.0.1/shadowshell-0.0.1-py3-none-any.whl

---
+++ packages/shadowshell/shadowshell-0.0.1-py3-none-any.whl/shadowshell.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Logger
+@author: shadowshell<shadowshell@foxmail.com>
+"""
+
+class ShadowShell:
+
+    def hello(self):
+        print("Hello.")
+ 
