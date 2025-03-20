9b4de99196ef2a306985640887d8706bbe3fa658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:49 +0000

    Add package dagster-ncsa/0.1.2/dagster_ncsa-0.1.2-py3-none-any.whl

---
+++ packages/dagster-ncsa/dagster_ncsa-0.1.2-py3-none-any.whl/dagster_ncsa/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2025 Ben Galewsky. All rights reserved.
+
+dagster-ncsa: A great package.A Python library providing useful components for using [Dagster](https://dagster.io/) to create academic research cloud datalakes
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+from .s3_resource_ncsa import S3ResourceNCSA
+
+__all__ = ["S3ResourceNCSA", "__version__"]
