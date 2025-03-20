74e500e32a9a87f9cce0542b194d9592ed57ec26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:32:08 +0000

    Add package hysetter/0.3.0/hysetter-0.3.0.tar.gz

---
+++ packages/hysetter/hysetter-0.3.0.tar.gz/hysetter-0.3.0/tests/test_hysetter.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import io
+
+import pytest
+from click.testing import CliRunner
+
+import hysetter as hs
+from hysetter.cli import cli
+
+
+@pytest.fixture
+def runner():
+    """Return a CliRunner."""
+    return CliRunner()
+
+
+def test_config(runner: CliRunner) -> None:
+    ret = runner.invoke(cli, ["config_demo.yml"])
+    assert ret.exit_code == 0
+    ret = runner.invoke(cli, ["config_demo.yml", "--overwrite"])
+    assert ret.exit_code == 0
+
+
+def test_show_versions() -> None:
+    f = io.StringIO()
+    hs.show_versions(file=f)
+    assert "SYS INFO" in f.getvalue()
