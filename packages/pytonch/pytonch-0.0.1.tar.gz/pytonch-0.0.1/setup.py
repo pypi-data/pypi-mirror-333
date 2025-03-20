fbce36aa040b557b5296c86e16dd656f2d2d9f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:05:54 +0000

    Add package pytonch/0.0.1/pytonch-0.0.1.tar.gz

---
+++ packages/pytonch/pytonch-0.0.1.tar.gz/pytonch-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='pytonch',
+    version='0.0.1',
+    author='MEGA',
+    author_email='vladhopo@gmail.com',
+    description='My Package' ,
+    packages=setuptools.find_packages(),
+)
