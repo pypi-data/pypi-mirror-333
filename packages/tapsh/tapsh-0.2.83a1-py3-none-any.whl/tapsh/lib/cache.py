56d8541624f196a3d15316e92990f5bacf74845b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:31:41 +0000

    Add package tapsh/0.2.83a1/tapsh-0.2.83a1-py3-none-any.whl

---
+++ packages/tapsh/tapsh-0.2.83a1-py3-none-any.whl/tapsh/lib/cache.py
@@ -0,0 +1,23 @@
+client_cache = {
+    "tables": {
+    },
+    "apis": {
+        "name_index": {}
+    },
+    "apiserver": {
+        "name_index": {}
+    },
+    "connectors": {},
+    "default_sink": None
+}
+
+system_server_conf = {
+    "api": "",
+    "access_code": "",
+    "token": "",
+    "user_id": "",
+    "username": "",
+    "cookies": {},
+    "ws_uri": "",
+    "auth_param": ""
+}
