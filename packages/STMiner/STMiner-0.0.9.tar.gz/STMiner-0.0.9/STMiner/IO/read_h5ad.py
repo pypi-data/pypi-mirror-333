87e5fbcebe8948f4114b1cf54cdb32986d937954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:07:02 +0000

    Add package STMiner/0.0.9/STMiner-0.0.9.tar.gz

---
+++ packages/STMiner/STMiner-0.0.9.tar.gz/STMiner-0.0.9/STMiner/IO/read_h5ad.py
@@ -0,0 +1,30 @@
+import scanpy as sc
+
+from STMiner.IO.IOUtil import *
+
+
+def read_h5ad(file, amplification=1, bin_size=1):
+    """
+    Read h5ad file to anndata and add more information for SEP pipline.
+    :param bin_size:
+    :type bin_size:
+    :param file: path to h5ad file
+    :type file: str
+    :param amplification:
+    :type amplification: int
+    :return: anndata
+    :rtype: Anndata
+    """
+    amplification = np.int32(amplification)
+    adata = sc.read_h5ad(file)
+
+    position = adata.obsm['spatial']
+    x_min = position[:, 0].min() * amplification
+    y_min = position[:, 1].min() * amplification
+    adata.obs['x'] = merge_bin_coordinate(position[:, 0] * amplification, x_min, bin_size=bin_size)
+    adata.obs['y'] = merge_bin_coordinate(position[:, 1] * amplification, y_min, bin_size=bin_size)
+    adata.misc = {'up': position[:, 1].min(),
+                  'down': position[:, 1].max(),
+                  'left': position[:, 0].min(),
+                  'right': position[:, 0].max()}
+    return adata
