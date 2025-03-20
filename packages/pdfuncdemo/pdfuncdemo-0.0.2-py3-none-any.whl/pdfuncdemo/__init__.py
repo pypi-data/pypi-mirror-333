a4914c3b3769708d73f50e179cb45fce8271291a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:32:02 +0000

    Add package pdfuncdemo/0.0.2/pdfuncdemo-0.0.2-py3-none-any.whl

---
+++ packages/pdfuncdemo/pdfuncdemo-0.0.2-py3-none-any.whl/pdfuncdemo/__init__.py
@@ -0,0 +1,39 @@
+## Copyright Pacific Digital Technology Pty
+## 2025-03-11
+
+
+########################################
+## classes
+########################################
+
+class FuncDemo:
+  version = "0.0.1"
+ 
+  def getFiles(self):
+    return 'got the files here. version {}'.format(self.version)
+    ## return 'got the files here. '
+  
+  def getWells(self):
+    return 'got the wells from here. version {}'.format(self.version)
+  
+       
+########################################
+## functions
+########################################
+
+def read_files(searchText):
+   func = FuncDemo()
+   return func.getFiles()
+
+def read_wells(searchText):
+   func = FuncDemo()
+   return func.getWells()
+
+
+########################################
+## variables
+########################################
+
+files = read_files("tty")
+wells = read_wells("xtr")
+
