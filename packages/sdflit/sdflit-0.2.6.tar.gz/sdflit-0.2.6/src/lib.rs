488ab25dafe91f121183007d97e1b83733d5822b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:06 +0000

    Add package sdflit/0.2.6/sdflit-0.2.6.tar.gz

---
+++ packages/sdflit/sdflit-0.2.6.tar.gz/sdflit-0.2.6/src/lib.rs
@@ -0,0 +1,45 @@
+mod accelerator;
+mod composition;
+mod material;
+mod object;
+mod primitive;
+mod sampler;
+mod scene;
+mod sdf;
+mod solid_geometry;
+mod vec3;
+
+use pyo3::prelude::*;
+
+#[pymodule]
+fn sdflit(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    // SDF and Primitives
+    m.add_class::<sdf::DynSDF>()?;
+    m.add_class::<primitive::FrustumCone>()?;
+    m.add_class::<primitive::RoundCone>()?;
+    m.add_class::<primitive::Sphere>()?;
+
+    // Composition
+    m.add_function(wrap_pyfunction!(composition::merge, m)?)?;
+    m.add_function(wrap_pyfunction!(composition::intersect, m)?)?;
+    m.add_function(wrap_pyfunction!(composition::subtract, m)?)?;
+
+    // Material
+    m.add_class::<material::DynMaterial>()?;
+    m.add_class::<material::ColoredMaterial>()?;
+    m.add_class::<material::LinearGradientMaterial>()?;
+
+    // Object
+    m.add_class::<object::DynObject>()?;
+    m.add_class::<object::SDFObject>()?;
+
+    // Scene
+    m.add_class::<scene::DynScene>()?;
+    m.add_class::<scene::ObjectsScene>()?;
+
+    // Sampler
+    m.add_class::<sampler::UniformSampler>()?;
+    m.add_class::<sampler::RangeSampler>()?;
+
+    Ok(())
+}
