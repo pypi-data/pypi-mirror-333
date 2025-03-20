e91d31748e18f704c44a2f0d7b18931f1e504766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:31:18 +0000

    Add package wpipe/0.1.7/wpipe-0.1.7-py3-none-any.whl

---
+++ packages/wpipe/wpipe-0.1.7-py3-none-any.whl/wpipe/exception/api_error.py
@@ -0,0 +1,37 @@
+from wpipe.log import new_logger
+
+logger = new_logger()
+
+
+class Codes:
+    UPDATE_TASK = 505
+    UPDATE_PROCESS_ERROR = 504
+    UPDATE_PROCESS_OK = 503
+    TASK_FAILED = 502
+    API_ERROR = 501
+
+
+class ApiError(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+        logger.error(message)
+
+
+class TaskError(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+        # logger.error(message)
+    def __str__(self):
+        return f"[Error Code: {self.error_code}] {super().__str__()}"
+
+
+class ProcessError(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+        # logger.error(message)
