8785694690b6f2340d8a54bb8ce506b0c6c027c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:27 +0000

    Add package credsweeper/1.11.1/credsweeper-1.11.1.tar.gz

---
+++ packages/credsweeper/credsweeper-1.11.1.tar.gz/credsweeper-1.11.1/credsweeper/filters/group/url_credentials_group.py
@@ -0,0 +1,34 @@
+from credsweeper.common.constants import GroupType
+from credsweeper.config import Config
+from credsweeper.filters import (ValueAllowlistCheck, ValueArrayDictionaryCheck, ValueBlocklistCheck,
+                                 ValueCamelCaseCheck, ValueDictionaryValueLengthCheck, ValueFilePathCheck,
+                                 ValueFirstWordCheck, ValueLastWordCheck, ValueMethodCheck, ValueNotAllowedPatternCheck,
+                                 ValuePatternCheck, ValueStringTypeCheck, ValueTokenCheck)
+from credsweeper.filters.group import Group
+
+
+class UrlCredentialsGroup(Group):
+    """UrlCredentialsGroup"""
+
+    def __init__(self, config: Config) -> None:
+        """URL credentials group class.
+
+        Similar to PasswordKeyword, but exclude all checks dependent on the variable name, as URL credentials have no
+        explicitly defined variable
+        """
+        super().__init__(config, GroupType.DEFAULT)
+        self.filters = [
+            ValueAllowlistCheck(),
+            ValueArrayDictionaryCheck(),
+            ValueBlocklistCheck(),
+            ValueCamelCaseCheck(),
+            ValueFilePathCheck(),
+            ValueFirstWordCheck(),
+            ValueLastWordCheck(),
+            ValueMethodCheck(),
+            ValueStringTypeCheck(config),
+            ValueNotAllowedPatternCheck(),
+            ValueTokenCheck(),
+            ValueDictionaryValueLengthCheck(min_len=4, max_len=80),
+            ValuePatternCheck(pattern_len=config.pattern_len)
+        ]
