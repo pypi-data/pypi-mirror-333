84929684ec8023749d78d4fe5f6a16d97225bf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:59 +0000

    Add package kittycad/0.7.6/kittycad-0.7.6-py3-none-any.whl

---
+++ packages/kittycad/kittycad-0.7.6-py3-none-any.whl/kittycad/models/client_metrics.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class ClientMetrics(BaseModel):
+    """ClientMetrics contains information regarding the state of the peer."""
+
+    rtc_frames_decoded: int
+
+    rtc_frames_dropped: int
+
+    rtc_frames_per_second: int
+
+    rtc_frames_received: int
+
+    rtc_freeze_count: int
+
+    rtc_jitter_sec: float
+
+    rtc_keyframes_decoded: int
+
+    rtc_total_freezes_duration_sec: float
+
+    model_config = ConfigDict(protected_namespaces=())
---
+++ packages/kittycad/kittycad-0.7.6-py3-none-any.whl/kittycad/models/o_auth2_client_info.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+
+class OAuth2ClientInfo(BaseModel):
+    """Information about an OAuth 2.0 client."""
+
+    csrf_token: Optional[str] = None
+
+    pkce_code_verifier: Optional[str] = None
+
+    url: Optional[str] = None
+
+    model_config = ConfigDict(protected_namespaces=())
