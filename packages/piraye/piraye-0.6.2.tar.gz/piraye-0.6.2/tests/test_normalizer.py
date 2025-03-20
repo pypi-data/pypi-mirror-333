011e9db0d28a2ef7281915ac226576b591bbcea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:25 +0000

    Add package piraye/0.6.2/piraye-0.6.2.tar.gz

---
+++ packages/piraye/piraye-0.6.2.tar.gz/piraye-0.6.2/tests/test_normalizer.py
@@ -0,0 +1,69 @@
+# testing Fibonacci number function
+# pylint: skip-file
+from ..piraye import NltkTokenizer, SpacyTokenizer
+from ..piraye import NormalizerBuilder
+
+
+def test_object():
+    norm = NormalizerBuilder().build()
+    assert norm is not None
+
+
+def test_not_changing_input():
+    text = "این یک متن فارسی ، English می‌باشد. این متن نباید تغییر کند.سال ۱۴۰۰ (1400)"
+    norm = NormalizerBuilder().build()
+    assert text == norm.normalize(text)
+
+
+def test_number():
+    text = "1.2"
+    norm = NormalizerBuilder().digit_fa().build()
+    assert text != norm.normalize(text)
+
+
+def test_puncs():
+    text = "شروع ، پایان"
+    norm = NormalizerBuilder().punctuation_en().build()
+    assert text != norm.normalize(text)
+
+
+def test_space():
+    text = "Hello  , Piraye(   NLP Tool   )  .  \n   \n"
+    norm = NormalizerBuilder().remove_extra_spaces().alphabet_en().punctuation_en().build()
+    assert "Hello, Piraye (NLP Tool).\n" != norm.normalize(text)
+
+
+def test_quotes():
+    text = "«"
+    norm = NormalizerBuilder().digit_en().punctuation_en().alphabet_fa() \
+        .tokenizing().remove_extra_spaces().build()
+    norm.normalize(text)
+    text = " «««« تست "
+    norm = NormalizerBuilder().digit_en().punctuation_en().alphabet_fa() \
+        .tokenizing().remove_extra_spaces().build()
+    norm.normalize(text)
+    text = " \" تست '' تست «««« تست "
+    norm = NormalizerBuilder().digit_en().punctuation_en().alphabet_fa() \
+        .tokenizing().remove_extra_spaces().build()
+    norm.normalize(text)
+
+
+def test_quotes_spacy():
+    tokenizer = SpacyTokenizer()
+    text = "«"
+    norm = NormalizerBuilder().digit_en().punctuation_en().alphabet_fa() \
+        .tokenizing().remove_extra_spaces().tokenizing(tokenizer=tokenizer).build()
+    norm.normalize(text)
+    text = " «««« تست "
+    norm = NormalizerBuilder().digit_en().punctuation_en().alphabet_fa() \
+        .tokenizing().remove_extra_spaces().build()
+    norm.normalize(text)
+    text = " \" تست '' تست «««« تست "
+    norm = NormalizerBuilder().digit_en().punctuation_en().alphabet_fa() \
+        .tokenizing().remove_extra_spaces().build()
+    norm.normalize(text)
+
+
+def test_normalizer():
+    tokens = NltkTokenizer().word_tokenize('\'\'Y\'"')
+    print(tokens)
---
+++ packages/piraye/piraye-0.6.2.tar.gz/piraye-0.6.2/tests/test_tokenizer.py
@@ -0,0 +1,77 @@
+# testing Fibonacci number function
+# pylint: skip-file
+from ..piraye import NltkTokenizer, SpacyTokenizer
+
+
+def test_object():
+    tokenizer = NltkTokenizer()
+    assert tokenizer is not None
+
+
+def test_sample():
+    text = "برای تست شماره ۲.۱ نوشته شده است"
+    tokenizer = NltkTokenizer()
+    assert len(tokenizer.word_tokenize(text)) == 7
+
+
+def test_sample_spacy():
+    text = "برای تست (شماره ۲.۱ نوشته) شده است"
+    tokenizer = SpacyTokenizer()
+    assert len(tokenizer.word_tokenize(text)) == 9
+
+
+def test_double_quotes():
+    text = "'\"\"تست\""
+    tokenizer = NltkTokenizer()
+    assert len(tokenizer.word_tokenize(text)) == 5
+
+
+def test_sentence_tokenizer():
+    text = "sentence1 sad. \n asd asd \n asdasd \n sentence 2."
+    tokenizer = NltkTokenizer()
+    assert len(tokenizer.sentence_tokenize(text)) == 2
+    assert len(tokenizer.sentence_span_tokenize(text)) == 2
+
+
+def test_paragraph_tokenizer():
+    text = "par1 sen1 sad.  par1 \n sen2. par1 \n\n sen3.\n par2 sen1.\n\n\n\n   par3 sen1. \n par4 sen1.\n\n"
+    tokenizer = NltkTokenizer()
+    assert len(tokenizer.paragraph_tokenize(text)) == 5
+    assert len(tokenizer.paragraph_span_tokenize(text)) == 5
+    assert len(tokenizer.paragraph_tokenize("par1 sen1 sad.")) == 1
+    assert len(tokenizer.paragraph_tokenize("par1 sen1 sad. par1 \n sen2.")) == 1
+
+
+def test_paragraph_tokenizer_spacy():
+    text = "par1 sen1 sad.  par1 \n sen2. par1 \n\n sen3.\n par2 sen1.\n\n\n\n   par3 sen1. \n par4 sen1.\n\n"
+    tokenizer = SpacyTokenizer()
+    assert len(tokenizer.paragraph_tokenize(text)) == 5
+    assert len(tokenizer.paragraph_span_tokenize(text)) == 5
+    assert len(tokenizer.paragraph_tokenize("par1 sen1 sad.")) == 1
+    assert len(tokenizer.paragraph_tokenize("par1 sen1 sad. par1 \n sen2.")) == 1
+
+
+def test_sentence_tokenizer_spacy():
+    text = "sentence1 sad. \n asd asd \n asdasd \n sentence 2."
+    tokenizer = SpacyTokenizer()
+    assert len(tokenizer.sentence_tokenize(text)) == 2
+    assert len(tokenizer.sentence_span_tokenize(text)) == 2
+
+
+def test_double_quotes2():
+    text = "«»"
+    tokenizer = NltkTokenizer()
+    assert len(tokenizer.word_tokenize(text)) == 2
+
+
+def test_link():
+    # To check nltk is functioning wrong for links
+    text = "این یک لینک تست است https://www.google.com "
+    tokenizer = NltkTokenizer()
+    assert len(tokenizer.word_tokenize(text)) != 9
+
+
+def test_link_spacy():
+    text = "این یک لینک، (تست) است https://www.google.com "
+    tokenizer = SpacyTokenizer()
+    assert len(tokenizer.word_tokenize(text)) == 9
