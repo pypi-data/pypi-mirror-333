ad31c7402250f13bd6e41697919d2326a7da2ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:19 +0000

    Add package ecotrade/0.1.8/ecotrade-0.1.8.tar.gz

---
+++ packages/ecotrade/ecotrade-0.1.8.tar.gz/ecotrade-0.1.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ecotrade',
+    version='0.1.8',
+    author='Zyber Pireci & Vishva Teja Janne',
+    author_email='supporto@ecotrade.bio',
+    description='An Ecotrade package to manage the software infrastructure',
+    # url='https://github.com/yourusername/your-repo',  # If you have a GitHub repo for the package
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Adjust the license to your choice
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],  # List your dependencies here
+    python_requires='>=3.6',  # Adjust this depending on your compatibility
+)
