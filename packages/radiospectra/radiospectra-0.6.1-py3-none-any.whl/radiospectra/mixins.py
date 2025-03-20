aa108e023651a3efc985c390d52a76f6d89fffcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:07:22 +0000

    Add package radiospectra/0.6.1/radiospectra-0.6.1-py3-none-any.whl

---
+++ packages/radiospectra/radiospectra-0.6.1-py3-none-any.whl/radiospectra/mixins.py
@@ -0,0 +1,73 @@
+class PcolormeshPlotMixin:
+    """
+    Class provides plotting functions using `~pcolormesh`.
+    """
+
+    def plot(self, axes=None, **kwargs):
+        """
+        Plot the spectrogram.
+
+        Parameters
+        ----------
+        axes : `matplotlib.axis.Axes`, optional
+            The axes where the plot will be added.
+        kwargs :
+            Arguments pass to the plot call `pcolormesh`.
+
+        Returns
+        -------
+        `matplotlib.collections.QuadMesh`
+        """
+        import matplotlib.dates as mdates
+        from matplotlib import pyplot as plt
+
+        if axes is None:
+            fig, axes = plt.subplots()
+        else:
+            fig = axes.get_figure()
+
+        if hasattr(self.data, "value"):
+            data = self.data.value
+        else:
+            data = self.data
+
+        title = f"{self.observatory}, {self.instrument}"
+        if self.instrument != self.detector:
+            title = f"{title}, {self.detector}"
+
+        axes.set_title(title)
+        axes.plot(self.times.datetime[[0, -1]], self.frequencies[[0, -1]], linestyle="None", marker="None")
+        if self.times.shape[0] == self.data.shape[0] and self.frequencies.shape[0] == self.data.shape[1]:
+            ret = axes.pcolormesh(self.times.datetime, self.frequencies.value, data, shading="auto", **kwargs)
+        else:
+            ret = axes.pcolormesh(self.times.datetime, self.frequencies.value, data[:-1, :-1], shading="auto", **kwargs)
+        axes.set_xlim(self.times.datetime[0], self.times.datetime[-1])
+        locator = mdates.AutoDateLocator(minticks=4, maxticks=8)
+        formatter = mdates.ConciseDateFormatter(locator)
+        axes.xaxis.set_major_locator(locator)
+        axes.xaxis.set_major_formatter(formatter)
+        fig.autofmt_xdate()
+        # Set current axes/image if pyplot is being used (makes colorbar work)
+        for i in plt.get_fignums():
+            if axes in plt.figure(i).axes:
+                plt.sca(axes)
+                plt.sci(ret)
+        return ret
+
+
+class NonUniformImagePlotMixin:
+    """
+    Class provides plotting functions using `NonUniformImage`.
+    """
+
+    def plotim(self, fig=None, axes=None, **kwargs):
+        import matplotlib.dates as mdates
+        from matplotlib import pyplot as plt
+        from matplotlib.image import NonUniformImage
+
+        if axes is None:
+            fig, axes = plt.subplots()
+
+        im = NonUniformImage(axes, interpolation="none", **kwargs)
+        im.set_data(mdates.date2num(self.times.datetime), self.frequencies.value, self.data)
+        axes.images.append(im)
---
+++ packages/radiospectra/radiospectra-0.6.1-py3-none-any.whl/radiospectra/spectrogram/sources/__init__.py
@@ -0,0 +1,26 @@
+"""
+Datasource-specific classes.
+
+This is where datasource specific logic is implemented.
+Each mission should have its own file with one or more classes defined.
+"""
+
+from ..spectrogram_factory import Spectrogram  # NOQA
+from .callisto import *  # NOQA
+from .eovsa import *  # NOQA
+from .ilofar357 import *  # NOQA
+from .psp_rfs import *  # NOQA
+from .rpw import *  # NOQA
+from .rstn import *  # NOQA
+from .swaves import *  # NOQA
+from .waves import *  # NOQA
+
+__all__ = [
+    "SWAVESSpectrogram",
+    "RFSSpectrogram",
+    "CALISTOSpectrogram",
+    "EOVSASpectrogram",
+    "RSTNSpectrogram",
+    "RPWSpectrogram",
+    "ILOFARMode357Spectrogram",
+]
