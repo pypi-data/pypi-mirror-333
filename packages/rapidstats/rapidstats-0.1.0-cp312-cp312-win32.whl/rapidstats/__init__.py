becb9c67d396bcb6e451d9436c8569aaca520e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:04 +0000

    Add package rapidstats/0.1.0/rapidstats-0.1.0-cp312-cp312-win32.whl

---
+++ packages/rapidstats/rapidstats-0.1.0-cp312-cp312-win32.whl/rapidstats/__init__.py
@@ -0,0 +1,6 @@
+from rapidstats import metrics, polars, preprocessing
+
+from ._bootstrap import Bootstrap
+from ._corr import correlation_matrix
+from ._distributions import norm
+from ._general import auc
