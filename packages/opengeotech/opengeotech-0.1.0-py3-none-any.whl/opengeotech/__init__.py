a15cecc49ba651a1fc7b0ed7fc40a47154904363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:36 +0000

    Add package opengeotech/0.1.0/opengeotech-0.1.0-py3-none-any.whl

---
+++ packages/opengeotech/opengeotech-0.1.0-py3-none-any.whl/opengeotech/__init__.py
@@ -0,0 +1,11 @@
+"""
+OpenGeoTech - A simple geotechnical engineering package.
+
+This package provides functions for geotechnical engineering calculations.
+"""
+
+__version__ = "0.1.2"
+
+from .soil_mechanics import effective_stress
+
+__all__ = ["effective_stress"] 
