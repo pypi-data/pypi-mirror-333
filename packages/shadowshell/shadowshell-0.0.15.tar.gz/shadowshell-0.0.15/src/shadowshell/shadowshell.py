f8e831295500838e4cdb6cc13bfe58126689ebd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:45 +0000

    Add package shadowshell/0.0.15/shadowshell-0.0.15.tar.gz

---
+++ packages/shadowshell/shadowshell-0.0.15.tar.gz/shadowshell-0.0.15/src/shadowshell/shadowshell.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Logger
+@author: shadow shell
+"""
+
+import requests
+
+class ShadowShell:
+
+    def hello(self):
+        print("Hello.")
+    
+    def request(self):
+        requests.get("https://wwww.baidu.com")
+
+def testserver():
+    os.system("ping shadowshell.xyz")
+    
+def cnnserver():
+    os.system("ssh admin@shadowshell.xyz")
---
+++ packages/shadowshell/shadowshell-0.0.15.tar.gz/shadowshell-0.0.15/tests/tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+tests
+
+@author: shadow shell
+"""
+
+from ./src/shadowshell/shadowshell import ShadowShell, testserver
+
+ShadowShell().hello()
+
+testserver()
+
