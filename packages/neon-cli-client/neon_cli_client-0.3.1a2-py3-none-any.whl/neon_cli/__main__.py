3bc0460f8af5ac0e3fd18d7fc1dc0b814d56b760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:54 +0000

    Add package neon-cli-client/0.3.1a2/neon_cli_client-0.3.1a2-py3-none-any.whl

---
+++ packages/neon-cli-client/neon_cli_client-0.3.1a2-py3-none-any.whl/neon_cli/__main__.py
@@ -0,0 +1,86 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+#
+# Copyright 2008-2025 Neongecko.com Inc. | All Rights Reserved
+#
+# Notice of License - Duplicating this Notice of License near the start of any file containing
+# a derivative of this software is a condition of license for this software.
+# Friendly Licensing:
+# No charge, open source royalty free use of the Neon AI software source and object is offered for
+# educational users, noncommercial enthusiasts, Public Benefit Corporations (and LLCs) and
+# Social Purpose Corporations (and LLCs). Developers can contact developers@neon.ai
+# For commercial licensing, distribution of derivative works or redistribution please contact licenses@neon.ai
+# Distributed on an "AS IS” basis without warranties or conditions of any kind, either express or implied.
+# Trademarks of Neongecko: Neon AI(TM), Neon Assist (TM), Neon Communicator(TM), Klat(TM)
+# Authors: Guy Daniels, Daniel McKnight, Regina Bloomstine, Elon Gasper, Richard Leeds
+#
+# Specialized conversational reconveyance options from Conversation Processing Intelligence Corp.
+# US Patents 2008-2021: US7424516, US20140161250, US20140177813, US8638908, US8068604, US8553852, US10530923, US10530924
+# China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
+#
+# This software is an enhanced derivation of the Mycroft Project which is licensed under the
+# Apache software Foundation software license 2.0 https://www.apache.org/licenses/LICENSE-2.0
+# Changes Copyright 2008-2025 Neongecko.com Inc. | All Rights Reserved
+#
+# Copyright 2017 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import sys
+import io
+import os.path
+import tempfile
+
+
+from neon_cli import start_cli_client
+
+sys.stdout = io.StringIO()
+sys.stderr = io.StringIO()
+
+
+def custom_except_hook(exctype, value, traceback):
+    print(sys.stdout.getvalue(), file=sys.__stdout__)
+    print(sys.stderr.getvalue(), file=sys.__stderr__)
+    sys.stdout, sys.stderr = sys.__stdout__, sys.__stderr__
+    sys.__excepthook__(exctype, value, traceback)
+
+
+sys.excepthook = custom_except_hook  # noqa
+
+
+def main():
+    import argparse
+    from neon_utils.configuration_utils import init_config_dir
+    from ovos_config.config import Configuration
+    init_config_dir()
+
+    logs_dir = dict(Configuration()).get("log_dir") or "/var/log/mycroft"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ipc_dir", dest="ipc_dir", type=str, help="the base",
+                        default=os.path.join(tempfile.gettempdir(),
+                                             "mycroft", "ipc"))
+    parser.add_argument("--logs_dir", dest="logs_dir", type=str,
+                        help="directory where mycroft logs are stored",
+                        default=logs_dir)
+    parser.add_argument("--lang", dest="lang", type=str,
+                        help="lang code",
+                        default="en-us")
+    parser.add_argument("--simple", dest="simple", type=bool,
+                        help="use simple cli without logs",
+                        default=False)
+    args = parser.parse_args()
+    start_cli_client("0.0.0.0", 8181, args.lang, args.ipc_dir, args.logs_dir,
+                     args.simple)
+
+
+if __name__ == "__main__":
+    main()
