6fa0b6e8044659cfb898828c4c4775724e9ee628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:07:47 +0000

    Add package lastmile/0.11.3/lastmile-0.11.3.tar.gz

---
+++ packages/lastmile/lastmile-0.11.3.tar.gz/lastmile-0.11.3/src/lastmile/types/project_list_params.py
@@ -0,0 +1,21 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["ProjectListParams", "Filters"]
+
+
+class ProjectListParams(TypedDict, total=False):
+    filters: Filters
+
+
+class Filters(TypedDict, total=False):
+    organization_id: Annotated[str, PropertyInfo(alias="organizationId")]
+    """search only projects associated with specific organization"""
+
+    query: str
+    """search query substring match for name and description"""
