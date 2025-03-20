e3f4ade1ba80e1a2ce55bc0b57ba448903f7df45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:07:00 +0000

    Add package flash-tokenizer/0.9.9/flash_tokenizer-0.9.9.tar.gz

---
+++ packages/flash-tokenizer/flash_tokenizer-0.9.9.tar.gz/flash_tokenizer-0.9.9/package_build/install_miniconda.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+export HOME=$(pwd)
+mkdir miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
+bash /tmp/miniconda.sh -b -u -p ./miniconda
+
+./miniconda/bin/conda create -n py39 python=3.9 -y
+./miniconda/bin/conda create -n py310 python=3.10 -y
+./miniconda/bin/conda create -n py311 python=3.11 -y
+./miniconda/bin/conda create -n py312 python=3.12 -yy
+
+./miniconda/envs/py39/bin/python -m pip install build twine pybind11 numpy==1.26.4 setuptools wheel
+./miniconda/envs/py310/bin/python -m pip install build twine pybind11 numpy==1.26.4 setuptools wheel
+./miniconda/envs/py311/bin/python -m pip install build twine pybind11 numpy==1.26.4 setuptools wheel
+./miniconda/envs/py312/bin/python -m pip install build twine pybind11 numpy==1.26.4 setuptools wheel
+
