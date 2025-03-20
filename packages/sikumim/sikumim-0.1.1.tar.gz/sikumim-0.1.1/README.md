a8940fe6828b549b5afba12accbbb78a82ec9c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:30 +0000

    Add package sikumim/0.1.1/sikumim-0.1.1.tar.gz

---
+++ packages/sikumim/sikumim-0.1.1.tar.gz/sikumim-0.1.1/README.md
@@ -0,0 +1,17 @@
+# Sikumim
+
+## Introduction
+A simple tool to quickly and easily download lecture notes (and more).
+
+All notes are published with the courtesy of **Regev Yehezkel Imra**.
+
+##  Instructions
+1. Install the library:
+    ```
+    pip install sikumim
+    ```
+2. Run the following command to download course material:
+    ```
+    python download.py <course-name> <output-path>
+    ```
+   
---
+++ packages/sikumim/sikumim-0.1.1.tar.gz/sikumim-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="sikumim",
+    version="0.1.1",
+    author="Eitan Sztuden",
+    description="A simple tool to download lecture notes, written by Regev Yehezkel Imra",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["download"],
+    entry_points={"console_scripts": ["download=download:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
