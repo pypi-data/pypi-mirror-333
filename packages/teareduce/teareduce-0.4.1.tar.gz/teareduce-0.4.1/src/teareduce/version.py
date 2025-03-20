34f6310441b788ec33b3c49d368da6b563d16656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:05:17 +0000

    Add package teareduce/0.4.1/teareduce-0.4.1.tar.gz

---
+++ packages/teareduce/teareduce-0.4.1.tar.gz/teareduce-0.4.1/src/teareduce/version.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023-2024 Universidad Complutense de Madrid
+#
+# This file is part of teareduce
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+version = '0.4.1'
+
+
+def main():
+    print('Version: ' + version)
+
+
+if __name__ == "__main__":
+    main()
