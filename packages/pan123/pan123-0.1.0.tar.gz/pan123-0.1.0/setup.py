e63553b9b98fc6dc2a4f327b992a07d6474e3a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:30:02 +0000

    Add package pan123/0.1.0/pan123-0.1.0.tar.gz

---
+++ packages/pan123/pan123-0.1.0.tar.gz/pan123-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pan123",
+    version="0.1.0",
+    author="SodaCodeSave&lixuehua",
+    author_email="soda_code@outlook.com",
+    description="This is an unofficial 123 Pan Open Platform API library, which can easily call the 123 Pan Open Platform in Python without the need to write repetitive code multiple times",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SodaCodeSave/Pan123",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
