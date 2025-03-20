90a40e5d9279974365cb2e5cd86933616ae3a332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:40 +0000

    Add package osism/0.20250312.0/osism-0.20250312.0.tar.gz

---
+++ packages/osism/osism-0.20250312.0.tar.gz/osism-0.20250312.0/osism/commands/manage.py
@@ -0,0 +1,298 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from re import findall
+import subprocess
+from urllib.parse import urljoin
+
+from cliff.command import Command
+import docker
+from jinja2 import Template
+from loguru import logger
+import requests
+
+from osism.data import TEMPLATE_IMAGE_CLUSTERAPI, TEMPLATE_IMAGE_OCTAVIA
+
+SUPPORTED_CLUSTERAPI_K8S_IMAGES = ["1.29", "1.30", "1.31"]
+
+
+class ImageClusterapi(Command):
+    def get_parser(self, prog_name):
+        parser = super(ImageClusterapi, self).get_parser(prog_name)
+
+        parser.add_argument(
+            "--base-url",
+            type=str,
+            help="Base URL",
+            default="https://swift.services.a.regiocloud.tech/swift/v1/AUTH_b182637428444b9aa302bb8d5a5a418c/openstack-k8s-capi-images/",
+        )
+        parser.add_argument(
+            "--cloud",
+            type=str,
+            help="Cloud name in clouds.yaml (will be overruled by OS_AUTH_URL envvar)",
+            default="admin",
+        )
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="Do not perform any changes (--dry-run passed to openstack-image-manager)",
+        )
+        parser.add_argument(
+            "--tag",
+            type=str,
+            help="Name of the tag used to identify managed images (use openstack-image-manager's default if unset)",
+            default=None,
+        )
+        parser.add_argument(
+            "--filter",
+            type=str,
+            help="Filter the version to be managed (e.g. 1.31)",
+            default=None,
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        base_url = parsed_args.base_url
+        cloud = parsed_args.cloud
+        filter = parsed_args.filter
+        tag = parsed_args.tag
+
+        if filter:
+            supported_cluterapi_k8s_images = [filter]
+        else:
+            supported_cluterapi_k8s_images = SUPPORTED_CLUSTERAPI_K8S_IMAGES
+
+        os.makedirs("/tmp/clusterapi", exist_ok=True)
+        for kubernetes_release in supported_cluterapi_k8s_images:
+            url = urljoin(base_url, f"last-{kubernetes_release}")
+
+            response = requests.get(url)
+            splitted = response.text.strip().split(" ")
+
+            logger.info(f"date: {splitted[0]}")
+            logger.info(f"image: {splitted[1]}")
+
+            r = findall(r".*ubuntu-2204-kube-v(.*\..*\..*).qcow2", splitted[1])
+            logger.info(f"version: {r[0].strip()}")
+
+            url = urljoin(base_url, splitted[1])
+            logger.info(f"url: {url}")
+
+            logger.info(f"checksum_url: {url}.CHECKSUM")
+            response_checksum = requests.get(f"{url}.CHECKSUM")
+            splitted_checksum = response_checksum.text.strip().split(" ")
+            logger.info(f"checksum: {splitted_checksum[0]}")
+
+            template = Template(TEMPLATE_IMAGE_CLUSTERAPI)
+            result = template.render(
+                image_url=url,
+                image_checksum=f"sha256:{splitted_checksum[0]}",
+                image_version=r[0].strip(),
+                image_builddate=splitted[0],
+            )
+            with open(f"/tmp/clusterapi/k8s-{kubernetes_release}.yml", "w+") as fp:
+                fp.write(result)
+
+        args = [
+            "openstack-image-manager",
+            "--images=/tmp/clusterapi",
+            "--cloud",
+            cloud,
+            "--filter",
+            "ubuntu-capi-image",
+        ]
+        if tag is not None:
+            args.extend(["--tag", tag])
+        if parsed_args.dry_run:
+            args.append("--dry-run")
+        subprocess.call(args)
+
+
+class ImageOctavia(Command):
+    def get_parser(self, prog_name):
+        parser = super(ImageOctavia, self).get_parser(prog_name)
+
+        parser.add_argument(
+            "--cloud", type=str, help="Cloud name in clouds.yaml", default="openstack"
+        )
+        parser.add_argument(
+            "--base-url",
+            type=str,
+            help="Base URL",
+            default="https://swift.services.a.regiocloud.tech/swift/v1/AUTH_b182637428444b9aa302bb8d5a5a418c/openstack-octavia-amphora-image/",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        cloud = parsed_args.cloud
+        base_url = parsed_args.base_url
+
+        client = docker.from_env()
+        container = client.containers.get("kolla-ansible")
+        openstack_release = container.labels["de.osism.release.openstack"]
+        url = urljoin(base_url, f"last-{openstack_release}")
+
+        response = requests.get(url)
+        splitted = response.text.strip().split(" ")
+
+        logger.info(f"date: {splitted[0]}")
+        logger.info(f"image: {splitted[1]}")
+
+        url = urljoin(base_url, splitted[1])
+        logger.info(f"url: {url}")
+
+        logger.info(f"checksum_url: {url}.CHECKSUM")
+        response_checksum = requests.get(f"{url}.CHECKSUM")
+        splitted_checksum = response_checksum.text.strip().split(" ")
+        logger.info(f"checksum: {splitted_checksum[0]}")
+
+        template = Template(TEMPLATE_IMAGE_OCTAVIA)
+        result = template.render(
+            image_url=url,
+            image_checksum=f"sha256:{splitted_checksum[0]}",
+            image_version=splitted[0],
+            image_builddate=splitted[0],
+        )
+
+        os.makedirs("/tmp/octavia", exist_ok=True)
+        with open("/tmp/octavia/octavia.yml", "w+") as fp:
+            fp.write(result)
+
+        subprocess.call(
+            "/usr/local/bin/openstack-image-manager --images=/tmp/octavia --cloud octavia --deactivate",
+            shell=True,
+        )
+
+
+class Images(Command):
+    def get_parser(self, prog_name):
+        parser = super(Images, self).get_parser(prog_name)
+
+        # NOTE: This is a workaround. argparse.REMAINDER does not work well to pass all
+        # arguments to openstack-image-manager. This is improved by switching from cliff
+        # to typer. Then openstack-image-manager can simply be included directly at this
+        # point.
+
+        parser.add_argument(
+            "--dry-run",
+            default=False,
+            help="Do not perform any changes",
+            action="store_true",
+        )
+        parser.add_argument(
+            "--hide",
+            default=True,
+            help="Hide images that should be deleted",
+            action="store_true",
+        )
+        parser.add_argument(
+            "--latest",
+            default=False,
+            help="Only import the latest version for images of type multi",
+            action="store_true",
+        )
+        parser.add_argument(
+            "--cloud", type=str, help="Cloud name in clouds.yaml", default="openstack"
+        )
+        parser.add_argument(
+            "--filter",
+            type=str,
+            help="Filter images with a regex on their name",
+            default=None,
+        )
+        parser.add_argument(
+            "--images",
+            type=str,
+            help="Path to the directory containing all image files or path to single image file",
+            default=None,
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        cloud = parsed_args.cloud
+        dry_run = parsed_args.dry_run
+        filter = parsed_args.filter
+        hide = parsed_args.hide
+        latest = parsed_args.latest
+        images = parsed_args.images
+
+        arguments = []
+        if cloud:
+            arguments.append(f"--cloud '{cloud}'")
+        if filter:
+            arguments.append(f"--filter '{filter}'")
+        if dry_run:
+            arguments.append("--dry-run")
+        if latest:
+            arguments.append("--latest")
+        if hide:
+            arguments.append("--hide")
+
+        if images:
+            arguments.append(f"--images '{images}'")
+        else:
+            arguments.append("--images /etc/images")
+
+        joined_arguments = " ".join(arguments)
+        subprocess.call(
+            f"/usr/local/bin/openstack-image-manager {joined_arguments}",
+            shell=True,
+        )
+
+
+class Flavors(Command):
+    def get_parser(self, prog_name):
+        parser = super(Flavors, self).get_parser(prog_name)
+
+        # NOTE: This is a workaround. argparse.REMAINDER does not work well to pass all
+        # arguments to openstack-image-manager. This is improved by switching from cliff
+        # to typer. Then openstack-flavor-manager can simply be included directly at this
+        # point.
+
+        parser.add_argument(
+            "--cloud", type=str, help="Cloud name in clouds.yaml", default="admin"
+        )
+        parser.add_argument(
+            "--name",
+            type=str,
+            help="Name of flavor definitions",
+            default="scs",
+            choices=["scs", "osism", "local", "url"],
+        )
+        parser.add_argument(
+            "--url",
+            type=str,
+            help="Overwrite the default URL where the flavor definitions are available",
+            default=None,
+        )
+        parser.add_argument(
+            "--recommended",
+            default=False,
+            help="Also create recommended flavors",
+            action="store_true",
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        cloud = parsed_args.cloud
+        name = parsed_args.name
+        recommended = parsed_args.recommended
+        url = parsed_args.url
+
+        arguments = [f"--name '{name}'"]
+        if cloud:
+            arguments.append(f"--cloud '{cloud}'")
+
+        if recommended:
+            arguments.append("--recommended")
+
+        if url:
+            arguments.append(f"--url '{url}'")
+
+        joined_arguments = " ".join(arguments)
+        subprocess.call(
+            f"/usr/local/bin/openstack-flavor-manager {joined_arguments}",
+            shell=True,
+        )
---
+++ packages/osism/osism-0.20250312.0.tar.gz/osism-0.20250312.0/osism/tasks/netbox.py
@@ -0,0 +1,247 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import subprocess
+
+from celery import Celery
+from celery.signals import worker_process_init
+import json
+import pynetbox
+from redis import Redis
+
+from osism import settings
+from osism.actions import (
+    check_configuration,
+    deploy_configuration,
+    diff_configuration,
+    generate_configuration,
+    manage_device,
+    manage_interface,
+)
+from osism.tasks import Config, openstack
+
+app = Celery("netbox")
+app.config_from_object(Config)
+
+redis = None
+nb = None
+
+
+@worker_process_init.connect
+def celery_init_worker(**kwargs):
+    global nb
+    global redis
+
+    redis = Redis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=settings.REDIS_DB,
+        socket_keepalive=True,
+    )
+    redis.ping()
+
+    if settings.NETBOX_URL and settings.NETBOX_TOKEN:
+        nb = pynetbox.api(settings.NETBOX_URL, token=settings.NETBOX_TOKEN)
+
+        if settings.IGNORE_SSL_ERRORS:
+            import requests
+
+            requests.packages.urllib3.disable_warnings()
+            session = requests.Session()
+            session.verify = False
+            nb.http_session = session
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    pass
+
+
+@app.task(bind=True, name="osism.tasks.netbox.periodic_synchronize_ironic")
+def periodic_synchronize_ironic(self):
+    """Synchronize the state of Ironic with Netbox"""
+    openstack.baremetal_node_list.apply_async((), link=synchronize_device_state.s())
+
+
+@app.task(bind=True, name="osism.tasks.netbox.run")
+def run(self, action, arguments):
+    pass
+
+
+@app.task(bind=True, name="osism.tasks.netbox.update_network_interface_name")
+def update_network_interface_name(self, mac_address, network_interface_name):
+    manage_interface.update_network_interface_name(mac_address, network_interface_name)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.import_device_types")
+def import_device_types(self, vendors, library=False):
+    global redis
+
+    if library:
+        env = {**os.environ, "BASE_PATH": "/devicetype-library/device-types/"}
+    else:
+        env = {**os.environ, "BASE_PATH": "/netbox/device-types/"}
+
+    if vendors:
+        p = subprocess.Popen(
+            f"python3 /import/main.py --vendors {vendors}",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            env=env,
+        )
+    else:
+        p = subprocess.Popen(
+            "python3 /import/main.py",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            env=env,
+        )
+
+    p.communicate()
+
+
+@app.task(bind=True, name="osism.tasks.netbox.synchronize_device_state")
+def synchronize_device_state(self, data):
+    """Synchronize the state of Ironic with Netbox"""
+
+    if type(data) == str:
+        data = json.loads(data)
+
+    if not data:
+        return
+
+    for device in data:
+        manage_device.set_provision_state(device["Name"], device["Provisioning State"])
+        manage_device.set_power_state(device["Name"], device["Power State"])
+
+
+@app.task(bind=True, name="osism.tasks.netbox.states")
+def states(self, data):
+    result = manage_device.get_states(data.keys())
+    return result
+
+
+@app.task(bind=True, name="osism.tasks.netbox.transitions")
+def transitions(self, data):
+    result = manage_device.get_transitions(data.keys())
+    return result
+
+
+@app.task(bind=True, name="osism.tasks.netbox.data")
+def data(self, collection, device, state):
+    result = manage_device.load_data_from_filesystem(collection, device, state)
+    return result
+
+
+@app.task(bind=True, name="osism.tasks.netbox.connect")
+def connect(self, device=None, state=None, data={}, enforce=False):
+    manage_device.run(device, state, data, enforce)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.set_state")
+def set_state(self, device=None, state=None, state_type=None):
+    manage_device.set_state(device, state, state_type)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.set_maintenance")
+def set_maintenance(self, device=None, state=None):
+    manage_device.set_maintenance(device, state)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.disable")
+def disable(self, name):
+    global nb
+
+    for interface in nb.dcim.interfaces.filter(device=name):
+        if str(interface.type) in ["Virtual"]:
+            continue
+
+        if "Port-Channel" in interface.name:
+            continue
+
+        if not interface.connected_endpoint and interface.enabled:
+            interface.enabled = False
+            interface.save()
+
+        # FIXME: only enable devices that are not disabled by configuration
+        if interface.connected_endpoint and not interface.enabled:
+            interface.enabled = True
+            interface.save()
+
+
+@app.task(bind=True, name="osism.tasks.netbox.generate")
+def generate(self, name, template=None):
+    generate_configuration.for_device(name, template)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.deploy")
+def deploy(self, name):
+    deploy_configuration.for_device(name)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.check")
+def check(self, name):
+    check_configuration.for_device(name)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.diff")
+def diff(self, name):
+    diff_configuration.for_device(name)
+
+
+@app.task(bind=True, name="osism.tasks.netbox.get_devices_not_yet_registered_in_ironic")
+def get_devices_not_yet_registered_in_ironic(
+    self, status="active", tags=["managed-by-ironic"], ironic_enabled=True
+):
+    global nb
+
+    devices = nb.dcim.devices.filter(
+        tag=tags, status=status, cf_ironic_enabled=[ironic_enabled]
+    )
+
+    result = []
+
+    for device in devices:
+        if (
+            "ironic_state" in device.custom_fields
+            and device.custom_fields["ironic_state"] != "registered"
+        ):
+            result.append(device.name)
+
+    return result
+
+
+@app.task(
+    bind=True,
+    name="osism.tasks.netbox.get_devices_that_should_have_an_allocation_in_ironic",
+)
+def get_devices_that_should_have_an_allocation_in_ironic(self):
+    global nb
+
+    devices = nb.dcim.devices.filter(
+        tag=["managed-by-ironic", "managed-by-osism"],
+        status="active",
+        cf_ironic_enabled=[True],
+        cf_ironic_state=["registered"],
+        cf_provision_state=["available"],
+        cf_introspection_state=["introspected"],
+        cf_device_type=["server"],
+    )
+
+    result = []
+
+    for device in devices:
+        result.append(device.name)
+
+    return result
+
+
+@app.task(bind=True, name="osism.tasks.netbox.ping")
+def ping(self):
+    global nb
+
+    status = nb.status()
+
+    return status
