6233bb64fe1205f17aa5f624b2af3b6041401bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:07:06 +0000

    Add package oarepo-model-builder-rdm/1.0.13/oarepo_model_builder_rdm-1.0.13.tar.gz

---
+++ packages/oarepo-model-builder-rdm/oarepo_model_builder_rdm-1.0.13.tar.gz/oarepo_model_builder_rdm-1.0.13/oarepo_model_builder_rdm/datatypes/components/marshmallow.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from oarepo_model_builder.datatypes import DataTypeComponent, ModelDataType
+from oarepo_model_builder.datatypes.components import MarshmallowModelComponent
+
+from .utils import replace_base_class
+
+
+class RDMMarshmallowModelComponent(DataTypeComponent):
+    eligible_datatypes = [ModelDataType]
+    depends_on = [MarshmallowModelComponent]
+
+    def before_model_prepare(
+        self, datatype: Any, *, context: dict[str, Any], **kwargs: Any
+    ):
+        if datatype.root.profile == "record":
+            replace_base_class(
+                datatype.definition["marshmallow"],
+                "oarepo_runtime.services.schema.marshmallow.BaseRecordSchema",
+                "oarepo_runtime.services.schema.marshmallow.RDMBaseRecordSchema",
+            )
