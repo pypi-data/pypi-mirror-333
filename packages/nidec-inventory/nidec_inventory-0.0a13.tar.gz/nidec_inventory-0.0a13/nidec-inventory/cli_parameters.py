0ffe077b5a2558fe249cbb208925aa47a4068e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:07:14 +0000

    Add package nidec-inventory/0.0a13/nidec_inventory-0.0a13.tar.gz

---
+++ packages/nidec-inventory/nidec_inventory-0.0a13.tar.gz/nidec_inventory-0.0a13/nidec-inventory/cli_parameters.py
@@ -0,0 +1,19 @@
+"""Gestión de los parámetros del CLI."""
+
+import argparse
+
+def get_cli_parameters(resources):
+    """Gestión de parámetros del CLI."""
+    loc = resources['loc']
+    parser = argparse.ArgumentParser(description=loc['menu']['descrip'])
+
+    parser.add_argument('-p', '--path_repo', type=str,
+                        help=loc['menu']['p'], required=True)
+    parser.add_argument('-f', '--file_out', type=str,
+                        help=loc['menu']['f'], required=True)
+    parser.add_argument('-o', '--output_format',
+                        type=str, choices=['csv', 'excel'],
+                        default='csv', required=False, help=loc['menu']['o'])
+
+    args = parser.parse_args()
+    return args.path_repo, args.file_out, args.output_format
