6919bfb9f04fe41785b50e348761abc3029d0e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:56 +0000

    Add package orionis/0.157.0/orionis-0.157.0.tar.gz

---
+++ packages/orionis/orionis-0.157.0.tar.gz/orionis-0.157.0/orionis/luminate/contracts/foundation/providers/service_providers_bootstrapper.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+from typing import List, Type
+from orionis.luminate.providers.service_provider import ServiceProvider
+
+class IServiceProvidersBootstrapper(ABC):
+
+    @abstractmethod
+    def _autoload(self) -> None:
+        """
+        Scans the provider directories and loads provider classes.
+
+        This method searches for Python files in the specified directories, imports them,
+        and registers any class that inherits from `ServiceProvider`.
+
+        Raises
+        ------
+        BootstrapRuntimeError
+            If there is an error loading a module.
+        """
+        pass
+
+    @abstractmethod
+    def _register(self, concrete: Type[ServiceProvider]) -> None:
+        """
+        Validates and registers a service provider class.
+
+        This method ensures that the provided class is valid (inherits from `ServiceProvider`,
+        has a `register` and `boot` method) and registers it in the
+        `_service_providers` dictionary.
+
+        Parameters
+        ----------
+        concrete : ServiceProvider
+            The service provider class to register
+        """
+        pass
+
+    @abstractmethod
+    def getBeforeServiceProviders(self) -> List[Type[ServiceProvider]]:
+        """
+        Retrieve the registered service providers.
+
+        Returns
+        -------
+        list
+            A list of registered service providers
+        """
+        pass
+
+    @abstractmethod
+    def getAfterServiceProviders(self) -> List[Type[ServiceProvider]]:
+        """
+        Retrieve the registered service providers.
+
+        Returns
+        -------
+        list
+            A list of registered service providers
+        """
+        pass
