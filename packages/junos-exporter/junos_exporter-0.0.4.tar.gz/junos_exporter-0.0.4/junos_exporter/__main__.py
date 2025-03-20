60589a029429ed005d09f6346060731329123bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:33 +0000

    Add package junos-exporter/0.0.4/junos_exporter-0.0.4.tar.gz

---
+++ packages/junos-exporter/junos_exporter-0.0.4.tar.gz/junos_exporter-0.0.4/junos_exporter/__main__.py
@@ -0,0 +1,34 @@
+import argparse
+
+import uvicorn
+
+
+def cli() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=9326,
+        help="listening port[default: 9326]",
+    )
+    parser.add_argument(
+        "-w",
+        "--workers",
+        type=int,
+        default=1,
+        help="number of worker processes[default: 1]",
+    )
+
+    args = parser.parse_args()
+    uvicorn.run(
+        "junos_exporter.api:app",
+        host="0.0.0.0",
+        port=args.port,
+        workers=args.workers,
+        log_config="log_config.yml",
+    )
+
+
+if __name__ == "__main__":
+    cli()
