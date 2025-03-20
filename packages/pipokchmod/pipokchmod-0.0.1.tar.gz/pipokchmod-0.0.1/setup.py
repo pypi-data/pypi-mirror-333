afe6e5f9e5f8ff8bb1e45eef37b6753b1f93e236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:05:44 +0000

    Add package pipokchmod/0.0.1/pipokchmod-0.0.1.tar.gz

---
+++ packages/pipokchmod/pipokchmod-0.0.1.tar.gz/pipokchmod-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='pipokchmod', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Nikollai',
+    author_email='nikolai_22_05@mail.ru',
+    description='My Package',
+    packages=setuptools.find_packages(),
+ )
