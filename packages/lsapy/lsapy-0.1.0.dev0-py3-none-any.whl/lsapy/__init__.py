21e425bd710c3aabb7641efd35ab3f79c8bda7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:53 +0000

    Add package lsapy/0.1.0.dev0/lsapy-0.1.0.dev0-py3-none-any.whl

---
+++ packages/lsapy/lsapy-0.1.0.dev0-py3-none-any.whl/lsapy/__init__.py
@@ -0,0 +1,9 @@
+"""A Python package for Land Suitability Analysis."""
+
+from lsapy.lsa import LandSuitability
+from lsapy.criteria import SuitabilityCriteria
+from lsapy.functions import *
+
+__author__ = "Baptiste Hamon"
+__email__ = "baptiste.hamon@pg.canterbury.ac.nz"
+__version__ = "0.1.0-dev0"
