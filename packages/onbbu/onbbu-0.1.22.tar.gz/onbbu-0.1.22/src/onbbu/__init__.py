1bedc3cd69db830394d708d52e7d8fcce246843f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:06:40 +0000

    Add package onbbu/0.1.22/onbbu-0.1.22.tar.gz

---
+++ packages/onbbu/onbbu-0.1.22.tar.gz/onbbu-0.1.22/src/onbbu/__init__.py
@@ -0,0 +1,12 @@
+from os import getcwd
+from sys import path
+
+BASE_DIR: str = getcwd()
+
+path.append(BASE_DIR)
+
+def main():
+    import asyncio
+    from .manager import cli
+
+    asyncio.run(cli())
