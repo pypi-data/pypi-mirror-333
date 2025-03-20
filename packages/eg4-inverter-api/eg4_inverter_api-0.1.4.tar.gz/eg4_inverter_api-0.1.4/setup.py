0bb028afabcc9cd1fa8bab8e34741dc693452ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:07:03 +0000

    Add package eg4-inverter-api/0.1.4/eg4_inverter_api-0.1.4.tar.gz

---
+++ packages/eg4-inverter-api/eg4_inverter_api-0.1.4.tar.gz/eg4_inverter_api-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eg4_inverter_api",
+    version="0.1.4",
+    description="A Python API for interacting with EG4 Inverter systems.",
+    author="Garreth Jeremiah",
+    author_email="twistedroutes76@example.com",
+    url="https://github.com/twistedroutes/eg4_inverter_api",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: APACHE License",
+    ],
+    python_requires=">=3.8",
+)
