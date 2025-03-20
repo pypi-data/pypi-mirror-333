5bdb638c1c45ec385f2d125c90a9d9c57545322c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:04 +0000

    Add package byted-overpass-ad-audit-pms/0.0.1/byted_overpass_ad_audit_pms-0.0.1.tar.gz

---
+++ packages/byted-overpass-ad-audit-pms/byted_overpass_ad_audit_pms-0.0.1.tar.gz/byted_overpass_ad_audit_pms-0.0.1/byted_overpass_ad_audit_pms/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_ad_audit_pms - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-ad-audit-pms/byted_overpass_ad_audit_pms-0.0.1.tar.gz/byted_overpass_ad_audit_pms-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_ad_audit_pms",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_ad_audit_pms",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_ad_audit_pms.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
