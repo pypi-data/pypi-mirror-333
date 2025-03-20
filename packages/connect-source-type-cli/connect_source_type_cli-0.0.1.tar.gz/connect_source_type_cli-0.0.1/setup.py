5bb52e5d7db0db775f1dbe3c34378320ee37742a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:47 +0000

    Add package connect-source-type-cli/0.0.1/connect_source_type_cli-0.0.1.tar.gz

---
+++ packages/connect-source-type-cli/connect_source_type_cli-0.0.1.tar.gz/connect_source_type_cli-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='connect-source-type-cli',
+version='0.0.1',
+author='Tiz Ismyname',
+author_email='jaymz@kommunpoet.se',
+description='More poetry to the people! One cannot live on water but one can live on poetry (if there is also food ... and also water and whatnot).',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
