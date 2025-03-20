b221a6fb2025e4ffdffe69b3f5ffb8b218fc05fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:45 +0000

    Add package json-stream-rs-tokenizer/0.4.28/json_stream_rs_tokenizer-0.4.28.tar.gz

---
+++ packages/json-stream-rs-tokenizer/json_stream_rs_tokenizer-0.4.28.tar.gz/json_stream_rs_tokenizer-0.4.28/tests/test_exceptions.py
@@ -0,0 +1,44 @@
+from io import StringIO
+import re
+
+import pytest
+
+from json_stream_rs_tokenizer import load
+
+
+def test_free_charater():
+    with pytest.raises(
+        ValueError,
+        match=re.escape("Invalid JSON character: 'a' at index 0"),
+    ):
+        list(load(StringIO("a")))
+
+
+def test_letter_in_number():
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            "A number must contain only digits.  Got 'a' at index 4"
+        ),
+    ):
+        list(load(StringIO("[123a]")))
+
+
+def test_invalid_number_starting_with_zero():
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            "A 0 must be followed by a '.' | a 'e'.  Got '1' at index 1"
+        ),
+    ):
+        list(load(StringIO(r"01")))
+
+
+def test_invalid_character_code():
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            "Unterminated unicode literal at end of file at index 5"
+        ),
+    ):
+        list(load(StringIO(r'"\uz"')))
