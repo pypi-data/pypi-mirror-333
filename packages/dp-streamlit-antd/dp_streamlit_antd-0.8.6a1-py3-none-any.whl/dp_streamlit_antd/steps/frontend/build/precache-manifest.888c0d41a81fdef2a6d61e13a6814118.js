ed1fe25dce846ac390d1a3f975f9570332535368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:06:04 +0000

    Add package dp-streamlit-antd/0.8.6a1/dp_streamlit_antd-0.8.6a1-py3-none-any.whl

---
+++ packages/dp-streamlit-antd/dp_streamlit_antd-0.8.6a1-py3-none-any.whl/dp_streamlit_antd/steps/frontend/build/precache-manifest.888c0d41a81fdef2a6d61e13a6814118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59521e886209c9579df0815a2f6ca2b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92eb9c941d8fd7820743",
+    "url": "./static/css/2.15aa6b1e.chunk.css"
+  },
+  {
+    "revision": "759a9cfa750a303fd019",
+    "url": "./static/css/main.22ab991f.chunk.css"
+  },
+  {
+    "revision": "92eb9c941d8fd7820743",
+    "url": "./static/js/2.b7b7b41a.chunk.js"
+  },
+  {
+    "revision": "617a910d62891f85c9de31fc517c5164",
+    "url": "./static/js/2.b7b7b41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759a9cfa750a303fd019",
+    "url": "./static/js/main.37872372.chunk.js"
+  },
+  {
+    "revision": "5da51559676de863d294",
+    "url": "./static/js/runtime-main.7cc49c53.js"
+  }
+]);
