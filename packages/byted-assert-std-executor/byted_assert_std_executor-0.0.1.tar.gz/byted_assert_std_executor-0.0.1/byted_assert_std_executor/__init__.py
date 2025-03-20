e15b236acabbecc319ceb64ebd83c9d90de52596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:25 +0000

    Add package byted-assert-std-executor/0.0.1/byted_assert_std_executor-0.0.1.tar.gz

---
+++ packages/byted-assert-std-executor/byted_assert_std_executor-0.0.1.tar.gz/byted_assert_std_executor-0.0.1/byted_assert_std_executor/__init__.py
@@ -0,0 +1,2 @@
+# byted_assert_std_executor - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-assert-std-executor/byted_assert_std_executor-0.0.1.tar.gz/byted_assert_std_executor-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_assert_std_executor",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_assert_std_executor",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_assert_std_executor.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
