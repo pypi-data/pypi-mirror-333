836369e04b29ee11e3f7dbfd111fd68c0502a206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:29:59 +0000

    Add package home-assistant-eltako-frontend/0.0.14/home_assistant_eltako_frontend-0.0.14.tar.gz

---
+++ packages/home-assistant-eltako-frontend/home_assistant_eltako_frontend-0.0.14.tar.gz/home_assistant_eltako_frontend-0.0.14/__init__.py
@@ -0,0 +1,15 @@
+"""Home Assistant Eltako Frontend."""
+
+from typing import Final
+
+
+def locate_dir() -> str:
+    """Return the location of the frontend files."""
+    return __path__[0]
+
+
+# The webcomponent name that loads the panel (main.ts)
+webcomponent_name: Final = "eltako-frontend"
+
+local_module_url: Final = './assets/index-BRXD8dGz.js'
+module_url: Final = '/eltako/assets/index-BRXD8dGz.js'
