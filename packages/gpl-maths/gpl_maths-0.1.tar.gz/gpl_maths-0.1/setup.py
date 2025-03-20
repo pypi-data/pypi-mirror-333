a3ea0e12b59f15fb38c0aa17a1a896209379c3a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:05:46 +0000

    Add package gpl-maths/0.1/gpl_maths-0.1.tar.gz

---
+++ packages/gpl-maths/gpl_maths-0.1.tar.gz/gpl_maths-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name ="gpl_maths",
+    version= "0.1",
+    author = 'Suresh Maha Rajashekar',
+    packages = find_packages(),
+    install_requires=['numpy'],
+
+)
