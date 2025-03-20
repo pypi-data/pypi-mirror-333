a1519ea6edf2d82ed56caa1a09de11d854132a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:06:12 +0000

    Add package rich-codex/1.2.10/rich_codex-1.2.10.tar.gz

---
+++ packages/rich-codex/rich_codex-1.2.10.tar.gz/rich_codex-1.2.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="rich-codex",
+    install_requires=[
+        "rich>=12.4.3",
+        "rich-click>=1.5",
+        "importlib-metadata; python_version < '3.8'",
+        "pyyaml",
+        "levenshtein>=0.18.1",
+        "jsonschema",
+        "GitPython",
+    ],
+    extras_require={
+        "cairo": "CairoSVG>=2.5.2",
+        "dev": "pre-commit",
+    },
+)
