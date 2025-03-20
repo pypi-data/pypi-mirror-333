0f48f64d12cd1a87815adb9add6fd1a2a2a960d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:23 +0000

    Add package fr-youssef-test/2.0.2/fr_youssef_test-2.0.2.tar.gz

---
+++ packages/fr-youssef-test/fr_youssef_test-2.0.2.tar.gz/fr_youssef_test-2.0.2/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='fr_youssef_test',
+    version='2.0.2',
+    packages=find_packages(),
+
+    # ... (other setup parameters)
+)
