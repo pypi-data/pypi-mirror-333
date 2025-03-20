6dce7f158b8b8d72a619bc9594a6959823d4d830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:29 +0000

    Add package byted-overpass-ecom-governance-bcp-rpc/0.0.1/byted_overpass_ecom_governance_bcp_rpc-0.0.1.tar.gz

---
+++ packages/byted-overpass-ecom-governance-bcp-rpc/byted_overpass_ecom_governance_bcp_rpc-0.0.1.tar.gz/byted_overpass_ecom_governance_bcp_rpc-0.0.1/byted_overpass_ecom_governance_bcp_rpc/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_ecom_governance_bcp_rpc - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-ecom-governance-bcp-rpc/byted_overpass_ecom_governance_bcp_rpc-0.0.1.tar.gz/byted_overpass_ecom_governance_bcp_rpc-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_ecom_governance_bcp_rpc",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_ecom_governance_bcp_rpc",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_ecom_governance_bcp_rpc.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
