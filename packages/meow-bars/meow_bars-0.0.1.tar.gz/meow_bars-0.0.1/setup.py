ff038c1bf534790bfa6b0d47f1c1fa43d747a76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:06:06 +0000

    Add package meow-bars/0.0.1/meow_bars-0.0.1.tar.gz

---
+++ packages/meow-bars/meow_bars-0.0.1.tar.gz/meow_bars-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name='meow_bars',
+    version='0.0.1',
+    author='inspektorbars',
+    author_email='dark6676@gmail.com',
+    description='Scary',
+    packages=setuptools.find_packages(),
+ )
