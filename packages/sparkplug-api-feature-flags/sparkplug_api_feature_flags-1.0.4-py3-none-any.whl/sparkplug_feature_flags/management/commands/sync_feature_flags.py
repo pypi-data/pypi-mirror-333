3711031e9d0bc0f33d91cb92db9b62ab03e77758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:10 +0000

    Add package sparkplug-api-feature-flags/1.0.4/sparkplug_api_feature_flags-1.0.4-py3-none-any.whl

---
+++ packages/sparkplug-api-feature-flags/sparkplug_api_feature_flags-1.0.4-py3-none-any.whl/sparkplug_feature_flags/management/commands/sync_feature_flags.py
@@ -0,0 +1,37 @@
+# django
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+# app
+from ...models import FeatureFlag
+
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = "Sync feature flags"
+    params = None
+
+    def handle(self, *args, **options: str) -> None:  # noqa: ARG002
+        admin_user = User.admins.all().first()
+
+        titles = [
+            item["title"]
+            for item in settings.FEATURE_FLAGS
+        ]
+
+        (
+            FeatureFlag.objects
+            .exclude(title__in=titles)
+            .delete()
+        )
+
+        for item in settings.FEATURE_FLAGS:
+            FeatureFlag.objects.get_or_create(
+                title=item["title"],
+                description=item["description"],
+                enabled=True,
+                creator=admin_user,
+            )
