27da6c89b25ea58f90fef4cc5ccc0a26d59af21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:01 +0000

    Add package fdc-shared-kernel/0.0.83/fdc_shared_kernel-0.0.83.tar.gz

---
+++ packages/fdc-shared-kernel/fdc_shared_kernel-0.0.83.tar.gz/fdc_shared_kernel-0.0.83/shared_kernel/datatype_mappings/connectors_to_system/redshift.py
@@ -0,0 +1,14 @@
+redshift_to_system = {
+    "BIGINT": "integer",
+    "INTEGER": "integer",
+    "REAL": "real",
+    "DECIMAL": "numeric",
+    "DOUBLE PRECISION": "double",
+    "BOOLEAN": "boolean",
+    "VARBYTE": "binary",
+    "VARCHAR": "string",
+    "CHARACTER VARYING": "string",
+    "DATE": "date",
+    "TIMESTAMPTZ": "datetime",
+    "NULL": "null"
+}
