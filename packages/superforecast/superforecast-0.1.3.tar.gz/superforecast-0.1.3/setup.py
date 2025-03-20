d83a56fa98b4dc0fe1e0d47920bedc826a097313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:05:01 +0000

    Add package superforecast/0.1.3/superforecast-0.1.3.tar.gz

---
+++ packages/superforecast/superforecast-0.1.3.tar.gz/superforecast-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="superforecast",
+    version="0.1.3",
+    description="Super Forecast - A modern forecasting API client powered by bio-inspired AI models",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="San Martim",
+    author_email="superforecast@dataspoc.com",
+    license="MIT",
+    packages=["superforecast"],
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": ["superforecast = superforecast.client:main"]
+    },
+    keywords=[
+        "forecast",
+        "forecasting",
+        "AI models",
+        "timeseries",
+        "demand forecasting",
+        "energy forecasting",
+        "financial market forecasting",
+    ],
+)
