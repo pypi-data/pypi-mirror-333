eb3b84918996f08c5684bff4e008bc8c61a68343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:07:23 +0000

    Add package pymsod/0.1.4/pymsod-0.1.4.tar.gz

---
+++ packages/pymsod/pymsod-0.1.4.tar.gz/pymsod-0.1.4/pymsod/server_paths.py
@@ -0,0 +1,42 @@
+from aenum import Enum
+import os
+
+
+class Flag(Enum):
+    """
+    Enum class for possible OneDrive environment variables.
+    """
+
+    _init_ = "value __doc__"
+
+    DEFAULT = "OneDrive", "Default OneDrive environment variable."
+    COMMERCIAL = "OneDriveCommercial", "OneDrive Commercial environment variable."
+    CONSUMER = "OneDriveConsumer", "OneDrive Consumer environment variable."
+
+
+def find(env_vars: list[Flag] = None) -> list[str]:
+    """
+    Search for OneDrive folders across all drives (Windows only).
+
+    Args:
+        env_vars (list[Flag], optional): Provided Parameters for OneDrive, e.g., ONEDRIVE.BASIC, "ONEDRIVE.COMMERCIAL", "ONEDRIVE.CONSUMER" Defaults to None.
+
+    Returns:
+        list[str]: List of OneDrive paths.
+    """
+    onedrive_paths = set()
+
+    if env_vars is None:
+        # 1. Check environment variables (OneDrive, OneDriveCommercial, OneDriveConsumer)
+        env_vars = [
+            Flag.DEFAULT,
+            Flag.COMMERCIAL,
+            Flag.CONSUMER,
+        ]
+
+    for var in env_vars:
+        path = os.getenv(var.value)
+        if path and os.path.isdir(path):
+            onedrive_paths.add(path)
+
+    return list(onedrive_paths)
