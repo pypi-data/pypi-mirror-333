e0133e61e493d7bc1aa4aa88d0ab7997fca9be5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:05:58 +0000

    Add package molodoymalchik/0.0.1/molodoymalchik-0.0.1.tar.gz

---
+++ packages/molodoymalchik/molodoymalchik-0.0.1.tar.gz/molodoymalchik-0.0.1/molodoymalchik/mmm.py
@@ -0,0 +1,2 @@
+def slozh (a, b):
+    return a+b
---
+++ packages/molodoymalchik/molodoymalchik-0.0.1.tar.gz/molodoymalchik-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name='molodoymalchik',
+    version='0.0.1',
+    author='Your Name',
+    author_email='petrkoldyshev@gmail.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
