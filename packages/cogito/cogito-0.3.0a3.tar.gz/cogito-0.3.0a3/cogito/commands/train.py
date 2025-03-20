aaac780c2bf80d6a0c441740337793b23b0db1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:57 +0000

    Add package cogito/0.3.0a3/cogito-0.3.0a3.tar.gz

---
+++ packages/cogito/cogito-0.3.0a3.tar.gz/cogito-0.3.0a3/cogito/commands/train.py
@@ -0,0 +1,34 @@
+import json
+
+import click
+
+from cogito.core.exceptions import ConfigFileNotFoundError
+from cogito.lib.training import run, setup
+
+
+@click.command()
+@click.option("--payload", type=str, required=True, help="The payload for the training")
+@click.pass_obj
+def train(ctx: click.Context, payload: str) -> None:
+    """
+    Run a cogito training with the specified payload, printing the result to stdout.
+
+    Example: python -m cogito.cli train --payload '{"key": "value"}'
+    """
+
+    try:
+        config_path = ctx.get("config_path")
+        payload_data = json.loads(payload)
+
+        setup(config_path)
+        result = run(config_path, payload_data)
+
+        click.echo(result)
+    except ConfigFileNotFoundError:
+        click.echo("No configuration file found. Please initialize the project first.")
+        exit(1)
+    except Exception as e:
+        # print stack trace
+        # traceback.print_exc()
+        click.echo(f"Error: {e}", err=True, color=True)
+        exit(1)
