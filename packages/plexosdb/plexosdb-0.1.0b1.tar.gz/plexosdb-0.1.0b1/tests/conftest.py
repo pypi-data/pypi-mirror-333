8293fd4c243eec2ab86b3cbabe45c7bed612b3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:29 +0000

    Add package plexosdb/0.1.0b1/plexosdb-0.1.0b1.tar.gz

---
+++ packages/plexosdb/plexosdb-0.1.0b1.tar.gz/plexosdb-0.1.0b1/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from loguru import logger
+from _pytest.logging import LogCaptureFixture
+
+DATA_FOLDER = "tests/data"
+
+
+@pytest.fixture
+def data_folder(pytestconfig):
+    return pytestconfig.rootpath.joinpath(DATA_FOLDER)
+
+
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,  # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
