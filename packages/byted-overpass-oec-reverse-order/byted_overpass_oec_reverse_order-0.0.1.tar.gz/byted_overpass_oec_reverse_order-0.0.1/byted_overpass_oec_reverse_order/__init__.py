394fa90664a02c37dcf12cba0e1af65bd2a06c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:30 +0000

    Add package byted-overpass-oec-reverse-order/0.0.1/byted_overpass_oec_reverse_order-0.0.1.tar.gz

---
+++ packages/byted-overpass-oec-reverse-order/byted_overpass_oec_reverse_order-0.0.1.tar.gz/byted_overpass_oec_reverse_order-0.0.1/byted_overpass_oec_reverse_order/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_oec_reverse_order - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-oec-reverse-order/byted_overpass_oec_reverse_order-0.0.1.tar.gz/byted_overpass_oec_reverse_order-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_oec_reverse_order",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_oec_reverse_order",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_oec_reverse_order.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
