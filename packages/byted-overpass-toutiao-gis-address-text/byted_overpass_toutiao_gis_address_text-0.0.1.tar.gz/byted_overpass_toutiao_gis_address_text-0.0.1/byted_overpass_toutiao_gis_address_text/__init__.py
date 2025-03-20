1c5d1e587f76970fcc4b4387f7eb17a90fedd4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:05:40 +0000

    Add package byted-overpass-toutiao-gis-address-text/0.0.1/byted_overpass_toutiao_gis_address_text-0.0.1.tar.gz

---
+++ packages/byted-overpass-toutiao-gis-address-text/byted_overpass_toutiao_gis_address_text-0.0.1.tar.gz/byted_overpass_toutiao_gis_address_text-0.0.1/byted_overpass_toutiao_gis_address_text/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_toutiao_gis_address_text - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-toutiao-gis-address-text/byted_overpass_toutiao_gis_address_text-0.0.1.tar.gz/byted_overpass_toutiao_gis_address_text-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_toutiao_gis_address_text",
+    version="0.0.1",
+    packages=find_packages(),
+    description="This package is a reserved placeholder to prevent dependency confusion attacks and package name squatting.",
+    long_description="This package is registered by ByteDance to mitigate supply chain security risks. It does not contain functional code and is not intended for actual use in software development.",
+    long_description_content_type="text/plain",
+    author="ByteDance Security Team",
+    author_email="security@bytedance.com",
+    url="https://github.com/bytedance/byted_overpass_toutiao_gis_address_text",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Developers",
+        "Development Status :: 7 - Inactive",
+    ],
+)
