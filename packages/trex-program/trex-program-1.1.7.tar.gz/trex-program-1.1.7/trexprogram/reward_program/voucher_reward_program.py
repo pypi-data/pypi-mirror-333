c035e89ee28e97eeeae2393e3fa744ba36a2c256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:32:04 +0000

    Add package trex-program/1.1.7/trex-program-1.1.7.tar.gz

---
+++ packages/trex-program/trex-program-1.1.7.tar.gz/trex-program-1.1.7/trexprogram/reward_program/voucher_reward_program.py
@@ -0,0 +1,30 @@
+'''
+Created on 21 Apr 2021
+
+@author: jacklok
+'''
+
+from trexprogram.reward_program.reward_program_base import VoucherRewardProgramBase,\
+    GiveawayRewardBaseProgram
+from trexconf import program_conf
+
+class VoucherProgram(VoucherRewardProgramBase):
+    
+    def __init__(self, merchant_acct, program_configuration):
+        super(VoucherProgram, self).__init__(merchant_acct, program_configuration)
+        
+        
+class VoucherGiveawayProgram(VoucherProgram, GiveawayRewardBaseProgram):
+    
+    def __init__(self, program_configuration):
+        super(VoucherGiveawayProgram, self).__init__(program_configuration)
+        self.reward_format          = self.get_reward_format()
+        self.giveaway_method        = program_configuration.get('giveaway_method')
+        
+        
+        if self.reward_format!=self.get_reward_format():
+            raise Exception('Invalid program configuration')    
+    
+    def get_reward_format(self):
+        return program_conf.REWARD_FORMAT_VOUCHER        
+    
