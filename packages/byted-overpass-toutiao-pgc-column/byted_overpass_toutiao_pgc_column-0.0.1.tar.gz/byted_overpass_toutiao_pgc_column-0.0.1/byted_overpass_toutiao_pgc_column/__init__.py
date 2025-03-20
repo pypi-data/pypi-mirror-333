684060a76dabf3de869ebb8abf1e29bf0c519b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:28 +0000

    Add package byted-overpass-toutiao-pgc-column/0.0.1/byted_overpass_toutiao_pgc_column-0.0.1.tar.gz

---
+++ packages/byted-overpass-toutiao-pgc-column/byted_overpass_toutiao_pgc_column-0.0.1.tar.gz/byted_overpass_toutiao_pgc_column-0.0.1/byted_overpass_toutiao_pgc_column/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_toutiao_pgc_column - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-toutiao-pgc-column/byted_overpass_toutiao_pgc_column-0.0.1.tar.gz/byted_overpass_toutiao_pgc_column-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_toutiao_pgc_column",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_toutiao_pgc_column",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_toutiao_pgc_column.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
