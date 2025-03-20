987f8efc0d6f66f3012b74ee27f6a8248694f376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:06:20 +0000

    Add package tik-scripts/0.0.7/tik_scripts-0.0.7.tar.gz

---
+++ packages/tik-scripts/tik_scripts-0.0.7.tar.gz/tik_scripts-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+NAME = "tik-scripts"
+VERSION = "0.0.7"
+REQUIRES = [
+    "loguru>=0.5.3",
+    "orjson>=3.5.4"
+]
+LONG_DESCRIPTION = open("README.md", "r").read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    license="MIT",
+    author="TikTuzki",
+    author_email="tiktuzki@gmail.com",
+    packages=find_packages(".", include=["tik_scripts"]),
+    package_dir={"": "."},
+    url="https://github.com/TikTzuki/tik-scripts",
+    keywords=["Tik", "Script", "Utility"],
+    install_requires=REQUIRES,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION
+)
