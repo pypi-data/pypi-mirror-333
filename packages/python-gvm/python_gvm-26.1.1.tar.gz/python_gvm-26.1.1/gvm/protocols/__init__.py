8089cb8d261cc6f1643782cbeadb14c19d5af101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:06:04 +0000

    Add package python-gvm/26.1.1/python_gvm-26.1.1.tar.gz

---
+++ packages/python-gvm/python_gvm-26.1.1.tar.gz/python_gvm-26.1.1/gvm/protocols/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2018-2024 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+"""
+Package for supported Greenbone Protocol versions.
+
+Currently `GMP version 22.04`_, `GMP version 22.05`_
+and `OSP version 1`_ are supported.
+
+.. _GMP version 22.04:
+    https://docs.greenbone.net/API/GMP/gmp-22.04.html
+.. _GMP version 22.05:
+    https://docs.greenbone.net/API/GMP/gmp-22.05.html
+.. _OSP version 1:
+    https://docs.greenbone.net/API/OSP/osp-1.2.html
+"""
