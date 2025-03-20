933f5f6979e873661c04f991196f63d198859cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:07:56 +0000

    Add package flexget/3.15.26/flexget-3.15.26.tar.gz

---
+++ packages/flexget/flexget-3.15.26.tar.gz/flexget-3.15.26/flexget/tests/test_migrate.py
@@ -0,0 +1,25 @@
+import pytest
+
+from .conftest import MockManager
+
+
+class TestMigrate:
+    config = """
+        tasks:
+          test:
+            mock:
+              - {title: 'foobar'}
+            accept_all: yes
+    """
+
+    @pytest.mark.filecopy('db-r1042.sqlite', '__tmp__/upgrade_test.sqlite')
+    def test_upgrade(self, request, tmp_path):
+        db_filename = tmp_path.joinpath('upgrade_test.sqlite')
+        filename = db_filename.as_posix()
+        database_uri = f'sqlite:///{filename}'
+        # This will raise an error if the upgrade wasn't successful
+        mockmanager = MockManager(
+            self.config, request.cls.__name__, db_uri=database_uri, tmp_path=tmp_path
+        )
+        mockmanager.shutdown()
+        # TODO: verify we actually loaded the old config, and didn't just create a new one or something
