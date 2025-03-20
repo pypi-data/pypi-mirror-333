eaabfbf067de5a25848390db9ef738462f471905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:29:44 +0000

    Add package pan-scm-sdk/0.3.19/pan_scm_sdk-0.3.19.tar.gz

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.3.19.tar.gz/pan_scm_sdk-0.3.19/scm/config/deployment/__init__.py
@@ -0,0 +1,6 @@
+# scm/config/deployment/__init__.py
+
+from .remote_networks import RemoteNetworks
+from .service_connections import ServiceConnection
+
+__all__ = ["RemoteNetworks", "ServiceConnection"]
