906a74be02891b4f77833b2aad8b22c3c7ab20d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:06:21 +0000

    Add package rwkv-fla/0.7.202503131137/rwkv_fla-0.7.202503131137-py3-none-any.whl

---
+++ packages/rwkv-fla/rwkv_fla-0.7.202503131137-py3-none-any.whl/rwkvfla/modules/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from rwkvfla.modules.convolution import (ImplicitLongConvolution, LongConvolution,
+                                     ShortConvolution)
+from rwkvfla.modules.fused_bitlinear import BitLinear, FusedBitLinear
+from rwkvfla.modules.fused_cross_entropy import FusedCrossEntropyLoss
+from rwkvfla.modules.fused_kl_div import FusedKLDivLoss
+from rwkvfla.modules.fused_linear_cross_entropy import FusedLinearCrossEntropyLoss
+from rwkvfla.modules.fused_norm_gate import (FusedLayerNormSwishGate,
+                                         FusedLayerNormSwishGateLinear,
+                                         FusedRMSNormSwishGate,
+                                         FusedRMSNormSwishGateLinear)
+from rwkvfla.modules.layernorm import (GroupNorm, GroupNormLinear, LayerNorm,
+                                   LayerNormLinear, RMSNorm, RMSNormLinear)
+from rwkvfla.modules.mlp import GatedMLP
+from rwkvfla.modules.rotary import RotaryEmbedding
+
+__all__ = [
+    'ImplicitLongConvolution', 'LongConvolution', 'ShortConvolution',
+    'BitLinear', 'FusedBitLinear',
+    'FusedCrossEntropyLoss', 'FusedLinearCrossEntropyLoss', 'FusedKLDivLoss',
+    'GroupNorm', 'GroupNormLinear', 'LayerNorm', 'LayerNormLinear', 'RMSNorm', 'RMSNormLinear',
+    'FusedLayerNormSwishGate', 'FusedLayerNormSwishGateLinear', 'FusedRMSNormSwishGate', 'FusedRMSNormSwishGateLinear',
+    'GatedMLP',
+    'RotaryEmbedding'
+]
