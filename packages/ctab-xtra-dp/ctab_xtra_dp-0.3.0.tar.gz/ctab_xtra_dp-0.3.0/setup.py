07561f32ff66cdcd9c3b9c1f13847113c65997ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:29:19 +0000

    Add package ctab-xtra-dp/0.3.0/ctab_xtra_dp-0.3.0.tar.gz

---
+++ packages/ctab-xtra-dp/ctab_xtra_dp-0.3.0.tar.gz/ctab_xtra_dp-0.3.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ctab_xtra_dp",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here, e.g., ["numpy", "requests"]
+    description="A sample Python package",
+    long_description=open("ctab_xtra_dp/README.md").read(),
+    long_description_content_type="text/markdown",
+    author="kem0sabe",
+    author_email="martivl@stud.ntnu.no",
+    url="https://github.com/Kem0sabe/Package_example",  # Update with your GitHub or website
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
