71f9551dda8ca4f8cf467a2f97fd65b9d70c32aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:24 +0000

    Add package faucet-extractor-poc/0.1.2/faucet_extractor_poc-0.1.2.tar.gz

---
+++ packages/faucet-extractor-poc/faucet_extractor_poc-0.1.2.tar.gz/faucet_extractor_poc-0.1.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+    setup(
+        name="faucet_extractor_poc",
+        version="0.1.2",
+        author="Richard Szilagyi",
+        author_email="niagarafinance@proton.me",
+        description="Testnet faucet extractor (Proof of Concept)",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/niagarafinance/faucet-extractor-poc",
+        package_dir={"": "src"},
+        packages=find_packages(
+            where="src", include=["faucet_extractor_poc", "faucet_extractor_poc.*"]
+        ),
+        package_data={"": ["*.py"]},
+        include_package_data=True,
+        install_requires=[
+            "python-dotenv",
+            "requests",
+        ],
+        entry_points={
+            "console_scripts": [
+                "faucet_extractor_poc=faucet_extractor_poc.extract:main",
+            ],
+        },
+        classifiers=[
+            "License :: Other/Proprietary License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+        ],
+        python_requires=">=3.10",
+    )
