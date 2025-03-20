5439f093c437858a08fe4f3747b017e316a2456c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:23 +0000

    Add package coinbase-agentkit/0.1.7.dev202503130/coinbase_agentkit-0.1.7.dev202503130.tar.gz

---
+++ packages/coinbase-agentkit/coinbase_agentkit-0.1.7.dev202503130.tar.gz/coinbase_agentkit-0.1.7.dev202503130/coinbase_agentkit/__init__.py
@@ -0,0 +1,60 @@
+"""Coinbase AgentKit - Framework for enabling AI agents to take actions onchain."""
+
+from .__version__ import __version__
+from .action_providers import (
+    Action,
+    ActionProvider,
+    basename_action_provider,
+    cdp_api_action_provider,
+    cdp_wallet_action_provider,
+    compound_action_provider,
+    create_action,
+    erc20_action_provider,
+    morpho_action_provider,
+    pyth_action_provider,
+    superfluid_action_provider,
+    twitter_action_provider,
+    wallet_action_provider,
+    weth_action_provider,
+    wow_action_provider,
+)
+from .agentkit import AgentKit, AgentKitConfig
+from .wallet_providers import (
+    CdpWalletProvider,
+    CdpWalletProviderConfig,
+    EthAccountWalletProvider,
+    EthAccountWalletProviderConfig,
+    EvmWalletProvider,
+    SmartWalletProvider,
+    SmartWalletProviderConfig,
+    WalletProvider,
+)
+
+__all__ = [
+    "AgentKit",
+    "AgentKitConfig",
+    "Action",
+    "ActionProvider",
+    "create_action",
+    "basename_action_provider",
+    "WalletProvider",
+    "CdpWalletProvider",
+    "CdpWalletProviderConfig",
+    "EvmWalletProvider",
+    "EthAccountWalletProvider",
+    "EthAccountWalletProviderConfig",
+    "SmartWalletProvider",
+    "SmartWalletProviderConfig",
+    "erc20_action_provider",
+    "cdp_api_action_provider",
+    "cdp_wallet_action_provider",
+    "compound_action_provider",
+    "morpho_action_provider",
+    "pyth_action_provider",
+    "superfluid_action_provider",
+    "twitter_action_provider",
+    "wallet_action_provider",
+    "weth_action_provider",
+    "wow_action_provider",
+    "__version__",
+]
