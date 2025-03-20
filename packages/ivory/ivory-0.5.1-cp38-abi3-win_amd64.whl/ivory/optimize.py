ee0328b84a9892c4b824968f4d3061db985d229f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:32:00 +0000

    Add package ivory/0.5.1/ivory-0.5.1-cp38-abi3-win_amd64.whl

---
+++ packages/ivory/ivory-0.5.1-cp38-abi3-win_amd64.whl/ivory/optimize.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from polars.plugins import register_plugin_function
+
+if TYPE_CHECKING:
+    from polars import Expr
+
+    from .typing import IntoExprColumn
+
+
+def linear_fit(x: IntoExprColumn, y: IntoExprColumn) -> Expr:
+    """Perform linear regression on the input series.
+
+    This function fits a linear model to the input series and
+    returns the slope and intercept.
+
+    Args:
+        x (IntoExprColumn): The independent variable.
+        y (IntoExprColumn): The dependent variable.
+
+    Returns:
+        Expr: A polars expression that evaluates to a struct
+        with the slope and intercept.
+    """
+    return register_plugin_function(
+        args=[x, y],
+        plugin_path=Path(__file__).parent,
+        function_name="linear_fit",
+        is_elementwise=False,
+        returns_scalar=True,
+    )
