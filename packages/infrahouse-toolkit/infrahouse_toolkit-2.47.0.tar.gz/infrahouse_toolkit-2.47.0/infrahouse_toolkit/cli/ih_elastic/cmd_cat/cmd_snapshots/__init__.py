964ea735367b19998ec651dfd67e631bb265d0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:21 +0000

    Add package infrahouse-toolkit/2.47.0/infrahouse_toolkit-2.47.0.tar.gz

---
+++ packages/infrahouse-toolkit/infrahouse_toolkit-2.47.0.tar.gz/infrahouse_toolkit-2.47.0/infrahouse_toolkit/cli/ih_elastic/cmd_cat/cmd_snapshots/__init__.py
@@ -0,0 +1,24 @@
+"""
+.. topic:: ``ih-elastic cat snapshots``
+
+    A ``ih-elastic cat snapshots`` subcommand.
+
+    See ``ih-elastic cat snapshots --help`` for more details.
+"""
+
+from logging import getLogger
+
+import click
+from elasticsearch.client import CatClient
+
+LOG = getLogger(__name__)
+
+
+@click.command(name="snapshots")
+@click.pass_context
+def cmd_snapshots(ctx):
+    """
+    Returns all snapshots.
+    """
+    client = CatClient(ctx.obj["es"])
+    LOG.info("\n%s", client.snapshots(repository="_all", format=ctx.obj["format"], v=True))
