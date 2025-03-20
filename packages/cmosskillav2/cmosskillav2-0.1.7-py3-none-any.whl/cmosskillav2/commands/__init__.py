58f607c92cf232e4b7ae82681c9289df1aab4927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:06:57 +0000

    Add package cmosskillav2/0.1.7/cmosskillav2-0.1.7-py3-none-any.whl

---
+++ packages/cmosskillav2/cmosskillav2-0.1.7-py3-none-any.whl/cmosskillav2/commands/__init__.py
@@ -0,0 +1,8 @@
+"""
+Command modules for the CMosSkillAV2 terminal emulator.
+
+This package contains modules for various command types:
+- File operations (cd, ls, mkdir, etc.)
+- Python script execution
+- Package management (pip commands)
+"""
