7298cae6441808972b9f12287d01de2fecb620e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:43 +0000

    Add package semantic-kernel/1.24.1/semantic_kernel-1.24.1.tar.gz

---
+++ packages/semantic-kernel/semantic_kernel-1.24.1.tar.gz/semantic_kernel-1.24.1/semantic_kernel/connectors/memory/pinecone/pinecone_settings.py
@@ -0,0 +1,24 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from typing import ClassVar
+
+from pydantic import SecretStr
+
+from semantic_kernel.kernel_pydantic import KernelBaseSettings
+from semantic_kernel.utils.feature_stage_decorator import experimental
+
+
+@experimental
+class PineconeSettings(KernelBaseSettings):
+    """Pinecone model settings.
+
+    Args:
+    - api_key: SecretStr - Pinecone API key
+        (Env var PINECONE_API_KEY)
+    - namespace: str - Pinecone namespace (optional, default is "")
+    """
+
+    env_prefix: ClassVar[str] = "PINECONE_"
+
+    api_key: SecretStr
+    namespace: str = ""
