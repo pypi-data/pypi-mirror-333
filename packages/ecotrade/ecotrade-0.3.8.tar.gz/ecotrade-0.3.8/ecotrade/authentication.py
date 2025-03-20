12dff17a5fdc31d4a58849ace196fc64b7930571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:05:56 +0000

    Add package ecotrade/0.3.8/ecotrade-0.3.8.tar.gz

---
+++ packages/ecotrade/ecotrade-0.3.8.tar.gz/ecotrade-0.3.8/ecotrade/authentication.py
@@ -0,0 +1,40 @@
+import pyodbc
+import bcrypt
+
+class Auth:
+    _authenticated = False
+    def __init__(self, email: str, password: str):
+        PROD_ETMS_DB =  self.db_conn_string = "DRIVER={SQL Server};SERVER=192.168.5.35\SQLEXPRESS;DATABASE=ETMS_Production;UID=deal;PWD=deal2023!" 
+
+
+        self.email = email
+        self.password = password.encode("utf-8")
+        self.db_conn_string = PROD_ETMS_DB
+
+    def authenticate(self):
+        try:
+            conn = pyodbc.connect(self.db_conn_string)  # Try to connect with the connection string
+            cursor = conn.cursor()
+
+            query = "SELECT password FROM [ETMS_Production].[dbo].[etms_users] WHERE email = ?"
+            cursor.execute(query, (self.email,))
+            result = cursor.fetchone()
+
+            if not result:
+                return "Failed to authenticate: User not found"
+
+            stored_hashed_password = result[0]
+
+            if isinstance(stored_hashed_password, bytes):
+                stored_hashed_password = stored_hashed_password.decode("utf-8")
+
+            if bcrypt.checkpw(self.password, stored_hashed_password.encode("utf-8")):
+                Auth._authenticated = True
+                conn.close()
+                return "Authentication successful"
+            else:
+                conn.close()
+                return "Failed to authenticate: Incorrect password"
+
+        except Exception as e:
+            return f"Error during authentication: {str(e)}"
