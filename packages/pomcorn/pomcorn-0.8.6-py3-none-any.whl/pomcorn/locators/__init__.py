7c5b27e198b4b57b762b86a133fc747f458bfbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:37 +0000

    Add package pomcorn/0.8.6/pomcorn-0.8.6-py3-none-any.whl

---
+++ packages/pomcorn/pomcorn-0.8.6-py3-none-any.whl/pomcorn/locators/__init__.py
@@ -0,0 +1,35 @@
+from pomcorn.locators.base_locators import (
+    Locator,
+    TInitLocator,
+    TLocator,
+    XPathLocator,
+)
+from pomcorn.locators.xpath_locators import (
+    ButtonWithTextLocator,
+    ClassLocator,
+    DataTestIdLocator,
+    ElementWithTextLocator,
+    IdLocator,
+    InputByLabelLocator,
+    NameLocator,
+    PropertyLocator,
+    TagNameLocator,
+    TextAreaByLabelLocator,
+)
+
+__all__ = (
+    "ButtonWithTextLocator",
+    "ClassLocator",
+    "DataTestIdLocator",
+    "ElementWithTextLocator",
+    "IdLocator",
+    "InputByLabelLocator",
+    "Locator",
+    "NameLocator",
+    "PropertyLocator",
+    "TInitLocator",
+    "TLocator",
+    "TagNameLocator",
+    "TextAreaByLabelLocator",
+    "XPathLocator",
+)
