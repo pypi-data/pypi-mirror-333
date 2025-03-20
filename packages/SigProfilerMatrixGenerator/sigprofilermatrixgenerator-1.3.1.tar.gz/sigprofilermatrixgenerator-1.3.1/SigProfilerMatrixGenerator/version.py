b7eb4092e29bd97d2b8a0358dadb86941c69c639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:56 +0000

    Add package SigProfilerMatrixGenerator/1.3.1/sigprofilermatrixgenerator-1.3.1.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/sigprofilermatrixgenerator-1.3.1.tar.gz/sigprofilermatrixgenerator-1.3.1/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.3.1'
+version = '1.3.1'
+Update = 'v1.3.1: Update SV matrix calls to pandas and numpy to use >= 2.0.0 syntax'
+
+	
