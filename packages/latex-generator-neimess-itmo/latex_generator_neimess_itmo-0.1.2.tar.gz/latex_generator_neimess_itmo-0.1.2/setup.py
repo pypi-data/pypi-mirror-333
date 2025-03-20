c34752d579be0c37d7c2df5ac6adc2ab088cb6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:40 +0000

    Add package latex-generator-neimess-itmo/0.1.2/latex_generator_neimess_itmo-0.1.2.tar.gz

---
+++ packages/latex-generator-neimess-itmo/latex_generator_neimess_itmo-0.1.2.tar.gz/latex_generator_neimess_itmo-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latex_generator_neimess_itmo",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Maxim Gavrilenko",
+    author_email="sh0ckerZ7714@gmail.com",
+    description="A simple package to generate LaTeX tables and images",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
