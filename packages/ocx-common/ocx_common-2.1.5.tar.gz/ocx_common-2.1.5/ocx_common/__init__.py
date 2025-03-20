ab1fa49df2c20d444471e96d5835a7a11f689f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:07:56 +0000

    Add package ocx-common/2.1.5/ocx_common-2.1.5.tar.gz

---
+++ packages/ocx-common/ocx_common-2.1.5.tar.gz/ocx_common-2.1.5/ocx_common/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2024. OCX Consortium https://3docx.org. See the LICENSE
+from loguru import logger
+
+__version__ = "2.1.5"
+
+logger.disable("src.ocx_common")
