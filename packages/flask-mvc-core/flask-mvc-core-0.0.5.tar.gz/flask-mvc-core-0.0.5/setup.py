a5497a73c69f268f51eba7ed4c9b2c33a678dbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:04:56 +0000

    Add package flask-mvc-core/0.0.5/flask-mvc-core-0.0.5.tar.gz

---
+++ packages/flask-mvc-core/flask-mvc-core-0.0.5.tar.gz/flask-mvc-core-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-mvc-core',
+    version='0.0.5',
+    description='A simple MVC package for Flask',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Célio Junior',
+    author_email='profissional.celiojunior@gmail.com',
+    url='https://github.com/seu-usuario/flask-mvc-core',
+    packages=find_packages(include=['flask_mvc', 'flask_mvc.*']),
+    include_package_data=True,
+    install_requires=['Flask>=3.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
