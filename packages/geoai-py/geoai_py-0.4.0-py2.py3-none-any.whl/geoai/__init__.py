c7c2d7e56d9eeec20bd685dfbf75237322e03d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:32:03 +0000

    Add package geoai-py/0.4.0/geoai_py-0.4.0-py2.py3-none-any.whl

---
+++ packages/geoai-py/geoai_py-0.4.0-py2.py3-none-any.whl/geoai/__init__.py
@@ -0,0 +1,35 @@
+"""Top-level package for geoai."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = "giswqs@gmail.com"
+__version__ = "0.4.0"
+
+
+import os
+import sys
+
+
+def set_proj_lib_path():
+    """Set the PROJ_LIB environment variable based on the current conda environment."""
+    try:
+        # Get conda environment path
+        conda_env_path = os.environ.get("CONDA_PREFIX") or sys.prefix
+
+        # Set PROJ_LIB environment variable
+        proj_path = os.path.join(conda_env_path, "share", "proj")
+        gdal_path = os.path.join(conda_env_path, "share", "gdal")
+
+        # Check if the directory exists before setting
+        if os.path.exists(proj_path):
+            os.environ["PROJ_LIB"] = proj_path
+        if os.path.exists(gdal_path):
+            os.environ["GDAL_DATA"] = gdal_path
+    except Exception as e:
+        print(e)
+        return
+
+
+if "google.colab" not in sys.modules:
+    set_proj_lib_path()
+
+from .geoai import *
