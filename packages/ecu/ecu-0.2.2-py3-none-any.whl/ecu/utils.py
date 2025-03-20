e709cb9d09667f6a32036812e71223b504e94155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:06:57 +0000

    Add package ecu/0.2.2/ecu-0.2.2-py3-none-any.whl

---
+++ packages/ecu/ecu-0.2.2-py3-none-any.whl/ecu/utils.py
@@ -0,0 +1,25 @@
+import typing
+import contextlib
+
+def ensure(*codes: str) -> typing.ContextManager:
+    '''
+    Ensures to cleanup after a function exit.
+    '''
+
+    cleanup = ''.join('\x1b[' + code for code in codes)
+
+    def decorator(func: typing.Callable):
+        def wrapper(*args, **kwargs):
+            try:
+                print('\x1b7', end = '')
+                return func(*args, **kwargs)
+            
+            finally:
+                # Cleanup
+                print('\x1b8\x1b[J' + cleanup, end = '')
+        
+        return wrapper
+    
+    return decorator
+
+# EOF
