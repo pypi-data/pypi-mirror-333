3f0b8d8bfa4c11eaea437a6d2b4669a33d6ec046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:34 +0000

    Add package draive/0.48.0/draive-0.48.0-py3-none-any.whl

---
+++ packages/draive/draive-0.48.0-py3-none-any.whl/draive/workflow/types.py
@@ -0,0 +1,67 @@
+from collections.abc import Sequence
+from typing import Protocol, runtime_checkable
+
+from draive.lmm import LMMContext
+from draive.multimodal import MultimodalContent
+
+__all__ = [
+    "StageCondition",
+    "StageContextProcessing",
+    "StageMerging",
+    "StageProcessing",
+    "StageResultProcessing",
+    "StageStateProcessing",
+]
+
+
+@runtime_checkable
+class StageProcessing(Protocol):
+    async def __call__(
+        self,
+        *,
+        context: LMMContext,
+        result: MultimodalContent,
+    ) -> tuple[LMMContext, MultimodalContent]: ...
+
+
+@runtime_checkable
+class StageMerging(Protocol):
+    async def __call__(
+        self,
+        *,
+        branches: Sequence[tuple[LMMContext, MultimodalContent] | BaseException],
+    ) -> tuple[LMMContext, MultimodalContent]: ...
+
+
+@runtime_checkable
+class StageCondition(Protocol):
+    async def __call__(
+        self,
+        context: LMMContext,
+        result: MultimodalContent,
+    ) -> bool: ...
+
+
+@runtime_checkable
+class StageResultProcessing(Protocol):
+    async def __call__(
+        self,
+        content: MultimodalContent,
+    ) -> MultimodalContent: ...
+
+
+@runtime_checkable
+class StageStateProcessing(Protocol):
+    async def __call__(
+        self,
+        context: LMMContext,
+        result: MultimodalContent,
+    ) -> None: ...
+
+
+@runtime_checkable
+class StageContextProcessing(Protocol):
+    async def __call__(
+        self,
+        context: LMMContext,
+    ) -> LMMContext: ...
