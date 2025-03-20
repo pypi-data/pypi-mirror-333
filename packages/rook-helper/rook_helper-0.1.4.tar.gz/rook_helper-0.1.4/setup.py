35fb92e35010bf188e1ce02e3b7487598620e2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:02 +0000

    Add package rook-helper/0.1.4/rook_helper-0.1.4.tar.gz

---
+++ packages/rook-helper/rook_helper-0.1.4.tar.gz/rook_helper-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rook_helper',
+    packages=['rook_helper'],
+    version='0.1.4',
+    license='MIT',
+    description='Helper',
+    author='Tomas Rosas',
+    author_email='tomas.rosas@tryrook.io',
+    url='https://bitbucket.org/rook-workspace/rook_helpers/src',
+    keywords=['Rook', 'helper'],
+    install_requires=[],
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.11'
+        ]
+    )
