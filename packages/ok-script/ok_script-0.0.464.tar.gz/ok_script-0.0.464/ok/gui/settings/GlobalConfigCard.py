befb1799e3e13c6fc7db57e9d103253f5f59e9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:30 +0000

    Add package ok-script/0.0.464/ok_script-0.0.464.tar.gz

---
+++ packages/ok-script/ok_script-0.0.464.tar.gz/ok_script-0.0.464/ok/gui/settings/GlobalConfigCard.py
@@ -0,0 +1,13 @@
+from ok import Config
+from ok import ConfigOption
+from ok.gui.tasks.ConfigCard import ConfigCard
+
+
+class GlobalConfigCard(ConfigCard):
+    def __init__(self, config: Config, option: ConfigOption):
+        super().__init__(option.name, config, option.description, option.default_config, option.config_description,
+                         option.config_type, option.icon)
+
+    def reset_clicked(self):
+        self.config.reset_to_default()
+        self.update_config()
