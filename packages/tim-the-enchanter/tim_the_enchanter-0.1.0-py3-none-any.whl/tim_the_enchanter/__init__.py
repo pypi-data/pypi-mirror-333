24e05dda31e85bbd54a05b42c9b727c878cb10bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:06:50 +0000

    Add package tim-the-enchanter/0.1.0/tim_the_enchanter-0.1.0-py3-none-any.whl

---
+++ packages/tim-the-enchanter/tim_the_enchanter-0.1.0-py3-none-any.whl/tim_the_enchanter/__init__.py
@@ -0,0 +1,16 @@
+"""Tim The Enchanter - A lightweight, flexible performance tracking library for Python applications."""
+
+from .tim_the_enchanter import (
+    TimTheEnchanter,
+    TimTheEnchanterReportFormat,
+    TimTheEnchanterTimingEvent,
+    tim_the_enchanter,
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "TimTheEnchanter",
+    "TimTheEnchanterReportFormat",
+    "TimTheEnchanterTimingEvent",
+    "tim_the_enchanter",
+]
