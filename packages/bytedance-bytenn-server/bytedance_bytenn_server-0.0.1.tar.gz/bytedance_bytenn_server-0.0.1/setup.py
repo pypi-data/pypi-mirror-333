650a73da0eeb8e46a55c022e03b4030f97e032d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:01 +0000

    Add package bytedance-bytenn-server/0.0.1/bytedance_bytenn_server-0.0.1.tar.gz

---
+++ packages/bytedance-bytenn-server/bytedance_bytenn_server-0.0.1.tar.gz/bytedance_bytenn_server-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_bytenn_server",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_bytenn_server",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_bytenn_server.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
