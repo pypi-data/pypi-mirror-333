475103642d020093840bd1a6601c1424d94d86b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:07:39 +0000

    Add package SAMBA-ilum/1.0.0.445/samba_ilum-1.0.0.445.tar.gz

---
+++ packages/SAMBA-ilum/samba_ilum-1.0.0.445.tar.gz/samba_ilum-1.0.0.445/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+from pathlib import Path
+from typing import Optional
+import json
+
+setup(
+    name = "SAMBA_ilum",
+    version = "1.0.0.445",
+    entry_points={'console_scripts': ['samba_ilum = samba_ilum:main']},
+    description = "...",
+    author = "Augusto de Lelis Araujo", 
+    author_email = "augusto-lelis@outlook.com",
+    license = "Closed source",
+    install_requires=['matplotlib',
+                      'pyfiglet',
+                      'requests',
+                      'plotly',
+                      'scipy',
+                      'numpy',
+                      'uuid',
+                      'vasprocar'],
+    package_data={"": ['*.dat', '*.png', '*.jpg', '*']},
+)
+
+# python3 -m pip install --upgrade twine
+# python setup.py sdist
+# python -m twine upload dist/*
