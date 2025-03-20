72a8c73d62409bb7c6888dd9170e2f7d8b2846b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:30 +0000

    Add package byted-overpass-ad-creative-zhongdi-demo/0.0.1/byted_overpass_ad_creative_zhongdi_demo-0.0.1.tar.gz

---
+++ packages/byted-overpass-ad-creative-zhongdi-demo/byted_overpass_ad_creative_zhongdi_demo-0.0.1.tar.gz/byted_overpass_ad_creative_zhongdi_demo-0.0.1/byted_overpass_ad_creative_zhongdi_demo/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_ad_creative_zhongdi_demo - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-ad-creative-zhongdi-demo/byted_overpass_ad_creative_zhongdi_demo-0.0.1.tar.gz/byted_overpass_ad_creative_zhongdi_demo-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_ad_creative_zhongdi_demo",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_ad_creative_zhongdi_demo",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_ad_creative_zhongdi_demo.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
