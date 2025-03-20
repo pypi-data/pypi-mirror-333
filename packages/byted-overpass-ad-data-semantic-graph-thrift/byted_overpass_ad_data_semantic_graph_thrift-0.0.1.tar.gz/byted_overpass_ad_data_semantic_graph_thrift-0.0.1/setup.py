6f9d6db5c243e55ab6a027eb8711f87f8cf646ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:27 +0000

    Add package byted-overpass-ad-data-semantic-graph-thrift/0.0.1/byted_overpass_ad_data_semantic_graph_thrift-0.0.1.tar.gz

---
+++ packages/byted-overpass-ad-data-semantic-graph-thrift/byted_overpass_ad_data_semantic_graph_thrift-0.0.1.tar.gz/byted_overpass_ad_data_semantic_graph_thrift-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_ad_data_semantic_graph_thrift",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_ad_data_semantic_graph_thrift",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_ad_data_semantic_graph_thrift.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
