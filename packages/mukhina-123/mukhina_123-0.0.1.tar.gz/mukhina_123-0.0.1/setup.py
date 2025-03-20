c24a09d3a9f22f1cdf76649fb0683b9b84864cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:06:16 +0000

    Add package mukhina-123/0.0.1/mukhina_123-0.0.1.tar.gz

---
+++ packages/mukhina-123/mukhina_123-0.0.1.tar.gz/mukhina_123-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = 'mukhina_123',
+    version = '0.0.1',
+    author = 'Anna',
+    author_email = 'a.mukhina@gmail.com',
+    description = 'My pack',
+    packages = setuptools.find_packages(),    
+    )
