8e67ad6499b593997e7f609c39aa292409704cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:05:48 +0000

    Add package waveprop/0.0.12/waveprop-0.0.12.tar.gz

---
+++ packages/waveprop/waveprop-0.0.12.tar.gz/waveprop-0.0.12/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="waveprop",
+    version="0.0.12",
+    author="Eric Bezzam",
+    author_email="ebezzam@gmail.com",
+    description="Functions and scripts to simulate free-space optical wave propagation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebezzam/waveprop",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "torch>=1.12.1",
+        "torchvision>=0.13.1",
+        "opencv-python",
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "pyffs",
+        "progressbar",
+    ],
+    include_package_data=True,
+)
