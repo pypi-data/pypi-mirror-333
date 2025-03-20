8ae817b4d539da360355b31bedd40ac880d4e425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:06:43 +0000

    Add package bytedance-easy-rc-doc/0.0.1/bytedance_easy_rc_doc-0.0.1.tar.gz

---
+++ packages/bytedance-easy-rc-doc/bytedance_easy_rc_doc-0.0.1.tar.gz/bytedance_easy_rc_doc-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_easy_rc_doc",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_easy_rc_doc",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_easy_rc_doc.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
