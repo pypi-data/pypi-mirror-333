5d5972f2f6d5fd8625ca46e078e7662493003e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:31:22 +0000

    Add package cronpie/0.1.1/cronpie-0.1.1.tar.gz

---
+++ packages/cronpie/cronpie-0.1.1.tar.gz/cronpie-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cronpie',
+    version='0.1.1',
+    packages=find_packages(include=['cronpie', 'cronpie.*']),
+    install_requires=[
+        'schedule==1.1.0'
+    ],
+    author='Marjon Godito',
+    description='A simple cron job scheduler with less boilerplate code',
+)
