edb6e6483b7a8d5509e97dbe64bad8fd3465efc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:05:52 +0000

    Add package bm-configurator/0.0.0/bm_configurator-0.0.0-py3-none-any.whl

---
+++ packages/bm-configurator/bm_configurator-0.0.0-py3-none-any.whl/bm_configurator/__init__.py
@@ -0,0 +1,5 @@
+from .config import BaseModelConfig, HardwareRequirements, DiffusionModelConfig, LLMModelConfig
+from .configurator import ModelConfigurator
+
+__version__ = "0.0.0"
+__all__ = ['BaseModelConfig', 'HardwareRequirements', 'ModelConfigurator', 'DiffusionModelConfig', 'LLMModelConfig']
