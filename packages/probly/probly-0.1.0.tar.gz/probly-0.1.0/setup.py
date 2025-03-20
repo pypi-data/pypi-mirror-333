23af1bdd010eb23d7b0a8fac1e46b68378329111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:06:07 +0000

    Add package probly/0.1.0/probly-0.1.0.tar.gz

---
+++ packages/probly/probly-0.1.0.tar.gz/probly-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="probly",
+    version="0.1.0",
+    author="Paul Hofman et al.",
+    author_email="paul.hofman@ifi.lmu.de",
+    description="Uncertainty Representation and Quantification in Machine Learning",
+    long_description="Uncertainty Representation and Quantification in Machine Learning.",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.10",
+    license="MIT",
+    license_files=["LICENSE"],
+    zip_safe=True
+)
