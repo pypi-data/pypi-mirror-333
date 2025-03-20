8c8a58ef634abd5c4533b80921a4724dd9f0a499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:56 +0000

    Add package vizmath/0.0.47/vizmath-0.0.47.tar.gz

---
+++ packages/vizmath/vizmath-0.0.47.tar.gz/vizmath-0.0.47/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+setup(
+    name="vizmath",
+    version="0.0.47",
+    description="Visualization math toolkit.",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nickgerend/vizmath",
+    author="Nick Gerend",
+    author_email="nickgerend@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(exclude=['vizmath.examples*','vizmath.tests*']),
+    install_requires=["numpy", "scipy", "matplotlib", "pandas"],
+    include_package_data=True,
+    package_data={'': ['data/*.csv']},
+)
---
+++ packages/vizmath/vizmath-0.0.47.tar.gz/vizmath-0.0.47/vizmath/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.47"
+from vizmath.functions import *
+from vizmath.draw import *
+from vizmath.radial_treemap import *
+from vizmath.crystal_bar_chart import *
+from vizmath.beeswarm import *
+from vizmath.quadtile_chart import *
+from vizmath.multichord_diagram import *
+from vizmath.stream_chart import *
