419368a0562ff7bf4115b45eaae1c61b335df3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:30:01 +0000

    Add package gOdoo-cli/0.14.2/godoo_cli-0.14.2-py3-none-any.whl

---
+++ packages/gOdoo-cli/godoo_cli-0.14.2-py3-none-any.whl/godoo_cli/__about__.py
@@ -0,0 +1,11 @@
+"""Version information."""
+
+from typing import Tuple
+
+__version__ = "0.14.2"  # Initial version, will be managed by Hatch
+
+
+def get_version_tuple() -> Tuple[int, int, int]:
+    """Convert the version string into a tuple of integers."""
+    major, minor, patch = map(int, __version__.split("."))
+    return major, minor, patch
