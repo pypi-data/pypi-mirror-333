02ce542e315da203c474e1c1919d64eca248155b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:56 +0000

    Add package pypolymlp/0.9.7/pypolymlp-0.9.7.tar.gz

---
+++ packages/pypolymlp/pypolymlp-0.9.7.tar.gz/pypolymlp-0.9.7/src/pypolymlp/api/run_polymlp.py
@@ -0,0 +1,84 @@
+"""Command lines for developing polynomial MLP from file."""
+
+import argparse
+import signal
+import time
+
+import numpy as np
+
+from pypolymlp.core.utils import print_credit
+from pypolymlp.mlp_dev.pypolymlp import Pypolymlp
+
+
+def run():
+
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--infile",
+        nargs="*",
+        type=str,
+        default=["polymlp.in"],
+        help="Input file name",
+    )
+    parser.add_argument(
+        "--no_sequential",
+        action="store_true",
+        help="Use normal feature calculations",
+    )
+    parser.add_argument(
+        "--learning_curve",
+        action="store_true",
+        help="Learning curve calculations",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=None,
+        help="Batch size of feature calculations",
+    )
+    args = parser.parse_args()
+    np.set_printoptions(legacy="1.21")
+    print_credit()
+
+    verbose = True
+    polymlp = Pypolymlp()
+    polymlp.load_parameter_file(args.infile)
+    polymlp.load_datasets(train_ratio=0.9)
+    if verbose:
+        polymlp.print_params()
+    polymlp.save_params(filename="polymlp_params.yaml")
+
+    if args.learning_curve:
+        tlearn1 = time.time()
+        polymlp.fit_learning_curve(verbose=verbose)
+        polymlp.save_learning_curve(filename="polymlp_learning_curve.dat")
+        tlearn2 = time.time()
+
+    t1 = time.time()
+    polymlp.fit(
+        sequential=not args.no_sequential,
+        batch_size=args.batch_size,
+        verbose=verbose,
+    )
+    # polymlp.save_mlp(filename="polymlp.lammps", yaml=False)
+    polymlp.save_mlp(filename="polymlp.yaml", yaml=True)
+    t2 = time.time()
+    polymlp.estimate_error(log_energy=True, verbose=verbose)
+    t3 = time.time()
+    polymlp.save_errors(filename="polymlp_error.yaml")
+
+    if verbose:
+        print("Regression: best model", flush=True)
+        print("  alpha: ", polymlp.summary.alpha, flush=True)
+        print("elapsed_time:", flush=True)
+        if args.learning_curve:
+            print(
+                "  learning curve:     ",
+                "{:.3f}".format(tlearn2 - tlearn1),
+                "(s)",
+                flush=True,
+            )
+        print("  features, fit:      ", "{:.3f}".format(t2 - t1), "(s)", flush=True)
+        print("  error:              ", "{:.3f}".format(t3 - t2), "(s)", flush=True)
---
+++ packages/pypolymlp/pypolymlp-0.9.7.tar.gz/pypolymlp-0.9.7/src/pypolymlp/mlp_dev/core/features_attr.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+from collections import defaultdict
+
+import numpy as np
+
+from pypolymlp.core.data_format import PolymlpParams
+from pypolymlp.cxx.lib import libmlpcpp
+
+
+def get_features_attr(params: PolymlpParams, element_swap: bool = False):
+
+    params.element_swap = element_swap
+    obj = libmlpcpp.FeaturesAttr(params.as_dict())
+
+    type_pairs = obj.get_type_pairs()
+    atomtype_pair_dict = defaultdict(list)
+    for type1, tps in enumerate(type_pairs):
+        for type2, tp in enumerate(tps):
+            atomtype_pair_dict[tp].append([type1, type2])
+
+    radial_ids = obj.get_radial_ids()
+    tcomb_ids = obj.get_tcomb_ids()
+    polynomial_attr = obj.get_polynomial_ids()
+    if params.model.feature_type == "pair":
+        features_attr = list(zip(radial_ids, tcomb_ids))
+    elif params.model.feature_type == "gtinv":
+        gtinv_ids = obj.get_gtinv_ids()
+        features_attr = list(zip(radial_ids, gtinv_ids, tcomb_ids))
+
+    return features_attr, polynomial_attr, atomtype_pair_dict
+
+
+def write_polymlp_params_yaml(params, filename="polymlp_params.yaml"):
+
+    np.set_printoptions(legacy="1.21")
+    f = open(filename, "w")
+    features_attr, polynomial_attr, atomtype_pair_dict = get_features_attr(params)
+
+    elements = np.array(params.elements)
+    print("radial_params:", file=f)
+    for i, p in enumerate(params.model.pair_params):
+        print("- radial_id: ", i, file=f)
+        print("  params:    ", list(p), file=f)
+        print("", file=f)
+    print("", file=f)
+
+    print("atomtype_pairs:", file=f)
+    for k, v in atomtype_pair_dict.items():
+        print("- pair_id:       ", k, file=f)
+        print("  pair_elements: ", file=f)
+        for v1 in v:
+            print("  - ", list(elements[v1]), file=f)
+        print("", file=f)
+    print("", file=f)
+
+    print("features:", file=f)
+    seq_id = 0
+    if params.model.feature_type == "pair":
+        for i, attr in enumerate(features_attr):
+            print("- id:               ", seq_id, file=f)
+            print("  feature_id:       ", i, file=f)
+            print("  radial_id:        ", attr[0], file=f)
+            print("  atomtype_pair_ids:", attr[1], file=f)
+            print("", file=f)
+            seq_id += 1
+        print("", file=f)
+
+    elif params.model.feature_type == "gtinv":
+        gtinv = params.model.gtinv
+        for i, attr in enumerate(features_attr):
+            print("- id:               ", seq_id, file=f)
+            print("  feature_id:       ", i, file=f)
+            print("  radial_id:        ", attr[0], file=f)
+            print("  gtinv_id:         ", attr[1], file=f)
+            print(
+                "  l_combination:    ",
+                gtinv.l_comb[attr[1]],
+                file=f,
+            )
+            print("  atomtype_pair_ids:", attr[2], file=f)
+            print("", file=f)
+            seq_id += 1
+        print("", file=f)
+
+    if len(polynomial_attr) > 0:
+        print("polynomial_features:", file=f)
+        for i, attr in enumerate(polynomial_attr):
+            print("- id:          ", seq_id, file=f)
+            print("  feature_ids: ", attr, file=f)
+            print("", file=f)
+            seq_id += 1
+
+    f.close()
+    return seq_id
+
+
+def get_num_features(params):
+    features_attr, polynomial_attr, atomtype_pair_dict = get_features_attr(params)
+    n_fearures = len(features_attr) + len(polynomial_attr)
+    return n_fearures
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    from pypolymlp.core.parser_polymlp_params import ParamsParser
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--infile",
+        type=str,
+        default="polymlp.in",
+        help="Input file name",
+    )
+    args = parser.parse_args()
+
+    params_dict = ParamsParser(args.infile).get_params()
+    write_polymlp_params_yaml(params_dict)
