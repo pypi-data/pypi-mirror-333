11e637139da00e4138295aeca2e8b5cabce30dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:07:10 +0000

    Add package dagster-webserver/1.10.5/dagster-webserver-1.10.5.tar.gz

---
+++ packages/dagster-webserver/dagster-webserver-1.10.5.tar.gz/dagster-webserver-1.10.5/dagster_webserver/webapp/build/_next/static/IX5IMj7UqsOOE4RlVK4cy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-32689758ddd6be0b.js"],"/_error":["static/chunks/pages/_error-27536e07fe299122.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
