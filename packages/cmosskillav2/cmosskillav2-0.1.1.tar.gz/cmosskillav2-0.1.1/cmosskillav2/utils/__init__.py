2933ae1e75488bb43d2870982ef2d8f235a0d4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:05:57 +0000

    Add package cmosskillav2/0.1.1/cmosskillav2-0.1.1.tar.gz

---
+++ packages/cmosskillav2/cmosskillav2-0.1.1.tar.gz/cmosskillav2-0.1.1/cmosskillav2/utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+Utility modules for the CMosSkillAV2 terminal emulator.
+
+This package contains utilities for:
+- Color and syntax highlighting
+- Command completion
+- Command history management
+- Help system
+- Configuration management
+"""
