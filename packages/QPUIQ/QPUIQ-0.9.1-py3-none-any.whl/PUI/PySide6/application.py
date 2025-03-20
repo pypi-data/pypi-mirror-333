dd98837f04d00552cf482892355c34aef75bda33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:07:10 +0000

    Add package QPUIQ/0.9.1/QPUIQ-0.9.1-py3-none-any.whl

---
+++ packages/QPUIQ/QPUIQ-0.9.1-py3-none-any.whl/PUI/PySide6/application.py
@@ -0,0 +1,50 @@
+from .. import *
+from .base import *
+
+class Application(QtPUIView):
+    def __init__(self, icon=None):
+        super().__init__()
+        self.ui = None
+        self.icon = icon
+
+    def redraw(self):
+        if self.ui:
+            super().redraw()
+        else:
+            self.sync()
+
+    def update(self, prev=None):
+        if not self.ui:
+            from PySide6 import QtWidgets
+            self.ui = QtWidgets.QApplication([])
+            if self.icon:
+                self.ui.setWindowIcon(QtGui.QIcon(self.icon))
+
+        super().update(prev)
+
+    def addChild(self, idx, child):
+        child.outer.show()
+
+    def removeChild(self, idx, child):
+        child.outer.close()
+
+    def start(self):
+        self.ui.exec()
+
+    def quit(self):
+        self.ui.quit()
+
+def PUIApp(func):
+    def func_wrapper(*args, **kwargs):
+        class PUIAppWrapper(Application):
+            def __init__(self, name):
+                self.name = name
+                super().__init__()
+
+            def content(self):
+                return func(*args, **kwargs)
+
+        ret = PUIAppWrapper(func.__name__)
+        return ret
+
+    return func_wrapper
