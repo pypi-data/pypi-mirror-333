0020b404286fd0f37c187734647cfa2d8b75d789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:04 +0000

    Add package template-tool/0.1/template_tool-0.1.tar.gz

---
+++ packages/template-tool/template_tool-0.1.tar.gz/template_tool-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+name='template_tool',
+version='0.1',
+description='A package for manage python templates',
+author='sk2011se',
+author_email='eatebarisajjad@gmail.com',
+packages=['template_tool', 'template_tool.colorization', 'template_tool.actions'],
+install_requires=[
+'colorama',
+'argparse'
+]
+)
