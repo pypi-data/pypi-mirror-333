9829cab5af6b0d4ba3155c8a5c558ec6163eaefc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:06:28 +0000

    Add package library-manager-ausaf/0.3.0/library_manager_ausaf-0.3.0-py3-none-any.whl

---
+++ packages/library-manager-ausaf/library_manager_ausaf-0.3.0-py3-none-any.whl/library_manager/__init__.py
@@ -0,0 +1,13 @@
+from .library_manager import (
+    load_library,
+    save_library,
+    display_header,
+    add_book,
+    remove_book,
+    search_book,
+    display_books,
+    display_statistics,
+    read_book,
+)
+
+load_library()
