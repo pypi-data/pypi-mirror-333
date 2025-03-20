1313a668fa112703cbe2626b65261a4bc38867f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:10:07 +0000

    Add package SBG-TIR-granules/0.1.2/sbg_tir_granules-0.1.2.tar.gz

---
+++ packages/SBG-TIR-granules/sbg_tir_granules-0.1.2.tar.gz/sbg_tir_granules-0.1.2/tests/test_import_SBG_TIR_granules.py
@@ -0,0 +1,19 @@
+def test_import_SBG_TIR_granules():
+    import SBG_TIR_granules
+
+def test_import_L2T_LSTE():
+    from SBG_TIR_granules import L2TLSTE
+
+def test_import_L2T_STARS():
+    from SBG_TIR_granules import L2TSTARS
+
+## FIXME include L4T ETAUX
+
+def test_import_L4T_JET():
+    from SBG_TIR_granules import L4TJET
+
+def test_import_L4T_ESI():
+    from SBG_TIR_granules import L4TESI
+
+def test_import_L4T_WUE():
+    from SBG_TIR_granules import L4TWUE
