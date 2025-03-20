c04382d6f570cd6c372e6a3eebba54c15ee385ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:31:07 +0000

    Add package docoreai/0.1.0/docoreai-0.1.0.tar.gz

---
+++ packages/docoreai/docoreai-0.1.0.tar.gz/docoreai-0.1.0/docore_ai/__init__.py
@@ -0,0 +1,5 @@
+from .model import intelligence_profiler  # Developer -> from docore_ai import intelligence_profiler
+from .model import normal_prompt
+from .model import normal_prompt_with_intelligence
+
+
