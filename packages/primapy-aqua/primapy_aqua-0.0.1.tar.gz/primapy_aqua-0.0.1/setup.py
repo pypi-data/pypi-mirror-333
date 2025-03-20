2164c473b09744248e2ce90c95da712176e4c909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:27 +0000

    Add package primapy-aqua/0.0.1/primapy_aqua-0.0.1.tar.gz

---
+++ packages/primapy-aqua/primapy_aqua-0.0.1.tar.gz/primapy_aqua-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="primapy-aqua",
+    version="0.0.1",
+    author="Prima",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.10',
+)
