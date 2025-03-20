0d120cf54f621acee76fb4cb05443a66e6369e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:06:18 +0000

    Add package MeUtils/2025.3.13.19.7.25/MeUtils-2025.3.13.19.7.25-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2025.3.13.19.7.25-py3-none-any.whl/examples/_openaisdk/openai_embeddings.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : openai_siliconflow
+# @Time         : 2024/6/26 10:42
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+import os
+
+from meutils.pipe import *
+from openai import OpenAI
+
+client = OpenAI(
+
+    api_key=os.getenv("OPENAI_API_KEY_OPENAI"),
+    # api_key=os.getenv("SILICONFLOW_API_KEY"),
+    # # api_key="sk-gcxjtocodgggxwnhqqsaqznpkpxndktwlgmgkmzljyajjsfp",
+    # base_url="https://api.siliconflow.com/v1",
+
+    # base_url="https://api.bltcy.ai/v1"
+)
+
+model = "BAAI/bge-large-zh-v1.5"
+model = "bge-large-zh-v1.5"
+model = "text-embedding-3-small"
+# model = "text-embedding-ada-002"
+#
+# model = "BAAI/bge-m3"
+
+with timer("bs1"):
+    response = client.embeddings.create(
+        input=["查" * 1000] * 1,
+        model=model
+    )
