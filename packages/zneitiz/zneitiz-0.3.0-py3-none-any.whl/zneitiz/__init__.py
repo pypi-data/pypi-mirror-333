40a3687238f8461f1e9ed87811042e79689ef057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:32 +0000

    Add package zneitiz/0.3.0/zneitiz-0.3.0-py3-none-any.whl

---
+++ packages/zneitiz/zneitiz-0.3.0-py3-none-any.whl/zneitiz/__init__.py
@@ -0,0 +1,20 @@
+from typing import NamedTuple, Literal
+
+from .client import *
+from .image import *
+from ._enums import *
+from ._exceptions import *
+
+__version__: str = '0.3.0'
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+_major, _minor, _micro = (int(i) for i in __version__.split('.'))
+
+version_info: VersionInfo = VersionInfo(major=_major, minor=_minor, micro=_micro, releaselevel='final', serial=0)
