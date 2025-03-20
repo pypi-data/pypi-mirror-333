a64a7ca18d140fa5651206aa726644eec6d0418a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:06:45 +0000

    Add package iprm/0.0.1rc2/iprm-0.0.1rc2-cp312-cp312-win_amd64.whl

---
+++ packages/iprm/iprm-0.0.1rc2-cp312-cp312-win_amd64.whl/iprm/main.py
@@ -0,0 +1,5 @@
+from iprm.cli.app import main as cli_main
+from iprm.cli.app import cmake_main as cli_cmake_main
+from iprm.cli.app import meson_main as cli_meson_main
+from iprm.studio_cxx.app import main as studio_cxx_main
+from iprm.studio_rust.app import main as studio_rust_main
