6f33f346d0a3a32d9340dd3d5c4235f2b485d25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:29:18 +0000

    Add package MapAvro/0.1.5/mapavro-0.1.5-py3-none-any.whl

---
+++ packages/MapAvro/mapavro-0.1.5-py3-none-any.whl/MapAvro/__init__.py
@@ -0,0 +1,5 @@
+from .avroConverter import AvroConverter
+from .utils.maps import B2A_MAP, A2B_MAP
+from .utils.utils import normalizer
+
+__all__ = ["AvroConverter", "B2A_MAP", "A2B_MAP", "normalizer"]
