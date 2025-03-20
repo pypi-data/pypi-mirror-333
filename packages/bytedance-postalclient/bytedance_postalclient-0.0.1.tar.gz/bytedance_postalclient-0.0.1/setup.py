1a6ccd073afb70435b6d33c41da783d9285469ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:06:42 +0000

    Add package bytedance-postalclient/0.0.1/bytedance_postalclient-0.0.1.tar.gz

---
+++ packages/bytedance-postalclient/bytedance_postalclient-0.0.1.tar.gz/bytedance_postalclient-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_postalclient",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_postalclient",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_postalclient.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
