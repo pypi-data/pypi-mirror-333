1c5dba6f5d858ccf7bee575d05547127894e2340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:29:47 +0000

    Add package fixnow/0.1.2/fixnow-0.1.2.tar.gz

---
+++ packages/fixnow/fixnow-0.1.2.tar.gz/fixnow-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fixnow",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "pre-commit",
+        "pylint",
+        "mypy",
+        "pytest",
+        "argparse",
+        "rich",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fixnow=fixnow.main:main",  
+        ]
+    },
+    author="Structax",
+    author_email="your-email@example.com",
+    description="AI-powered Git commit fixer",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Structax/fixnow",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
