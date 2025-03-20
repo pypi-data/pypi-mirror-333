157f095406fd68a620698e6d0b2391fd8ffd8728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:58 +0000

    Add package mlcoder/0.1.1/mlcoder-0.1.1.tar.gz

---
+++ packages/mlcoder/mlcoder-0.1.1.tar.gz/mlcoder-0.1.1/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mlcoder',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    author='Md. Sazzad Hissain Khan',
+    author_email='hissain.khan@gmail.com',
+    description='Get the collection of Python code used frequently in Machine Learning using pip package :).',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hissain/mlcoder',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
