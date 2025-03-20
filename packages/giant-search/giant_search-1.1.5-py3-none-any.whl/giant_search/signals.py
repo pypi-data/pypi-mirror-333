b50508db82923f22a376bc776cb85b4af4a5732e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:35 +0000

    Add package giant-search/1.1.5/giant_search-1.1.5-py3-none-any.whl

---
+++ packages/giant-search/giant_search-1.1.5-py3-none-any.whl/giant_search/signals.py
@@ -0,0 +1,58 @@
+from importlib import import_module
+
+from django.apps import apps
+from django.conf import settings
+from django.db.backends.signals import connection_created
+from django.db.models import QuerySet
+from django.dispatch import receiver
+from watson.search import is_registered
+
+from giant_search.utils import register_for_search
+
+DB_ENGINE = settings.DATABASES['default']['ENGINE']
+
+db_backend = import_module(DB_ENGINE + ".base")
+
+
+@receiver(connection_created, sender=db_backend.DatabaseWrapper)
+def initial_connection_to_db(sender, **kwargs):
+    # Get a list of all models that implement the SearchableMixin
+    for app in apps.all_models.values():
+        for model in app.values():
+            if hasattr(model, "is_searchable"):
+                # We have search_fields, try to register the model.
+                register_kwargs = {"model": model.get_search_queryset()}
+
+                # If the model defines which fields should be searchable,
+                #   pass them to the register() call.
+                try:
+                    search_fields = model.get_search_fields()
+                    if search_fields:
+                        register_kwargs["fields"] = search_fields
+                except AttributeError:
+                    pass
+
+                # Do not register more than once:
+                model_ = register_kwargs['model']
+                if isinstance(model_, QuerySet):
+                    model_ = model_.model
+                if not is_registered(model_):
+                    register_for_search(**register_kwargs)
+
+    # Register Page Titles / PageContents
+    try:
+        from cms.models import Title
+    except ImportError:
+        from cms.models import PageContent
+        from cms.utils.i18n import get_public_languages
+
+        languages = get_public_languages(site_id=settings)
+        model_ = PageContent
+        filters_ = {"language__in": languages}
+    else:
+        model_ = Title
+        filters_ = {"published": True, "publisher_is_draft": False}
+
+    if not is_registered(model_):
+        register_for_search(model_.objects.filter(**filters_))
+
