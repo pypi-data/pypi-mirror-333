942661447e1842e9299df71eb3442a2576c301aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:30:01 +0000

    Add package aurora-mvc/0.9.5/aurora_mvc-0.9.5-py3-none-any.whl

---
+++ packages/aurora-mvc/aurora_mvc-0.9.5-py3-none-any.whl/aurora/connector.py
@@ -0,0 +1,81 @@
+################
+# Dependencies #
+################
+import importlib
+config = importlib.import_module('config')
+
+
+###################
+# Database Engine #
+###################
+debug = getattr(config, "DEBUG")
+db_engine = getattr(config, 'DB_ENGINE') 
+db_engines = ['AuroraSQL']
+
+# Check Database Engine
+if not db_engine in db_engines:
+    error = '''Unsupported Database Engine!\n'''
+    error += f'''Supported Database Engines: {', '.join(db_engines)}'''
+
+    # Check debug mode
+    if debug:
+        # Raise error
+        raise Exception(error)
+
+    else:
+        # Print error
+        print(error)
+        exit()
+
+
+################
+# Database API #
+################
+db_system  = getattr(config, 'DB_SYSTEM')
+db_systems = ['SQLite', 'MySQL', 'Postgres']
+
+# Check Database System
+if db_system in db_systems:
+    # Import the database class
+    # SQLite Database
+    if db_system == 'SQLite':
+        try:
+            import sqlite3 as DatabaseAPI
+            from sqlite3 import Error as DatabaseError
+
+        except:
+            pass
+    
+    # MySQL Database
+    elif db_system == 'MySQL':
+        try:
+            from mysql import connector as DatabaseAPI
+            from mysql.connector import Error as DatabaseError
+
+        except:
+            pass
+    
+    # Postgres Database
+    elif db_system == 'Postgres':
+        try:
+            import psycopg2 as DatabaseAPI
+            import psycopg2.extras as DatabaseDict
+            from psycopg2 import DatabaseError
+
+        except:
+            pass
+
+# Raise error
+else:
+    error = '''Unsupported Database System!\n'''
+    error += f'''Supported Database Systems: {', '.join(db_systems)}'''
+
+    # Check debug mode
+    if debug:
+        # Raise error
+        raise Exception(error)
+
+    else:
+        # Print error
+        print(error)
+        exit()
