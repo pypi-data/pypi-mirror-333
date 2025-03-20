0ce45f03d4b1811c5656828f5d36529e545cdfd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:25 +0000

    Add package piraye/0.6.2/piraye-0.6.2-py3-none-any.whl

---
+++ packages/piraye/piraye-0.6.2-py3-none-any.whl/piraye/__init__.py
@@ -0,0 +1,11 @@
+"""This module includes Normalizer and NormalizerBuilder"""
+from .normalizer import Normalizer
+from .tasks.normalizer.multi_lingual_normalizer import MultiLingualNormalizer
+from .tasks.normalizer.multi_lingual_normalizer_builder import MultiLingualNormalizerBuilder
+from .tasks.normalizer.normalizer_builder import NormalizerBuilder
+from .tasks.tokenizer.nltk_tokenizer import NltkTokenizer
+from .tasks.tokenizer.spacy_tokenizer import SpacyTokenizer
+from .tokenizer import Tokenizer
+
+__all__ = ["Normalizer", "Tokenizer", "NormalizerBuilder", "MultiLingualNormalizer", "MultiLingualNormalizerBuilder",
+           "NltkTokenizer", "SpacyTokenizer"]
