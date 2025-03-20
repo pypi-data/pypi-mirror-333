0c580bc8c985aa0f6faa4bdde65deab0312e09e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:07:16 +0000

    Add package dnasecure/0.0.2/dnasecure-0.0.2.tar.gz

---
+++ packages/dnasecure/dnasecure-0.0.2.tar.gz/dnasecure-0.0.2/dnasecure/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Main entry point for the DNAsecure package when executed as a module.
+"""
+
+import sys
+from dnasecure.cli import main
+
+if __name__ == "__main__":
+    sys.exit(main()) 
