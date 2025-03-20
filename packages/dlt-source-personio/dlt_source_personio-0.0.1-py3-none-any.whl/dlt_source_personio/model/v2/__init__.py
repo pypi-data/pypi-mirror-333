db2d5ce8ffb95e3f7ca0b57e6ed52e7ae4a0de5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:07:39 +0000

    Add package dlt-source-personio/0.0.1/dlt_source_personio-0.0.1-py3-none-any.whl

---
+++ packages/dlt-source-personio/dlt_source_personio-0.0.1-py3-none-any.whl/dlt_source_personio/model/v2/__init__.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class MyBaseModel(BaseModel):
+    pass
+
+
+class MyAuthBaseModel(MyBaseModel):
+    pass
+
+
+class MyPersonBaseModel(MyBaseModel):
+    pass
+
+
+class MyEmploymentBaseModel(MyBaseModel):
+    pass
