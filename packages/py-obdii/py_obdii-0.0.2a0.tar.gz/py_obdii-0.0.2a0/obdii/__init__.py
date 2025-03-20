c569007a5e15daf393df81674211eed0c423f3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:05:58 +0000

    Add package py-obdii/0.0.2a0/py_obdii-0.0.2a0.tar.gz

---
+++ packages/py-obdii/py_obdii-0.0.2a0.tar.gz/py_obdii-0.0.2a0/obdii/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'obdii'
+__author__ = 'PaulMarisOUMary'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2025-present PaulMarisOUMary'
+__version__ = '0.0.2a0'
+
+from .connection import Connection
+from .commands import commands
+from .modes.modeat import at_commands
