ae36c5c611bab8b475d27e3878a66f094323a351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:18 +0000

    Add package nacal/0.2.2/nacal-0.2.2-py3-none-any.whl

---
+++ packages/nacal/nacal-0.2.2-py3-none-any.whl/nacal/__init__.py
@@ -0,0 +1,9 @@
+""" 
+Módulo nacal: Notación Asociativa para un curso de Álgebra Lineal (NAcAL).
+"""
+
+__version__ = "0.2.2"
+__author__  = "Marcos Bujosa"
+__name__    = "nacal"
+
+from .nacal import *
