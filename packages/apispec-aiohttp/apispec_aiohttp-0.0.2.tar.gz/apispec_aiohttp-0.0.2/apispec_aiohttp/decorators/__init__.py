013e24bb440e9f2b278f8272e138deaffea428c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:51 +0000

    Add package apispec-aiohttp/0.0.2/apispec_aiohttp-0.0.2.tar.gz

---
+++ packages/apispec-aiohttp/apispec_aiohttp-0.0.2.tar.gz/apispec_aiohttp-0.0.2/apispec_aiohttp/decorators/__init__.py
@@ -0,0 +1,13 @@
+# ruff: noqa: F401
+from .docs import docs
+from .request import (
+    cookies_schema,
+    form_schema,
+    headers_schema,
+    json_schema,
+    match_info_schema,
+    querystring_schema,
+    request_schema,
+    use_kwargs,
+)
+from .response import marshal_with, response_schema
