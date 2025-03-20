623c56cb141802e880ca095bdf9c0576370e04bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:12 +0000

    Add package ddd-cli/1.3.24/ddd_cli-1.3.24.tar.gz

---
+++ packages/ddd-cli/ddd_cli-1.3.24.tar.gz/ddd_cli-1.3.24/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ddd-cli',
+    version='1.3.24',
+    description='CLI for adding DDD support to Python/Django projects',
+    long_description=long_description,
+    long_description_content_type="text/markdown",      
+    author='Ragnar Bermúdez La O',
+    author_email='ragnarbermudezlao@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,  
+    install_requires=['django', 'jinja2', 'colorama'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ddd = ddd.management.cli:main',  # Registra el comando `ddd`
+        ],
+    },
+)
