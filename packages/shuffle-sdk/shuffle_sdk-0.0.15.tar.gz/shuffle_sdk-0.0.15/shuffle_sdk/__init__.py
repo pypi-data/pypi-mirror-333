0ba129dacfe377f83e6f0c93da617f7f9a6b8d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:06:45 +0000

    Add package shuffle-sdk/0.0.15/shuffle_sdk-0.0.15.tar.gz

---
+++ packages/shuffle-sdk/shuffle_sdk-0.0.15.tar.gz/shuffle_sdk-0.0.15/shuffle_sdk/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+from .shuffle_sdk import AppBase  # Adjust the import path as necessary
+
+__all__ = ["AppBase", "csv_parse"]  # Define the public API of your package
+
+#print("Initializing shuffle_sdk package...")
+__version__ = '0.0.15'
+
