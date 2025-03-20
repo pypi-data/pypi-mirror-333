30cdfeb547c73003d79990db25a0e5b333792656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:31 +0000

    Add package account-codes-jp/0.3.4/account_codes_jp-0.3.4.tar.gz

---
+++ packages/account-codes-jp/account_codes_jp-0.3.4.tar.gz/account_codes_jp-0.3.4/src/account_codes_jp/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.3.4"
+
+from ._blue_return import get_blue_return_accounts
+from ._common import (
+    SUNDRY,
+    Account,
+    AccountSundry,
+    AccountType,
+    get_account_ambiguous_factory,
+    get_account_type_factory,
+)
+from ._edinet import ETaxAccountProtocol, Industry, get_edinet_accounts
+
+__all__ = [
+    "SUNDRY",
+    "Account",
+    "AccountSundry",
+    "AccountType",
+    "ETaxAccountProtocol",
+    "Industry",
+    "get_account_ambiguous_factory",
+    "get_account_type_factory",
+    "get_blue_return_accounts",
+    "get_edinet_accounts",
+]
