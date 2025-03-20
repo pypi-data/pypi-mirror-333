228b6887882c2707c555f10fa20aa1e8f71fec18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:40 +0000

    Add package vidcrawler/1.0.47/vidcrawler-1.0.47-py2.py3-none-any.whl

---
+++ packages/vidcrawler/vidcrawler-1.0.47-py2.py3-none-any.whl/vidcrawler/run_tests.py
@@ -0,0 +1,16 @@
+"""
+Trampoline to get tests.
+"""
+
+
+def main() -> None:
+    """Command hooking point for vidcrawler_tests"""
+    import os  # pylint: disable=import-outside-toplevel
+    import sys  # pylint: disable=import-outside-toplevel
+
+    os.chdir(os.path.dirname(__file__))
+    sys.exit(os.system("pytest --verbose testing"))
+
+
+if __name__ == "__main__":
+    main()
