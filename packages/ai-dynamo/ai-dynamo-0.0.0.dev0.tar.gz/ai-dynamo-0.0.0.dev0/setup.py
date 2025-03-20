6d4e633bab337dbe666f4e26b5aa9d7cb837cf18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:26 +0000

    Add package ai-dynamo/0.0.0.dev0/ai-dynamo-0.0.0.dev0.tar.gz

---
+++ packages/ai-dynamo/ai-dynamo-0.0.0.dev0.tar.gz/ai-dynamo-0.0.0.dev0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="ai-dynamo",
+    version="0.0.0.dev0",
+    description="Reserved package for upcoming project",
+    long_description="",
+    author="Nvidia",
+    packages=[], 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.10',
+)
