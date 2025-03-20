f3be089ad8da6e315b679e487e6803526733156b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:19 +0000

    Add package foundationallm/0.9.7b105/foundationallm-0.9.7b105-py3-none-any.whl

---
+++ packages/foundationallm/foundationallm-0.9.7b105-py3-none-any.whl/foundationallm/models/messages/message_history_item.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+from foundationallm.models.orchestration import ContentArtifact
+
+class MessageHistoryItem(BaseModel):
+    """
+        Represents an historic message sender and text item.
+
+        Parameters
+        ----------
+        sender : str
+            The sender of the message (e.g., "Agent", "User")
+        text : str
+            The message text.
+    """
+    sender: str
+    text: str
+    content_artifacts : Optional[List[ContentArtifact]] = []
