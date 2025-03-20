77fb91d420220681d02d8c8e7abd470053d05936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:06:56 +0000

    Add package RockyRoad/0.0.461/RockyRoad-0.0.461-py3-none-any.whl

---
+++ packages/RockyRoad/RockyRoad-0.0.461-py3-none-any.whl/rockyroad/modules/portal_configurations.py
@@ -0,0 +1,26 @@
+from .module_imports import key
+from uplink.retry.when import status_5xx
+from uplink import (
+    Consumer,
+    get as http_get,
+    returns,
+    headers,
+    retry,
+)
+
+
+@headers({"Ocp-Apim-Subscription-Key": key})
+@retry(max_attempts=20, when=status_5xx())
+class _Portal_Configurations(Consumer):
+    """Inteface to portal configurations resource for the RockyRoad API."""
+
+    def __init__(self, Resource, *args, **kw):
+        self._base_url = Resource._base_url
+        super().__init__(base_url=Resource._base_url, *args, **kw)
+
+    @returns.json
+    @http_get("portal/configurations/file-resources")
+    def get_file_resources(
+        self,
+    ):
+        """This call will return portal configurations for file resources."""
