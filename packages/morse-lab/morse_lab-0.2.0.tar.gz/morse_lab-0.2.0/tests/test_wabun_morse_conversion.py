ed4ede6f94314351d680a76a3c176e629dcadff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:50 +0000

    Add package morse-lab/0.2.0/morse_lab-0.2.0.tar.gz

---
+++ packages/morse-lab/morse_lab-0.2.0.tar.gz/morse_lab-0.2.0/tests/test_wabun_morse_conversion.py
@@ -0,0 +1,8 @@
+import pytest
+from morse_lab import text_to_morse_code, morse_code_to_text, WABUN_MORSE_CODE_DICT
+
+def test_wabun_morse_conversion():
+    original_text = "カタカナ"  
+    morse = text_to_morse_code(original_text, WABUN_MORSE_CODE_DICT)  
+    decoded_text = morse_code_to_text(morse, WABUN_MORSE_CODE_DICT) 
+    assert decoded_text == original_text
