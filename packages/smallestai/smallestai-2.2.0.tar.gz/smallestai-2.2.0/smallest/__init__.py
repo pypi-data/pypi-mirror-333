f3aba10a62acad3666d66ca04a7f399adf2cad26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:05:55 +0000

    Add package smallestai/2.2.0/smallestai-2.2.0.tar.gz

---
+++ packages/smallestai/smallestai-2.2.0.tar.gz/smallestai-2.2.0/smallest/__init__.py
@@ -0,0 +1,5 @@
+from smallest.tts import Smallest
+from smallest.async_tts import AsyncSmallest
+from smallest.stream_tts import TextToAudioStream
+
+__all__ = ["Smallest", "AsyncSmallest", "TextToAudioStream"]
