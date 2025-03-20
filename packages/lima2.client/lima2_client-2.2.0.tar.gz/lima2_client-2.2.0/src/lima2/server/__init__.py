002bfce08f50fdf923f0cfdf391bdefae0778a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:41 +0000

    Add package lima2.client/2.2.0/lima2_client-2.2.0.tar.gz

---
+++ packages/lima2.client/lima2_client-2.2.0.tar.gz/lima2_client-2.2.0/src/lima2/server/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Lima2 project
+#
+# Copyright (c) 2020-2024 Beamline Control Unit, ESRF
+# Distributed under the MIT license. See LICENSE for more info.
+
+from lima2.server.control import Control
+from lima2.server.receiver import Receiver
