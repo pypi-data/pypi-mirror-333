4ee0927109e350d0a6cc79e96a73f5f077bca8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:06:53 +0000

    Add package brython-easier/0.1.4/brython_easier-0.1.4.tar.gz

---
+++ packages/brython-easier/brython_easier-0.1.4.tar.gz/brython_easier-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='brython_easier',
+    version='0.1.4',
+    packages=[''],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MatveyKostis/brython_easier',
+    license='GPL-3.0 license',
+    author='MatveyKostis',
+    author_email='matveykostiapk@gmail.com',
+    description='An easier brython library '
+)
+
