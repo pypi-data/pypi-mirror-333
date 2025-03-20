0f545b03b74ecefafa714fc3b726e111ec7a3516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:25 +0000

    Add package haliax/1.4.dev340/haliax-1.4.dev340.tar.gz

---
+++ packages/haliax/haliax-1.4.dev340.tar.gz/haliax-1.4.dev340/src/haliax/nn/linear.py
@@ -0,0 +1,83 @@
+import math
+from typing import Optional
+
+import equinox as eqx
+from jax.random import PRNGKey
+
+import haliax as hax
+
+from ..axis import AxisSpec
+from ..core import NamedArray
+from ..jax_utils import named_call
+from ..quantization import DotGeneralOp
+
+
+class Linear(eqx.Module):
+    """A named Linear layer. This module allows you to specify multiple named axes for both input
+    and output, which is occasionally useful."""
+
+    weight: NamedArray
+    bias: Optional[NamedArray]
+
+    In: AxisSpec = eqx.static_field()
+    Out: AxisSpec = eqx.static_field()
+    dot_general: DotGeneralOp = eqx.field(default_factory=DotGeneralOp.default)
+
+    @staticmethod
+    def init(
+        In: AxisSpec,
+        Out: AxisSpec,
+        *,
+        key: PRNGKey,
+        use_bias: bool = True,
+        out_first: bool = True,
+        dot_general: Optional[DotGeneralOp] = None,
+        init_scale: float = 1.0,
+    ) -> "Linear":
+        """
+        Args:
+            In: AxisSpec: The input axis spec
+            Out: AxisSpec: The output axis spec
+            key: PRNGKeyArray: The PRNG key to use for initialization
+            use_bias: bool: Whether to use a bias term
+            out_first: bool: Whether to put output axes first in the weight matrix. out_first is how PyTorch does it.
+            dot_general: Callable: The dot_general function to use. Defaults to jax.lax.dot_general.
+            init_scale: float: The scale to use for initialization. We scale init by 1/sqrt(Input.size)*init_scale
+        """
+        joint_spec = hax.concat_axis_specs(Out, In) if out_first else hax.concat_axis_specs(In, Out)
+        input_size = hax.axis_size(In)
+        weight = hax.random.truncated_normal(key, joint_spec, -3, 3) * (init_scale / math.sqrt(input_size))
+        bias = hax.zeros(Out) if use_bias else None
+
+        if dot_general is None:
+            dot_general = DotGeneralOp.default()
+
+        return Linear(weight, bias, In, Out, dot_general=dot_general)
+
+    @named_call
+    def __call__(self, inputs, *, key: Optional[PRNGKey] = None):
+        """
+        Args:
+            inputs (NamedArray): Input array
+            key: Not used, but there for compat with other modules
+        """
+        del key
+        q = inputs.dot(self.weight, axis=self.In, dot_general=self.dot_general)
+        q = hax.auto_sharded(q)
+
+        if self.bias is not None:
+            q = q + self.bias
+            q = hax.auto_sharded(q)
+
+        return q
+
+    @property
+    def _out_first(self):
+        """
+        Returns: bool: Whether the output axes are first in the weight matrix
+        """
+        # We do it this way because of scan layers
+        if isinstance(self.Out, hax.Axis):
+            return self.weight.axes[-1] != self.Out
+        else:
+            return self.weight.axes[-len(self.Out) :] != self.Out
---
+++ packages/haliax/haliax-1.4.dev340.tar.gz/haliax-1.4.dev340/src/haliax/nn/normalization.py
@@ -0,0 +1,87 @@
+from typing import Optional, TypeVar
+
+import equinox as eqx
+from jax import nn as jnn
+from jax import numpy as jnp
+
+import haliax
+import haliax as hax
+
+from ..axis import AxisSelection, AxisSpec
+from ..core import NamedArray
+from ..types import Scalar
+from ..wrap import unwrap_namedarrays, wrap_axiswise_call, wrap_reduction_call
+
+
+A = TypeVar("A", Scalar, NamedArray, jnp.ndarray)
+
+
+class LayerNorm(eqx.Module):
+    r"""
+    Normalises the input along the specified axis (or axes), using the mean and variance of the
+    input along that axis.
+    """
+    axis: AxisSpec = eqx.static_field()
+    weight: Optional[NamedArray]
+    bias: Optional[NamedArray]
+
+    eps: float = eqx.static_field(default=1e-5)
+
+    @staticmethod
+    def init(axis: AxisSpec, eps: float = 1e-5, use_weight: bool = True, use_bias: bool = True):
+        if use_weight:
+            weight = hax.ones(axis)
+        else:
+            weight = None
+        if use_bias:
+            bias = hax.zeros(axis)
+        else:
+            bias = None
+
+        return LayerNorm(axis, weight, bias, eps)
+
+    def __call__(self, x: NamedArray) -> NamedArray:
+        mean = x.mean(self.axis)
+        var = x.var(self.axis)
+        inv = hax.rsqrt(var + self.eps)
+        out = (x - mean) * inv
+
+        if self.weight is not None:
+            out = self.weight * out
+        if self.bias is not None:
+            out = out + self.bias
+        return out
+
+
+def logsumexp(a: A, axis: Optional[AxisSelection] = None) -> A:
+    # TODO: logsumexp indirectly supports where via `b`. we should support it directly
+    return wrap_reduction_call(jnn.logsumexp, a, axis=axis, single_axis_only=False, supports_where=False)
+
+
+# TODO: support where in softmax, etc
+
+
+def softmax(a: A, axis: Optional[AxisSelection] = None) -> A:
+    return wrap_axiswise_call(jnn.softmax, a, axis=axis, single_axis_only=False)
+
+
+def log_softmax(a: A, axis: Optional[AxisSelection] = None) -> A:
+    return wrap_axiswise_call(jnn.log_softmax, a, axis=axis, single_axis_only=False)
+
+
+def standardize(
+    x: NamedArray,
+    axis: AxisSpec,
+    *,
+    mean: Optional[NamedArray] = None,
+    variance: Optional[NamedArray] = None,
+    epsilon: float = 1e-5,
+    where: Optional[NamedArray] = None,
+) -> NamedArray:
+    """Analogous to [jax.nn.standardize][], but with support for NamedArrays."""
+    x, mean, variance, where = haliax.broadcast_arrays(x, mean, variance, where)  # type: ignore
+    raw_x, mean, variance, where = unwrap_namedarrays(x, mean, variance, where)
+    axis_indices = x._lookup_indices(axis)
+
+    plain = jnn.standardize(raw_x, axis_indices, mean=mean, variance=variance, epsilon=epsilon, where=where)
+    return NamedArray(plain, x.axes)
