2dc0add7f83dc247927da3981f4990cdca0ce5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:17 +0000

    Add package pw-signal-processor/0.1.1/pw_signal_processor-0.1.1.tar.gz

---
+++ packages/pw-signal-processor/pw_signal_processor-0.1.1.tar.gz/pw_signal_processor-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="pw-signal-processor",           
+    version="0.1.1",
+    author="PinkWink",
+    author_email="pinkwink@pinklab.art",
+    description="A simple library for moving average and 1st order low-pass filter",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pinklab-art/signal_processor", 
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
