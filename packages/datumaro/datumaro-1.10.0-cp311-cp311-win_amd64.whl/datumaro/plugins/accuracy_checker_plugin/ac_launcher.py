f5e7814c3feb425361d56ae18f2ea65e0b65b1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:06:42 +0000

    Add package datumaro/1.10.0/datumaro-1.10.0-cp311-cp311-win_amd64.whl

---
+++ packages/datumaro/datumaro-1.10.0-cp311-cp311-win_amd64.whl/datumaro/plugins/accuracy_checker_plugin/ac_launcher.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+import os.path as osp
+
+import yaml
+
+from datumaro.components.cli_plugin import CliPlugin
+from datumaro.components.launcher import Launcher
+from datumaro.components.lazy_plugin import extra_deps
+
+from .details.ac import GenericAcLauncher as _GenericAcLauncher
+
+
+@extra_deps("tensorflow", "openvino.tools.accuracy_checker")
+class AcLauncher(Launcher, CliPlugin):
+    """
+    Generic model launcher with Accuracy Checker backend.
+    """
+
+    @classmethod
+    def build_cmdline_parser(cls, **kwargs):
+        parser = super().build_cmdline_parser(**kwargs)
+        parser.add_argument(
+            "-c",
+            "--config",
+            type=osp.abspath,
+            required=True,
+            help="Path to the launcher configuration file (.yml)",
+        )
+        return parser
+
+    def __init__(self, config, model_dir=None):
+        model_dir = model_dir or ""
+        with open(osp.join(model_dir, config), "r", encoding="utf-8") as f:
+            config = yaml.safe_load(f)
+        self._launcher = _GenericAcLauncher.from_config(config)
+
+    def launch(self, inputs):
+        return self._launcher.launch(inputs)
+
+    def categories(self):
+        return self._launcher.categories()
