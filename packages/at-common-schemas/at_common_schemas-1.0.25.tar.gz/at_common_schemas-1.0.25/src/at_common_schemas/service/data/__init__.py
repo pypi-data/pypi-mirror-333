02b2c8350a3f0b63eeab6863200e0f1b3bc6f057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:05:52 +0000

    Add package at-common-schemas/1.0.25/at_common_schemas-1.0.25.tar.gz

---
+++ packages/at-common-schemas/at_common_schemas-1.0.25.tar.gz/at_common_schemas-1.0.25/src/at_common_schemas/service/data/__init__.py
@@ -0,0 +1,78 @@
+from .calendar import (
+    CalendarEarningsListRequest,
+    CalendarEarningsListResponse,
+    CalendarDividendsListRequest,
+    CalendarDividendsListResponse,
+    CalendarSplitsListRequest,
+    CalendarSplitsListResponse,
+)
+
+
+from .news import (
+    NewsLatestGeneralNewsListRequest,
+    NewsLatestGeneralNewsListResponse,
+    NewsLatestStockNewsListRequest,
+    NewsLatestStockNewsListResponse,
+    NewsLatestCryptoNewsListRequest,
+    NewsLatestCryptoNewsListResponse,
+    NewsLatestForexNewsListRequest, 
+    NewsLatestForexNewsListResponse,
+    NewsStockSearchRequest,
+    NewsStockSearchResponse
+)
+
+from .directory import (
+    DirectorySymbolListRequest,
+    DirectorySymbolListResponse,
+    DirectoryExchangeListRequest,
+    DirectoryExchangeListResponse,
+    DirectorySectorListRequest,
+    DirectorySectorListResponse,
+    DirectoryIndustryListRequest,
+    DirectoryIndustryListResponse,
+    DirectoryCountryListRequest,
+    DirectoryCountryListResponse,
+)
+
+from .quote import (
+    QuoteGetRequest,
+    QuoteGetResponse,
+)
+
+__all__ = [
+    # Calendar
+    "CalendarEarningsListRequest",
+    "CalendarEarningsListResponse",
+    "CalendarDividendsListRequest",
+    "CalendarDividendsListResponse",
+    "CalendarSplitsListRequest",
+    "CalendarSplitsListResponse",
+
+    # News
+    "NewsLatestGeneralNewsListRequest",
+    "NewsLatestGeneralNewsListResponse",
+    "NewsLatestStockNewsListRequest",
+    "NewsLatestStockNewsListResponse",
+    "NewsLatestCryptoNewsListRequest",
+    "NewsLatestCryptoNewsListResponse",
+    "NewsLatestForexNewsListRequest",
+    "NewsLatestForexNewsListResponse",
+    "NewsStockSearchRequest",
+    "NewsStockSearchResponse",
+
+    # Directory
+    "DirectorySymbolListRequest",
+    "DirectorySymbolListResponse",
+    "DirectoryExchangeListRequest",
+    "DirectoryExchangeListResponse",
+    "DirectorySectorListRequest",
+    "DirectorySectorListResponse",
+    "DirectoryIndustryListRequest",
+    "DirectoryIndustryListResponse",
+    "DirectoryCountryListRequest",
+    "DirectoryCountryListResponse",
+
+    # Quote
+    "QuoteGetRequest",
+    "QuoteGetResponse",
+]
