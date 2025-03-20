a29fd93111f74cf03847660e25f766767c539055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:18 +0000

    Add package automyte/0.1.12/automyte-0.1.12-py3-none-any.whl

---
+++ packages/automyte/automyte-0.1.12-py3-none-any.whl/automyte/__init__.py
@@ -0,0 +1,9 @@
+from automyte.automaton import *
+from automyte.config import *
+from automyte.discovery import *
+from automyte.history import *
+from automyte.project import *
+from automyte.tasks import *
+from automyte.vcs import *
+
+# TODO: Maybe define actual exports in __all__ ?
