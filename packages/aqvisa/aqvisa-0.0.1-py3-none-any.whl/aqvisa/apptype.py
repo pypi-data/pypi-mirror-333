fb4c6e463ca10b646941a4b588c2e9245941a53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:06:24 +0000

    Add package aqvisa/0.0.1/aqvisa-0.0.1-py3-none-any.whl

---
+++ packages/aqvisa/aqvisa-0.0.1-py3-none-any.whl/aqvisa/apptype.py
@@ -0,0 +1,23 @@
+"""
+This module contains the application type for the AqVISA library.
+"""
+
+from enum import IntEnum
+
+
+class AppType(IntEnum):
+    """
+    This enum contains the application type for the AqVISA library.
+    """
+
+    # TravelLogic Application
+    TRAVELLOGIC = 0
+
+    # BusFinder & Logic Analyzer Application
+    BUSFINDER_LOGICANALYZER = 1
+
+    # TravelBus Application
+    TRAVELBUS = 2
+
+    # Mixed Signal Oscilloscope Application
+    MIXEDSIGNALOSCILLOSCOPE = 3
