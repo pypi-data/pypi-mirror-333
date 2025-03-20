864387e4dd4bdb7563f59a31f1e9903a1052069d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:48 +0000

    Add package ctab-xtra-dp/0.3.1/ctab_xtra_dp-0.3.1.tar.gz

---
+++ packages/ctab-xtra-dp/ctab_xtra_dp-0.3.1.tar.gz/ctab_xtra_dp-0.3.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+folder = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(folder, "README.md") 
+
+
+try:
+    with open(readme_path) as f:
+        long_description = f.read()
+except FileNotFoundError:
+    print(f"README not found at {readme_path}!")
+    long_description = "Description not available"
+
+
+setup(
+    name="ctab_xtra_dp",
+    version="0.3.1",
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here, e.g., ["numpy", "requests"]
+    description="A sample Python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="kem0sabe",
+    author_email="martivl@stud.ntnu.no",
+    url="https://github.com/Kem0sabe/Package_example",  # Update with your GitHub or website
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
