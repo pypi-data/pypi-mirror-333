51d17beb1141d8b45ddf4235aab24d25458595a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:26 +0000

    Add package tach/0.28.1/tach-0.28.1-cp37-abi3-win32.whl

---
+++ packages/tach/tach-0.28.1-cp37-abi3-win32.whl/tach/parsing/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from tach.parsing.config import (
+    dump_project_config_to_toml,
+    extend_and_validate,
+    parse_project_config,
+)
+
+__all__ = ["parse_project_config", "dump_project_config_to_toml", "extend_and_validate"]
