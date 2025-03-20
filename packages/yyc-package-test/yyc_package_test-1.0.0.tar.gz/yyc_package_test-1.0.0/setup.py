d29027f9d559149d31d6293ac542faa8af0fd8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:29:21 +0000

    Add package yyc-package-test/1.0.0/yyc_package_test-1.0.0.tar.gz

---
+++ packages/yyc-package-test/yyc_package_test-1.0.0.tar.gz/yyc_package_test-1.0.0/setup.py
@@ -0,0 +1,11 @@
+# from distutils.core import setup
+from setuptools import setup
+
+# 读取文件内容
+def readme_file():
+    with open("README.rst", encoding='utf-8') as rf:
+        return rf.read()
+
+setup(name="yyc_package_test", version="1.0.0", description="this is yyc's first package",
+      packages=["yyc_package"], py_modules=["Tool"], author="yyc",
+      long_description=readme_file())
