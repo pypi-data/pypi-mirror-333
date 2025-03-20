5c10fbb36c7cfb743fe79f777d81a561fe729dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:21 +0000

    Add package pyneuphonic/1.5.12/pyneuphonic-1.5.12.tar.gz

---
+++ packages/pyneuphonic/pyneuphonic-1.5.12.tar.gz/pyneuphonic-1.5.12/pyneuphonic/__init__.py
@@ -0,0 +1,5 @@
+from pyneuphonic.agents import Agent
+from pyneuphonic.client import Neuphonic
+from pyneuphonic.models import TTSConfig, WebsocketEvents, AgentConfig
+from pyneuphonic.player import AudioPlayer, AsyncAudioPlayer, AsyncAudioRecorder
+from pyneuphonic._utils import save_audio, async_save_audio
