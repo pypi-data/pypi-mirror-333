d85e35a4b6d9ada04eaf11496540207d4b4fff2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:06:01 +0000

    Add package orionis/0.186.0/orionis-0.186.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.186.0-py3-none-any.whl/orionis/luminate/support/asyn_run.py
@@ -0,0 +1,22 @@
+import asyncio
+from typing import Any, Callable
+
+class AsyncExecutor:
+
+    @staticmethod
+    def run(callback: Callable[..., Any]) -> None:
+        """
+        Runs a coroutine synchronously.
+
+        Parameters
+        ----------
+        callback : Callable[..., Any]
+            The coroutine to run.
+        """
+        try:
+            loop = asyncio.get_running_loop()
+            loop.run_until_complete(callback())
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(callback())
