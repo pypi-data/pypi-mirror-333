195a30c592ab6e8ac8c611a497124f291adde1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:08:00 +0000

    Add package gfw-api-python-client/1.0.0/gfw_api_python_client-1.0.0.tar.gz

---
+++ packages/gfw-api-python-client/gfw_api_python_client-1.0.0.tar.gz/gfw_api_python_client-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="gfw-api-python-client",
+    version="1.0.0",
+    description="Reserved package name for Global Fishing Watch",
+    author="Global Fishing Watch",
+    packages=[],
+)
