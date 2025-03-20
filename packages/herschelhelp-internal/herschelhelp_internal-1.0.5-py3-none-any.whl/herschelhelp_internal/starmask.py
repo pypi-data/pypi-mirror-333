8c410e50e80eaac34916323aa97535c98a939af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:28 +0000

    Add package herschelhelp-internal/1.0.5/herschelhelp_internal-1.0.5-py3-none-any.whl

---
+++ packages/herschelhelp-internal/herschelhelp_internal-1.0.5-py3-none-any.whl/herschelhelp_internal/starmask.py
@@ -0,0 +1,241 @@
+import logging
+#from collections import Counter
+
+#import matplotlib as mpl
+import numpy as np
+#import seaborn as sns
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.table import Table
+#from astropy.io import fits
+from astropy.wcs import WCS
+
+import pyregion
+#import pyregion._region_filter as filter
+#import pyregion._region_filter as region_filter
+
+
+from pymoc import MOC
+#from pymoc.util.catalog import catalog_to_cells
+import healpy as hp
+from scipy.constants import pi
+
+from .utils import inMoc
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def create_holes(gaia, 
+                 out_file, 
+                 region_moc_file,
+                 radius = 10 * u.arcsec, 
+                 AB = [np.NaN,np.NaN], 
+                 mag_lim = 16):
+    """Create a ds9 region file of circles around every GAIA object
+
+    This function loops through every object in GAIA in a given field and writes
+    a ds9 circle at that location. The default radius is 10 arcsec. This is the
+    simplest possible star mask and will be updated to vary as a function of 
+    magnitude as well as include rectangular diffraction spikes
+
+    Parameters
+    ----------
+    gaia: string
+        Location of the fits table of GAIA objects in the target field.
+    out_file: string
+        Location of output region file.
+    region_moc_file: string
+        Location of MOC describing region to generate holes in.
+    radius: astropy quantity (distance)
+        Radius for considering sources as duplicates.
+    AB: list of two floats
+        Parameters defining hole radius as function of magnitude according to 
+        10**(A + B * m)
+
+    Returns
+    -------
+    string
+        Location of produced starmask .reg file.
+
+    """
+    
+    
+    
+    stars = Table.read(gaia)['field', 'ra','dec', 'phot_g_mean_mag']
+
+    print('There are ' + str(len(stars)) + ' GAIA stars in ' + stars[0]['field'])
+
+    region_moc = MOC(filename = region_moc_file)
+    
+    #write the ds9regions to file
+    f = open(out_file, 'w+')
+    
+    for star in stars[inMoc(stars['ra'],stars['dec'],region_moc)]:
+            
+        if star['phot_g_mean_mag'] < 16 and AB ==[np.NaN,np.NaN]:
+            f.writelines('circle(' 
+                         + str(star['ra']) + ', ' 
+                         + str(star['dec']) + ', '
+                         + str(radius/u.arcsec) + '")\n')
+        elif star['phot_g_mean_mag'] < mag_lim and AB != [np.NaN,np.NaN]:
+        	#If AB present then define annulus inner 1 arc sec and out r_50 from AB
+            r_50 = (10**(AB[0] + AB[1] * star['phot_g_mean_mag'])) * u.arcsec
+            f.writelines('circle(' 
+                         + str(star['ra']) + ', ' 
+                         + str(star['dec']) + ', ' # 1.0", '
+                         + str(r_50/u.arcsec) + '")\n')
+    
+
+    f.close()
+    print( 'Starmask written to ' + out_file)
+    #return 'starmask written to data/starmask.reg'
+    
+
+def reg2moc(region_file, fieldmoc, target, ra_typ = 0.0, dec_typ = 0.0, order = 12):
+    """Convert a ds9 region file into MOC/fits format
+    
+    This function takes a ds9 region file and converts it to a MOC of given order. 
+    
+    The default order is 13.
+    
+    Parameters
+    ----------
+    region_file: string
+        Location of ds9 region file.
+    fieldmoc: string
+        Location of the MOC describing the unmasked field 
+    target: string
+        Location of output MOC fits file.
+    ra_typ: float
+        A ra position in the field  required due to header requiring a WCS
+    dec_typ: float
+        A dec position in the field  required due to header requiring a WCS
+    order: int
+        Order of MOC to use
+
+    Returns
+    -------
+    string
+        Location of produced starmask MOC .fits file.
+    """
+    
+    #The code requires an image WCS
+    w = WCS("""
+WCSAXES =                    2 / Number of coordinate axes
+CRPIX1  =                0.5 / Pixel coordinate of reference point
+CRPIX2  =                0.5 / Pixel coordinate of reference point
+CDELT1  =                0.675 / [deg] Coordinate increment at reference point
+CDELT2  =                0.675 / [deg] Coordinate increment at reference point
+RADECSYSa= 'ICRS    '           / International Celestial Ref. System
+CUNIT1  = 'deg     '                / Units of coordinate increment and value
+CUNIT2  = 'deg     '                / Units of coordinate increment and value
+CTYPE1  = 'RA---TAN'           / 
+CTYPE2  = 'DEC--TAN'           / 
+CRVAL1  =                  {} / [deg] Coordinate value at reference point
+CRVAL2  =                  {} / [deg] Coordinate value at reference point
+LONPOLE =                  0.0 / [deg] Native longitude of celestial pole
+LATPOLE = 90.0 / [deg] Native latitude of celestial pole
+""".format(ra_typ,dec_typ) )
+#CTYPE1  = 'RA---TAN'           / galactic longitude, Hammer-Aitoff projection
+#CTYPE2  = 'DEC--TAN'           / galactic latitude, Hammer-Aitoff projection
+    
+    # The ds9 -> MOC converter
+    
+    # Use the HELP-coverage moc of the field you want to use
+    # available at hedam.lam.fr/HELP/coverages.html
+    # example is with the XMM-LSS MOC
+    #moc_file = 'XMM-LSS_MOC.fits'
+    moc = MOC(filename = fieldmoc)
+    
+    # The order of detail for the final MOC which  incorporates the region file
+    
+    NSIDE = hp.order2nside(order)
+    
+    # calculate the ra and dec of every healpix pixel of order = ORDER
+    hp_idx = list(moc.flattened(order))
+    theta, phi = hp.pix2ang(NSIDE, hp_idx, nest = True)
+    ra,dec = np.degrees(phi), -np.degrees(theta-pi/2)
+    x,y = w.wcs_world2pix(ra,dec,0)
+    
+    # Opens region file, with wcs, needs wcs to convert to pixel coordinates to make the inside function work.
+    # If the ds9 region file is in pixel coordinates than the relevent wcs should be used, if not than the suplied
+    # wcs will apply the same conversion to the map and the region file.
+    
+    r = pyregion.open(region_file).as_imagecoord(w)
+    # from r a certain region can be selected if not all regions are relevant, for instance r = r[0] or r = [3:9]
+    
+    r2 = r.get_filter(w)
+    
+    # bad_region_mask = TRUE if the region file contains the regions which should be removed from the MOC
+    # bad_region_mask = FALSE if the region file contains the regions which should be kept.
+    bad_region_mask = True
+    if bad_region_mask == True:
+        in_range = np.ones(np.size(x), dtype=bool)
+        mask = r2.inside(x,y)
+        in_range[mask] = False
+        mask = in_range
+    else:
+        mask = r2.inside(x,y)
+    
+    # if there are several different ds9 region files:
+    # (in shell) >>> ls *reg > region_files.txt
+    # in python:
+    # name = np.loadtxt('region_files.txt',usecols = [0],dtype = str)
+    # in_range = np.ones(np.size(x), dtype=bool)
+    # for j in range(0,np.size(name)):
+    #    r = pyregion.open(name[j]).as_imagecoord(w)
+    #    r2 = r.get_filter(w2)
+    #    in_mask = r2.inside(x,y)
+    #    in_range[in_mask] = False
+    # mask = in_range
+    
+    # creates new MOC, with the ds9 region taken out OR only keeping the ds9 region
+    hp_idx = np.array(hp_idx)
+    MOC(order=order,cells = hp_idx[mask]).write(target, overwrite=True)
+    
+#    
+#def cat2moc(starcatfile,fieldmocfile,radius=10*u.arcsec,order=17):
+#    """Turn a star catalogue directly into a MOC
+#       
+#    Parameters
+#    ----------
+#    starcat: string
+#        Location of ds9 region file.
+#    fieldmoc: string
+#        Location of star catalogue
+#    radius: astropy quantity angle
+#        size of circle to mask
+#    order: int
+#        order of MOC cells to use
+#
+#    Returns
+#    -------
+#    string
+#        Location of produced starmask MOC .fits file.
+#    """
+#    startable = Table.read(starcatfile)
+#    starcat = SkyCoord.guess_from_table(startable)
+#    cells = catalog_to_cells(starcat, radius, order)
+#    fieldmoc = MOC(fieldmocfile)
+#    maskedmoc = fieldmoc.remove
+
+def flag_artefacts(masterlist, starmask):
+    """Take a masterlist and flag sources in artefact producing region of star
+    
+    Parameters
+    ----------
+    masterlist: astropy Table
+        Full list of sources
+    starmask: string
+        Location of starmask
+    """
+    print('function not written yet')
+
+if __name__ == '__main__':
+    #create_starmask('data/GAIA_CDFS-SWIRE.fits', 'data/starmask_CDFS-SWIRE.reg')
+    reg2moc('data/holes_ELAIS-N1.reg',
+            'data/ELAIS-N1_MOC.fits',
+            'data/holes-ELAIS-N1-O17_MOC.fits',
+            order=17)
+    
---
+++ packages/herschelhelp-internal/herschelhelp_internal-1.0.5-py3-none-any.whl/herschelhelp_internal/utils.py
@@ -0,0 +1,374 @@
+import pkg_resources
+
+import healpy as hp
+import numpy as np
+from sfdmap2 import sfdmap
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.stats import sigma_clipped_stats
+from astropy.table import Column, Table
+
+import yaml
+
+def mag_to_flux(magnitudes, errors_on_magnitudes=None):
+    """Convert AB magnitudes and errors to fluxes
+
+    Given AB magnitudes and associated errors, this function returns the
+    corresponding fluxes and associated flux errors (in Jy).
+
+    The fluxes are computed with this formula:
+        F = 10^((8.9 - Mag)/2.5)
+    and the errors on fluxes with this one:
+        F_err = ln(10)/2.5 * F * Mag_err
+
+
+    Parameters
+    ----------
+    magnitudes: float or array-like of floats
+        AB magnitudes of the sources.
+    errors_on_magnitudes: float or array-like of floats
+        Error on each magnitudes. None if there are no errors.
+
+    Returns
+    -------
+    fluxes: float or array-like of floats
+        The fluxes in Jy.
+    errors: float or array-like of floats
+        The errors on fluxes in Jy or None.
+    """
+    magnitudes = np.array(magnitudes)
+    fluxes = 10 ** ((8.9 - magnitudes)/2.5)
+
+    if errors_on_magnitudes is not None:
+        errors_on_magnitudes = np.array(errors_on_magnitudes)
+        errors = np.log(10)/2.5 * fluxes * errors_on_magnitudes
+    else:
+        errors = None
+
+    return fluxes, errors
+
+
+def flux_to_mag(fluxes, errors_on_fluxes=None):
+    """Convert fluxes and errors to magnitudes
+
+    Given flux densities in Jy with associated errors, this function returns
+    the corresponding AB magnitudes en errors.
+
+    The magnitudes are computed with this formula:
+        M = 2.5 * (23 - log10(F)) - 48.6
+    and the errors on magnitudes with this one
+        M_err = 2.5/ln(10) * F_err / F
+
+    Parameters
+    ----------
+    fluxes: float or array-like of floats
+        The fluxes in Jy.
+    errors_on_fluxes: float or array-like of floats
+        The flux errors in Jy None if there are no errors.
+
+    Returns
+    -------
+    magnitudes: float or array-like of floats
+        The AB magnitudes.
+    errors: float or array-like of floats
+        The errors on AB magnitudes.
+    """
+    fluxes = np.array(fluxes)
+    magnitudes = 2.5 * (23 - np.log10(fluxes)) - 48.6
+
+    if errors_on_fluxes is not None:
+        errors_on_fluxes = np.array(errors_on_fluxes)
+        errors = 2.5 / np.log(10) * errors_on_fluxes / fluxes
+    else:
+        errors = None
+
+    return magnitudes, errors
+
+
+def aperture_correction(mag, mag_target, stellarity=None, mag_min=None,
+                        mag_max=None):
+    """Compute aperture correction
+
+    Given some magnitudes and target magnitudes this function computes the
+    aperture correction to apply to the first magnitudes to match the targets.
+    It may optionnaly use a stellarity index and magnitude limits to make the
+    computation on a supset of the provided magnitudes.
+
+    The computation is done by sigma-clipping the difference of magnitudes at
+    3 sigma.
+
+    Parameters
+    ----------
+    mag: array of floats
+        The magnitudes to correct.
+    mag_target: array of floats
+        The target magnitudes. The length of the array must be the same as for
+        mag.
+    stellarity: array of floats
+        The stellarity of each source. If it is provided, only the sources with
+        a stellarity above 0.9 will be used in the computation.
+    mag_min: float
+        If this parameter is provided, only the sources with a magnitude above
+        its value will be used in the computation.
+    mag_max: float
+        If this parameter is provided, only the sources with a magnitude bellow
+        its value will be used in the computation.
+
+    Returns
+    -------
+    mag_diff: float
+        The magnitude to add to the magnitudes to match the targets.
+    num: integer
+        The number of sources used in the computation.
+    std: float
+        The final standard deviation on the sigma clipping procedure.
+
+    """
+
+    mask = ~np.isnan(mag) & ~np.isnan(mag_target)
+    if stellarity is not None:
+        mask &= (stellarity > 0.9)
+    if mag_min is not None:
+        mask &= (mag >= mag_min)
+    if mag_max is not None:
+        mask &= (mag <= mag_max)
+
+    num = mask.sum()
+
+    if num == 0:
+        raise Exception("Not enough sources!")
+
+    # As we are looking for the value to add to the magnitudes to match the
+    # target, the difference must be the targets minus the magnitudes.
+    mag_diff = mag_target - mag
+
+    _, median, std = sigma_clipped_stats(mag_diff[mask], sigma=3.0)
+
+    # FIXME: Why do we use the median of the sigma clipping? (It's on Eduardo
+    # code).
+
+    return median, num, std
+
+
+def astrometric_correction(coords, ref_coords, max_radius=0.6*u.arcsec,
+                           near_ra0=False):
+    """Compute the offset between coordinates and reference
+
+    This function compute the RA, Dec offsets between a list of coordinates and
+    the list of reference coordinates.  This is used for correcting the
+    astrometry of a catalogue with respect to a reference catalogue.
+
+    If the coordinates are around the ra=0 separation, set near_ra0 parameter
+    to True.
+
+    Parameters
+    ----------
+    coords: astropy.coordinates.SkyCoord
+        The studied catalogue coordinates.
+    ref_coords: astropy.coordinates.SkyCoord
+        The coordinates of the reference catalogue.
+    max_radius: quantity
+        Maximum radius to look for counterparts.
+    near_ra0: bool
+        Set to True when the coordinates are around the ra=0 limit; the ra will
+        be transformed to be between -180 and 180 to avoid large differences
+        like 359 - 1 deg. Default to False.
+
+    Returns
+    -------
+    quantity, quantity
+        Tuple delta_RA, delta_Dec to be added to the coordinates to match
+        the reference.
+
+    """
+
+    # We cross-match the two catalogues and only keep the matches that are
+    # closer that the maximum radius
+    idx, d2d, _ = ref_coords.match_to_catalog_sky(coords)
+    to_keep = d2d < max_radius
+
+    if near_ra0:
+        ra = coords[idx].ra.wrap_at(180 * u.deg)[to_keep]
+        ref_ra = ref_coords.ra.wrap_at(180 * u.deg)[to_keep]
+    else:
+        ra = coords[idx].ra[to_keep]
+        ref_ra = ref_coords.ra[to_keep]
+
+    dec = coords[idx].dec[to_keep]
+    ref_dec = ref_coords.dec[to_keep]
+
+    # As we want the values to be added to match the reference, the difference
+    # must be the reference minus the coordinates.
+    ra_diff = ref_ra - ra
+    dec_diff = ref_dec - dec
+
+    _, delta_ra, _ = sigma_clipped_stats(ra_diff.arcsec, sigma=3.0)
+    _, delta_dec, _ = sigma_clipped_stats(dec_diff.arcsec, sigma=3.0)
+
+    return delta_ra * u.arcsec, delta_dec * u.arcsec
+
+
+def coords_to_hpidx(ra, dec, order):
+    """Convert coordinates to HEALPix indexes
+
+    Given to list of right ascension and declination, this function computes
+    the HEALPix index (in nested scheme) at each position, at the given order.
+
+    Parameters
+    ----------
+    ra: array or list of floats
+        The right ascensions of the sources.
+    dec: array or list of floats
+        The declinations of the sources.
+    order: int
+        HEALPix order.
+
+    Returns
+    -------
+    array of int
+        The HEALPix index at each position.
+
+    """
+    ra, dec = np.array(ra), np.array(dec)
+
+    theta = 0.5 * np.pi - np.radians(dec)
+    phi = np.radians(ra)
+    healpix_idx = hp.ang2pix(2**order, theta, phi, nest=True)
+
+    return healpix_idx
+
+
+def inMoc(ra, dec, moc):
+    """Find source position in a MOC
+
+    Given a list of positions and a Multi Order Coverage (MOC) map, this
+    function return a boolean mask with True for sources that fall inside the
+    MOC and False elsewhere.
+
+    Parameters
+    ----------
+    ra: array or list of floats
+        The right ascensions of the sources.
+    dec: array or list of floats
+        The declinations of the sources.
+    moc: pymoc.MOC
+        The MOC read by pymoc
+
+    Returns
+    -------
+    array of booleans
+        The boolean mask with True for sources that fall inside the MOC.
+
+    """
+    source_healpix_cells = coords_to_hpidx(
+        np.array(ra), np.array(dec), moc.order
+    )
+
+    # Array of all the HEALpix cell ids of the MOC at its maximum order.
+    moc_healpix_cells = np.array(list(moc.flattened()))
+
+    # We look for sources that are in the MOC and return the mask
+    return np.in1d(source_healpix_cells, moc_healpix_cells)
+
+
+def gen_help_id(ra, dec, base_id=b"HELP_J"):
+    """Generate the HELP identifiers for a list of sources
+
+    Parameters
+    ----------
+    ra: array of float
+        Right ascensions of the sources in degrees (J2000).
+    dec: array of float
+        Declinations of the sources in degrees (J2000).
+    base_id: bytes
+        Begining of the identifiers.
+
+    Returns
+    -------
+    astropy.table.Column
+        A column with HELP identifiers
+
+    """
+    ra = np.array(ra) * u.degree
+    dec = np.array(dec) * u.degree
+    coords = SkyCoord(ra, dec)
+
+    idcol = np.array(coords.to_string(style='hmsdms', precision=3),
+                     dtype=np.string_)
+    idcol = np.char.replace(idcol, b'h', b'')
+    idcol = np.char.replace(idcol, b'm', b'')
+    idcol = np.char.replace(idcol, b's ', b'')
+    idcol = np.char.replace(idcol, b'd', b'')
+    idcol = np.char.replace(idcol, b's', b'')
+    idcol = np.full(idcol.shape, base_id, dtype=np.object) + idcol
+
+    return Column(data=idcol.astype(np.string_), name="help_id")
+
+
+def ebv(ra, dec):
+    """Computes E(B-V) at the position.
+
+    This function computes the E(B-V) using the sfdmap package.  This package
+    uses the E(B-V) values from the Schlegel, Finkbeiner & Davis (1998) dust
+    map and a scaling of 0.86 is applied to the values to reflect the
+    recalibration by Schlafly & Finkbeiner (2011).
+
+    Parameters
+    ----------
+    ra: array-like of floats
+        The right ascensions of the positions.
+    dec: array-like of floats
+        The declinations of the positions.
+
+    Returns
+    -------
+    ebv_col: astropy.table.Column
+        An astropy table column named `ebv`.
+    """
+    dust_maps = sfdmap.SFDMap(
+        pkg_resources.resource_filename(__name__, 'sfd_data')
+    )
+
+    return Column(dust_maps.ebv(ra, dec), "ebv")
+
+
+
+def add_column_meta(catalogue, column_yml_file):
+    """Add column descriptions and other meta data
+        
+    Given a table and a yml file with a list of column names 
+    and descriptions and ucd descriptors write all these to 
+    the appropriate columns in the table.
+
+
+    Parameters
+    ----------
+    catalogue: astropy.table.Table object
+        The table to add descriptions to
+    column_yml_file: String
+        link to yml file describing column definitions.
+
+    Returns
+    -------
+    catalogue: astropy.table.Table object
+        The output catalogue as input but with added column meta.
+    
+    """
+    meta = yaml.load(open(column_yml_file, "r"))
+    
+    for col in catalogue.itercols():
+        try:
+            unit = meta[col.name]['unit']
+            if unit != "None":
+                catalogue[col.name].unit = unit
+            catalogue[col.name].description = meta[col.name]['description']
+        except KeyError:
+            print("Column {} has missing data".format(col.name))
+
+        
+    return catalogue
+        
+    
+    
+
+
