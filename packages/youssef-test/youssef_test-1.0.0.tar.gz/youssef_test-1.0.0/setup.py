931b470adbf583bd0fa7b11b665c242b33c7c09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:30 +0000

    Add package youssef-test/1.0.0/youssef_test-1.0.0.tar.gz

---
+++ packages/youssef-test/youssef_test-1.0.0.tar.gz/youssef_test-1.0.0/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='filereceivertest',
+    version='2.0.0',
+    packages=find_packages(),
+
+    # ... (other setup parameters)
+)
