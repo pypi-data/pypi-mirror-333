bfda77fee5b0f65dfec91441284003383f7497e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:07:08 +0000

    Add package python-event-sourcery/0.4.0a13/python_event_sourcery-0.4.0a13.tar.gz

---
+++ packages/python-event-sourcery/python_event_sourcery-0.4.0a13.tar.gz/python_event_sourcery-0.4.0a13/event_sourcery/event_store/__init__.py
@@ -0,0 +1,56 @@
+__all__ = [
+    "Backend",
+    "BackendFactory",
+    "Dispatcher",
+    "Entry",
+    "Event",
+    "Context",
+    "EventRegistry",
+    "EventStore",
+    "ExplicitVersioning",
+    "InMemoryBackendFactory",
+    "Listener",
+    "WrappedEvent",
+    "NO_VERSIONING",
+    "Position",
+    "RawEvent",
+    "Recorded",
+    "RecordedRaw",
+    "StreamId",
+    "StreamUUID",
+    "TransactionalBackend",
+    "Versioning",
+    "exceptions",
+    "factory",
+    "interfaces",
+    "subscription",
+    "TenantId",
+]
+
+from event_sourcery.event_store import exceptions, factory, interfaces, subscription
+from event_sourcery.event_store.dispatcher import Dispatcher, Listener
+from event_sourcery.event_store.event import (
+    Context,
+    Entry,
+    Event,
+    EventRegistry,
+    Position,
+    RawEvent,
+    Recorded,
+    RecordedRaw,
+    WrappedEvent,
+)
+from event_sourcery.event_store.event_store import EventStore
+from event_sourcery.event_store.factory import (
+    Backend,
+    BackendFactory,
+    TransactionalBackend,
+)
+from event_sourcery.event_store.in_memory import InMemoryBackendFactory
+from event_sourcery.event_store.stream_id import StreamId, StreamUUID
+from event_sourcery.event_store.tenant_id import TenantId
+from event_sourcery.event_store.versioning import (
+    NO_VERSIONING,
+    ExplicitVersioning,
+    Versioning,
+)
