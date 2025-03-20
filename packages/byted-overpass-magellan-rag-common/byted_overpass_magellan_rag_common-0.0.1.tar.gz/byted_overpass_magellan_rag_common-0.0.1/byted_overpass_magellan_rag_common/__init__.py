790d212a447e6b14e59f8c36e1d42fb58bc71f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:30 +0000

    Add package byted-overpass-magellan-rag-common/0.0.1/byted_overpass_magellan_rag_common-0.0.1.tar.gz

---
+++ packages/byted-overpass-magellan-rag-common/byted_overpass_magellan_rag_common-0.0.1.tar.gz/byted_overpass_magellan_rag_common-0.0.1/byted_overpass_magellan_rag_common/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_magellan_rag_common - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-magellan-rag-common/byted_overpass_magellan_rag_common-0.0.1.tar.gz/byted_overpass_magellan_rag_common-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_magellan_rag_common",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_magellan_rag_common",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_magellan_rag_common.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
