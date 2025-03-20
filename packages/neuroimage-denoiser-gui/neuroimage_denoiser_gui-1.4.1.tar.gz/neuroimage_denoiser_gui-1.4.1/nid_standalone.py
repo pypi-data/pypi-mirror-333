8155d812d7cdae8786612cf48a619a3930624803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:27 +0000

    Add package neuroimage-denoiser-gui/1.4.1/neuroimage_denoiser_gui-1.4.1.tar.gz

---
+++ packages/neuroimage-denoiser-gui/neuroimage_denoiser_gui-1.4.1.tar.gz/neuroimage_denoiser_gui-1.4.1/nid_standalone.py
@@ -0,0 +1,6 @@
+# C 2025 Andreas Brilka
+# This script is used for creating standalone exe using pyinstaller
+
+import neuroimage_denoiser
+import neuroimage_denoiser_gui
+gui = neuroimage_denoiser_gui.NDenoiser_GUI()
