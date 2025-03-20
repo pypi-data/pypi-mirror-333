1e909bc782b714bfa4a8f3e26b21997e49dfd6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:14 +0000

    Add package maleo-core/0.1.36/maleo_core-0.1.36.tar.gz

---
+++ packages/maleo-core/maleo_core-0.1.36.tar.gz/maleo_core-0.1.36/maleo_core/middlewares/rate_limit.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+from datetime import datetime, timedelta
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+from maleo_core.models import BaseSchemas
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, limit:int = 10, window:int = 60):
+        super().__init__(app)
+        self.limit = limit
+        self.window = timedelta(seconds=window)
+        self.requests = defaultdict(list)
+
+    async def dispatch(self, request: Request, call_next):
+        client_ip = request.client.host
+        now = datetime.now()
+
+        #* Filter requests within the window
+        self.requests[client_ip] = [timestamp for timestamp in self.requests[client_ip] if now - timestamp <= self.window]
+
+        #* Check if the request count exceeds the limit
+        if len(self.requests[client_ip]) >= self.limit:
+            return JSONResponse(content=BaseSchemas.Response.RateLimitExceeded().model_dump(), status_code=status.HTTP_429_TOO_MANY_REQUESTS)
+
+        #* Add the current request timestamp
+        self.requests[client_ip].append(now)
+        return await call_next(request)
+
+def add_rate_limit_middleware(app:FastAPI) -> None:
+    """
+    Adds Rate Limit middleware to the FastAPI application.
+
+    This middleware limits how many request can the endpoint process within a window from a user.
+
+    Args:
+        app: FastAPI
+            The FastAPI application instance to which the middleware will be added.
+
+    Returns:
+        None: The function modifies the FastAPI app by adding Process Time middleware.
+
+    Example:
+    ```python
+    add_rate_limit_middleware(app=app)
+    ```
+    """
+    app.add_middleware(RateLimitMiddleware)
