891eb185d8d3192f09e1e9b412b9971b6d6adf35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:07 +0000

    Add package search-string-overvaagning/0.5.3/search_string_overvaagning-0.5.3-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/search-string-overvaagning/search_string_overvaagning-0.5.3-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/search_string/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from .search_string import SearchString
+from .search_string_collection import SearchStringCollection
+from .version import __version__
+
+__all__ = ('SearchString', 'SearchStringCollection', '__version__')
