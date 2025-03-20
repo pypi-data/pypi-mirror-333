919907ff7d30ecdf465445971485da233f5582a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:33 +0000

    Add package utwrite/0.0.10/utwrite-0.0.10.tar.gz

---
+++ packages/utwrite/utwrite-0.0.10.tar.gz/utwrite-0.0.10/utwrite/headers.py
@@ -0,0 +1,33 @@
+r"""headers.py
+
+File to host different headers wanted to use for the unittest test file.
+Make a new header entry for your custom setup.
+
+"""
+HEADER={}
+
+HEADER['default'] = r"""
+import sys
+import os
+import unittest
+from utwrite.unittest_cases import *
+
+%s
+class Test_%s_AUTO(%s):
+"""
+
+HEADER['maya'] = r"""
+
+import unittest
+import sys
+import os
+from maya import cmds
+from maya import OpenMaya
+from utwrite.unittest_cases import *
+import maya.standalone
+maya.standalone.initialize()
+
+%s
+class Test_%s_AUTO(%s):
+
+"""
