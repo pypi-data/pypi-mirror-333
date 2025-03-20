2d2ebd9a5055ccc52543975f625882e431aa6213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:23 +0000

    Add package nonebot-adapter-chaoxing/0.0.8/nonebot_adapter_chaoxing-0.0.8-py3-none-any.whl

---
+++ packages/nonebot-adapter-chaoxing/nonebot_adapter_chaoxing-0.0.8-py3-none-any.whl/nonebot/adapters/chaoxing/utils.py
@@ -0,0 +1,11 @@
+from nonebot.utils import logger_wrapper
+
+log = logger_wrapper("ChaoXing")
+
+
+def escape(s: str) -> str:
+    return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+
+
+def unescape(s: str) -> str:
+    return s.replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&")
