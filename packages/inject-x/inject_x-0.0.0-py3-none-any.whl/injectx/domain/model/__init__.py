b4a76755499af867cc55dc3b0152fa5599ebe34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:33 +0000

    Add package inject-x/0.0.0/inject_x-0.0.0-py3-none-any.whl

---
+++ packages/inject-x/inject_x-0.0.0-py3-none-any.whl/injectx/domain/model/__init__.py
@@ -0,0 +1,11 @@
+from .presentation import Presentation
+from .repository import Repository
+from .service import Service
+from .config import Config
+
+__all__ = [
+    "Presentation",
+    "Repository",
+    "Service",
+    "Config",
+]
