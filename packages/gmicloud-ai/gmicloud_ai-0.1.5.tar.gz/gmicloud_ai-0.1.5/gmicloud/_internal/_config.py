587cd69e358c83bc3d55837f8923574485cbe589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:06:12 +0000

    Add package gmicloud-ai/0.1.5/gmicloud_ai-0.1.5.tar.gz

---
+++ packages/gmicloud-ai/gmicloud_ai-0.1.5.tar.gz/gmicloud_ai-0.1.5/gmicloud/_internal/_config.py
@@ -0,0 +1,3 @@
+ARTIFACT_SERVICE_BASE_URL = "https://ce-tot.gmicloud-dev.com/api/v1/ie/artifact"
+TASK_SERVICE_BASE_URL = "https://ce-tot.gmicloud-dev.com/api/v1/ie/task"
+IAM_SERVICE_BASE_URL = "https://ce-tot.gmicloud-dev.com/api/v1"
