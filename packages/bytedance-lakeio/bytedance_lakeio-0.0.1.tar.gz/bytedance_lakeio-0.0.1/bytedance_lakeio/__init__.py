d21a74771131931a3ffc889614f9cee1bf54b5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:32 +0000

    Add package bytedance-lakeio/0.0.1/bytedance_lakeio-0.0.1.tar.gz

---
+++ packages/bytedance-lakeio/bytedance_lakeio-0.0.1.tar.gz/bytedance_lakeio-0.0.1/bytedance_lakeio/__init__.py
@@ -0,0 +1,2 @@
+# bytedance_lakeio - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedance-lakeio/bytedance_lakeio-0.0.1.tar.gz/bytedance_lakeio-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_lakeio",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_lakeio",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_lakeio.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
