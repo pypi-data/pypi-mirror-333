c5d65ad1db70bc98661617ec8f63b03604a160db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:58 +0000

    Add package gOdoo-cli/0.14.4/godoo_cli-0.14.4-py3-none-any.whl

---
+++ packages/gOdoo-cli/godoo_cli-0.14.4-py3-none-any.whl/godoo_cli/commands/config.py
@@ -0,0 +1,43 @@
+"""Configuration management module for Odoo.
+
+This module provides functionality to manage Odoo configuration files,
+particularly odoo.conf. It allows setting configuration options through
+command-line arguments.
+"""
+
+import logging
+from configparser import ConfigParser
+from pathlib import Path
+from typing import Annotated, List
+
+import typer
+
+from ..cli_common import CommonCLI
+
+CLI = CommonCLI()
+LOGGER = logging.getLogger(__name__)
+
+
+def set_odoo_config(
+    options: Annotated[List[str], typer.Argument(help="odoo.conf options by key=value")],
+    odoo_conf_path: Annotated[Path, CLI.odoo_paths.conf_path],
+):
+    """Set odoo.conf values."""
+    conf_path = Path(odoo_conf_path)
+    odoo_conf = ConfigParser()
+    odoo_conf.read(conf_path)
+    LOGGER.info("Setting Odoo Conf Options for Stage")
+    custom_opts = {}
+    for op in options:
+        if "=" not in op:
+            LOGGER.error("Cannot parse Option: '%s' --> missing = sign", op)
+            raise ValueError("Configuration option must be in format key=value")
+        op_split = op.split("=")
+        custom_opts[op_split[0]] = op_split[1]
+    LOGGER.debug("Writing Conf Options: %s", custom_opts)
+    if "options" not in odoo_conf:
+        odoo_conf.add_section("options")
+        odoo_conf["options"] = {}
+    odoo_conf["options"].update(custom_opts)
+    conf_path.touch(exist_ok=True)
+    odoo_conf.write(conf_path.open("w"))
