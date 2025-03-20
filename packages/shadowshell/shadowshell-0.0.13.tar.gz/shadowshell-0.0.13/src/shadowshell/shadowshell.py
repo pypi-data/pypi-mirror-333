4845e011d7c18528098fdf43701135e58f12834b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:49 +0000

    Add package shadowshell/0.0.13/shadowshell-0.0.13.tar.gz

---
+++ packages/shadowshell/shadowshell-0.0.13.tar.gz/shadowshell-0.0.13/src/shadowshell/shadowshell.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Logger
+@author: shadow shell
+"""
+
+import requests
+
+class ShadowShell:
+
+    def hello(self):
+        print("Hello.")
+    
+    def request(self):
+        requests.get("wwww.baidu.com")
