655188f8be9e74cadde00d8fc1aab370b246c79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:52 +0000

    Add package pdf-craft/0.0.2/pdf_craft-0.0.2.tar.gz

---
+++ packages/pdf-craft/pdf_craft-0.0.2.tar.gz/pdf_craft-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="pdf-craft",
+  version="0.0.2",
+  author="Tao Zeyu",
+  author_email="i@taozeyu.com",
+  url="https://github.com/oomol-lab/pdf-craft",
+  description="PDF craft can convert PDF files into various other formats. This project will focus on processing PDF files of scanned books. The project has just started.",
+  packages=find_packages(),
+  long_description=open("./README.md", encoding="utf8").read(),
+  long_description_content_type="text/markdown",
+  install_requires=[
+    "pyMuPDF>=1.25.3,<2.0.0",
+    "shapely>=2.0.6,<3.0.0",
+    "tiktoken>=0.9.0,<1.0.0",
+    "doc-page-extractor==0.0.6",
+  ],
+)
