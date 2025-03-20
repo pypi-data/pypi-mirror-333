63a0ef1f90c143bf5d2795fc998668c432c2cd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:05:00 +0000

    Add package femtum-sdk/0.15.1/femtum_sdk-0.15.1-py3-none-any.whl

---
+++ packages/femtum-sdk/femtum_sdk-0.15.1-py3-none-any.whl/femtum_sdk/core/health_pb2.pyi
@@ -0,0 +1,14 @@
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class HealthDto(_message.Message):
+    __slots__ = ("status", "version")
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    VERSION_FIELD_NUMBER: _ClassVar[int]
+    status: str
+    version: str
+    def __init__(self, status: _Optional[str] = ..., version: _Optional[str] = ...) -> None: ...
