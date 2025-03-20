4d24d17be3aa759337cdcb3503f75c3d9d50b92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:45 +0000

    Add package hiddenlayer-sdk/2.0.5/hiddenlayer_sdk-2.0.5-py3-none-any.whl

---
+++ packages/hiddenlayer-sdk/hiddenlayer_sdk-2.0.5-py3-none-any.whl/hiddenlayer/sdk/constants.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class ScanStatus(str, Enum):
+    DONE = "done"
+    ACCEPTED = "accepted"
+    FAILED = "failed"
+    PENDING = "pending"
+    CREATED = "created"
+    RETRY = "retry"
+
+
+class ApiErrors(str, Enum):
+    NON_ADHOC_SENSOR_DELETE = "only adhoc sensors may be deleted"
+    SENSOR_EXISTS = "already exists"
