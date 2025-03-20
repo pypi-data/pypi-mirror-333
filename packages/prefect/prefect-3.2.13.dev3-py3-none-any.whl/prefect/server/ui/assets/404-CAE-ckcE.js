86ba3fa82108ba61e74127d6e07c5104a02c603c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:06:01 +0000

    Add package prefect/3.2.13.dev3/prefect-3.2.13.dev3-py3-none-any.whl

---
+++ packages/prefect/prefect-3.2.13.dev3-py3-none-any.whl/prefect/server/ui/assets/404-CAE-ckcE.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,n as t,dB as n}from"./index-BmJMa-fn.js";import{u as r}from"./usePageTitle-Dof3od4f.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(a(),o(t(n)))}});export{m as default};
+//# sourceMappingURL=404-CAE-ckcE.js.map
