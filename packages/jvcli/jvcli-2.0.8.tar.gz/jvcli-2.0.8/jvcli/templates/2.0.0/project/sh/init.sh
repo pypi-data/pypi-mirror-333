93fdc542de63f91eb8728131fa6b77c23b3f1606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:07:06 +0000

    Add package jvcli/2.0.8/jvcli-2.0.8.tar.gz

---
+++ packages/jvcli/jvcli-2.0.8.tar.gz/jvcli-2.0.8/jvcli/templates/2.0.0/project/sh/init.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Script to create jivas user, login, and initialize jivas graph
+
+function initialize() {
+
+    if lsof -i :$JIVAS_PORT >/dev/null; then
+
+        # Try to login first
+        JIVAS_TOKEN=$(curl --silent --show-error --no-progress-meter \
+        --request POST \
+        --header 'Content-Type: application/json' \
+        --header 'Accept: application/json' \
+        --data '{"password": "'"$JIVAS_PASSWORD"'","email": "'"$JIVAS_USER"'"}' \
+        "http://localhost:$JIVAS_PORT/user/login" | grep -o '"token":"[^"]*' | sed 's/"token":"//')
+
+        echo $JIVAS_TOKEN
+
+        # Check if login was successful
+        if [ -z "$JIVAS_TOKEN" ] || [ "$JIVAS_TOKEN" == "null" ]; then
+            echo "Login failed. Registering user..."
+
+            # Register user if login failed
+            curl --silent --show-error --no-progress-meter \
+            --request POST \
+            --header 'Content-Type: application/json' \
+            --header 'Accept: application/json' \
+            --data '{
+            "password": "'"$JIVAS_PASSWORD"'",
+            "email": "'"$JIVAS_USER"'"
+            }' \
+            "http://localhost:$JIVAS_PORT/user/register"
+
+            # Attempt to login again after registration
+            JIVAS_TOKEN=$(curl --silent --show-error --no-progress-meter \
+            --request POST \
+            --header 'Content-Type: application/json' \
+            --header 'Accept: application/json' \
+            --data '{"password": "'"$JIVAS_PASSWORD"'","email": "'"$JIVAS_USER"'"}' \
+            "http://localhost:$JIVAS_PORT/user/login" | grep -o '"token":"[^"]*' | sed 's/"token":"//')
+        fi
+
+
+        # Print token
+        echo "Jivas token: $JIVAS_TOKEN"
+
+        echo "Initializing jivas graph..."
+
+        # Initialize agents
+        curl --silent --show-error --no-progress-meter \
+        --request POST \
+        -H 'accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -H "Authorization: Bearer $JIVAS_TOKEN" \
+        --data '{}' \
+        "http://localhost:$JIVAS_PORT/walker/init_agents"
+
+        exit
+
+    else
+        echo "Server is not running on port $JIVAS_PORT. Waiting..."
+    fi
+
+    exit
+}
+
+# Main loop to check if a process is running on port $JIVAS_PORT
+while true; do
+    initialize
+    sleep 2  # Wait for 2 seconds before checking again
+done
