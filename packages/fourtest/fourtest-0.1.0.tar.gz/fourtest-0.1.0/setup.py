d73dfb4892d937f7142b5db597bef81155749a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:38 +0000

    Add package fourtest/0.1.0/fourtest-0.1.0.tar.gz

---
+++ packages/fourtest/fourtest-0.1.0.tar.gz/fourtest-0.1.0/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="fourtest",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "fourtest"
+    ],
+        entry_points={
+        "console_scripts": [
+            "fourtest-api=fourtest.api:run"
+        ]
+    },
+    author="fourchains",
+    description="A simple example fourtest",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    tests_require=["pytest"],
+)
