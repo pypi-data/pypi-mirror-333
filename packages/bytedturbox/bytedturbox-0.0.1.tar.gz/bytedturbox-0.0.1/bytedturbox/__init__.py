52d7f4bce7910ef999d45aa5954ae0905174f47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:27 +0000

    Add package bytedturbox/0.0.1/bytedturbox-0.0.1.tar.gz

---
+++ packages/bytedturbox/bytedturbox-0.0.1.tar.gz/bytedturbox-0.0.1/bytedturbox/__init__.py
@@ -0,0 +1,2 @@
+# bytedturbox - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedturbox/bytedturbox-0.0.1.tar.gz/bytedturbox-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedturbox",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedturbox",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedturbox.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
