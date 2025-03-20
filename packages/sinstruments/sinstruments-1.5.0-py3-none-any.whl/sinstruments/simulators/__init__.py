05104c5f93ece4329f6576ef5191997622d154ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:19 +0000

    Add package sinstruments/1.5.0/sinstruments-1.5.0-py3-none-any.whl

---
+++ packages/sinstruments/sinstruments-1.5.0-py3-none-any.whl/sinstruments/simulators/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the sinstruments project
+#
+# Copyright (c) 2018-present Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+"""Repository of simulators
+
+.. autosummary::
+    :toctree:
+
+    icepap
+    elettra
+"""
