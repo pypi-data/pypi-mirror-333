b997256554fbfccb0c027024954bdbb63fe46973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:59 +0000

    Add package franklinwh/0.1.0/franklinwh-0.1.0.tar.gz

---
+++ packages/franklinwh/franklinwh-0.1.0.tar.gz/franklinwh-0.1.0/bin/fetch.py
@@ -0,0 +1,18 @@
+import sys
+import pprint
+from franklinwh import Client
+
+def main(argv):
+    token = argv[1]
+    gateway = argv[2]
+
+    client = Client(token, gateway)
+    # pprint.pprint(client.get_stats())
+    # pprint.pprint(client.get_controllable_loads())
+    # pprint.pprint(client.get_accessory_list())
+    # pprint.pprint(client.get_equipment_list())
+    pprint.pprint(client._get_smart_switch_state())
+
+
+if __name__ == "__main__":
+    main(sys.argv)
