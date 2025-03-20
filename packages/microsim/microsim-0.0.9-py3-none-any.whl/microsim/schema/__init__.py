8f8e3bc86dcdf6079ff9645057fdbb2b3037adf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:06:45 +0000

    Add package microsim/0.0.9/microsim-0.0.9-py3-none-any.whl

---
+++ packages/microsim/microsim-0.0.9-py3-none-any.whl/microsim/schema/__init__.py
@@ -0,0 +1,52 @@
+import logging
+
+from .backend import BackendName, DeviceName, NumpyAPI
+from .detectors import CameraCCD, CameraCMOS, CameraEMCCD
+from .lens import ObjectiveLens
+from .modality import Confocal, Identity, Modality, Widefield
+from .optical_config import (
+    Bandpass,
+    LightSource,
+    Longpass,
+    OpticalConfig,
+    Shortpass,
+    SpectrumFilter,
+)
+from .sample import CosemLabel, Fluorophore, FluorophoreDistribution, MatsLines, Sample
+from .settings import Settings
+from .simulation import Simulation
+from .space import DownscaledSpace, ExtentScaleSpace, ShapeExtentSpace, ShapeScaleSpace
+from .spectrum import Spectrum
+
+logging.getLogger().setLevel(logging.INFO)
+__all__ = [
+    "BackendName",
+    "Bandpass",
+    "CameraCCD",
+    "CameraCMOS",
+    "CameraEMCCD",
+    "Confocal",
+    "CosemLabel",
+    "DeviceName",
+    "DownscaledSpace",
+    "ExtentScaleSpace",
+    "Fluorophore",
+    "FluorophoreDistribution",
+    "Identity",
+    "LightSource",
+    "Longpass",
+    "MatsLines",
+    "Modality",
+    "NumpyAPI",
+    "ObjectiveLens",
+    "OpticalConfig",
+    "Sample",
+    "Settings",
+    "ShapeExtentSpace",
+    "ShapeScaleSpace",
+    "Shortpass",
+    "Simulation",
+    "Spectrum",
+    "SpectrumFilter",
+    "Widefield",
+]
