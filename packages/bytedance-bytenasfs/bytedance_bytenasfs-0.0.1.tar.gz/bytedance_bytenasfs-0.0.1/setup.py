32db3105b20a289068972acd06ead7e81af3948b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:27 +0000

    Add package bytedance-bytenasfs/0.0.1/bytedance_bytenasfs-0.0.1.tar.gz

---
+++ packages/bytedance-bytenasfs/bytedance_bytenasfs-0.0.1.tar.gz/bytedance_bytenasfs-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_bytenasfs",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_bytenasfs",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_bytenasfs.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
