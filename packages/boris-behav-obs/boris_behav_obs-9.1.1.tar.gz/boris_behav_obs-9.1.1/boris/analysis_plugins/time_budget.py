712953038d7b91a5947efcd47ae707cf164b51d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:22 +0000

    Add package boris-behav-obs/9.1.1/boris_behav_obs-9.1.1.tar.gz

---
+++ packages/boris-behav-obs/boris_behav_obs-9.1.1.tar.gz/boris_behav_obs-9.1.1/boris/analysis_plugins/time_budget.py
@@ -0,0 +1,103 @@
+"""
+BORIS plugin
+
+Time budget
+"""
+
+import pandas as pd
+import numpy as np
+
+__version__ = "0.2.0"
+__version_date__ = "2025-01-25"
+__plugin_name__ = "Time budget"
+__author__ = "Olivier Friard - University of Torino - Italy"
+
+
+def run(df: pd.DataFrame):
+    """
+    Calculate the following values:
+
+    - Total number of occurences of behavior
+    - Total duration of behavior (in seconds)
+    - Duration mean of behavior (in seconds)
+    - Standard deviation of behavior duration (in seconds)
+    - Inter-event intervals mean (in seconds)
+    - Inter-event intervals standard deviation (in seconds)
+    - % of total subject observation duration
+    """
+
+    group_by = ["Subject", "Behavior"]
+
+    dfs = [
+        df.groupby(group_by)["Behavior"].count().reset_index(name="number of occurences"),
+        df.groupby(group_by)["Duration (s)"].sum().reset_index(name="total duration"),
+        df.groupby(group_by)["Duration (s)"].mean().astype(float).round(3).reset_index(name="duration mean"),
+        df.groupby(group_by)["Duration (s)"].std().astype(float).round(3).reset_index(name="duration std dev"),
+    ]
+
+    # inter events
+    df2 = df.sort_values(by=["Observation id", "Subject", "Behavior", "Start (s)"])
+    df2["diff"] = df2.groupby(["Observation id", "Subject", "Behavior"])["Start (s)"].shift(periods=-1) - df2["Stop (s)"]
+
+    dfs.append(df2.groupby(group_by)["diff"].mean().astype(float).round(3).reset_index(name="inter-event intervals mean"))
+
+    dfs.append(df2.groupby(group_by)["diff"].std().astype(float).round(3).reset_index(name="inter-event intervals std dev"))
+
+    # % of total subject observation time
+
+    interval = (df.groupby(["Subject"])["Stop (s)"].max() - df.groupby(["Subject"])["Start (s)"].min()).replace(0, np.nan)
+
+    dfs.append(
+        (100 * df.groupby(group_by)["Duration (s)"].sum() / interval)
+        .astype(float)
+        .round(3)
+        .reset_index(name="% of total subject observation duration")
+    )
+
+    merged_df = dfs[0]
+    for df in dfs[1:]:
+        merged_df = pd.merge(merged_df, df, on=group_by)
+
+    return merged_df
+
+
+def main(df: pd.DataFrame, observations_list: list = [], parameters: dict = {}) -> pd.DataFrame:
+    """
+    filter by selected observations.
+    filter by selected subjects.
+    filter by selected behaviors.
+    filter by time interval.
+    """
+
+    # filter selected observations
+    if observations_list:
+        df = df[df["Observation id"].isin(observations_list)]
+
+    if parameters:
+        # filter selected subjects
+        df = df[df["Subject"].isin(parameters["selected subjects"])]
+
+        # filter selected behaviors
+        df = df[df["Behavior"].isin(parameters["selected behaviors"])]
+
+        # filter selected time interval
+        if parameters["start time"] is not None and parameters["end time"] is not None:
+            MIN_TIME = parameters["start time"]
+            MAX_TIME = parameters["end time"]
+
+            df_interval = df[
+                (
+                    ((df["Start (s)"] >= MIN_TIME) & (df["Start (s)"] <= MAX_TIME))
+                    | ((df["Stop (s)"] >= MIN_TIME) & (df["Stop (s)"] <= MAX_TIME))
+                )
+                | ((df["Start (s)"] < MIN_TIME) & (df["Stop (s)"] > MAX_TIME))
+            ]
+
+            df_interval.loc[df["Start (s)"] < MIN_TIME, "Start (s)"] = MIN_TIME
+            df_interval.loc[df["Stop (s)"] > MAX_TIME, "Stop (s)"] = MAX_TIME
+
+            df_interval.loc[:, "Duration (s)"] = df_interval["Stop (s)"] - df_interval["Start (s)"]
+
+            df = df_interval
+
+    return run(df)
---
+++ packages/boris-behav-obs/boris_behav_obs-9.1.1.tar.gz/boris_behav_obs-9.1.1/boris/plugins.py
@@ -0,0 +1,79 @@
+"""
+BORIS
+Behavioral Observation Research Interactive Software
+Copyright 2012-2025 Olivier Friard
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+  MA 02110-1301, USA.
+"""
+
+import logging
+from PySide6.QtGui import QAction
+from . import config as cfg
+from pathlib import Path
+
+
+def add_plugins_to_menu(self):
+    """
+    add plugins to the plugins menu
+    """
+    for plugin_name in self.config_param.get(cfg.ANALYSIS_PLUGINS, {}):
+        logging.debug(f"adding plugin '{plugin_name}' to menu")
+        # Create an action for each submenu option
+        action = QAction(self, triggered=self.run_plugin)
+        action.setText(plugin_name)
+
+        self.menu_plugins.addAction(action)
+
+
+def get_plugin_name(plugin_path: str):
+    """
+    get name of plugin
+    """
+    # search plugin name
+    plugin_name = None
+    with open(plugin_path, "r") as f_in:
+        for line in f_in:
+            if line.startswith("__plugin_name__"):
+                plugin_name = line.split("=")[1].strip().replace('"', "")
+                break
+    return plugin_name
+
+
+def load_plugins(self):
+    """
+    load selected plugins in analysis menu
+    """
+    self.menu_plugins.clear()
+    self.config_param[cfg.ANALYSIS_PLUGINS] = {}
+
+    # load BORIS plugins
+    for file_ in (Path(__file__).parent / "analysis_plugins").glob("*.py"):
+        if file_.name == "__init__.py":
+            continue
+        plugin_name = get_plugin_name(file_)
+        if plugin_name is not None and plugin_name not in self.config_param.get(cfg.EXCLUDED_PLUGINS, set()):
+            self.config_param[cfg.ANALYSIS_PLUGINS][plugin_name] = str(file_)
+
+    # load personal plugins
+    if self.config_param.get(cfg.PERSONAL_PLUGINS_DIR, ""):
+        for file_ in Path(self.config_param.get(cfg.PERSONAL_PLUGINS_DIR, "")).glob("*.py"):
+            if file_.name == "__init__.py":
+                continue
+            plugin_name = get_plugin_name(file_)
+            if plugin_name is not None and plugin_name not in self.config_param.get(cfg.EXCLUDED_PLUGINS, set()):
+                self.config_param[cfg.ANALYSIS_PLUGINS][plugin_name] = str(file_)
+
+    logging.debug(f"{self.config_param.get(cfg.ANALYSIS_PLUGINS, {})=}")
