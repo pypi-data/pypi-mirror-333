913e2c2c133828c19e3d0c47a8dc73218efafe4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:29 +0000

    Add package semantic-link-labs/0.9.6/semantic_link_labs-0.9.6-py3-none-any.whl

---
+++ packages/semantic-link-labs/semantic_link_labs-0.9.6-py3-none-any.whl/sempy_labs/lakehouse/__init__.py
@@ -0,0 +1,30 @@
+from sempy_labs.lakehouse._get_lakehouse_columns import get_lakehouse_columns
+from sempy_labs.lakehouse._get_lakehouse_tables import get_lakehouse_tables
+from sempy_labs.lakehouse._lakehouse import (
+    lakehouse_attached,
+    optimize_lakehouse_tables,
+    vacuum_lakehouse_tables,
+    run_table_maintenance,
+)
+
+from sempy_labs.lakehouse._shortcuts import (
+    # create_shortcut,
+    create_shortcut_onelake,
+    delete_shortcut,
+    reset_shortcut_cache,
+    list_shortcuts,
+)
+
+__all__ = [
+    "get_lakehouse_columns",
+    "get_lakehouse_tables",
+    "lakehouse_attached",
+    "optimize_lakehouse_tables",
+    # create_shortcut,
+    "create_shortcut_onelake",
+    "delete_shortcut",
+    "vacuum_lakehouse_tables",
+    "reset_shortcut_cache",
+    "run_table_maintenance",
+    "list_shortcuts",
+]
