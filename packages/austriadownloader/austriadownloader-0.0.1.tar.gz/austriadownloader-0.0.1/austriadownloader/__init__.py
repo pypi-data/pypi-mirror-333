e0b2531f39259d32f3af88c89868f147ef5f2a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:06:07 +0000

    Add package austriadownloader/0.0.1/austriadownloader-0.0.1.tar.gz

---
+++ packages/austriadownloader/austriadownloader-0.0.1.tar.gz/austriadownloader-0.0.1/austriadownloader/__init__.py
@@ -0,0 +1,8 @@
+from austriadownloader.download import download
+
+__all__ = ["download"]
+
+# Dynamic version import
+import importlib.metadata
+
+__version__ = importlib.metadata.version("austriadownloader")
