3df04f140c08d808e95f0c426a813384e663c4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:46 +0000

    Add package aipolabs/0.0.1b5/aipolabs-0.0.1b5-py3-none-any.whl

---
+++ packages/aipolabs/aipolabs-0.0.1b5-py3-none-any.whl/aipolabs/meta_functions/__init__.py
@@ -0,0 +1,11 @@
+import aipolabs.meta_functions._aci_execute_function as ACIExecuteFunction
+import aipolabs.meta_functions._aci_get_function_definition as ACIGetFunctionDefinition
+import aipolabs.meta_functions._aci_search_apps as ACISearchApps
+import aipolabs.meta_functions._aci_search_functions as ACISearchFunctions
+
+__all__ = [
+    "ACISearchApps",
+    "ACISearchFunctions",
+    "ACIGetFunctionDefinition",
+    "ACIExecuteFunction",
+]
