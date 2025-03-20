a42c411db2a957ede960dc6c630d564b9025d391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:49 +0000

    Add package autoverse-cli/0.17.2/autoverse_cli-0.17.2-py3-none-any.whl

---
+++ packages/autoverse-cli/autoverse_cli-0.17.2-py3-none-any.whl/avrs/requests/race_control.py
@@ -0,0 +1,41 @@
+
+from avrs.requests.request import AvrsApiRequest
+
+class AvrsRaceControlRequest(AvrsApiRequest):
+    def __init__(self, parser, cfg):
+        AvrsApiRequest.__init__(self, parser, cfg, 'RaceControl', 'Ego')
+        psr = parser.add_parser('race-control', help='command an object to move to a landmark')
+
+        psr.add_argument(
+            '--session-type', 
+            type=int, 
+            default=-1,
+            help='the id of the session type to set')
+
+        psr.add_argument(
+            '--track-flag', 
+            type=int, 
+            default=-1,
+            help='the id of the track flag to set')
+
+        psr.add_argument(
+            '--car-flag', 
+            type=int, 
+            default=-1,
+            help='the id of the car flag to set')
+
+        psr.add_argument(
+            '--object-name', 
+            default='Ego', 
+            help='the name of the car to set flags on')
+
+
+        psr.set_defaults(func=self.send_request)
+
+    def get_request_body(self, args):
+        self.target_object_id = args.object_name
+        return {
+            'sessionType': args.session_type,
+            'trackFlag': args.track_flag,
+            'carFlag': args.car_flag
+        }
