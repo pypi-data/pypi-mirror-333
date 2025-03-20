a6c54ef16d0e26d2e84606a4a16ea0db96a00536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:37 +0000

    Add package get-mysqldb/0.0.1/get_mysqldb-0.0.1.tar.gz

---
+++ packages/get-mysqldb/get_mysqldb-0.0.1.tar.gz/get_mysqldb-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='get_mysqldb',  # 项目的名称,pip3 install get-time
+    version='0.0.1',  # 项目版本
+    author='MindLullaby',  # 项目作者
+    author_email='3203939025@qq.com',  # 作者email
+    # url='',  # 项目代码仓库
+    description='mysql数据存储',  # 项目描述
+    packages=['get_mysqldb'],  # 包名
+    install_requires=[
+        "pymysql",
+        "loguru",
+        "threading",
+        "dbutils==3.1.0"
+    ],
+)
