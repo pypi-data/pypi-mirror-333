a520eb6ee9c74d23621bfe9b4cd5ac6455b5c400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:51 +0000

    Add package jac-cloud/0.1.22/jac_cloud-0.1.22.tar.gz

---
+++ packages/jac-cloud/jac_cloud-0.1.22.tar.gz/jac_cloud-0.1.22/jac_cloud/core/context.py
@@ -0,0 +1,170 @@
+"""Core constructs for Jac Language."""
+
+from contextvars import ContextVar
+from dataclasses import dataclass, is_dataclass
+from os import getenv
+from typing import Any, Generic, TypeVar, cast
+
+from bson import ObjectId
+
+from fastapi import Request, WebSocket
+
+from jaclang.runtimelib.context import EXECUTION_CONTEXT, ExecutionContext
+
+from .architype import (
+    AccessLevel,
+    Anchor,
+    BaseArchitype,
+    NodeAnchor,
+    Root,
+    asdict,
+)
+from .memory import MongoDB
+
+
+SHOW_ENDPOINT_RETURNS = getenv("SHOW_ENDPOINT_RETURNS") == "true"
+JASECI_CONTEXT = ContextVar["JaseciContext | None"]("JaseciContext")
+
+SUPER_ROOT_ID = ObjectId("000000000000000000000000")
+PUBLIC_ROOT_ID = ObjectId("000000000000000000000001")
+SUPER_ROOT = NodeAnchor.ref(f"n::{SUPER_ROOT_ID}")
+PUBLIC_ROOT = NodeAnchor.ref(f"n::{PUBLIC_ROOT_ID}")
+
+RT = TypeVar("RT")
+
+
+@dataclass
+class ContextResponse(Generic[RT]):
+    """Default Context Response."""
+
+    status: int
+    reports: list[Any] | None = None
+    returns: list[RT] | None = None
+
+    def __serialize__(self) -> dict[str, Any]:
+        """Serialize response."""
+        return {
+            key: value
+            for key, value in self.__dict__.items()
+            if value is not None and not key.startswith("_")
+        }
+
+
+class JaseciContext(ExecutionContext):
+    """Execution Context."""
+
+    mem: MongoDB
+    reports: list
+    status: int
+    system_root: NodeAnchor
+    root: NodeAnchor
+    entry_node: NodeAnchor
+    base: ExecutionContext | None
+    connection: Request | WebSocket
+
+    def close(self) -> None:
+        """Clean up context."""
+        self.mem.close()
+
+    @staticmethod
+    def create(  # type: ignore[override]
+        connection: Request | WebSocket, entry: NodeAnchor | None = None
+    ) -> "JaseciContext":
+        """Create JacContext."""
+        from jaclang import Root
+
+        ctx = JaseciContext()
+        ctx.base = EXECUTION_CONTEXT.get(None)
+        ctx.connection = connection
+        ctx.mem = MongoDB()
+        ctx.reports = []
+        ctx.status = 200
+
+        system_root: NodeAnchor | None = None
+        if not isinstance(system_root := ctx.mem.find_by_id(SUPER_ROOT), NodeAnchor):
+            system_root = Root().__jac__  # type: ignore[attr-defined]
+            system_root.id = SUPER_ROOT_ID
+            system_root.state.connected = True
+            system_root.persistent = True
+            NodeAnchor.Collection.insert_one(system_root.serialize())
+            system_root.sync_hash()
+            ctx.mem.set(system_root.id, system_root)
+
+        ctx.system_root = system_root
+
+        if _root := getattr(connection, "_root", None):
+            ctx.root = _root
+            ctx.mem.set(_root.id, _root)
+        else:
+            if not isinstance(
+                public_root := ctx.mem.find_by_id(PUBLIC_ROOT), NodeAnchor
+            ):
+                public_root = Root().__jac__  # type: ignore[attr-defined]
+                public_root.id = PUBLIC_ROOT_ID
+                public_root.access.all = AccessLevel.WRITE
+                public_root.persistent = True
+                ctx.mem.set(public_root.id, public_root)
+
+            ctx.root = public_root
+
+        if entry:
+            if not isinstance(entry_node := ctx.mem.find_by_id(entry), NodeAnchor):
+                raise ValueError(f"Invalid anchor id {entry.ref_id} !")
+            ctx.entry_node = entry_node
+        else:
+            ctx.entry_node = ctx.root
+
+        if _ctx := JASECI_CONTEXT.get(None):
+            _ctx.close()
+        JASECI_CONTEXT.set(ctx)
+
+        return ctx
+
+    @staticmethod
+    def get() -> "JaseciContext":
+        """Get current JaseciContext."""
+        if ctx := JASECI_CONTEXT.get(None):
+            return ctx
+        raise Exception("JaseciContext is not yet available!")
+
+    @staticmethod
+    def get_root() -> Root:  # type: ignore[override]
+        """Get current root."""
+        return cast(Root, JaseciContext.get().root.architype)
+
+    def response(self, returns: list[Any]) -> dict[str, Any]:
+        """Return serialized version of reports."""
+        resp = ContextResponse[Any](status=self.status)
+
+        if self.reports:
+            for key, val in enumerate(self.reports):
+                self.clean_response(key, val, self.reports)
+            resp.reports = self.reports
+
+        for key, val in enumerate(returns):
+            self.clean_response(key, val, returns)
+
+        if SHOW_ENDPOINT_RETURNS:
+            resp.returns = returns
+
+        return resp.__serialize__()
+
+    def clean_response(
+        self, key: str | int, val: Any, obj: list | dict  # noqa: ANN401
+    ) -> None:
+        """Cleanup and override current object."""
+        match val:
+            case list():
+                for idx, lval in enumerate(val):
+                    self.clean_response(idx, lval, val)
+            case dict():
+                for key, dval in val.items():
+                    self.clean_response(key, dval, val)
+            case Anchor():
+                cast(dict, obj)[key] = val.report()
+            case BaseArchitype():
+                cast(dict, obj)[key] = val.__jac__.report()
+            case val if is_dataclass(val) and not isinstance(val, type):
+                cast(dict, obj)[key] = asdict(val)
+            case _:
+                pass
---
+++ packages/jac-cloud/jac_cloud-0.1.22.tar.gz/jac_cloud-0.1.22/jac_cloud/plugin/cli.py
@@ -0,0 +1,193 @@
+"""Module for registering CLI plugins for jaseci."""
+
+from getpass import getpass
+from os import getenv
+from os.path import split
+from pickle import load
+from typing import Any
+
+from jaclang.cli.cmdreg import cmd_registry
+from jaclang.plugin.default import hookimpl
+from jaclang.runtimelib.context import ExecutionContext
+from jaclang.runtimelib.machine import JacMachine, JacProgram
+
+from pymongo.errors import ConnectionFailure, OperationFailure
+
+from ..core.architype import BulkWrite, NodeAnchor
+from ..core.context import PUBLIC_ROOT_ID, SUPER_ROOT_ID
+from ..jaseci.datasources import Collection
+from ..jaseci.models import User as BaseUser
+from ..jaseci.utils import logger
+
+
+class JacCmd:
+    """Jac CLI."""
+
+    @staticmethod
+    @hookimpl
+    def create_cmd() -> None:
+        """Create Jac CLI cmds."""
+
+        @cmd_registry.register
+        def serve(filename: str, host: str = "0.0.0.0", port: int = 8000) -> None:
+            from jaclang import jac_import
+            from jac_cloud import FastAPI
+
+            """Serve the jac application."""
+            base, mod = split(filename)
+            base = base if base else "./"
+            mod = mod[:-4]
+
+            FastAPI.enable()
+            jctx = ExecutionContext.create()
+
+            if filename.endswith(".jac"):
+                jac_import(
+                    target=mod,
+                    base_path=base,
+                    cachable=True,
+                    override_name="__main__",
+                )
+            elif filename.endswith(".jir"):
+                with open(filename, "rb") as f:
+                    JacMachine(base).attach_program(
+                        JacProgram(mod_bundle=load(f), bytecode=None, sem_ir=None)
+                    )
+                    jac_import(
+                        target=mod,
+                        base_path=base,
+                        cachable=True,
+                        override_name="__main__",
+                    )
+            else:
+                jctx.close()
+                JacMachine.detach()
+                raise ValueError("Not a valid file!\nOnly supports `.jac` and `.jir`")
+
+            FastAPI.start(host=host, port=port)
+
+            jctx.close()
+            JacMachine.detach()
+
+        @cmd_registry.register
+        def create_system_admin(
+            filename: str, email: str = "", password: str = ""
+        ) -> str:
+            from jaclang import jac_import
+
+            if not getenv("DATABASE_HOST"):
+                raise NotImplementedError(
+                    "DATABASE_HOST env-var is required for this API!"
+                )
+
+            base, mod = split(filename)
+            base = base if base else "./"
+            mod = mod[:-4]
+
+            if filename.endswith(".jac"):
+                jac_import(
+                    target=mod,
+                    base_path=base,
+                    cachable=True,
+                    override_name="__main__",
+                )
+            elif filename.endswith(".jir"):
+                with open(filename, "rb") as f:
+                    JacMachine(base).attach_program(
+                        JacProgram(mod_bundle=load(f), bytecode=None, sem_ir=None)
+                    )
+                    jac_import(
+                        target=mod,
+                        base_path=base,
+                        cachable=True,
+                        override_name="__main__",
+                    )
+
+            if not email:
+                trial = 0
+                while (email := input("Email: ")) != input("Confirm Email: "):
+                    if trial > 2:
+                        raise ValueError("Email don't match! Aborting...")
+                    print("Email don't match! Please try again.")
+                    trial += 1
+
+            if not password:
+                trial = 0
+                while (password := getpass()) != getpass(prompt="Confirm Password: "):
+                    if trial > 2:
+                        raise ValueError("Password don't match! Aborting...")
+                    print("Password don't match! Please try again.")
+                    trial += 1
+
+            user_model = BaseUser.model()
+            user_request = user_model.register_type()(
+                email=email,
+                password=password,
+                **user_model.system_admin_default(),
+            )
+
+            Collection.apply_indexes()
+            with user_model.Collection.get_session() as session, session.start_transaction():
+                req_obf: dict = user_request.obfuscate()
+                req_obf.update(
+                    {
+                        "root_id": SUPER_ROOT_ID,
+                        "is_activated": True,
+                        "is_admin": True,
+                    }
+                )
+
+                retry = 0
+                while True:
+                    try:
+                        default_data: dict[str, Any] = {
+                            "name": None,
+                            "root": None,
+                            "access": {
+                                "all": "NO_ACCESS",
+                                "roots": {"anchors": {}},
+                            },
+                            "architype": {},
+                            "edges": [],
+                        }
+
+                        if not NodeAnchor.Collection.find_by_id(
+                            PUBLIC_ROOT_ID, session=session
+                        ):
+                            NodeAnchor.Collection.insert_one(
+                                {"_id": PUBLIC_ROOT_ID, **default_data},
+                                session=session,
+                            )
+                        if not NodeAnchor.Collection.find_by_id(
+                            SUPER_ROOT_ID, session=session
+                        ):
+                            NodeAnchor.Collection.insert_one(
+                                {"_id": SUPER_ROOT_ID, **default_data},
+                                session=session,
+                            )
+                        if id := (
+                            user_model.Collection.insert_one(req_obf, session=session)
+                        ).inserted_id:
+                            BulkWrite.commit(session)
+                            return f"System Admin created with id: {id}"
+                        raise SystemError("Can't create System Admin!")
+                    except (ConnectionFailure, OperationFailure) as ex:
+                        if (
+                            ex.has_error_label("TransientTransactionError")
+                            and retry <= BulkWrite.SESSION_MAX_TRANSACTION_RETRY
+                        ):
+                            retry += 1
+                            logger.error(
+                                "Error executing bulk write! "
+                                f"Retrying [{retry}/{BulkWrite.SESSION_MAX_TRANSACTION_RETRY}] ..."
+                            )
+                            continue
+                        logger.exception(
+                            f"Error executing bulk write after max retry [{BulkWrite.SESSION_MAX_TRANSACTION_RETRY}] !"
+                        )
+                        raise
+                    except Exception:
+                        logger.exception("Error executing bulk write!")
+                        raise
+
+            raise Exception("Can't process registration. Please try again!")
