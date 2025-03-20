eb220075721455a6d8089ffa0ff980c4106ec9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:57 +0000

    Add package petroscope/0.0.7/petroscope-0.0.7.tar.gz

---
+++ packages/petroscope/petroscope-0.0.7.tar.gz/petroscope-0.0.7/petroscope/segmentation/__init__.py
@@ -0,0 +1,9 @@
+from . import classes
+from .classes import LumenStoneClasses
+from .model import GeoSegmModel
+from .eval import SegmDetailedTester, SegmEvaluator
+from . import models
+from . import balancer
+from . import utils
+from . import augment
+from . import vis
