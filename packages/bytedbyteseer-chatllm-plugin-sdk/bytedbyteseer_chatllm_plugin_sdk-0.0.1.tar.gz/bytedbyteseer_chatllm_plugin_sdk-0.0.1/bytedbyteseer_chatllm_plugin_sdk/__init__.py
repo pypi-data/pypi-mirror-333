8aa2ced26e1715db157e162363145ed410fea4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:29 +0000

    Add package bytedbyteseer-chatllm-plugin-sdk/0.0.1/bytedbyteseer_chatllm_plugin_sdk-0.0.1.tar.gz

---
+++ packages/bytedbyteseer-chatllm-plugin-sdk/bytedbyteseer_chatllm_plugin_sdk-0.0.1.tar.gz/bytedbyteseer_chatllm_plugin_sdk-0.0.1/bytedbyteseer_chatllm_plugin_sdk/__init__.py
@@ -0,0 +1,2 @@
+# bytedbyteseer_chatllm_plugin_sdk - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedbyteseer-chatllm-plugin-sdk/bytedbyteseer_chatllm_plugin_sdk-0.0.1.tar.gz/bytedbyteseer_chatllm_plugin_sdk-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedbyteseer_chatllm_plugin_sdk",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedbyteseer_chatllm_plugin_sdk",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedbyteseer_chatllm_plugin_sdk.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
