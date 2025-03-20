c94b42ac0316fad554d01c1904f2aeaac7ab0f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:58 +0000

    Add package byted-overpass-tiktok-local-merchant/0.0.1/byted_overpass_tiktok_local_merchant-0.0.1.tar.gz

---
+++ packages/byted-overpass-tiktok-local-merchant/byted_overpass_tiktok_local_merchant-0.0.1.tar.gz/byted_overpass_tiktok_local_merchant-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_tiktok_local_merchant",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_tiktok_local_merchant",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_tiktok_local_merchant.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
