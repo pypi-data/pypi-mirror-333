834eef9138dfd235ebf7c9745bfce9614cefca99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:35 +0000

    Add package transcriber/0.1.0/transcriber-0.1.0-py3-none-any.whl

---
+++ packages/transcriber/transcriber-0.1.0-py3-none-any.whl/Transcriber/__init__.py
@@ -0,0 +1,6 @@
+"""Transcriber - A tool to transcribe audio files using Whisper models."""
+
+from Transcriber import transcriber, config
+
+__version__ = "0.1.0"
+__all__ = ["transcriber", "config"]
