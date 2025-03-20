bd4f9406d16575b948ea0613ad0387f60afe779e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:07:22 +0000

    Add package djangocms-photo-gallery/0.0.4.dev2/djangocms_photo_gallery-0.0.4.dev2.tar.gz

---
+++ packages/djangocms-photo-gallery/djangocms_photo_gallery-0.0.4.dev2.tar.gz/djangocms_photo_gallery-0.0.4.dev2/src/djangocms_photo_gallery/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangocmsPhotoGalleryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djangocms_photo_gallery'
+    verbose_name = 'django CMS Photo Gallery'
