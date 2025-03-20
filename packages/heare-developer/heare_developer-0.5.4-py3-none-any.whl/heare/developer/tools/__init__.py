077eb5c9942b3acef2188d12611105c462f6b2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:14 +0000

    Add package heare-developer/0.5.4/heare_developer-0.5.4-py3-none-any.whl

---
+++ packages/heare-developer/heare_developer-0.5.4-py3-none-any.whl/heare/developer/tools/__init__.py
@@ -0,0 +1,27 @@
+from .subagent import agent
+from .files import read_file, write_file, list_directory, edit_file
+from .repl import run_bash_command, python_repl
+from .web import web_search, safe_curl
+
+ALL_TOOLS = [
+    read_file,
+    write_file,
+    list_directory,
+    run_bash_command,
+    edit_file,
+    web_search,
+    agent,
+    safe_curl,
+    python_repl,
+]
+
+
+try:
+    from heare.developer.clients.plane_so import get_project_from_config
+    from heare.developer.tools.issues import PLANE_TOOLS
+
+    project = get_project_from_config()
+    if project:
+        ALL_TOOLS += PLANE_TOOLS
+except Exception:
+    pass
