ac26c4e914cfa3bfaa012518d030ca7d8775e8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:05:00 +0000

    Add package file-keeper/0.0.1/file_keeper-0.0.1.tar.gz

---
+++ packages/file-keeper/file_keeper-0.0.1.tar.gz/file_keeper-0.0.1/src/file_keeper/core/types.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Any, Iterator, Protocol
+
+
+class PStream(Protocol):
+    def read(self, size: Any = ..., /) -> bytes: ...
+
+    def __iter__(self) -> Iterator[bytes]: ...
+
+
+class PSeekableStream(PStream):
+    def tell(self) -> int: ...
+
+    def seek(self, offset: int, whence: int = 0) -> int: ...
+
+
+class PData(Protocol):
+    location: str
+    size: int
+    content_type: str
+    hash: str
+    storage_data: dict[str, Any]
+
+
+__all__ = [
+    "PData",
+    "PStream",
+    "PSeekableStream",
+]
