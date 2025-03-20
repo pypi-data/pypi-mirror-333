00de8341ec110241780227abd32e26183d965fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:53 +0000

    Add package dandy/0.11.2/dandy-0.11.2-py3-none-any.whl

---
+++ packages/dandy/dandy-0.11.2-py3-none-any.whl/dandy/llm/service/request/ollama.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing_extensions import Union
+
+from dandy.llm.service.request.request import BaseRequestBody
+
+
+class OllamaRequestOptions(BaseModel):
+    num_ctx: Union[int, None] = None
+    num_predict: Union[int, None] = None
+    seed: Union[int, None] = None
+    temperature: Union[float, None] = None
+
+
+class OllamaRequestBody(BaseRequestBody):
+    options: OllamaRequestOptions
+    stream: bool = False
+    format: Union[dict, None] = {}
+
+    def get_context_length(self) -> int:
+        return self.options.num_ctx
+
+    def get_max_completion_tokens(self) -> int:
+        return self.options.num_predict
+
+    def get_seed(self):
+        return self.options.seed
+
+    def get_temperature(self):
+        return self.options.temperature
+
+    def set_format_to_json_schema(self, json_schema: dict):
+        self.format = json_schema
+
+    def set_format_to_text(self):
+        self.format = None
