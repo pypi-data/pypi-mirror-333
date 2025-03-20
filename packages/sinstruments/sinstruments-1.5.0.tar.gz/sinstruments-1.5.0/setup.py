b2b493afa0a9469c5c4df0eb468608efe69adf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:19 +0000

    Add package sinstruments/1.5.0/sinstruments-1.5.0.tar.gz

---
+++ packages/sinstruments/sinstruments-1.5.0.tar.gz/sinstruments-1.5.0/setup.py
@@ -0,0 +1,11 @@
+#
+# This file is part of the sinstruments project
+#
+# Copyright (c) 2018-present Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+"""The setup script."""
+
+from setuptools import setup
+
+setup()
