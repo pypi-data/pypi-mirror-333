571805b6c0985f7a3af6be0c4e47f429d25e968f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:06:41 +0000

    Add package redturtle.bandi/1.6.0/redturtle.bandi-1.6.0.tar.gz

---
+++ packages/redturtle.bandi/redturtle.bandi-1.6.0.tar.gz/redturtle.bandi-1.6.0/redturtle/bandi/querystring/queryparser.py
@@ -0,0 +1,27 @@
+import DateTime
+from collections import namedtuple
+from plone.app.querystring.queryparser import _lessThan, _largerThan
+
+Row = namedtuple("Row", ["index", "operator", "values"])
+
+
+def _afterDateTime(context, row):  # noqa
+    try:
+        value = DateTime.DateTime(row.values)
+    except DateTime.interfaces.SyntaxError:
+        value = DateTime.DateTime()
+
+    row = Row(index=row.index, operator=row.operator, values=value)
+
+    return _largerThan(context, row)
+
+
+def _beforeDateTime(context, row):  # noqa
+    try:
+        value = DateTime.DateTime(row.values)
+    except DateTime.interfaces.SyntaxError:
+        value = DateTime.DateTime()
+
+    row = Row(index=row.index, operator=row.operator, values=value)
+
+    return _lessThan(context, row)
