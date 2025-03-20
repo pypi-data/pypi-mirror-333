604f26f708eb1fbb3477387aff648007c0067039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:07:20 +0000

    Add package cdm-csv-loader/0.1.7/cdm_csv_loader-0.1.7.tar.gz

---
+++ packages/cdm-csv-loader/cdm_csv_loader-0.1.7.tar.gz/cdm_csv_loader-0.1.7/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import subprocess
+
+# Function to install R packages
+# def install_r_packages():
+#     r_packages = ['readr', 'lubridate', 'dplyr', 'DatabaseConnector', 'CommonDataModel']
+#     try:
+#         for package in r_packages:
+#             subprocess.check_call(['R', '-e', f'if (!requireNamespace("{package}", quietly = TRUE)) install.packages("{package}")'])
+#     except subprocess.CalledProcessError as e:
+#         print(f"Failed to install R package: {e}")
+
+# # Install R packages before the Python package setup
+# install_r_packages()
+
+setup(
+    name='cdm_csv_loader',
+    version='0.1.7',
+    packages=find_packages(),
+    python_requires='>=3.9',
+    install_requires=[
+        'pandas>=1.0.0',
+        'rpy2==3.5.12'
+    ],
+    description='A package for loading OHDSI CDM CSV files into a relational database.',
+    long_description=open('README2.md').read(),  # Detailed description from your README
+    long_description_content_type="text/markdown",
+    author='David Chibuike Ikechi Akwuru, Jeremiah Akintomide',
+    author_email='akwuru.david@ul.ie, akintomide.jeremiah@ul.ie',
+    url='https://github.com/DavidIkechi/ohdsi_cdm_loader.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license="MIT",
+    maintainer='David Chibuike Ikechi Akwuru, Jeremiah Akintomide',
+    maintainer_email='akwuru.david@ul.ie, akintomide.jeremiah@ul.ie',
+)
