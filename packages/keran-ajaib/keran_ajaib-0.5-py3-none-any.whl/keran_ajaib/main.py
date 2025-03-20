8c98ad647b113ccd9436e387a8f12294ede04793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:05:26 +0000

    Add package keran-ajaib/0.5/keran_ajaib-0.5-py3-none-any.whl

---
+++ packages/keran-ajaib/keran_ajaib-0.5-py3-none-any.whl/keran_ajaib/main.py
@@ -0,0 +1,14 @@
+import random
+import merlin
+
+def get_random_int_100():
+    return random.randint(0, 100)
+
+def get_random_int_200():
+    return random.randint(0, 200)
+
+def get_random_int_300():
+    return random.randint(0, 300)
+
+def merlinize():
+    a = merlin.merlin
