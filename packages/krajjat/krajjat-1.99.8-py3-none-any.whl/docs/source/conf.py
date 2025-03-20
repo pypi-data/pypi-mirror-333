08b763cd8d75340363f1cb4edef74da88439eb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:25 +0000

    Add package krajjat/1.99.8/krajjat-1.99.8-py3-none-any.whl

---
+++ packages/krajjat/krajjat-1.99.8-py3-none-any.whl/docs/source/conf.py
@@ -0,0 +1,45 @@
+import os, sys
+sys.path.insert(0, os.path.abspath("../.."))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'krajjat'
+copyright = '2023, Romain Pastureau'
+author = 'Romain Pastureau'
+release = '1.99.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    #'sphinx-autodoc-typehints',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    "style_nav_header_background": "#99CC00"
+}
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
+
+napoleon_use_param = True
