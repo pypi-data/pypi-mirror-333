973951bf6fd4c2814dd21244fe2c38ba182a098a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:06:19 +0000

    Add package fusion-tools/3.4.2/fusion_tools-3.4.2.tar.gz

---
+++ packages/fusion-tools/fusion_tools-3.4.2.tar.gz/fusion_tools-3.4.2/tests/test_image_overlay.py
@@ -0,0 +1,40 @@
+"""Testing image overlay component
+"""
+
+import os
+import sys
+import threading
+sys.path.append('./src/')
+from fusion_tools import Visualization
+from fusion_tools.components import SlideMap, SlideImageOverlay
+
+
+def main():
+
+    slide_list = ['C:\\Users\\samuelborder\\Downloads\\10H_LargeGlobalFlatfield.tifStitchedO.svs']
+
+    annotations = [
+        SlideImageOverlay(
+            image_path = 'C:\\Users\\samuelborder\\Downloads\\10H_LargeGlobalFlatfield.tif Y00003 X00008.jpg'
+        )
+    ]
+    
+    vis_session = Visualization(
+        local_slides = slide_list,
+        local_annotations=annotations,
+        components = [
+            [
+                SlideMap()
+            ]
+        ],
+        app_options = {
+            'port': 8060
+        }
+    )
+
+    vis_session.start()
+
+
+if __name__=='__main__':
+    main()
+
