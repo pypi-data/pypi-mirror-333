81072c9d681ee649335b3f531100f106d09b1d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:07:00 +0000

    Add package apantias/2.0.0/apantias-2.0.0-py3-none-any.whl

---
+++ packages/apantias/apantias-2.0.0-py3-none-any.whl/apantias/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "2.0.0"
+__author__ = "Florian Heinrich"
+__credits__ = "HEPHY Vienna"
+
+from . import analysis
+from . import display
+from . import utils
+from . import file_io
+from . import bin_to_h5
+from .standard import Default
+
+print(f"APANTIAS version {__version__} loaded.")
