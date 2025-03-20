418444f7669128fb8650cccfc264b401503bdb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:38 +0000

    Add package talentro-definitions/0.1.4/talentro_definitions-0.1.4-py3-none-any.whl

---
+++ packages/talentro-definitions/talentro_definitions-0.1.4-py3-none-any.whl/talentro/integrations/models.py
@@ -0,0 +1,24 @@
+import uuid
+
+from sqlalchemy import Column, JSON
+from sqlmodel import SQLModel, Field
+
+
+class Integration(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    name: str = Field(index=True)
+    icon: str = Field(index=True)
+    type: str = Field(index=True)
+    tag: str = Field(index=True, nullable=True)
+    description: str = Field(index=True, nullable=True)
+    code_reference: str = Field(index=True)
+    setup_config: dict = Field(sa_column=Column(JSON))
+    company_id: str = Field(index=True)
+
+
+class Link(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    company: uuid.UUID = Field(index=True)
+    auth_config: dict = Field(sa_column=Column(JSON))
+    integration_id: int | None = Field(default=None, foreign_key="integration.id")
+
