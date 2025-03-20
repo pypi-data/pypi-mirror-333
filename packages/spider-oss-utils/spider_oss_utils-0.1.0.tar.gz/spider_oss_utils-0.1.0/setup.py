7565cdad3d966486e5088a6d2ca76b38fe1f17fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:05:18 +0000

    Add package spider-oss-utils/0.1.0/spider_oss_utils-0.1.0.tar.gz

---
+++ packages/spider-oss-utils/spider_oss_utils-0.1.0.tar.gz/spider_oss_utils-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spider_oss_utils',
+    version='0.1.0',
+    description='一个简单的文件上传工具包',
+    author='MindLullaby',
+    author_email='3203939025@qq.com',
+    packages=find_packages(),
+    install_requires=[],  # 列出包依赖的其他库
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
