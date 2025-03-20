b9c76185cf6506a447ff86f2f024a6cf3c25d914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:04 +0000

    Add package shadowshell/0.0.11/shadowshell-0.0.11-py3-none-any.whl

---
+++ packages/shadowshell/shadowshell-0.0.11-py3-none-any.whl/shadowshell/git_shell.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+GitBroker
+
+@author: shadow shell
+"""
+
+import os
+
+class GitShell:
+    
+    def acp(self):
+        os.system("git add -A && git commit -m 'UPDATE: update somethings.' && git push")
+            
+    def view_config(self):
+        os.system("git config user.name & git config user.email")
+
