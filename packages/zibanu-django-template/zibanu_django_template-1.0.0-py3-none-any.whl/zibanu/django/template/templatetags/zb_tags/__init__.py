702fdfac2c1c1de2c1b4e18937fd7306ac1f8ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:07:49 +0000

    Add package zibanu-django-template/1.0.0/zibanu_django_template-1.0.0-py3-none-any.whl

---
+++ packages/zibanu-django-template/zibanu_django_template-1.0.0-py3-none-any.whl/zibanu/django/template/templatetags/zb_tags/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: "Jhony Alexander Gonzalez Córdoba"
+# Date:         7/03/2025 2:59 p. m.
+# Project:      zibanu-django
+# Module Name:  __init__.py
+# Description:  
+# ****************************************************************
+from django import template
+
+from .zb_concat import zb_concat
+from .zb_divide import zb_divide
+from .zb_multiply import zb_multiply
+from .zb_query_filter import zb_query_filter
+from .zb_static_uri import zb_static_uri
+from .zb_subtotal_dict import zb_subtotal_dict
+from .zb_sum_dict import zb_sum_dict
+
+register = template.Library()
+
+register.simple_tag(func=zb_concat, name='zb_concat')
+register.filter(name="zb_divide", function=zb_divide)
+register.filter(name="zb_multiply", function=zb_multiply)
+register.simple_tag(func=zb_query_filter, name='zb_query_filter')
+register.tag(name='zb_static_uri', compile_function=zb_static_uri)
+register.simple_tag(func=zb_subtotal_dict, name='zb_subtotal_dict')
+register.simple_tag(func=zb_sum_dict, name='zb_sum_dict')
