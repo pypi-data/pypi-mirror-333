860d182bc0b116f3e4defdec43a16b02c8e859a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:28 +0000

    Add package nmea2000/0.1.2/nmea2000-0.1.2.tar.gz

---
+++ packages/nmea2000/nmea2000-0.1.2.tar.gz/nmea2000-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nmea2000',
+    packages=find_packages(),
+    version='0.1.2',
+    description='NMEA 2000 encoder and decoder',
+    long_description=open('README.md').read(),  # Readme file for long description
+    long_description_content_type="text/markdown",
+    author='Tomer-W',
+    url="https://github.com/tomer-w/nmea2000",
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==8.3.5'],
+    test_suite='tests',
+    entry_points={
+        "console_scripts": [
+            "nmea2000-cli=nmea2000.cli:main"
+        ]
+    },
+)
