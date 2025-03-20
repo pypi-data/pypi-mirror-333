deba019e871800989ab22ceeb836ee6a18ea6ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:42 +0000

    Add package djinntoux/0.0.8.19/djinntoux-0.0.8.19.tar.gz

---
+++ packages/djinntoux/djinntoux-0.0.8.19.tar.gz/djinntoux-0.0.8.19/djinntoux/urls2.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+
+'''  admin/base_site.html override template also affects the title '''
+admin.site.site_title = 'REAL Django Admin'   # DEFAULT: "Django site admin"
+admin.site.site_header = 'Real Django Admin'  # DEFAULT: "Django Administration"
+admin.site.index_title = 'Site Admin Home'    # DEFAULT: "Site Administration"
+
+
+urlpatterns = [
+    path(settings.ADMIN_PATH, admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('vto_core.urls')),
+]
