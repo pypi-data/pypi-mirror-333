5a8783612359ac37c25286b79872009dfc0d62a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:05:53 +0000

    Add package segmentmytif/0.3.0/segmentmytif-0.3.0-py3-none-any.whl

---
+++ packages/segmentmytif/segmentmytif-0.3.0-py3-none-any.whl/segmentmytif/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about segmentmytif."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Christiaan Meijer"
+__email__ = "c.meijer@esciencecenter.nl"
+__version__ = "0.3.0"
