3c3f730a33734bf0541ade98c9e98c969955abaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:51 +0000

    Add package rnaglib/3.3.1/rnaglib-3.3.1-py3-none-any.whl

---
+++ packages/rnaglib/rnaglib-3.3.1-py3-none-any.whl/rnaglib/tasks/RNA_Prot/demo.py
@@ -0,0 +1,33 @@
+from rnaglib.learning.task_models import PygModel
+from rnaglib.tasks import ProteinBindingSite
+from rnaglib.transforms import GraphRepresentation
+
+ta = ProteinBindingSite("RNA_Prot", recompute=False, debug=False, precomputed=False)
+
+# Add representation
+ta.dataset.add_representation(GraphRepresentation(framework="pyg"))
+
+# Splitting dataset
+ta.get_split_loaders(recompute=False)  # set to false once it has run with true once
+
+# Train model
+# Either by hand:
+# for epoch in range(100):
+#     for batch in task.train_dataloader:
+#         graph = batch["graph"].to(self.device)
+#         ...
+
+# Or using a wrapper class
+model = PygModel(
+    ta.metadata["description"]["num_node_features"],
+    ta.metadata["description"]["num_classes"],
+    graph_level=False,
+)
+model.configure_training(learning_rate=0.01)
+model.train_model(ta, epochs=10)
+
+# Final evaluation
+test_metrics = model.evaluate(ta, split="test")
+# Print metrics
+for k, v in test_metrics.items():
+    print(f"Test {k}: {v:.4f}")
