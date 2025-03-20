1139e721a0ec513bf0751fe251853009ed9a7026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:06:50 +0000

    Add package blue-assistant/4.182.1/blue_assistant-4.182.1.tar.gz

---
+++ packages/blue-assistant/blue_assistant-4.182.1.tar.gz/blue_assistant-4.182.1/blue_assistant/help/functions.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from blue_options.terminal import show_usage, xtra
+from abcli.help.generic import help_functions as generic_help_functions
+
+from blue_assistant import ALIAS
+from blue_assistant.help.script import help_functions as help_script
+from blue_assistant.help.web import help_functions as help_web
+
+
+def help_browse(
+    tokens: List[str],
+    mono: bool,
+) -> str:
+    options = "actions|repo"
+
+    return show_usage(
+        [
+            "@assistant",
+            "browse",
+            f"[{options}]",
+        ],
+        "browse blue_assistant.",
+        mono=mono,
+    )
+
+
+help_functions = generic_help_functions(plugin_name=ALIAS)
+
+help_functions.update(
+    {
+        "browse": help_browse,
+        "script": help_script,
+        "web": help_web,
+    }
+)
