cea05abc1e900f32a1eb390773036fa984256878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:08:36 +0000

    Add package r2r/3.4.6/r2r-3.4.6.tar.gz

---
+++ packages/r2r/r2r-3.4.6.tar.gz/r2r-3.4.6/core/utils/__init__.py
@@ -0,0 +1,36 @@
+from shared.utils.base_utils import (
+    decrement_version,
+    deep_update,
+    format_search_results_for_llm,
+    format_search_results_for_stream,
+    generate_default_user_collection_id,
+    generate_document_id,
+    generate_extraction_id,
+    generate_id,
+    generate_user_id,
+    increment_version,
+    update_settings_from_dict,
+    validate_uuid,
+)
+from shared.utils.splitter.text import (
+    RecursiveCharacterTextSplitter,
+    TextSplitter,
+)
+
+__all__ = [
+    "format_search_results_for_stream",
+    "format_search_results_for_llm",
+    "generate_id",
+    "generate_document_id",
+    "generate_extraction_id",
+    "generate_user_id",
+    "increment_version",
+    "decrement_version",
+    "generate_default_user_collection_id",
+    "validate_uuid",
+    "update_settings_from_dict",
+    "deep_update",
+    # Text splitter
+    "RecursiveCharacterTextSplitter",
+    "TextSplitter",
+]
