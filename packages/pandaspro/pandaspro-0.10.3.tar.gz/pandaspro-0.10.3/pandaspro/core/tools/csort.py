763e1d7233d860668ed5420ab01f7e2d2119e91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:08:15 +0000

    Add package pandaspro/0.10.3/pandaspro-0.10.3.tar.gz

---
+++ packages/pandaspro/pandaspro-0.10.3.tar.gz/pandaspro-0.10.3/pandaspro/core/tools/csort.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from pandaspro.core.tools.utils import df_with_index_for_mask
+
+
+def csort(
+        data,
+        column,
+        order: str | list = None,
+        where: str = 'first',
+        before: str = None,
+        after: str = None,
+        inplace: bool = False
+):
+    """
+    Sorts the DataFrame by the given column according to a custom or dynamically generated order.
+    Automatically completes the orderlist with all unique values in the column if it's partially provided.
+    Optionally, positions rows with a specified value before or after another value.
+
+    :param data: DataFrame to sort
+    :param column: Column name on which to sort
+    :param order: List defining the custom order, dynamically completed if partially provided
+    :param where: How the value will be put: either in the first or last place
+    :param before: The value before which the specified value should be placed (optional)
+    :param after: The value after which the specified value should be placed (optional)
+    :param inplace: Whether to sort the DataFrame in place (default: False)
+
+    :return: Sorted DataFrame or None if sorted in place
+
+    """
+    if column in data.columns:
+        pass
+    elif column in data.index.names:
+        data = df_with_index_for_mask(data)
+    else:
+        raise ValueError(f'Column {column} not found in either the dataframe nor the index namelist')
+
+    orderlist = list(data[column].dropna().unique())
+
+    if isinstance(order, list):
+        provided_reorder = [x for x in order]
+        missing_reorder = [x for x in data[column].dropna().unique() if x not in order]
+        full_orderlist = provided_reorder + missing_reorder
+        orderlist = full_orderlist
+
+    elif isinstance(order, str):
+        value = order
+        # Order for certain value
+        if value and not before and not after:
+            if value in orderlist:
+                orderlist.remove(value)
+            if where == 'first':
+                orderlist.insert(0, value)
+            elif where == 'last':
+                orderlist.append(value)
+
+        # Reorder the list if value and before/after are specified
+        if value and (before or after):
+            if before and (value in orderlist) and (before in orderlist):
+                orderlist.remove(value)
+                before_index = orderlist.index(before)
+                orderlist.insert(before_index, value)
+            elif after and (value in orderlist) and (after in orderlist):
+                orderlist.remove(value)
+                after_index = orderlist.index(after) + 1
+                orderlist.insert(after_index, value)
+
+    cat_type = pd.CategoricalDtype(categories=orderlist, ordered=True)
+    data['__cpd_sort'] = data[column].astype(cat_type)
+
+    if inplace:
+        data.sort_values(by='__cpd_sort', inplace=True, kind='mergesort')
+        if set(data.index.names) <= set(data.columns):
+            data.drop(list(data.index.names) + ['__cpd_sort'], axis=1, inplace=True)
+    else:
+        result = data.sort_values(by='__cpd_sort', kind='mergesort')
+        if result.index.names[0] is None:
+            result.drop('__cpd_sort', axis=1, inplace=True)
+        elif set(result.index.names) <= set(result.columns):
+            result.drop(list(data.index.names) + ['__cpd_sort'], axis=1, inplace=True)
+
+        return result
---
+++ packages/pandaspro/pandaspro-0.10.3.tar.gz/pandaspro-0.10.3/pandaspro/core/tools/search2df.py
@@ -0,0 +1,142 @@
+from termcolor import colored
+import pandas as pd
+from tabulate import tabulate
+
+
+def calculate_similarity(row1, row2, match_and_weights, debug=False):
+    """
+    This function is used to return the similarity index when comparing two dataframes with selected columns
+    the selected columns mapping is provided using the match_and_weights para
+
+    :param row1: the row of the data to be checked
+    :param row2: source databases row
+    :param match_and_weights: column name mapping and weight setting
+
+    Sample:
+    columns_mapping = {
+        'name': {'col':'name_full', 'weight': 0.7},
+        'title': {'col':'title', 'weight': 0.1},
+        'grade': {'col':'grade', 'weight': 0.1},
+        'nationality': {'col':'nationality', 'weight': 0.1}
+    }
+
+    :return: similarity score, the higher, the better the match is
+    """
+    try:
+        from fuzzywuzzy import fuzz
+    except ImportError:
+        raise ImportError(
+            "Please install 'fuzzywuzzy' package to enable this method, or you may use 'pip install your_package_name[fuzzy]' to install all the dependencies required the first time you install pandaspro")
+
+    total_similarity = 0.0
+
+    for small_col, data in match_and_weights.items():
+        large_col = str(data['col'])
+        weight = data['weight']
+        total_similarity += (fuzz.token_sort_ratio(row1[large_col], row2[small_col]) / 100.0) * weight
+    return total_similarity
+
+
+def search2df(data_small=None, data_large=None, dictionary=None, key=None, mapsample=None, threshold=0.9, show=True, debug=False):
+    """
+    This function used the calculate_similarity as listed above to create checking dev-reports and generate the key column
+    in the smaller (to be checked) dataframe according to the larger (source) dataframe.
+
+    :param debug: debug toggle
+    :param mapsample: show the mapsample
+    :param data_small: smaller, or to be checked dataframe
+    :param data_large: larger, or source dataframe
+    :param dictionary: mapping - this is to build relationship between two dataframes
+    :param key: the id column in the source dataframe that is to be generated in the smaller one
+
+    :param threshold: set the threshold to map the key column into the to-be-checked dataframe
+    :param show: if True, then display the mapping result, by default set to true
+
+    :return: the updated smaller dataframe with key
+    """
+
+    finaldf = data_small.copy()
+    if mapsample:
+        sample = {
+            'name': {'col': 'name_full', 'weight': 0.7},
+            'title': {'col': 'title', 'weight': 0.1},
+            'grade': {'col': 'grade', 'weight': 0.1},
+            'nationality': {'col': 'nationality', 'weight': 0.1}
+        }
+        return sample
+
+    count = 1
+    for idx_small, row_small in data_small.iterrows():
+        if show:
+            # print(colored(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", 'green'))
+            print(colored(f">>>> Count {count}/{len(data_small)}: \n", 'black'))
+            # print("Data on Left:")
+            # print("=======================")
+            # print(row_small[list(dictionary.keys())])
+        display_left_series = row_small[list(dictionary.keys())]
+        display_right_series = display_left_series.apply(lambda x: "")
+        found_match = False
+
+        for idx_large, row_large in data_large.iterrows():
+            similarity = calculate_similarity(row_large, row_small, dictionary, debug=debug)
+            if similarity > threshold:
+                # if show:
+                # print("\nData on Right:")
+                # print("=======================")
+                # print(row_large[[col['col'] for col in dictionary.values()]])
+                # print("")
+                display_right_series = row_large[[col['col'] for col in dictionary.values()]]
+                display_right_series.index = display_left_series.index
+
+                # Add new column
+                if isinstance(key, str):
+                    finaldf.at[idx_small, key] = row_large[key]
+                elif isinstance(key, list):
+                    for k in key:
+                        finaldf.at[idx_small, k] = row_large[k]
+                else:
+                    raise ValueError('Invalid Support Type for key')
+
+                found_match = True
+                break
+
+        # Print Results
+        if show:
+            comparison_data = {
+                "Left Data": display_left_series,
+                "": [""] * len(display_left_series),
+                "Right Data": display_right_series
+            }
+            comparison_df = pd.DataFrame(comparison_data, index=list(dictionary.keys()))
+            print(colored(tabulate(comparison_df, headers='keys', tablefmt='simple', showindex=True, stralign='center'), 'blue'))
+
+        if show and not found_match:
+            print(
+                colored(
+                    "\n###########################################\n [!] Searched but no results for this item\n###########################################",
+                    'red')
+            )
+
+        if show:
+            print("\n----------------------------------------------------------------------------------")
+            print("")
+
+        count += 1
+
+    return finaldf
+
+
+if __name__ == '__main__':
+    import pandaspro as cpd
+    import numpy as np
+    import wbhrdata as wb
+    path = r'C:\Users\wb539289\WBG\HRDC1 Files - New T-Drive\D- Reports\Analysis\Impact'
+    data = cpd.pwread(f'{path}/FY23 AFW Manager Impact Result.xlsx', cellrange='A4')[0]
+    data = data.inlist('division', np.nan, invert=True)
+
+    alldata = wb.sob()
+    mydict = {
+        'full_name': {'col': 'name_full', 'weight': 0.9},
+        'division': {'col': 'unit', 'weight': 0.1},
+    }
+    new = data.search2df(alldata, mydict, key='upi', show=True)
