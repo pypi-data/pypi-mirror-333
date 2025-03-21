421f9be369f35ee30bbcdb169763e91df0e161d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:58 +0000

    Add package gpus/0.1.1/gpus-0.1.1.tar.gz

---
+++ packages/gpus/gpus-0.1.1.tar.gz/gpus-0.1.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gpus",
+    version="0.1.1",
+    description="A web interface for monitoring NVIDIA GPUs",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="mrfakename",
+    author_email="me@mrfake.name",
+    url="https://github.com/fakerybakery/gpus",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "nvidia-ml-py",
+        "click",
+        "flask",
+        "flask-socketio",
+        "eventlet",  # For WebSocket support
+        "python-engineio>=4.0.0",
+        "python-socketio>=5.0.0",
+        "psutil",    # For process management
+    ],
+    entry_points={
+        'console_scripts': [
+            'gpus=gpus.cli:cli',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        'gpus': [
+            'templates/*.html',
+            'static/css/*.css',
+            'static/js/*.js',
+        ],
+    },
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
