f942d355e4ff65a3c401e459a7ddf508bdc31d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:59 +0000

    Add package vmecpp/0.2.1/vmecpp-0.2.1-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/vmecpp/vmecpp-0.2.1-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vmecpp/__about__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2024-present Proxima Fusion GmbH <info@proximafusion.com>
+#
+# SPDX-License-Identifier: MIT
+# NOTE: must also update src/vmecpp/cpp/MODULE.bazel
+__version__ = "0.2.1"
