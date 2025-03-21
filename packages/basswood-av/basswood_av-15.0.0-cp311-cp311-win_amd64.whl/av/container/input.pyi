cbf26bbc4d9780e36c27216c0b1ade3747964939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:16:16 +0000

    Add package basswood-av/15.0.0/basswood_av-15.0.0-cp311-cp311-win_amd64.whl

---
+++ packages/basswood-av/basswood_av-15.0.0-cp311-cp311-win_amd64.whl/av/container/input.pyi
@@ -0,0 +1,58 @@
+from typing import Any, Iterator, overload
+
+from av.audio.frame import AudioFrame
+from av.audio.stream import AudioStream
+from av.packet import Packet
+from av.stream import Stream
+from av.subtitles.stream import SubtitleStream
+from av.subtitles.subtitle import AssSubtitle, BitmapSubtitle
+from av.video.frame import VideoFrame
+from av.video.stream import VideoStream
+
+from .core import Container
+
+class InputContainer(Container):
+    start_time: int
+    duration: int | None
+    bit_rate: int
+    size: int
+
+    def __enter__(self) -> InputContainer: ...
+    def close(self) -> None: ...
+    def demux(self, *args: Any, **kwargs: Any) -> Iterator[Packet]: ...
+    @overload
+    def decode(self, video: int) -> Iterator[VideoFrame]: ...
+    @overload
+    def decode(self, audio: int) -> Iterator[AudioFrame]: ...
+    @overload
+    def decode(
+        self, subtitles: int
+    ) -> Iterator[AssSubtitle] | Iterator[BitmapSubtitle]: ...
+    @overload
+    def decode(self, *args: VideoStream) -> Iterator[VideoFrame]: ...
+    @overload
+    def decode(self, *args: AudioStream) -> Iterator[AudioFrame]: ...
+    @overload
+    def decode(
+        self, *args: SubtitleStream
+    ) -> Iterator[AssSubtitle] | Iterator[BitmapSubtitle]: ...
+    @overload
+    def decode(
+        self, *args: Any, **kwargs: Any
+    ) -> (
+        Iterator[VideoFrame]
+        | Iterator[AudioFrame]
+        | Iterator[AssSubtitle]
+        | Iterator[BitmapSubtitle]
+    ): ...
+    def seek(
+        self,
+        offset: int,
+        *,
+        backward: bool = True,
+        any_frame: bool = False,
+        stream: Stream | VideoStream | AudioStream | None = None,
+        unsupported_frame_offset: bool = False,
+        unsupported_byte_offset: bool = False,
+    ) -> None: ...
+    def flush_buffers(self) -> None: ...
