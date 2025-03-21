ab7e40ef72dd4d9baf0d7236d61f9924fbf07cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:09:07 +0000

    Add package MF-Tools/1.2.0/mf_tools-1.2.0.tar.gz

---
+++ packages/MF-Tools/mf_tools-1.2.0.tar.gz/mf_tools-1.2.0/src/MF_Tools/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.2.0'
+__author__ = 'John Connell - The Mathematic Fanatic'
+__description__ = 'Collection of helpful utilities for Manim'
+
+from .transforms import *
+from .updaters import *
+from .geometry import *
+from .misc import *
+from .animations import *
+
+#from manim_play_timeline import *
