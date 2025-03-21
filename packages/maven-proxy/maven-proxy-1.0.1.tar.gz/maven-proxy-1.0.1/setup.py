f1026ff3a811d1a684b9aac31aff0e48d73aa511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:16:42 +0000

    Add package maven-proxy/1.0.1/maven-proxy-1.0.1.tar.gz

---
+++ packages/maven-proxy/maven-proxy-1.0.1.tar.gz/maven-proxy-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="maven-proxy",
+    version="1.0.1",
+    author="qicongsheng",
+    author_email="qicongsheng@outlook.com",
+    description="Maven Repository Proxy with caching and authentication",
+    url="https://github.com/yourusername/maven-proxy",
+    packages=find_packages(),
+    package_data={
+      "maven_proxy": ["templates/*.html"]
+    },
+    install_requires=[
+      "flask",
+      "requests",
+      "flask_httpauth",
+      "apscheduler"
+    ],
+    entry_points={
+      "console_scripts": [
+        "maven-proxy=maven_proxy.app:main"
+      ]
+    }
+)
