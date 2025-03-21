eeabe14385b44ae2dcfca0d244d6504628b5ed46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:30 +0000

    Add package js-api/0.1.0/js_api-0.1.0.tar.gz

---
+++ packages/js-api/js_api-0.1.0.tar.gz/js_api-0.1.0/src/database.py
@@ -0,0 +1,27 @@
+import os
+from typing import Annotated
+
+from fastapi.params import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Session
+
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
