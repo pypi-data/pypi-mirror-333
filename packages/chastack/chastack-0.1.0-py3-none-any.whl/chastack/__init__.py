c21ab109ebf50e4c48230f5a41eb478e705b5225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:09:19 +0000

    Add package chastack/0.1.0/chastack-0.1.0-py3-none-any.whl

---
+++ packages/chastack/chastack-0.1.0-py3-none-any.whl/chastack/__init__.py
@@ -0,0 +1,32 @@
+"""
+===============
+chastack
+===============
+Librería de útiles del stack de Ch'aska en Python 3.
+
+* Repositorio del proyecto: https://github.com/Hernanatn/chastack/python
+
+Derechos de autor (c) 2025 Ch'aska SRL. Distribuído bajo licencia MIT.
+Autores:
+ - Hernan ATN | herni@cajadeideas.ar 
+ - Joaquín C. | correajoaquin7@gmail.com
+"""
+
+from solteron import Solteron
+from sobrecargar import sobrecargar
+import chastack_bdd
+import chastack_utiles_flask
+import chastack_utiles_google
+
+#__all__ = [
+#    'sobrecargar',
+#    'Solteron',
+#    'ProtocoloBaseDeDatos',
+#    'BaseDeDatos_MySQL',
+#    'Tabla',
+#    'Registro',
+#    'EnumSQl',
+#]
+
+if __name__ == '__main__':
+    print(__doc__)
