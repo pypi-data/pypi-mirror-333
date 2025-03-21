0273be27c9ecde6d8c02424179ef3df3f2359840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:27 +0000

    Add package taskex/0.1.1/taskex-0.1.1-py3-none-any.whl

---
+++ packages/taskex/taskex-0.1.1-py3-none-any.whl/taskex/models/task_run.py
@@ -0,0 +1,26 @@
+import time
+from typing import Any, Optional
+from pydantic import (
+    BaseModel,
+    StrictFloat,
+    StrictInt,
+    StrictStr,
+)
+
+from .run_status import RunStatus
+from .task_type import TaskType
+
+
+class TaskRun(BaseModel):
+    run_id: StrictInt
+    status: RunStatus
+    error: Optional[StrictStr] = None
+    trace: Optional[StrictStr] = None
+    start: StrictInt | StrictFloat = time.monotonic()
+    end: Optional[StrictInt | StrictFloat] = None
+    elapsed: StrictInt | StrictFloat = 0
+    result: Optional[Any] = None
+    task_type: TaskType = TaskType.CALLABLE
+
+    def complete(self):
+        return self.status in [RunStatus.COMPLETE, RunStatus.CANCELLED, RunStatus.FAILED]
