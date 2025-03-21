e6a51dd9e044c27b6f151d174d616cec07f5dd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:32 +0000

    Add package optionvisualizer-wrapper/0.1.12/optionvisualizer_wrapper-0.1.12.tar.gz

---
+++ packages/optionvisualizer-wrapper/optionvisualizer_wrapper-0.1.12.tar.gz/optionvisualizer_wrapper-0.1.12/src/optionvisualizer_wrapper/config.py
@@ -0,0 +1,8 @@
+plotly_global_config = {"displaylogo": False}
+
+#                 full_html=full_html, include_plotlyjs="cdn", config=plotly_global_config
+plotly_to_full_html_config = {
+    "include_plotlyjs": "cdn",
+    "config": plotly_global_config,
+    "full_html": True,
+}
