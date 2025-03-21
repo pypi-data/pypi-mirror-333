ec5fe2ad18e3143670200711e819f95ff985ce91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:35:00 +0000

    Add package pandaspro/0.10.5/pandaspro-0.10.5.tar.gz

---
+++ packages/pandaspro/pandaspro-0.10.5.tar.gz/pandaspro-0.10.5/pandaspro/test/putxl_test.py
@@ -0,0 +1,15 @@
+from pandaspro.io.excel.putexcel import PutxlSet
+from pandaspro.sampledf.sampledf import wbuse_pivot
+
+
+path = './pandaspro/tests/sampledf.xlsx'
+
+ps = PutxlSet(path)
+ps.putxl(
+    wbuse_pivot,
+    sheet_name='newtab3',
+    cell='B2',
+    index=True,
+    design='wbblue',
+    tab_color='blue'
+)
---
+++ packages/pandaspro/pandaspro-0.10.5.tar.gz/pandaspro-0.10.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pandaspro',
+    version='0.10.5',
+    description='Upgraded pandas package for easier dataframe operations',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.24',
+        'pandas~=2.2.1',
+        'openpyxl>=3.1.5',
+        'xlwings>=0.31.10',
+        'jinja2>=3.1.4',
+        'termcolor>=2.4.0',
+        'tabulate>=0.9.0',
+        'maya>=0.6.1',
+        'PyPDF2>=3.0.1',
+        'pendulum<3.0',
+    ],
+    python_requires='>=3.8',
+    py_modules=[],
+    author='Shiyao Wang and Xueqi Li',
+    author_email='sw1016@georgetown.edu',
+    url='https://github.com/soluentre/pandaspro',
+    long_description='''
+pandaspro is the upgrade version of pandas Python pacakge with more user-friendly dataframe manipulating methods and 
+better Excel-based exporting and formatting apis 
+''',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
