4fb1bb05960ad474ca6edac4198d62052b1bd0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:10:30 +0000

    Add package m9s-account-de/7.0.3/m9s_account_de-7.0.3.tar.gz

---
+++ packages/m9s-account-de/m9s_account_de-7.0.3.tar.gz/m9s_account_de-7.0.3/tests/test_module.py
@@ -0,0 +1,12 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class AccountDeTestCase(ModuleTestCase):
+    "Test Account De module"
+    module = 'account_de'
+
+
+del ModuleTestCase
