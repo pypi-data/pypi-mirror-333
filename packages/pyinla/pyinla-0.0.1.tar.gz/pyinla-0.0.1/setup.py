7bcfff4420172a4555e040a8f3a2763dd260e034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:48 +0000

    Add package pyinla/0.0.1/pyinla-0.0.1.tar.gz

---
+++ packages/pyinla/pyinla-0.0.1.tar.gz/pyinla-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="pyinla",
+    version="0.0.1",
+    author="Your Name",
+    author_email="your-email@example.com",
+    description="Placeholder for the pyinla package",
+    long_description="This is a placeholder package to reserve the name.",
+    long_description_content_type="text/plain",
+    url="https://github.com/yourusername/pyinla",
+    packages=["pyinla"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
