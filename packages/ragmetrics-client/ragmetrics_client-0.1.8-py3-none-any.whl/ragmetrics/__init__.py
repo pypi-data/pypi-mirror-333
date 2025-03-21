40c050c25b94ac67fc7099d199e38b50adda095e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:34:25 +0000

    Add package ragmetrics-client/0.1.8/ragmetrics_client-0.1.8-py3-none-any.whl

---
+++ packages/ragmetrics-client/ragmetrics_client-0.1.8-py3-none-any.whl/ragmetrics/__init__.py
@@ -0,0 +1,8 @@
+"""
+RagMetrics Python Package
+"""
+
+from ragmetrics.api import login, monitor
+from ragmetrics.dataset import Example, Dataset
+from ragmetrics.tasks import Task
+from ragmetrics.experiments import Experiment, Cohort
