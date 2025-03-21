2057ab382d6638998d8b22e43ea9695f46ba483c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:08:13 +0000

    Add package pandaspro/0.10.2/pandaspro-0.10.2.tar.gz

---
+++ packages/pandaspro/pandaspro-0.10.2.tar.gz/pandaspro-0.10.2/pandaspro/core/api.py
@@ -0,0 +1,45 @@
+from pandaspro.core.frame import FramePro, cpdBaseFrameMapper, cpdBaseFrameList
+
+from pandaspro.core.tools.dfilter import dfilter
+from pandaspro.core.tools.tab import tab
+from pandaspro.core.tools.varnames import varnames
+from pandaspro.core.tools.csort import csort
+from pandaspro.core.tools.lowervarlist import lowervarlist
+from pandaspro.core.tools.consecgrouper import ConsecGrouper as consecgrouper
+from pandaspro.core.tools.utils import (
+    df_with_index_for_mask,
+    create_column_color_dict
+)
+from pandaspro.core.tools.replace_left_with_right import replace_left_with_right
+
+from pandaspro.core.dates.methods import (
+    bdate
+)
+
+from pandaspro.core.stringfunc import (
+    parse_method,
+    parse_wild,
+    wildcardread,
+    str2list
+)
+
+
+__all__ = [
+    "bdate",
+    "dfilter",
+    "FramePro",
+    "tab",
+    "varnames",
+    "parse_wild",
+    "parse_method",
+    "wildcardread",
+    "str2list",
+    "csort",
+    "df_with_index_for_mask",
+    "lowervarlist",
+    "create_column_color_dict",
+    "cpdBaseFrameMapper",
+    "cpdBaseFrameList",
+    "consecgrouper",
+    "replace_left_with_right"
+]
---
+++ packages/pandaspro/pandaspro-0.10.2.tar.gz/pandaspro-0.10.2/pandaspro/email/api.py
@@ -0,0 +1,7 @@
+from pandaspro.email.email import DataFetcher as emailfetcher
+from pandaspro.email.email import create_mail_class
+
+__all__ = [
+    'emailfetcher',
+    'create_mail_class',
+]
