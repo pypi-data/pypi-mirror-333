67f5b263295f448cb66a2fa359de7ffd87b07a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:08:08 +0000

    Add package grizzly-framework/0.23.0/grizzly_framework-0.23.0-py3-none-any.whl

---
+++ packages/grizzly-framework/grizzly_framework-0.23.0-py3-none-any.whl/sapphire/__init__.py
@@ -0,0 +1,22 @@
+"""
+Sapphire HTTP server
+"""
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from .certificate_bundle import CertificateBundle
+from .core import Sapphire, create_listening_socket
+from .job import Served
+from .server_map import ServerMap
+
+__all__ = (
+    "CertificateBundle",
+    "Sapphire",
+    "Served",
+    "ServerMap",
+    "create_listening_socket",
+)
+__author__ = "Tyson Smith"
+__credits__ = ["Tyson Smith"]
