4b0efa30dd9c0d30af687d9c69e718c2a451f973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:08:19 +0000

    Add package imgemb/0.1.0/imgemb-0.1.0.tar.gz

---
+++ packages/imgemb/imgemb-0.1.0.tar.gz/imgemb-0.1.0/image_embeddings/image_embeddings/__init__.py
@@ -0,0 +1,11 @@
+"""
+Image Embeddings Library
+=======================
+
+A lightweight Python library for generating image embeddings with minimal dependencies.
+"""
+
+from .embedder import ImageEmbedder
+
+__version__ = "0.1.0"
+__all__ = ["ImageEmbedder"]
