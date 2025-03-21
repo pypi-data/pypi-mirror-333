11b5b842b39fad597f9a59e81ddfa688d51b00c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:10:19 +0000

    Add package django-glue/0.7.10/django_glue-0.7.10-py3-none-any.whl

---
+++ packages/django-glue/django_glue-0.7.10-py3-none-any.whl/django_glue/core/utils.py
@@ -0,0 +1,9 @@
+from django.core import serializers
+
+
+def camel_to_snake(string):
+    return ''.join(['_' + c.lower() if c.isupper() else c for c in string]).lstrip('_')
+
+
+def serialize_object_to_json(model_object):
+    return serializers.serialize('json', [model_object, ])
---
+++ packages/django-glue/django_glue-0.7.10-py3-none-any.whl/django_glue/middleware.py
@@ -0,0 +1,20 @@
+from django.urls import resolve
+
+from django_glue.session import GlueSession, GlueKeepLiveSession
+
+
+class GlueMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_url = resolve(request.path_info).url_name
+
+        if current_url not in ['django_glue_data_handler', 'django_glue_keep_live_handler']:
+            glue_session = GlueSession(request)
+            glue_keep_live_session = GlueKeepLiveSession(request)
+            glue_session.clean(glue_keep_live_session.clean_and_get_expired_unique_names())
+
+        response = self.get_response(request)
+
+        return response
