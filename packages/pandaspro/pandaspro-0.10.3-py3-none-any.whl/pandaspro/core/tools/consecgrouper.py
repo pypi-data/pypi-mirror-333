c3dd496d2ce39daac4e01f86c70d4734c404a6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:08:15 +0000

    Add package pandaspro/0.10.3/pandaspro-0.10.3-py3-none-any.whl

---
+++ packages/pandaspro/pandaspro-0.10.3-py3-none-any.whl/pandaspro/core/tools/consecgrouper.py
@@ -0,0 +1,72 @@
+from pandaspro.core.tools.corder import corder
+
+
+def unify_to_list(para: int | str | list):
+    if isinstance(para, int | str):
+        para_list = [para]
+    elif isinstance(para, list):
+        para_list = para
+    else:
+        raise ValueError('Incorrect type of argument [para]: only supports int/str/list')
+    return para_list
+
+
+class ConsecGrouper:
+    def __init__(
+            self,
+            df,
+            groupby,
+    ):
+        self.df = df
+        self.df_grouped = None
+
+        # Split Section
+        if isinstance(groupby, str):
+            self.groupby = [groupby]
+        elif isinstance(groupby, list):
+            self.groupby = groupby
+        else:
+            raise ValueError('Incorrect type for group_by, can only be str/list')
+
+    # noinspection PyShadowingNames
+    def group(self):
+        self.df_grouped = self.df.copy()
+        self.df_grouped['group_num'] = (self.df_grouped[self.groupby] != self.df_grouped[self.groupby].shift()).any(axis=1).cumsum()
+        self.df_grouped['group_id'] = self.df_grouped[['group_num'] + self.groupby].astype(str).agg('-'.join, axis=1)
+        self.df_grouped = corder(self.df_grouped, ['group_num', 'group_id'])
+        return self.df_grouped
+
+    def extract(
+            self,
+            value_at_top: str | list = None,
+            value_at_bottom: str | list = None,
+    ):
+        grouped_df = self.group()
+        group_id_dict = {'group_num': 'first', 'group_id': 'first'}
+        group_by_dict = {var: 'first' for var in unify_to_list(self.groupby)}
+        top_dict = {}
+        bottom_dict = {}
+
+        if value_at_top is None and value_at_bottom is None:
+            raise ValueError('Please specify value_at_top/value_at_bottom or both.')
+        else:
+            if value_at_top:
+                top_dict = {var: 'first' for var in unify_to_list(value_at_top)}
+            if value_at_bottom:
+                bottom_dict = {var: 'last' for var in unify_to_list(value_at_bottom)}
+
+        agg_dict = {}
+        for d in [group_id_dict, group_by_dict, top_dict, bottom_dict]:
+            for key, value in d.items():
+                if key not in agg_dict:
+                    agg_dict[key] = []
+                agg_dict[key].append(value)
+        # print(agg_dict)
+
+        grouped_df = grouped_df.groupby('group_num').agg(agg_dict).reset_index(drop=True)
+        grouped_df.columns = [col[0] if col[0] in unify_to_list(['group_num', 'group_id'] + self.groupby)
+                              else '_'.join(filter(None, col)).rstrip('_')
+                              for col in grouped_df.columns]
+        grouped_df.columns = [col.replace('first', 'top').replace('last', 'bottom') for col in grouped_df.columns]
+
+        return grouped_df
---
+++ packages/pandaspro/pandaspro-0.10.3-py3-none-any.whl/pandaspro/user_config/cd_sheets.py
@@ -0,0 +1,337 @@
+cd_sheets = {
+    "grade": [
+        {
+            "column": "Grade",
+            "rules": {
+                "GA": '#FCE4D6',
+                "GB": '#FCE4D6',
+                "GC": '#FCE4D6',
+                "GD": '#FCE4D6',
+                "GE": '#FFF2CC',
+                "GF": '#DDEBF7',
+                "GG": '#E2EFDA',
+                "GH": '#F2F2F2',
+                "GI": '#FFC9C9',
+                "UC": '#E5D1EB'
+            },
+            "applyto": 'all'
+        },
+    ],
+    "pg": [
+        {
+            "column": "PGs",
+            "rules": {
+                "COS": '#FCE4D6',
+                "EFI": '#FFF2CC',
+                "HD": '#DDEBF7',
+                "INF": '#E2EFDA',
+                "SD": '#D9D9D9',
+                "Prosperity": '#FFF2CC',
+                "People": '#DDEBF7',
+                "Infra.": '#E2EFDA',
+                "Planet": '#D9D9D9',
+                "AFWDE": '#FBE2D5',
+                "AFWVP": '#FBE2D5',
+                "Digital": "#F7C9D8"
+            },
+            "applyto": 'all'
+        },
+    ],
+    "vertical": [
+        {
+            "column": "Vertical/Front Office Unit",
+            "rules": {
+                "COS": '#FCE4D6',
+                "EFI": '#FFF2CC',
+                "HD": '#DDEBF7',
+                "INF": '#E2EFDA',
+                "SD": '#D9D9D9',
+                "Prosperity": '#FFF2CC',
+                "People": '#DDEBF7',
+                "Infra.": '#E2EFDA',
+                "Planet": '#D9D9D9',
+                "AFWDE": '#FBE2D5',
+                "AFWVP": '#FBE2D5',
+                "Digital": "#F7C9D8"
+            },
+            "applyto": 'all'
+        },
+    ],
+    "cmu_dept": [
+        {
+            "column": "CMU Dept",
+            "rules": {
+                'EAE': '#FFF2CC',
+                'HAE': '#DDEBF7',
+                'IAE': '#E2EFDA',
+                'SAE': '#D9D9D9',
+                'EAW': '#FFF2CC',
+                'HAW': '#DDEBF7',
+                'IAW': '#E2EFDA',
+                'SAW': '#D9D9D9',
+            },
+            "applyto": 'all'
+        },
+    ],
+    "ti": [
+        {
+            "column": "FY19 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY20 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY21 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY22 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY23 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY24 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY25 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY26 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY27 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY28 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY29 Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY24 Talent (Preliminary)",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+        {
+            "column": "FY24 Prelim. Talent",
+            "rules": {
+                "Top Talent": "#63be7b",
+                "Emerging Talent": "#ffeb84",
+                "Solid Talent": "#bfbfbf",
+            },
+        },
+    ],
+    "pr": [
+            {
+                "column": "Before Meeting PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "After Meeting PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY19 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY20 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY21 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY22 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY23 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY24 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY25 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY26 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY27 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY28 PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY24 PR (Preliminary)",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY24 Prelim. PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+{
+                "column": "FY25 PR (Preliminary)",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "FY25 Prelim. PR",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+            {
+                "column": "Final PR (Management Review)",
+                "rules": {
+                    3: "#bfbfbf",
+                    3.5: "#dfd5a2",
+                    4: "#ffeb84",
+                    5: "#63be7b",
+                },
+            },
+        ]
+}
