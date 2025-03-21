a3cc23a6d609ec774ce1b89c1b6f6374f1790aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:34 +0000

    Add package nathanxli-package/0.1/nathanxli_package-0.1.tar.gz

---
+++ packages/nathanxli-package/nathanxli_package-0.1.tar.gz/nathanxli_package-0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'nathanxli_package',
+    version = 0.1,
+    packages = find_packages(),
+    install_require = [
+        ###
+    ],
+)
