bd5653ea1fc7934c78ee666db04ee361468a42b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:08:51 +0000

    Add package pipbert/7.1.0/pipbert-7.1.0-py3-none-any.whl

---
+++ packages/pipbert/pipbert-7.1.0-py3-none-any.whl/pybert/common.py
@@ -0,0 +1,24 @@
+"""
+Definitions common to all PyBERT modules.
+
+Original author: David Banas <capn.freako@gmail.com>
+
+Original date:   May 13, 2024
+
+Copyright (c) 2024 David Banas; all rights reserved World wide.
+"""
+
+from typing import Any, Optional, TypeVar  # , TypeAlias  # pylint: disable=unused-import  # noqa: F401
+import numpy as np  # type: ignore  # pylint: disable=unused-import  # noqa: F401
+import numpy.typing as npt  # type: ignore
+
+Real = TypeVar('Real', float, float)
+Comp = TypeVar('Comp', complex, complex)
+# Rvec: TypeAlias = npt.NDArray[Real]
+# Cvec: TypeAlias = npt.NDArray[Comp]
+Rvec = npt.NDArray[Real]
+Cvec = npt.NDArray[Comp]
+
+# PI: float = np.pi
+PI: float = 3.14159265359
+TWOPI: float = 2. * PI
